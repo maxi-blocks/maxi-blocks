cd20d005d253d4e226f05bc37ad931553228f913
Hrach Karapetyan <hrach.karapetyan71@gmail.com>
2020-03-25 14:54:21 +0400

    checkout

--- src/blocks/block-icon-extra/save.js
+++ src/blocks/block-icon-extra/save.js
@@ -20,51 +20,83 @@ const save = (props) => {
         attributes: {
           title,
           subtitle,
+          description,
           titleLevel,
           subtitleLevel,
           hideTitle,
           hideSubtitle,
           titleTextAlign,
           subtitleAlign,
-          defaultBlockStyle
+          iconBackgroundColor,
+          iconSizeUnit,
+          iconSize,
+          iconRotationUnit,
+          iconRotate,
+          hideDescription,
+          descriptionTextAlign,
+          defaultBlockStyle,
+          extraClassName,
+          uniqueID
         },
     } = props;
 
     const titleStyles = {
       display: hideTitle ? 'none' : undefined,
+      fontWeight: '400',
       textAlign: titleTextAlign,
     };
 
     const subtitleStyles = {
       display: hideSubtitle ? 'none' : undefined,
+      fontWeight: '400',
       textAlign: subtitleAlign,
     };
 
+    const descriptionStyles = {
+      display: hideDescription ? 'none' : undefined,
+      fontWeight: '400',
+      textAlign: descriptionTextAlign,
+    };
+
+    const blockStyle = {};
+
+    let classes = classnames( className );
+    if ( uniqueID && (typeof uniqueID !== 'undefined') ) {
+        classes = classnames( classes, uniqueID )
+    }
+
     return (
       <div
-        className={'gx-block gx-icon-extra'}
+        className={blockStyle + ' gx-block gx-icon-extra ' + classes + ' ' + extraClassName}
         data-gx_initial_block_class={defaultBlockStyle}
       >
       <div class='gx-icon-extra-icon'>
       </div>
-        <div class='gx-icon-extra-title'>
+        <div class='gx-icon-extra-title-wrapper'>
             <RichText.Content
                 tagName={titleLevel}
                 style={titleStyles}
-                placeholder={__('Write title…', 'gutenberg-extra')}
                 value={title}
                 onChange={(value) => setAttributes({ title: value })}
                 className="gx-icon-extra-title"
             />
         </div>
-        <div class='gx-icon-extra-subtitle'>
+        <div class='gx-icon-extra-subtitle-wrapper'>
             <RichText.Content
                 tagName={subtitleLevel}
                 style={subtitleStyles}
-                placeholder={__('Write title…', 'gutenberg-extra')}
                 value={subtitle}
                 onChange={(value) => setAttributes({ subtitle: value })}
                 className="gx-icon-extra-subtitle"
+            />
+        </div>
+        <div class='gx-icon-extra-description-wrapper'>
+            <RichText.Content
+                tagName='h6'
+                style={descriptionStyles}
+                value={description}
+                onChange={(value) => setAttributes({ description: value })}
+                className="gx-icon-extra-description"
             />
         </div>
       </div>

f289a0b8003f567f344d5d135f3987e6d4d80e50
Hrach Karapetyan <hrach.karapetyan71@gmail.com>
2020-04-07 19:29:44 +0400

    color control integration and more

--- src/components/color-control/index.js
+++ src/components/color-control/index.js
@@ -1,6 +1,8 @@
 /**
  * WordPress dependencies
  */
+const { __ } = wp.i18n;
+const { Fragment } = wp.element;
 const {
     ColorPicker,
     __experimentalGradientPicker
@@ -10,21 +12,13 @@ const {
  * External dependencies
  */
 import { PopoverControl } from '../popover/';
-import { isNil } from 'lodash';
+import CheckBox from '../checkbox/';
+import iconsSettings from '../icons/icons-settings.js';
 
 /**
- * Attributes
+ * Styles
  */
-
-export const colorControlAttributesTest = {
-    gradientTest: {
-        type: 'array',
-        default: [
-            { offset: '0.00', color: 'rgba(238, 55, 11, 1)' },
-            { offset: '1.00', color: 'rgba(126, 32, 34, 1)' }
-        ]
-    }
-}
+import './editor.scss';
 
 /**
  * Block
@@ -32,12 +26,17 @@ export const colorControlAttributesTest = {
 const ColorControl = props => {
     const {
         label,
-        showColor = undefined,
+        disableColor = false,
         color,
         onColorChange,
-        showGradient = undefined,
+        colorIcon = iconsSettings.colorWheel,
+        disableGradient = false,
         gradient,
-        onGradientChange
+        onGradientChange,
+        gradientIcon = iconsSettings.gradient,
+        disableGradientAboveBackground = false,
+        gradientAboveBackground,
+        onGradientAboveBackgroundChange,
     } = props;
 
     const returnColor = val => {
@@ -46,7 +45,7 @@ const ColorControl = props => {
 
     const getPopovers = () => {
         let response = [];
-        if (!isNil(showColor)) {
+        if (!disableColor) {
             response.push(
                 {
                     content: (
@@ -54,19 +53,45 @@ const ColorControl = props => {
                             color={color}
                             onChangeComplete={val => onColorChange(returnColor(val))}
                         />
-                    )
+                    ),
+                    classNamePopover: "gx-colorcontrol-color-popover gx-popover",
+                    icon: colorIcon
                 },
             )
         }
-        if (!isNil(showGradient)) {
+        if (!disableGradient) {
             response.push(
                 {
                     content: (
-                        <__experimentalGradientPicker
-                            value={gradient}
-                            onChange={val => onGradientChange(val)}
-                        />
-                    )
+                        <Fragment>
+                            <__experimentalGradientPicker
+                                value={gradient}
+                                onChange={val => onGradientChange(val)}
+                            />
+                            {disableGradientAboveBackground &&
+                                <CheckBox
+                                    label={__('Above Background Image', 'gutenberg-extra')}
+                                    checked={gradientAboveBackground}
+                                    onChange={val => onGradientAboveBackgroundChange(val)}
+                                />
+                            }
+                        </Fragment>
+                    ),
+                    classNamePopover: "gx-colorcontrol-gradient-popover gx-popover",
+                    icon: <svg x="0px" y="0px" viewBox="0 0 24 24" xmlSpace="preserve">
+                    <linearGradient
+                      id="a"
+                      gradientUnits="userSpaceOnUse"
+                      x1={0.3129}
+                      y1={12}
+                      x2={23.687}
+                      y2={12}
+                    >
+                      <stop offset={0} stopColor="#fff" />
+                      <stop offset={1} />
+                    </linearGradient>
+                    <circle cx={12} cy={12} r={11.7} fill="url(#a)" />
+                  </svg>
                 }
             )
         }
@@ -75,11 +100,25 @@ const ColorControl = props => {
     }
 
     return (
-        <PopoverControl
-            label={label}
-            showReset
-            popovers={getPopovers()}
-        />
+        <div className="gx-colorcontrol-control">
+            <div className="gx-colorcontrol-color-display">
+                <span
+                    style={{
+                        background: gradient ? gradient : color,
+                    }}
+                ></span>
+            </div>
+            <PopoverControl
+                label={label}
+                showReset
+                onReset={() => {
+                    onColorChange('');
+                    onGradientChange('');
+                    onGradientAboveBackgroundChange(false);
+                }}
+                popovers={getPopovers()}
+            />
+        </div>
     )
 }
 

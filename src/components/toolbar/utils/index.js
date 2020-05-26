31568b643f8f3bf636155ac08e127f158c318ad0
Naaaaiix <jjuancap@gmail.com>
2020-05-26 17:17:46 +0530

    Basic control on toolbar

---
+++ src/components/toolbar/utils/index.js
@@ -0,0 +1,7 @@
+export { default as ColumnPattern } from'./columnpattern';
+export { default as Delete } from'./delete';
+export { default as DragAndDrop } from'./dragndrop';
+export { default as Duplicate } from'./duplicate';
+export { default as Favorite } from'./favorite';
+export { default as Link } from'./link';
+export { default as Style } from'./style';
---
+++ src/extensions/save/index.js
@@ -0,0 +1,52 @@
+/**
+ * WordPress Dependencies
+ */
+const { addFilter } = wp.hooks;
+
+/**
+ * General
+ */
+const allowedBlocks = [
+	'maxi-blocks/row-maxi',
+	'maxi-blocks/column-maxi',
+	'maxi-blocks/text-maxi',
+	'maxi-blocks/divider-maxi',
+	'maxi-blocks/image-maxi',
+	'maxi-blocks/section-maxi',
+	'maxi-blocks/test-maxi',
+];
+
+/**
+ * Add hyperlink element on Maxi Blocks with toolbar link activated
+ *
+ * @param {object} BlockSave Original saved object.
+ * @return {string} Wrapped component.
+ */
+
+const withSave = (element, blockType, attributes) => {
+	if (
+		allowedBlocks.includes(blockType.name) &&
+		!!attributes.linkSettings &&
+		!!JSON.parse(attributes.linkSettings).url
+	){
+		const linkSettings = JSON.parse(attributes.linkSettings);
+
+		return (
+			<a
+				className="maxi-link-wrapper"
+				href={linkSettings.url}
+				target={!!linkSettings.opensInNewTab ? '_blank' : '_self'}
+			>
+				{element}
+			</a>
+		)
+	}
+
+	return element;
+}
+
+addFilter(
+	'blocks.getSaveElement',
+	'maxi-blocks/save',
+	withSave
+);

4c6e62d281bda06070dbe99700a993548cd1cca5
Naaaaiix <jjuancap@gmail.com>
2020-06-04 13:08:34 +0530

    Create DefaultStylesControl - #152 Add DefaultStylesControl to BorderControl & BoxShadowControl Refactor BorderControl & BoxShadowControl to be a function and not a class

---
+++ src/components/box-shadow-control/defaults.js
@@ -0,0 +1,29 @@
+export const boxShadowTotal = {
+    "label": "Box Shadow",
+    "shadowColor": "#A2A2A2",
+    "defaultShadowColor": "#000",
+    "shadowHorizontal": 0,
+    "shadowVertical": 0,
+    "shadowBlur": 100,
+    "shadowSpread": 0
+}
+
+export const boxShadowBottom = {
+    "label": "Box Shadow",
+    "shadowColor": "#A2A2A2",
+    "defaultShadowColor": "#000",
+    "shadowHorizontal": 0,
+    "shadowVertical": 50,
+    "shadowBlur": 100,
+    "shadowSpread": -50
+}
+
+export const boxShadowSolid = {
+    "label": "Box Shadow",
+    "shadowColor": "#A2A2A2",
+    "defaultShadowColor": "#000",
+    "shadowHorizontal": 12,
+    "shadowVertical": 24,
+    "shadowBlur": 0,
+    "shadowSpread": 0
+}

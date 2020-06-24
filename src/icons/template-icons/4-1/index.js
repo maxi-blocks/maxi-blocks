ec588afde1c75e74d6d4a292d796da782927a6e5
javad sameei <j.sameei@gmail.com>
2020-06-24 18:03:11 +0430

    Need Javad magic here

--- src/icons/template-icons/4-1/index.js
+++ src/icons/template-icons/4-1/index.js
@@ -4,22 +4,8 @@
 const { SVG, Path } = wp.primitives;
 
 const fourOne = (
-  <SVG
-    width="24"
-    height="7"
-    x="0px"
-    y="0px"
-    viewBox="0 0 112 36"
-    xmlSpace="preserve"
-  >
-    <Path
-      d="M0 35V1h74v34H0zm111.5 0H78.8V1h32.7m.5 0v34"
-      fill="none"
-      stroke="#464a53"
-      strokeWidth={2}
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    />
+  <SVG xmlns="http://www.w3.org/2000/svg" viewBox="0 0 112 36">
+    <Path d="M0 35V1h74v34H0zm111.5 0H78.8V1h32.7m.5 0v34" />
   </SVG>
 );
 

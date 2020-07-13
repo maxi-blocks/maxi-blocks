72257fed083252c1a1aa65432f84bb4813ae8715
Naaaaiix <jjuancap@gmail.com>
2020-07-13 13:25:51 +0530

    Add bold and italic individual selector on Toolbar

--- src/components/toolbar/components/text-italic/index.js
+++ src/components/toolbar/components/text-italic/index.js
@@ -1,10 +1,22 @@
 /**
  * WordPress dependencies
  */
+const { Fragment } = wp.element;
+const { useSelect } = wp.data;
 const {
     Icon,
     Button,
 } = wp.components;
+const {
+    toggleFormat,
+    create,
+    toHTMLString
+} = wp.richText;
+
+/**
+ * External dependencies
+ */
+import { find } from 'lodash';
 
 /**
  * Styles and icons
@@ -18,36 +30,101 @@ import { toolbarItalic } from '../../../../icons';
 const TextItalic = props => {
     const {
         blockName,
-        typography,
+        content,
         onChange,
-        breakpoint
+        node
     } = props;
 
     if (blockName != 'maxi-blocks/text-maxi')
         return null;
 
-    const getBoldTypography = () => {
-        if (value[breakpoint]['font-style'] != 'italic') 
-            value[breakpoint]['font-style'] = 'italic';
-        else 
-            value[breakpoint]['font-style'] = 'normal';
+    /**
+     * Gets the all format objects at the start of the selection.
+     * 
+     * @param {Object} value                Value to inspect.
+     * @param {Array}  EMPTY_ACTIVE_FORMATS Array to return if there are no active
+     *                                      formats.
+     *
+     * @return {?Object} Active format objects.
+     * 
+     * @package Gutenberg
+     * @see packages/rich-text/src/get-active-formats.js
+     */
+    const getActiveFormats = (
+        { formats, start, end, activeFormats },
+        EMPTY_ACTIVE_FORMATS = []
+    ) => {
+        if (start === undefined) {
+            return EMPTY_ACTIVE_FORMATS;
+        }
 
-        onChange(JSON.stringify(value))
+        if (start === end) {
+            // For a collapsed caret, it is possible to override the active formats.
+            if (activeFormats) {
+                return activeFormats;
+            }
+
+            const formatsBefore = formats[start - 1] || EMPTY_ACTIVE_FORMATS;
+            const formatsAfter = formats[start] || EMPTY_ACTIVE_FORMATS;
+
+            // By default, select the lowest amount of formats possible (which means
+            // the caret is positioned outside the format boundary). The user can
+            // then use arrow keys to define `activeFormats`.
+            if (formatsBefore.length < formatsAfter.length) {
+                return formatsBefore;
+            }
+
+            return formatsAfter;
+        }
+
+        return formats[start] || EMPTY_ACTIVE_FORMATS;
     }
 
-    let value = JSON.parse(typography);
+    const { formatValue, isActive } = useSelect(
+        select => {
+            const {
+                getSelectionStart,
+                getSelectionEnd
+            } = select('core/block-editor');
+            const formatValue = create({
+                element: node,
+                html: content,
+            });
+            formatValue['start'] = getSelectionStart().offset;
+            formatValue['end'] = getSelectionEnd().offset;
+
+            const isActive = !!find(getActiveFormats(formatValue), { type: 'core/italic' });
+            return {
+                formatValue,
+                isActive
+            }
+        },
+        [getActiveFormats, node, content]
+    )
+
+    const onClick = () => {
+        const newFormat = toggleFormat(formatValue, { type: 'core/italic' });
+
+        const newContent = toHTMLString({
+            value: newFormat
+        })
+
+        onChange(newContent)
+    }
 
     return (
-        <Button
-            className='toolbar-item toolbar-item__italic'
-            onClick={getBoldTypography}
-            aria-pressed={value[breakpoint]['font-style'] === 'italic'}
-        >
-            <Icon
-                className='toolbar-item__icon'
-                icon={toolbarItalic}
-            />
-        </Button>
+        <Fragment>
+            <Button
+                className='toolbar-item toolbar-item__italic'
+                onClick={onClick}
+                aria-pressed={isActive}
+            >
+                <Icon
+                    className='toolbar-item__icon'
+                    icon={toolbarItalic}
+                />
+            </Button>
+        </Fragment>
     )
 }
 

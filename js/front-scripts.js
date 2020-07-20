97904a875436c431d23e4989f5d260d96c711f16
javad sameei <j.sameei@gmail.com>
2020-07-20 16:06:14 +0430

    Add Horizontal, Rotate, Scale and Fade

--- js/front-scripts.js
+++ js/front-scripts.js
@@ -40,65 +40,168 @@ motionElems.forEach(function(elem) {
 
 	const motionID = elem.getAttribute('data-motion-id');
 	const motionData = JSON.parse(elem.getAttribute('data-motion'));
-	const maxSpeed = 10;
-	const currentSpeed = motionData.vertical.speed;
-	const direction = motionData.vertical.direction;
-	const status = motionData.vertical.status;
-	const viewportTop = motionData.vertical.viewportTop;
-	const viewportBottom = motionData.vertical.viewportBottom;
-	if(!!parseInt(status)) {
-		const motionTimeLine = gsap.timeline({
-			scrollTrigger: {
-				trigger: ".maxi-motion-effect-"+ motionID +"",
-				start: "top "+ viewportTop +"%",
-				end: "bottom "+ viewportBottom +"%",
-				scrub: 1,
-				markers: true,
-				onEnter: self => {
-					self.trigger = elem;
-				},
+
+	if( "vertical" in motionData ) {
+
+		const maxSpeed = 10;
+		const currentSpeed = motionData.vertical.speed;
+		const direction = motionData.vertical.direction;
+		const status = motionData.vertical.status;
+		const viewportTop = motionData.vertical.viewportTop;
+		const viewportBottom = motionData.vertical.viewportBottom;
+
+		if(!!parseInt(status)) {
+			const motionTimeLine = gsap.timeline({
+				scrollTrigger: {
+					trigger: ".maxi-motion-effect-"+ motionID +"",
+					start: "top "+ viewportTop +"%",
+					end: "bottom "+ viewportBottom +"%",
+					scrub: 1,
+					markers: true,
+					onEnter: self => {
+						self.trigger = elem;
+					},
+				}
+			});
+			motionTimeLine.to(".maxi-motion-effect-"+ motionID +"", {
+				y: (direction === 'up') ?
+					((currentSpeed * vw) / maxSpeed) :
+					-((currentSpeed * vw) / maxSpeed),
+				duration: 1,
+				ease: "power1.out"
+			});
+		}
+	}
+
+	if( "horizontal" in motionData ) {
+
+		const maxSpeed = 10;
+		const currentSpeed = motionData.horizontal.speed;
+		const direction = motionData.horizontal.direction;
+		const status = motionData.horizontal.status;
+		const viewportTop = motionData.horizontal.viewportTop;
+		const viewportBottom = motionData.horizontal.viewportBottom;
+
+		if(!!parseInt(status)) {
+			const motionTimeLine = gsap.timeline({
+				scrollTrigger: {
+					trigger: ".maxi-motion-effect-"+ motionID +"",
+					start: "top "+ viewportTop +"%",
+					end: "bottom "+ viewportBottom +"%",
+					scrub: 1,
+					markers: true,
+					onEnter: self => {
+						self.trigger = elem;
+					},
+				}
+			});
+			motionTimeLine.to(".maxi-motion-effect-"+ motionID +"", {
+				x: (direction === 'left') ?
+					-((currentSpeed * vw) / maxSpeed) :
+					((currentSpeed * vw) / maxSpeed),
+				duration: 1,
+				ease: "power1.out"
+			});
+		}
+	}
+
+	if( "rotate" in motionData ) {
+
+		const maxSpeed = 10;
+		const currentSpeed = motionData.rotate.speed;
+		const direction = motionData.rotate.direction;
+		const status = motionData.rotate.status;
+		const viewportTop = motionData.rotate.viewportTop;
+		const viewportBottom = motionData.rotate.viewportBottom;
+
+		if(!!parseInt(status)) {
+			const motionTimeLine = gsap.timeline({
+				scrollTrigger: {
+					trigger: ".maxi-motion-effect-"+ motionID +"",
+					start: "top "+ viewportTop +"%",
+					end: "bottom "+ viewportBottom +"%",
+					scrub: 1,
+					markers: true,
+					onEnter: self => {
+						self.trigger = elem;
+					},
+				}
+			});
+			motionTimeLine.to(".maxi-motion-effect-"+ motionID +"", {
+				rotation: (direction === 'left') ?
+					-((currentSpeed * vw) / maxSpeed) :
+					((currentSpeed * vw) / maxSpeed),
+				duration: 1,
+				ease: "power1.out"
+			});
+		}
+	}
+
+	if( "scale" in motionData ) {
+
+		const currentSpeed = motionData.scale.speed;
+		const direction = motionData.scale.direction;
+		const status = motionData.scale.status;
+		const viewportTop = motionData.scale.viewportTop;
+		const viewportBottom = motionData.scale.viewportBottom;
+
+		if(!!parseInt(status)) {
+			const motionTimeLine = gsap.timeline({
+				scrollTrigger: {
+					trigger: ".maxi-motion-effect-"+ motionID +"",
+					start: "top "+ viewportTop +"%",
+					end: "bottom "+ viewportBottom +"%",
+					scrub: 1,
+					markers: true,
+					onEnter: self => {
+						self.trigger = elem;
+					},
+				}
+			});
+			motionTimeLine.to(".maxi-motion-effect-"+ motionID +"", {
+				scale: (direction === 'up') ? -currentSpeed : currentSpeed,
+				duration: 1,
+				ease: "power1.out"
+			});
+		}
+	}
+
+	if( "fade" in motionData ) {
+
+		const maxSpeed = 10;
+		const currentSpeed = motionData.fade.speed;
+		const direction = motionData.fade.direction;
+		const status = motionData.fade.status;
+		const viewportTop = motionData.fade.viewportTop;
+		const viewportBottom = motionData.fade.viewportBottom;
+
+		if(!!parseInt(status)) {
+			const motionTimeLine = gsap.timeline({
+				scrollTrigger: {
+					trigger: ".maxi-motion-effect-"+ motionID +"",
+					start: "top "+ viewportTop +"%",
+					end: "bottom "+ viewportBottom +"%",
+					scrub: 1,
+					markers: true,
+					onEnter: self => {
+						self.trigger = elem;
+					},
+				}
+			});
+			if((direction === 'in')) {
+				motionTimeLine.to(".maxi-motion-effect-"+ motionID +"", {
+					opacity: (10 - currentSpeed) / 10,
+					duration: 1,
+					ease: "power1.out"
+				});
+			} else {
+				motionTimeLine.from(".maxi-motion-effect-"+ motionID +"", {
+					opacity: (10 - currentSpeed) / 10,
+					duration: 1,
+					ease: "power1.out"
+				});
 			}
-		});
-		motionTimeLine.to(".maxi-motion-effect-"+ motionID +"", {
-			y: (direction === 'up') ? ((currentSpeed * vw) / maxSpeed) : -((currentSpeed * vw) / maxSpeed),
-			duration: 1,
-			ease: "power1.out"
-		});
+		}
 	}
 
 });
-
-
-
-
-
-// const motionElems = document.querySelectorAll(".maxi-motion-effect");
-// motionElems.forEach(function(elem) {
-// 	const motionID = elem.getAttribute('data-motion-id');
-// 	const motionData = JSON.parse(elem.getAttribute('data-motion'));
-
-// 	const maxSpeed = 10;
-// 	const currentSpeed = motionData.vertical.speed;
-// 	const direction = motionData.vertical.direction;
-// 	const status = motionData.vertical.status;
-// 	const viewportTop = motionData.vertical.viewportTop;
-// 	const viewportBottom = motionData.vertical.viewportBottom;
-// 	const motionTimeLine = gsap.timeline({
-// 		scrollTrigger: {
-// 			trigger: ".maxi-motion-effect-"+ motionID +"",
-// 			start: "top "+ viewportTop +"%",
-// 			end: "bottom "+ viewportBottom +"%",
-// 			scrub: 1,
-// 			markers: true,
-// 			onEnter: self => {
-// 				self.trigger = elem;
-// 			},
-// 		}
-// 	});
-// 	motionTimeLine.to(".maxi-motion-effect-"+ motionID +"", {
-// 		x: (direction === 'right') ? ((currentSpeed * vw) / maxSpeed) : -((currentSpeed * vw) / maxSpeed),
-// 		duration: 1,
-// 		ease: "power1.out"
-// 	});
-
-// });
--- src/components/motion-control/index.js
+++ src/components/motion-control/index.js
@@ -51,6 +51,11 @@ const ShapeDividerControl = props => {
 
     let {
         vertical: verticalOptions,
+        horizontal: horizontalOptions,
+        rotate: rotateOptions,
+        scale: scaleOptions,
+        fade: fadeOptions,
+        blur: blurOptions,
     } = value;
 
     const [motionStatus, setMotionStatus] = useState('vertical');
@@ -77,7 +82,6 @@ const ShapeDividerControl = props => {
             {
             motionStatus === 'vertical' &&
                 <Fragment>
-            {console.log(typeof verticalOptions.status, verticalOptions.status)}
                     <div className='maxi-fancy-radio-control'>
                         <RadioControl
                             label={__('Enable Vertical', 'maxi-block')}
@@ -111,7 +115,7 @@ const ShapeDividerControl = props => {
                         disableUnit
                         min={0}
                         max={10}
-                        initial={4}
+                        initial={1}
                         step={0.1}
                         value={verticalOptions.speed}
                         onChangeValue={val => {
@@ -148,31 +152,351 @@ const ShapeDividerControl = props => {
             {
             motionStatus === 'horizontal' &&
                 <Fragment>
-                    <h1>horizontal Settings</h1>
+                    <div className='maxi-fancy-radio-control'>
+                        <RadioControl
+                            label={__('Enable Horizontal', 'maxi-block')}
+                            selected={parseInt(horizontalOptions.status)}
+                            options={
+                                [
+                                    { label: __('Yes', 'maxi-block'), value: 1 },
+                                    { label: __('No', 'maxi-block'), value: 0 },
+                                ]
+                            }
+                            onChange={val => {
+                                horizontalOptions.status = val;
+                                onChange(JSON.stringify(value));
+                            }}
+                        />
+                    </div>
+                    <SelectControl
+                        label={__('Direction', 'maxi-blocks')}
+                        value={horizontalOptions.direction}
+                        options={[
+                            { label: 'To left', value: 'left' },
+                            { label: 'To Right', value: 'right' },
+                        ]}
+                        onChange={val => {
+                            horizontalOptions.direction = val;
+                            onChange(JSON.stringify(value));
+                        }}
+                    />
+                    <SizeControl
+                        label={__('Speed', 'maxi-blocks')}
+                        disableUnit
+                        min={0}
+                        max={10}
+                        initial={5}
+                        step={0.1}
+                        value={horizontalOptions.speed}
+                        onChangeValue={val => {
+                            horizontalOptions.speed = val;
+                            onChange(JSON.stringify(value));
+                        }}
+                    />
+                    <SizeControl
+                        label={__('Viewport Top', 'maxi-blocks')}
+                        disableUnit
+                        min={0}
+                        max={100}
+                        initial={100}
+                        value={horizontalOptions.viewportTop}
+                        onChangeValue={val => {
+                            horizontalOptions.viewportTop = val;
+                            onChange(JSON.stringify(value));
+                        }}
+                    />
+                    <SizeControl
+                        label={__('Viewport Bottom', 'maxi-blocks')}
+                        disableUnit
+                        min={0}
+                        max={100}
+                        initial={0}
+                        value={horizontalOptions.viewportBottom}
+                        onChangeValue={val => {
+                            horizontalOptions.viewportBottom = val;
+                            onChange(JSON.stringify(value));
+                        }}
+                    />
                 </Fragment>
             }
             {
             motionStatus === 'rotate' &&
                 <Fragment>
-                    <h1>rotate Settings</h1>
+                    <div className='maxi-fancy-radio-control'>
+                        <RadioControl
+                            label={__('Enable Rotate', 'maxi-block')}
+                            selected={parseInt(rotateOptions.status)}
+                            options={
+                                [
+                                    { label: __('Yes', 'maxi-block'), value: 1 },
+                                    { label: __('No', 'maxi-block'), value: 0 },
+                                ]
+                            }
+                            onChange={val => {
+                                rotateOptions.status = val;
+                                onChange(JSON.stringify(value));
+                            }}
+                        />
+                    </div>
+                    <SelectControl
+                        label={__('Direction', 'maxi-blocks')}
+                        value={rotateOptions.direction}
+                        options={[
+                            { label: 'To left', value: 'left' },
+                            { label: 'To Right', value: 'right' },
+                        ]}
+                        onChange={val => {
+                            rotateOptions.direction = val;
+                            onChange(JSON.stringify(value));
+                        }}
+                    />
+                    <SizeControl
+                        label={__('Speed', 'maxi-blocks')}
+                        disableUnit
+                        min={0}
+                        max={10}
+                        initial={0.3}
+                        step={0.1}
+                        value={rotateOptions.speed}
+                        onChangeValue={val => {
+                            rotateOptions.speed = val;
+                            onChange(JSON.stringify(value));
+                        }}
+                    />
+                    <SizeControl
+                        label={__('Viewport Top', 'maxi-blocks')}
+                        disableUnit
+                        min={0}
+                        max={100}
+                        initial={100}
+                        value={rotateOptions.viewportTop}
+                        onChangeValue={val => {
+                            rotateOptions.viewportTop = val;
+                            onChange(JSON.stringify(value));
+                        }}
+                    />
+                    <SizeControl
+                        label={__('Viewport Bottom', 'maxi-blocks')}
+                        disableUnit
+                        min={0}
+                        max={100}
+                        initial={0}
+                        value={rotateOptions.viewportBottom}
+                        onChangeValue={val => {
+                            rotateOptions.viewportBottom = val;
+                            onChange(JSON.stringify(value));
+                        }}
+                    />
                 </Fragment>
             }
             {
             motionStatus === 'scale' &&
                 <Fragment>
-                    <h1>scale Settings</h1>
+                    <div className='maxi-fancy-radio-control'>
+                        <RadioControl
+                            label={__('Enable Scale', 'maxi-block')}
+                            selected={parseInt(scaleOptions.status)}
+                            options={
+                                [
+                                    { label: __('Yes', 'maxi-block'), value: 1 },
+                                    { label: __('No', 'maxi-block'), value: 0 },
+                                ]
+                            }
+                            onChange={val => {
+                                scaleOptions.status = val;
+                                onChange(JSON.stringify(value));
+                            }}
+                        />
+                    </div>
+                    <SelectControl
+                        label={__('Direction', 'maxi-blocks')}
+                        value={scaleOptions.direction}
+                        options={[
+                            { label: 'Scale Up', value: 'up' },
+                            { label: 'Scale Down', value: 'down' },
+                        ]}
+                        onChange={val => {
+                            scaleOptions.direction = val;
+                            onChange(JSON.stringify(value));
+                        }}
+                    />
+                    <SizeControl
+                        label={__('Speed', 'maxi-blocks')}
+                        disableUnit
+                        min={0}
+                        max={10}
+                        initial={5}
+                        step={0.1}
+                        value={scaleOptions.speed}
+                        onChangeValue={val => {
+                            scaleOptions.speed = val;
+                            onChange(JSON.stringify(value));
+                        }}
+                    />
+                    <SizeControl
+                        label={__('Viewport Top', 'maxi-blocks')}
+                        disableUnit
+                        min={0}
+                        max={100}
+                        initial={100}
+                        value={scaleOptions.viewportTop}
+                        onChangeValue={val => {
+                            scaleOptions.viewportTop = val;
+                            onChange(JSON.stringify(value));
+                        }}
+                    />
+                    <SizeControl
+                        label={__('Viewport Bottom', 'maxi-blocks')}
+                        disableUnit
+                        min={0}
+                        max={100}
+                        initial={0}
+                        value={scaleOptions.viewportBottom}
+                        onChangeValue={val => {
+                            scaleOptions.viewportBottom = val;
+                            onChange(JSON.stringify(value));
+                        }}
+                    />
                 </Fragment>
             }
             {
             motionStatus === 'fade' &&
                 <Fragment>
-                    <h1>fade Settings</h1>
+                    <div className='maxi-fancy-radio-control'>
+                        <RadioControl
+                            label={__('Enable Fade', 'maxi-block')}
+                            selected={parseInt(fadeOptions.status)}
+                            options={
+                                [
+                                    { label: __('Yes', 'maxi-block'), value: 1 },
+                                    { label: __('No', 'maxi-block'), value: 0 },
+                                ]
+                            }
+                            onChange={val => {
+                                fadeOptions.status = val;
+                                onChange(JSON.stringify(value));
+                            }}
+                        />
+                    </div>
+                    <SelectControl
+                        label={__('Direction', 'maxi-blocks')}
+                        value={fadeOptions.direction}
+                        options={[
+                            { label: 'Fade In', value: 'in' },
+                            { label: 'Fade Out', value: 'out' },
+                        ]}
+                        onChange={val => {
+                            fadeOptions.direction = val;
+                            onChange(JSON.stringify(value));
+                        }}
+                    />
+                    <SizeControl
+                        label={__('Level', 'maxi-blocks')}
+                        disableUnit
+                        min={0}
+                        max={10}
+                        initial={5}
+                        step={0.1}
+                        value={fadeOptions.speed}
+                        onChangeValue={val => {
+                            fadeOptions.speed = val;
+                            onChange(JSON.stringify(value));
+                        }}
+                    />
+                    <SizeControl
+                        label={__('Viewport Top', 'maxi-blocks')}
+                        disableUnit
+                        min={0}
+                        max={100}
+                        initial={100}
+                        value={fadeOptions.viewportTop}
+                        onChangeValue={val => {
+                            fadeOptions.viewportTop = val;
+                            onChange(JSON.stringify(value));
+                        }}
+                    />
+                    <SizeControl
+                        label={__('Viewport Bottom', 'maxi-blocks')}
+                        disableUnit
+                        min={0}
+                        max={100}
+                        initial={0}
+                        value={fadeOptions.viewportBottom}
+                        onChangeValue={val => {
+                            fadeOptions.viewportBottom = val;
+                            onChange(JSON.stringify(value));
+                        }}
+                    />
                 </Fragment>
             }
             {
             motionStatus === 'blur' &&
                 <Fragment>
-                    <h1>blur Settings</h1>
+                    <div className='maxi-fancy-radio-control'>
+                        <RadioControl
+                            label={__('Enable Blur', 'maxi-block')}
+                            selected={parseInt(blurOptions.status)}
+                            options={
+                                [
+                                    { label: __('Yes', 'maxi-block'), value: 1 },
+                                    { label: __('No', 'maxi-block'), value: 0 },
+                                ]
+                            }
+                            onChange={val => {
+                                blurOptions.status = val;
+                                onChange(JSON.stringify(value));
+                            }}
+                        />
+                    </div>
+                    <SelectControl
+                        label={__('Direction', 'maxi-blocks')}
+                        value={blurOptions.direction}
+                        options={[
+                            { label: 'Blur In', value: 'in' },
+                            { label: 'Blur Out', value: 'out' },
+                        ]}
+                        onChange={val => {
+                            blurOptions.direction = val;
+                            onChange(JSON.stringify(value));
+                        }}
+                    />
+                    <SizeControl
+                        label={__('Level', 'maxi-blocks')}
+                        disableUnit
+                        min={0}
+                        max={10}
+                        initial={5}
+                        step={0.1}
+                        value={blurOptions.speed}
+                        onChangeValue={val => {
+                            blurOptions.speed = val;
+                            onChange(JSON.stringify(value));
+                        }}
+                    />
+                    <SizeControl
+                        label={__('Viewport Top', 'maxi-blocks')}
+                        disableUnit
+                        min={0}
+                        max={100}
+                        initial={100}
+                        value={blurOptions.viewportTop}
+                        onChangeValue={val => {
+                            blurOptions.viewportTop = val;
+                            onChange(JSON.stringify(value));
+                        }}
+                    />
+                    <SizeControl
+                        label={__('Viewport Bottom', 'maxi-blocks')}
+                        disableUnit
+                        min={0}
+                        max={100}
+                        initial={0}
+                        value={blurOptions.viewportBottom}
+                        onChangeValue={val => {
+                            blurOptions.viewportBottom = val;
+                            onChange(JSON.stringify(value));
+                        }}
+                    />
                 </Fragment>
             }
         </div>

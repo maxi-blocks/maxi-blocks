f60d00f6356015e73f0ff03e01e2888dabd2c748
Javad <65819265+javadsamiee@users.noreply.github.com>
2020-09-08 22:05:44 +0430

    issue 51 (#413)
    * Fix Display Block radio option style
    Fix Transform styles
    
    * Fix sync and reset
    
    * Fix default origin
    
    Co-authored-by: javad sameei <j.sameei@gmail.com>

--- src/components/transform-control/rotate-control.js
+++ src/components/transform-control/rotate-control.js
@@ -1,7 +1,7 @@
 /**
  * WordPress dependencies
  */
-const { Button } = wp.components;
+const { Button, BaseControl } = wp.components;
 const { useState, useEffect } = wp.element;
 
 /**
@@ -33,130 +33,145 @@ const RotateControl = props => {
 	return (
 		<div className='maxi-transform-control__rotate-control'>
 			<div className='maxi-transform-control__rotate-control__item'>
-				<input
-					type='range'
-					className='maxi-transform-control__rotate-control__item__range'
-					value={yAxis}
-					onChange={e => {
-						changeYAxis(Number(e.target.value));
-						onChange(xAxis, Number(e.target.value), zAxis);
-					}}
-					min='0'
-					max='100'
-					orient='vertical'
-				/>
-				<input
-					type='number'
-					placeholder='0deg'
-					className='maxi-transform-control__rotate-control__item__input'
-					value={yAxis}
-					onChange={e => {
-						const newValue = !isEmpty(e.target.value)
-							? Number(e.target.value)
-							: '';
-						changeYAxis(newValue);
-						onChange(xAxis, newValue, zAxis);
-					}}
-				/>
-				<Button
-					className='components-maxi-control__reset-button'
-					onClick={() => {
-						changeYAxis('');
-						onChange(xAxis, '', zAxis);
-					}}
-					// aria-label={sprintf(
-					//     __('Reset %s settings', 'maxi-blocks'),
-					//     value.label.toLowerCase()
-					// )}
-					action='reset'
-					type='reset'
+				<BaseControl
+					label={__('X', 'maxi-blocks')}
+					className='maxi-transform-control__rotate-control__item__label'
 				>
-					{reset}
-				</Button>
+					<input
+						type='range'
+						className='maxi-transform-control__rotate-control__item__range'
+						value={yAxis}
+						onChange={e => {
+							changeYAxis(Number(e.target.value));
+							onChange(xAxis, Number(e.target.value), zAxis);
+						}}
+						min='0'
+						max='100'
+						orient='vertical'
+					/>
+					<input
+						type='number'
+						placeholder='0deg'
+						className='maxi-transform-control__rotate-control__item__input'
+						value={yAxis}
+						onChange={e => {
+							const newValue = !isEmpty(e.target.value)
+								? Number(e.target.value)
+								: '';
+							changeYAxis(newValue);
+							onChange(xAxis, newValue, zAxis);
+						}}
+					/>
+					<Button
+						className='components-maxi-control__reset-button'
+						onClick={() => {
+							changeYAxis('');
+							onChange(xAxis, '', zAxis);
+						}}
+						// aria-label={sprintf(
+						//     __('Reset %s settings', 'maxi-blocks'),
+						//     value.label.toLowerCase()
+						// )}
+						action='reset'
+						type='reset'
+					>
+						{reset}
+					</Button>
+				</BaseControl>
 			</div>
 			<div className='maxi-transform-control__rotate-control__item'>
-				<input
-					type='range'
-					className='maxi-transform-control__rotate-control__item__range'
-					value={xAxis}
-					onChange={e => {
-						changeXAxis(Number(e.target.value));
-						onChange(Number(e.target.value), yAxis, zAxis);
-					}}
-					min='0'
-					max='100'
-					orient='vertical'
-				/>
-				<input
-					type='number'
-					placeholder='0deg'
-					className='maxi-transform-control__rotate-control__item__input'
-					value={xAxis}
-					onChange={e => {
-						const newValue = !isEmpty(e.target.value)
-							? Number(e.target.value)
-							: '';
-						changeXAxis(newValue);
-						onChange(newValue, yAxis, zAxis);
-					}}
-				/>
-				<Button
-					className='components-maxi-control__reset-button'
-					onClick={() => {
-						changeXAxis('');
-						onChange('', yAxis, zAxis);
-					}}
-					// aria-label={sprintf(
-					//     __('Reset %s settings', 'maxi-blocks'),
-					//     value.label.toLowerCase()
-					// )}
-					action='reset'
-					type='reset'
+				<BaseControl
+					label={__('Y', 'maxi-blocks')}
+					className='maxi-transform-control__rotate-control__item__label'
 				>
-					{reset}
-				</Button>
+					<input
+						type='range'
+						className='maxi-transform-control__rotate-control__item__range'
+						value={xAxis}
+						onChange={e => {
+							changeXAxis(Number(e.target.value));
+							onChange(Number(e.target.value), yAxis, zAxis);
+						}}
+						min='0'
+						max='100'
+						orient='vertical'
+					/>
+					<input
+						type='number'
+						placeholder='0deg'
+						className='maxi-transform-control__rotate-control__item__input'
+						value={xAxis}
+						onChange={e => {
+							const newValue = !isEmpty(e.target.value)
+								? Number(e.target.value)
+								: '';
+							changeXAxis(newValue);
+							onChange(newValue, yAxis, zAxis);
+						}}
+					/>
+					<Button
+						className='components-maxi-control__reset-button'
+						onClick={() => {
+							changeXAxis('');
+							onChange('', yAxis, zAxis);
+						}}
+						// aria-label={sprintf(
+						//     __('Reset %s settings', 'maxi-blocks'),
+						//     value.label.toLowerCase()
+						// )}
+						action='reset'
+						type='reset'
+					>
+						{reset}
+					</Button>
+				</BaseControl>
 			</div>
 			<div className='maxi-transform-control__rotate-control__item'>
-				<input
-					type='range'
-					className='maxi-transform-control__rotate-control__item__range'
-					value={zAxis}
-					onChange={e => {
-						changeZAxis(Number(e.target.value));
-						onChange(xAxis, yAxis, Number(e.target.value));
-					}}
-					min='0'
-					max='100'
-					orient='vertical'
-				/>
-				<input
-					type='number'
-					placeholder='0deg'
-					className='maxi-transform-control__rotate-control__item__input'
-					value={zAxis}
-					onChange={e => {
-						const newValue = !isEmpty(e.target.value)
-							? Number(e.target.value)
-							: '';
-						changeZAxis(newValue);
-						onChange(xAxis, yAxis, newValue);
-					}}
-				/>
-				<Button
-					className='components-maxi-control__reset-button'
-					onClick={() => {
-						changeZAxis('');
-						onChange(xAxis, yAxis, '');
-					}}
-					// aria-label={sprintf(
-					//     __('Reset %s settings', 'maxi-blocks'),
-					//     value.label.toLowerCase()
-					// )}
-					action='reset'
-					type='reset'
+				<BaseControl
+					label={__('Z', 'maxi-blocks')}
+					className='maxi-transform-control__rotate-control__item__label'
 				>
-					{reset}
-				</Button>
+					<input
+						type='range'
+						className='maxi-transform-control__rotate-control__item__range'
+						value={zAxis}
+						onChange={e => {
+							changeZAxis(Number(e.target.value));
+							onChange(xAxis, yAxis, Number(e.target.value));
+						}}
+						min='0'
+						max='100'
+						orient='vertical'
+					/>
+					<input
+						type='number'
+						placeholder='0deg'
+						className='maxi-transform-control__rotate-control__item__input'
+						value={zAxis}
+						onChange={e => {
+							const newValue = !isEmpty(e.target.value)
+								? Number(e.target.value)
+								: '';
+							changeZAxis(newValue);
+							onChange(xAxis, yAxis, newValue);
+						}}
+					/>
+					<Button
+						className='components-maxi-control__reset-button'
+						onClick={() => {
+							changeZAxis('');
+							onChange(xAxis, yAxis, '');
+						}}
+						// aria-label={sprintf(
+						//     __('Reset %s settings', 'maxi-blocks'),
+						//     value.label.toLowerCase()
+						// )}
+						action='reset'
+						type='reset'
+					>
+						{reset}
+					</Button>
+				</BaseControl>
 			</div>
 		</div>
 	);

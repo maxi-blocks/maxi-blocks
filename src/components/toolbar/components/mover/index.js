8d647ae563810b79a5fcf042eca7896abe6d2f47
Javad <65819265+javadsamiee@users.noreply.github.com>
2020-09-21 11:43:45 +0330

    Issue 70 (#501)
    * Change columns movement icons
    
    * Add move up/down
    
    * Some bug fix and add disable for movement directions
    
    Co-authored-by: javad sameei <j.sameei@gmail.com>

--- src/components/toolbar/components/mover/index.js
+++ src/components/toolbar/components/mover/index.js
@@ -4,12 +4,19 @@
 const { __ } = wp.i18n;
 const { Draggable, Icon, Button, Tooltip } = wp.components;
 const { useSelect, useDispatch } = wp.data;
-const { useEffect, useRef } = wp.element;
+const { useEffect, useRef, Fragment } = wp.element;
+
+/**
+ * External dependencies
+ */
+import { castArray, first, last } from 'lodash';
 
 /**
  * Icons
  */
+import './editor.scss';
 import { toolbarMove } from '../../../../icons';
+import { moveUp, moveDown } from '../../../../icons';
 
 /**
  * Mover
@@ -17,22 +24,49 @@ import { toolbarMove } from '../../../../icons';
 const Mover = props => {
 	const { clientId, blockName } = props;
 
-	const { srcRootClientId, index, isDraggable } = useSelect(
+	const { moveBlocksDown, moveBlocksUp } = useDispatch('core/block-editor');
+
+	const {
+		srcRootClientId,
+		index,
+		isDraggable,
+		isTopDisabled,
+		isDownDisabled,
+	} = useSelect(
 		select => {
 			const {
 				getBlockIndex,
 				getBlockRootClientId,
 				getTemplateLock,
+				getBlockOrder,
+				getSelectedBlockClientIds,
 			} = select('core/block-editor');
 			const rootClientId = getBlockRootClientId(clientId);
 			const templateLock = rootClientId
 				? getTemplateLock(rootClientId)
 				: null;
+			const clientIds = getSelectedBlockClientIds();
+			const blockOrder = getBlockOrder(blockRootClientId);
+			const normalizedClientIds = castArray(clientIds);
+			const firstClientId = first(normalizedClientIds);
+			const blockRootClientId = getBlockRootClientId(firstClientId);
+			const firstBlockIndex = getBlockIndex(
+				firstClientId,
+				blockRootClientId
+			);
+			const lastBlockIndex = getBlockIndex(
+				last(normalizedClientIds),
+				blockRootClientId
+			);
+			const isFirstBlock = firstBlockIndex === 0;
+			const isLastBlock = lastBlockIndex === blockOrder.length - 1;
 
 			return {
 				index: getBlockIndex(clientId, rootClientId),
 				srcRootClientId: rootClientId,
 				isDraggable: templateLock !== 'all',
+				isTopDisabled: isFirstBlock,
+				isDownDisabled: isLastBlock,
 			};
 		},
 		[clientId]
@@ -42,7 +76,6 @@ const Mover = props => {
 		'core/block-editor'
 	);
 
-	// Stop dragging blocks if the block draggable is unmounted
 	useEffect(() => {
 		return () => {
 			if (isDragging.current) {
@@ -63,37 +96,67 @@ const Mover = props => {
 	};
 
 	return (
-		<Draggable
-			elementId={blockElementId}
-			transferData={transferData}
-			onDragStart={() => {
-				startDraggingBlocks();
-				isDragging.current = true;
-			}}
-			onDragEnd={() => {
-				stopDraggingBlocks();
-				isDragging.current = false;
-			}}
-		>
-			{({ onDraggableStart, onDraggableEnd }) => (
+		<Fragment>
+			<Draggable
+				elementId={blockElementId}
+				transferData={transferData}
+				onDragStart={() => {
+					startDraggingBlocks();
+					isDragging.current = true;
+				}}
+				onDragEnd={() => {
+					stopDraggingBlocks();
+					isDragging.current = false;
+				}}
+			>
+				{({ onDraggableStart, onDraggableEnd }) => (
+					<Tooltip
+						text={__('Mover', 'maxi-blocks')}
+						position='bottom center'
+					>
+						<Button
+							className='toolbar-item toolbar-item__move'
+							draggable={isDraggable}
+							onDragStart={onDraggableStart}
+							onDragEnd={onDraggableEnd}
+						>
+							<Icon
+								className='toolbar-item__icon'
+								icon={toolbarMove}
+							/>
+						</Button>
+					</Tooltip>
+				)}
+			</Draggable>
+			<div className='toolbar-item-move__vertically'>
 				<Tooltip
-					text={__('Mover', 'maxi-blocks')}
+					text={__('Move up', 'maxi-blocks')}
 					position='bottom center'
 				>
 					<Button
-						className='toolbar-item toolbar-item__move'
-						draggable={isDraggable}
-						onDragStart={onDraggableStart}
-						onDragEnd={onDraggableEnd}
+						aria-disabled={isTopDisabled}
+						onClick={() => {
+							moveBlocksUp([clientId], srcRootClientId);
+						}}
 					>
-						<Icon
-							className='toolbar-item__icon'
-							icon={toolbarMove}
-						/>
+						<Icon className='toolbar-item__icon' icon={moveUp} />
 					</Button>
 				</Tooltip>
-			)}
-		</Draggable>
+				<Tooltip
+					text={__('Move down', 'maxi-blocks')}
+					position='bottom center'
+				>
+					<Button
+						aria-disabled={isDownDisabled}
+						onClick={() =>
+							moveBlocksDown([clientId], srcRootClientId)
+						}
+					>
+						<Icon className='toolbar-item__icon' icon={moveDown} />
+					</Button>
+				</Tooltip>
+			</div>
+		</Fragment>
 	);
 };
 
---
+++ src/icons/move-icons/move-up/index.js
@@ -0,0 +1,16 @@
+/**
+ * WordPress dependencies
+ */
+const { SVG, Path } = wp.primitives;
+
+const moveUp = (
+	<SVG xmlns='http://www.w3.org/2000/svg' viewBox='0 0 15.094 9.375'>
+		<Path
+			fill='#fff'
+			fillRule='evenodd'
+			d='M14.765 9.013a.889.889 0 01-.7.351H.996a.958.958 0 01-.7-.293v-.058a.991.991 0 010-1.465L6.858.341V.282a.922.922 0 011.348 0l6.562 7.266a.993.993 0 010 1.465z'
+		/>
+	</SVG>
+);
+
+export default moveUp;

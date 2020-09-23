cddffa0eb84520bc20454f2e26b824636f2b7b3b
Josh <jjuancap@gmail.com>
2020-09-23 18:41:56 +0200

    Remove non-used formats

--- src/extensions/text/formats/formats.js
+++ src/extensions/text/formats/formats.js
@@ -1,10 +1,7 @@
-import color from './color';
 import link from './link';
-import size from './size';
-import underline from './underline';
 import custom from './custom';
 
-export default [color, link, size, underline, custom];
+export default [link, custom];
 
 export { default as defaultFontColorObject } from './color/default';
 export { default as defaultFontUnderlineObject } from './underline/default';

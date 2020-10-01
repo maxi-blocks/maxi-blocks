862f631aea0d60c76391183017105774a8ed9fc7
Javad <65819265+javadsamiee@users.noreply.github.com>
2020-10-01 14:13:29 +0330

    Fix SVG shape width and add disable/enable option (#537)
    Co-authored-by: javad sameei <j.sameei@gmail.com>

--- src/icons/shape-divider/mountains-bottom-opacity/index.js
+++ src/icons/shape-divider/mountains-bottom-opacity/index.js
@@ -4,12 +4,25 @@
 const { SVG, Path } = wp.primitives;
 
 const mountainsBottomOpacity = (
-	<SVG xmlns='http://www.w3.org/2000/svg' viewBox='0 0 1280 140'>
-		<Path
-			fillOpacity='.498'
-			d='M1209.3 36.6l-74.9 4.6-70.8 23.4-66.3-11.7-83.6 29.6-97.6-7-63.7 21.4-77 12.6-91.7-20.8-77.9 17.7-94.5-7.6-90.1 21.6-78.7-9.9-86.4 16.1-95.7 6.2-60.4-5V140h1280V0l-70.7 36.6z'
-		/>
-		<Path d='M1214.3 76l-79.9-10-70.8 28.3-66.3-20.9-83.6 32.9L816.1 89l-73.7 32.1-77-1.5-82.7-14-76.9 21.8-94.5-17.7-90.1 27.1-78.7-19.8-86.2 14-95.7 3.1L0 126.7V140h1280V35.8L1214.3 76z' />
+	<SVG
+		xmlns='http://www.w3.org/2000/svg'
+		xlink='http://www.w3.org/1999/xlink'
+		preserveAspectRatio='none'
+		viewBox='0 0 1280 134'
+	>
+		<defs>
+			<Path
+				id='mountains-bottom-opacity-a'
+				fill-opacity='.498'
+				d='M1279.95 99.15L1280 135H0V8.3L60.6.9 156.25 4l86.35 13.9 78.7-9.75 90.1 17.1 94.45-7.7L582.8 29.4l82.65-13.95 76.95 8.5 73.7 22.1 97.6-7.3 83.65 22.95 66.3-10.95 70.8 18.2h79.85z'
+			/>
+			<Path
+				id='mountains-bottom-opacity-b'
+				d='M1280 134.95v.05H0V7.1l60.6-4.85 95.65 31.5 86.35-9.2 78.7 36.1 90.1-24.4 94.45 38.4L583.8 46.3l91.65 25.25L752.4 38.2l63.7 21.4 97.6-7.05 83.65 29.6 66.3-11.7 70.8 23.35 74.85 4.6z'
+			/>
+		</defs>
+		<use href='#mountains-bottom-opacity-a' />
+		<use href='#mountains-bottom-opacity-b' />
 	</SVG>
 );
 
--- src/icons/shape-divider/triangle-bottom/index.js
+++ src/icons/shape-divider/triangle-bottom/index.js
@@ -4,12 +4,19 @@
 const { SVG, Path } = wp.primitives;
 
 const tirangleBottom = (
-	<SVG xmlns='http://www.w3.org/2000/svg' viewBox='0 0 1280 140'>
-		<Path
-			fill='#000'
-			fillRule='nonzero'
-			d='M560 0l80 140H0V0h560m720 0v140H640L720 0h560z'
-		/>
+	<SVG
+		xmlns='http://www.w3.org/2000/svg'
+		xlink='http://www.w3.org/1999/xlink'
+		preserveAspectRatio='none'
+		viewBox='0 0 1280 140'
+	>
+		<defs>
+			<Path
+				id='tirangle-bottom'
+				d='M560 0l80 140H0V0h560m720 0v140H640L720 0h560z'
+			/>
+		</defs>
+		<use href='#tirangle-bottom' />
 	</SVG>
 );
 

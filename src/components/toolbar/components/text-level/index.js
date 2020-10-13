c5d40816e35187d78e7620f499ba0f2c1a691272
Javad <65819265+javadsamiee@users.noreply.github.com>
2020-10-13 12:07:17 +0330

    Issue 486 (#567)
    * Add current alignment to toolbar
    
    * Add active status for headings in toolbar
    
    Co-authored-by: javad sameei <j.sameei@gmail.com>

--- src/components/toolbar/components/text-level/index.js
+++ src/components/toolbar/components/text-level/index.js
@@ -13,7 +13,15 @@ import ToolbarPopover from '../toolbar-popover';
  * Styles and icons
  */
 import './editor.scss';
-import { toolbarHeadingsLevel } from '../../../../icons';
+import {
+	toolbarH1,
+	toolbarH2,
+	toolbarH3,
+	toolbarH4,
+	toolbarH5,
+	toolbarH6,
+	toolbarP,
+} from '../../../../icons';
 
 /**
  * TextLevel
@@ -31,11 +39,32 @@ const TextLevel = props => {
 
 	if (blockName !== 'maxi-blocks/text-maxi' || isList) return null;
 
+	const levelIcon = textLevel => {
+		switch (textLevel) {
+			case 'h1':
+				return toolbarH1;
+			case 'h2':
+				return toolbarH2;
+			case 'h3':
+				return toolbarH3;
+			case 'h4':
+				return toolbarH4;
+			case 'h5':
+				return toolbarH5;
+			case 'h6':
+				return toolbarH6;
+			case 'p':
+				return toolbarP;
+			default:
+				return toolbarP;
+		}
+	};
+
 	return (
 		<ToolbarPopover
 			className='toolbar-item__text-level'
 			tooltip={__('Text level', 'maxi-blocks')}
-			icon={toolbarHeadingsLevel}
+			icon={levelIcon(textLevel)}
 			content={
 				<FontLevelControl
 					value={textLevel}
---
+++ src/icons/toolbar-icons/h4/index.js
@@ -0,0 +1,27 @@
+/**
+ * WordPress dependencies
+ */
+const { SVG, Path } = wp.primitives;
+
+const toolbarH4 = (
+	<SVG
+		xmlns='http://www.w3.org/2000/svg'
+		id='Layer_1'
+		x='0'
+		y='0'
+		version='1.1'
+		viewBox='0 0 24 24'
+		space='preserve'
+	>
+		<Path
+			d='M2.5 3.2v7.3h8.1V3.2h2.2v17.6h-2.2v-8.2H2.5v8.2H.4V3.2h2.1z'
+			class='st0'
+		/>
+		<Path
+			d='M19.3 20.8v-4.6h-7.5v-1.5L19 3.8h2.4v10.5h2.3v1.8h-2.3v4.6h-2.1zm0-6.4V8.7c0-.9 0-1.8.1-2.7h-.1c-.5 1-.9 1.7-1.3 2.5l-4 5.8v.1h5.3z'
+			class='st0'
+		/>
+	</SVG>
+);
+
+export default toolbarH4;

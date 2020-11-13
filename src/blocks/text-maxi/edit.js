f3a90b2d4d9dd146c9627f264ce9f36ba3493e94
Javad <65819265+javadsamiee@users.noreply.github.com>
2020-11-13 10:20:52 +0100

    Add border hover status (#682)
    Co-authored-by: javad sameei <j.sameei@gmail.com>

--- src/blocks/text-maxi/edit.js
+++ src/blocks/text-maxi/edit.js
@@ -40,7 +40,7 @@ import {
  * External dependencies
  */
 import classnames from 'classnames';
-import { isEmpty, isObject } from 'lodash';
+import { isEmpty, isObject, isNil } from 'lodash';
 
 /**
  * Content
@@ -133,10 +133,15 @@ class edit extends MaxiBlock {
 			boxShadowHover: {
 				...getBoxShadowObject(JSON.parse(boxShadowHover)),
 			},
-			borderHover: { ...JSON.parse(borderHover) },
 			borderWidthHover: { ...JSON.parse(borderHover).borderWidth },
 			borderRadiusHover: { ...JSON.parse(borderHover).borderRadius },
 		};
+
+		if (!isNil(borderHover) && !!JSON.parse(borderHover).status) {
+			response.borderHover = {
+				...JSON.parse(borderHover),
+			};
+		}
 
 		return response;
 	}

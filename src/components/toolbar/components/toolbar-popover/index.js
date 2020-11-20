84f55907c85209a26b16411dba6f1b8f527abe57
javad sameei <j.sameei@gmail.com>
2020-11-20 12:36:24 +0330

    Change "ToolbarPopover" to Class again

--- src/components/toolbar/components/toolbar-popover/index.js
+++ src/components/toolbar/components/toolbar-popover/index.js
@@ -1,8 +1,8 @@
 /**
  * WordPress dependencies
  */
-const { useDispatch } = wp.data;
-const { Fragment, useState } = wp.element;
+const { dispatch } = wp.data;
+const { Fragment, Component } = wp.element;
 const {
 	Button,
 	Icon,
@@ -31,63 +31,82 @@ import './editor.scss';
 /**
  * Component
  */
+class ToolbarPopover extends Component {
+	state = {
+		isOpen: false,
+	};
 
-const ToolbarPopover = props => {
-	const {
-		className,
-		tooltip,
-		icon,
-		content,
-		advancedOptions = false,
-	} = props;
+	onToggle() {
+		const { isOpen } = this.state;
 
-	const [isOpen, setIsOpen] = useState(false);
+		this.setState({
+			isOpen: !isOpen,
+		});
+	}
 
-	const { openGeneralSidebar } = useDispatch('core/edit-post');
+	handleFocusOutside() {
+		this.setState({
+			isOpen: false,
+		});
+	}
 
-	const classes = classnames(
-		'toolbar-item',
-		'toolbar-item__button',
-		className
-	);
+	render() {
+		const {
+			className,
+			tooltip,
+			icon,
+			content,
+			advancedOptions = false,
+		} = this.props;
 
-	return (
-		<Fragment>
-			<Tooltip text={tooltip} position='bottom center'>
-				<Button
-					className={classes}
-					onClick={() => setIsOpen(!isOpen)}
-					aria-expanded={false}
-					action='popup'
-				>
-					<Icon className='toolbar-item__icon' icon={icon} />
-				</Button>
-			</Tooltip>
-			{isOpen && content && (
-				<Popover
-					className='toolbar-item__popover'
-					noArrow={false}
-					position='top center'
-					focusOnMount
-					isAlternate
-					shouldAnchorIncludePadding
-				>
-					{content}
-					{!!advancedOptions && (
-						<IconButton
-							className='toolbar-item__popover__advanced-button'
-							icon={toolbarAdvancedSettings}
-							onClick={() =>
-								openGeneralSidebar('edit-post/block').then(() =>
-									openSidebar(advancedOptions)
-								)
-							}
-						/>
-					)}
-				</Popover>
-			)}
-		</Fragment>
-	);
-};
+		const { isOpen } = this.state;
+
+		const { openGeneralSidebar } = dispatch('core/edit-post');
+
+		const classes = classnames(
+			'toolbar-item',
+			'toolbar-item__button',
+			className
+		);
+
+		return (
+			<Fragment>
+				<Tooltip text={tooltip} position='bottom center'>
+					<Button
+						className={classes}
+						onClick={() => this.onToggle()}
+						aria-expanded={isOpen}
+						action='popup'
+					>
+						<Icon className='toolbar-item__icon' icon={icon} />
+					</Button>
+				</Tooltip>
+				{isOpen && content && (
+					<Popover
+						className='toolbar-item__popover'
+						noArrow={false}
+						position='top center'
+						focusOnMount
+						isAlternate
+						shouldAnchorIncludePadding
+					>
+						{content}
+						{!!advancedOptions && (
+							<IconButton
+								className='toolbar-item__popover__advanced-button'
+								icon={toolbarAdvancedSettings}
+								onClick={() =>
+									openGeneralSidebar(
+										'edit-post/block'
+									).then(() => openSidebar(advancedOptions))
+								}
+							/>
+						)}
+					</Popover>
+				)}
+			</Fragment>
+		);
+	}
+}
 
 export default withFocusOutside(ToolbarPopover);

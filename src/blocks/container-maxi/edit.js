5b10a1982cd1fe11b9c2cc1d0b12aa4ce304ff27
Javad <65819265+javadsamiee@users.noreply.github.com>
2020-12-02 10:21:05 +0100

    Issue 725 (#726)
    * Some fixes
    
    * Minor fix
    
    Co-authored-by: javad sameei <j.sameei@gmail.com>

--- src/blocks/container-maxi/edit.js
+++ src/blocks/container-maxi/edit.js
@@ -139,7 +139,7 @@ class edit extends MaxiBlock {
 		response = Object.assign(
 			response,
 			setBackgroundStyles(
-				uniqueID,
+				`${uniqueID} .maxi-container-block__wrapper`,
 				background,
 				backgroundHover,
 				overlay,
@@ -285,11 +285,6 @@ class edit extends MaxiBlock {
 							data-align={fullWidth}
 							data-maxi_initial_block_class={defaultBlockStyle}
 						>
-							<__experimentalBackgroundDisplayer
-								background={background}
-								blockClassName={uniqueID}
-							/>
-
 							<__experimentalArrowDisplayer arrow={arrow} />
 
 							{!!shapeDividerValue.top.status && (
@@ -299,6 +294,10 @@ class edit extends MaxiBlock {
 							)}
 
 							<div className='maxi-container-block__wrapper'>
+								<__experimentalBackgroundDisplayer
+									background={background}
+									blockClassName={uniqueID}
+								/>
 								<InnerBlocks
 									templateLock={false}
 									__experimentalTagName='div'

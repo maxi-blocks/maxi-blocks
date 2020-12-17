4a00c7f397595b67dc61f199d82066be174ecd75
Javad <65819265+javadsamiee@users.noreply.github.com>
2020-12-17 17:13:26 +0100

    Issue 759 (#764)
    * Some fixes
    
    * Remove remaining margins
    
    Co-authored-by: javad sameei <j.sameei@gmail.com>

--- src/components/font-level-control/index.js
+++ src/components/font-level-control/index.js
@@ -9,7 +9,6 @@ const { useState } = wp.element;
  * Internal dependencies
  */
 import defaultTypography from '../../extensions/defaults/typography';
-import defaultMargin from '../../extensions/defaults/margin';
 import { getDefaultProp } from '../../utils';
 
 /**
@@ -27,38 +26,24 @@ import './editor.scss';
  * Component
  */
 const FontLevelControl = props => {
-	const {
-		className,
-		value,
-		fontOptions,
-		fontOptionsHover,
-		marginOptions,
-		onChange,
-	} = props;
+	const { className, value, fontOptions, fontOptionsHover, onChange } = props;
 
 	const [state, setState] = useState({
 		lastLevel: value,
 		p: {},
 		pHover: {},
-		pMargin: {},
 		h1: {},
 		h1Hover: {},
-		h1Margin: {},
 		h2: {},
 		h2Hover: {},
-		h2Margin: {},
 		h3: {},
 		h3Hover: {},
-		h3Margin: {},
 		h4: {},
 		h4Hover: {},
-		h4Margin: {},
 		h5: {},
 		h5Hover: {},
-		h5Margin: {},
 		h6: {},
 		h6Hover: {},
-		h6Margin: {},
 	});
 
 	const classes = classnames('maxi-font-level-control', className);
@@ -73,10 +58,6 @@ const FontLevelControl = props => {
 				? JSON.parse(fontOptionsHover)
 				: fontOptionsHover,
 
-			[`${state.lastLevel}Margin`]: !isObject(marginOptions)
-				? JSON.parse(marginOptions)
-				: marginOptions,
-
 			lastLevel: value,
 		});
 	};
@@ -85,12 +66,10 @@ const FontLevelControl = props => {
 		saveOldies(value);
 		let fontOptResponse = {};
 		let fontOptResponseHover = {};
-		let marginOptResponse = {};
 
 		if (!isEmpty(state[value])) {
 			fontOptResponse = state[value];
 			fontOptResponseHover = state[`${value}Hover`];
-			marginOptResponse = state[`${value}Margin`];
 		} else if (!isNil(fontOptions)) {
 			const oldFontOptions = !isObject(fontOptions)
 				? JSON.parse(fontOptions)
@@ -104,14 +83,12 @@ const FontLevelControl = props => {
 			fontOptResponseHover = JSON.parse(
 				getDefaultProp(null, 'typographyHover')
 			);
-			marginOptResponse = defaultMargin[value];
 		}
 
 		onChange({
 			textLevel: value,
 			typography: JSON.stringify(fontOptResponse),
 			typographyHover: JSON.stringify(fontOptResponseHover),
-			margin: JSON.stringify(marginOptResponse),
 		});
 	};
 

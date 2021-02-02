de52b4be788f2b647dd260a6600a5eeac9cf76dc
javad sameei <j.sameei@gmail.com>
2021-02-02 20:15:01 +0330

    Clean utils file

--- src/extensions/styles/index.js
+++ src/extensions/styles/index.js
@@ -2,5 +2,4 @@ import './store';
 
 export { default as stylesGenerator } from './stylesGenerator';
 export { default as styleResolver } from './stylesResolver';
-export * from './utils';
 export * from './defaults';

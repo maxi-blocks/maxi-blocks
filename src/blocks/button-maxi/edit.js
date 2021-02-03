a0aecc6780c5ae8750726805132d51317f7a715b
javad sameei <j.sameei@gmail.com>
2021-02-03 13:05:39 +0330

    Fix props withoutInteractiveFormatting and placeholder are repeat

--- src/blocks/button-maxi/edit.js
+++ src/blocks/button-maxi/edit.js
@@ -143,7 +143,6 @@ class edit extends MaxiBlock {
 						<RichText
 							ref={this.buttonRef}
 							withoutInteractiveFormatting
-							placeholder={__('Set some text…', 'maxi-blocks')}
 							className='maxi-button-block__content'
 							value={attributes.buttonContent}
 							identifier='content'
@@ -151,7 +150,6 @@ class edit extends MaxiBlock {
 								setAttributes({ buttonContent })
 							}
 							placeholder={__('Set some text…', 'maxi-blocks')}
-							withoutInteractiveFormatting
 						/>
 					</div>
 				</__experimentalBlock>

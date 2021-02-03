41d3119dccf96927be6e0b2eb9377e0b1d5e1aab
javad sameei <j.sameei@gmail.com>
2021-02-03 17:57:54 +0330

    Remove old Defaults.js file

--- src/extensions/styles/index.js
+++ src/extensions/styles/index.js
@@ -6,4 +6,3 @@ export { default as getGroupAttributes } from './getGroupAttributes';
 export { default as getLastBreakpointAttribute } from './getLastBreakpointAttribute';
 export { default as getDefaultAttribute } from './getDefaultAttribute';
 export { default as getAttributeKey } from './getAttributeKey';
-export * from './defaults';

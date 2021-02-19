5e74fdfbb5d3a387e0fc4a3b63c954b068309099
Josh_soporte <50477222+Naaaaiix@users.noreply.github.com>
2021-02-19 17:00:23 +0100

    Cloud Library API (#938)
    * Connected to the new Cloud Library API
    Start copying the old Cloud Library site
    
    * Create new Library
    Create new Library Store
    
    * Add Library Cloud

---
+++ src/editor/library/index.js
@@ -0,0 +1,72 @@
+/**
+ * WordPress dependencies.
+ */
+const { __ } = wp.i18n;
+const { Modal } = wp.components;
+const { useSelect } = wp.data;
+const { useState, Fragment } = wp.element;
+
+/**
+ * Internal dependencies
+ */
+import './store';
+import LibraryToolbar from './toolbar';
+import LibraryContainer from './container';
+import LibrarySpinner from './spinner';
+
+/**
+ * External dependencies
+ */
+import classnames from 'classnames';
+import { isEmpty } from 'lodash';
+
+/**
+ * Styles
+ */
+import './editor.scss';
+
+/**
+ * Component
+ */
+const CloudLibrary = props => {
+	const { onClose, className } = props;
+
+	const [type, setType] = useState('patterns');
+
+	const { cloudData } = useSelect(select => {
+		const { receiveMaxiCloudLibrary } = select('maxiBlocks/cloudLibrary');
+		const cloudData = receiveMaxiCloudLibrary(type);
+
+		return {
+			cloudData,
+		};
+	});
+
+	const classes = classnames('maxi-library-modal', className);
+
+	return (
+		<Modal
+			title={__('Maxi Cloud Library', 'maxi-blocks')}
+			className={classes}
+			shouldCloseOnEsc
+			shouldCloseOnClickOutside={false}
+			onRequestClose={onClose}
+		>
+			{(isEmpty(cloudData) && <LibrarySpinner />) || (
+				<Fragment>
+					<LibraryToolbar
+						type={type}
+						onChange={type => setType(type)}
+					/>
+					<LibraryContainer
+						cloudData={cloudData}
+						type={type}
+						onRequestClose={onClose}
+					/>
+				</Fragment>
+			)}
+		</Modal>
+	);
+};
+
+export default CloudLibrary;

81479955dfe14fc87bb9b43be920618db7ffacd0
Josh_soporte <50477222+Naaaaiix@users.noreply.github.com>
2021-03-17 14:08:58 +0100

    Issue 1126 (#1156)

---
+++ src/extensions/text/fonts/loadFonts.js
@@ -0,0 +1,52 @@
+/**
+ * WordPress dependencies
+ */
+const { __ } = wp.i18n;
+const { select } = wp.data;
+
+/**
+ * Prepares the styles to be ready for JS FontFace API
+ *
+ * @param {obj} variant Concrete variant of the font with name and url
+ * @returns {obj} Styles options for load the font on FontFace API
+ */
+const getFontStyle = variant => {
+	const styles = variant.split(/([0-9]+)/).filter(Boolean);
+	if (styles.length > 1) {
+		return {
+			style: `${styles[1]}`,
+			weight: `${styles[0]}`,
+		};
+	}
+	const regExp = new RegExp('([0-9]+)', 'gm');
+	if (styles[0].search(regExp) >= 0) {
+		// number
+		return { weight: `${styles[0]}` };
+	}
+	return { style: `${styles[0]}` };
+};
+
+/**
+ * Loads the font on background using JS FontFace API
+ * FontFaceSet API uses check() to check if a font exists, but needs to compare with some exact value:
+ * in this case is used '12px' as a standard that returns if the font has been loaded.
+ *
+ * @param {string} font Name of the selected font
+ */
+const loadFonts = font => {
+	if (font && document.fonts && !document.fonts.check(`12px ${font}`)) {
+		const { files } = select('maxiBlocks/fonts').getFont(font);
+
+		// FontFace API
+		Object.entries(files).forEach(variant => {
+			const style = getFontStyle(variant[0]);
+			const fontLoad = new FontFace(font, `url(${variant[1]})`, style);
+			document.fonts.add(fontLoad);
+			fontLoad.loaded.catch(err => {
+				console.error(__(`Font hasn't been able to download: ${err}`));
+			});
+		});
+	}
+};
+
+export default loadFonts;

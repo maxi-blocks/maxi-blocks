07ab262178aaa3175bdfd48c78d521faa5cdda63
Javad <65819265+javadsamiee@users.noreply.github.com>
2021-03-19 11:39:51 +0330

    New responsive style for toolbar (#1171)

--- src/icons/general-icons/index.js
+++ src/icons/general-icons/index.js
@@ -1,2 +1,3 @@
-export { default as responsive } from './responsive';
 export { default as main } from './main';
+export { default as cloudLib } from './cloud_lib';
+export { default as globalOptions } from './global_options';

2f97ec5963ffd64fc5000c217ae70a5cd3817287
javad sameei <mirjavadsamiee@gmail.com>
2021-03-23 12:05:10 +0430

    Add extra shapes and clean them

---
+++ src/icons/shape-icons/shape22/index.js
@@ -0,0 +1,9 @@
+const shape22 = `
+<svg xmlns="http://www.w3.org/2000/svg" x="0" y="0" version="1.1" viewBox="0 0 36.1 36.1" xml:space="preserve">
+	<g>
+		<path d="M19.3 1L1.1 35h33.8L19.3 1z" />
+	</g>
+</svg>
+`;
+
+export default shape22;
---
+++ src/icons/shape-icons/shape31/index.js
@@ -0,0 +1,9 @@
+const shape31 = `
+<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0" y="0" version="1.1" viewBox="0 0 36.1 36.1" xml:space="preserve">
+	<g>
+		<path d="M6.5 6.5c3.9-3.9 10.2-3.9 14.1 0l8.8 8.8c3.9 3.9 3.9 10.2 0 14.1-3.9 3.9-10.2 3.9-14.1 0l-8.8-8.8c-3.9-3.8-3.9-10.1 0-14.1z" />
+	</g>
+</svg>
+`;
+
+export default shape31;

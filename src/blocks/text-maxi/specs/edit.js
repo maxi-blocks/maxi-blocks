bfab9ff9cd103183aba9f23edf735cbd01aa7e3d
Josh <jjuancap@gmail.com>
2021-04-16 10:59:30 +0200

    Implement e2e tests

---
+++ src/blocks/text-maxi/specs/edit.js
@@ -0,0 +1,21 @@
+/**
+ * WordPress
+ */
+import {
+	createNewPost,
+	insertBlock,
+	getEditedPostContent,
+} from '@wordpress/e2e-test-utils';
+
+describe('TextMaxi', () => {
+	beforeEach(async () => {
+		await createNewPost();
+	});
+
+	it('Writes a sentence on Text Maxi', async () => {
+		await insertBlock('Text Maxi');
+		await page.keyboard.type('Testing Text Maxi');
+
+		expect(await getEditedPostContent()).toMatchSnapshot();
+	});
+});

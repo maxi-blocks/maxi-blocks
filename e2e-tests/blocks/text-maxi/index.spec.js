14638bc3a66c4c5e6c55651e16cb63a362d063e6
Alejandro <alejandro9kgm@gmail.com>
2021-04-27 09:41:26 +0200

    text-style-test Merge

--- e2e-tests/blocks/text-maxi/index.spec.js
+++ e2e-tests/blocks/text-maxi/index.spec.js
@@ -8,6 +8,7 @@ import {
 	getEditedPostContent,
 	pressKeyWithModifier,
 	openPreviewPage,
+	pressKeyTimes,
 } from '@wordpress/e2e-test-utils';
 
 const linkExample = 'test.com';
@@ -28,13 +29,7 @@ describe('TextMaxi', () => {
 
 		await insertBlock('Text Maxi');
 		await page.keyboard.type('Testing Text Maxi...onSplit');
-		await page.keyboard.press('ArrowLeft');
-		await page.keyboard.press('ArrowLeft');
-		await page.keyboard.press('ArrowLeft');
-		await page.keyboard.press('ArrowLeft');
-		await page.keyboard.press('ArrowLeft');
-		await page.keyboard.press('ArrowLeft');
-		await page.keyboard.press('ArrowLeft');
+		await pressKeyTimes('ArrowLeft', '7');
 		await page.keyboard.press('Enter');
 
 		expect(await getEditedPostContent()).toMatchSnapshot();
@@ -45,17 +40,7 @@ describe('TextMaxi', () => {
 		await page.keyboard.type('Test Text Maxi...');
 		await insertBlock('Text Maxi');
 		await page.keyboard.type('...OnMerge');
-		await page.keyboard.press('ArrowLeft');
-		await page.keyboard.press('ArrowLeft');
-		await page.keyboard.press('ArrowLeft');
-		await page.keyboard.press('ArrowLeft');
-		await page.keyboard.press('ArrowLeft');
-		await page.keyboard.press('ArrowLeft');
-		await page.keyboard.press('ArrowLeft');
-		await page.keyboard.press('ArrowLeft');
-		await page.keyboard.press('ArrowLeft');
-		await page.keyboard.press('ArrowLeft');
-		await page.keyboard.press('ArrowLeft');
+		await pressKeyTimes('ArrowLeft', '11');
 		await page.keyboard.press('Delete');
 
 		expect(await getEditedPostContent()).toMatchSnapshot();
@@ -139,5 +124,64 @@ describe('TextMaxi', () => {
 			contentWrapper => contentWrapper.innerHTML.trim()
 		);
 		expect(content).toMatchSnapshot();
+	});
+
+	it('Testing Text Maxi with custom formats when split', async () => {
+		await insertBlock('Text Maxi');
+		await page.keyboard.type('Testing Text Maxi Bold');
+		pressKeyWithModifier('shift', 'ArrowLeft');
+		pressKeyWithModifier('shift', 'ArrowLeft');
+		pressKeyWithModifier('shift', 'ArrowLeft');
+		pressKeyWithModifier('shift', 'ArrowLeft');
+		await page.$eval('.toolbar-item__bold', button => button.click());
+		await page.keyboard.press('ArrowLeft');
+		await page.keyboard.press('Enter');
+
+		expect(await getEditedPostContent()).toMatchSnapshot();
+	});
+
+	it(' Testing Text Maxi with custom formats when merge from top block to bottom one', async () => {
+		await insertBlock('Text Maxi');
+		await page.keyboard.type('Testing Text Maxi.');
+		await insertBlock('Text Maxi');
+		await page.keyboard.type('.Bold');
+		pressKeyWithModifier('shift', 'ArrowLeft');
+		pressKeyWithModifier('shift', 'ArrowLeft');
+		pressKeyWithModifier('shift', 'ArrowLeft');
+		pressKeyWithModifier('shift', 'ArrowLeft');
+		pressKeyWithModifier('shift', 'ArrowLeft');
+
+		await page.$eval('.toolbar-item__bold', button => button.click());
+		const selectMaxiTextDiv = await page.$('.maxi-text-block');
+		const selectMaxiTextP = await selectMaxiTextDiv.$(
+			'.block-editor-rich-text__editable'
+		);
+		await selectMaxiTextP.focus();
+		await pressKeyTimes('ArrowRight', '18');
+		await page.keyboard.press('Delete');
+
+		expect(await getEditedPostContent()).toMatchSnapshot();
+	});
+
+	it('Testing Text Maxi with custom formats when merge from bottom block to top one', async () => {
+		await insertBlock('Text Maxi');
+		await page.keyboard.type('Testing Text Maxi.');
+		await insertBlock('Text Maxi');
+		await page.keyboard.type('.Bold');
+		pressKeyWithModifier('shift', 'ArrowLeft');
+		pressKeyWithModifier('shift', 'ArrowLeft');
+		pressKeyWithModifier('shift', 'ArrowLeft');
+		pressKeyWithModifier('shift', 'ArrowLeft');
+		pressKeyWithModifier('shift', 'ArrowLeft');
+		await page.$eval('.toolbar-item__bold', button => button.click());
+		const selectMaxiTextDiv = await page.$('.maxi-text-block');
+		const selectMaxiTextP = await selectMaxiTextDiv.$(
+			'.block-editor-rich-text__editable'
+		);
+		await selectMaxiTextP.focus();
+		await page.keyboard.press('ArrowDown');
+		await page.keyboard.press('Backspace');
+
+		expect(await getEditedPostContent()).toMatchSnapshot();
 	});
 });

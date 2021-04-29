d86e7f4ec846bfadaf76644a31c969bfab5e8f5a
Javad <65819265+javadsamiee@users.noreply.github.com>
2021-04-29 17:37:38 +0430

    Issue 1323 (#1336)
    * Basic implementation for global styles
    
    * Global attribute

---
+++ src/extensions/styles/defaults/global.js
@@ -0,0 +1,31 @@
+import { breakpoints } from './';
+
+const global = {
+	...breakpoints,
+	blockStyle: {
+		type: 'string',
+	},
+	blockStyleBackground: {
+		type: 'number',
+		default: 1,
+	},
+	defaultBlockStyle: {
+		type: 'string',
+		default: 'maxi-def-light',
+	},
+	extraClassName: {
+		type: 'string',
+		default: '',
+	},
+	isFirstOnHierarchy: {
+		type: 'boolean',
+	},
+	linkSettings: {
+		type: 'object',
+	},
+	uniqueID: {
+		type: 'string',
+	},
+};
+
+export default global;

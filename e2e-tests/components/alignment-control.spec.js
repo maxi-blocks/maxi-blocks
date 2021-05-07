4f619fb99de2142eb090a32aef07a29c84ec9b07
Alejandro <alejandro9kgm@gmail.com>
2021-05-07 10:05:06 +0200

    minor fix

--- e2e-tests/components/alignment-control.spec.js
+++ e2e-tests/components/alignment-control.spec.js
@@ -20,14 +20,20 @@ describe('Alignment', () => {
 		await insertBlock('Text Maxi');
 		await page.keyboard.type('Testing Alignment');
 		await page.$eval('.toolbar-item__alignment', button => button.click());
+		await page.waitForSelector(
+			'.components-popover__content .maxi-alignment-control__no-label'
+		);
 		const alignmentSettings = await page.$$(
-			'.components-radio-control__options'
+			'.components-popover__content .maxi-alignment-control__no-label .components-radio-control__option'
 		);
-		await alignmentSettings[1].$eval(
-			'.components-radio-control__option',
-			setting => setting.click()
-		);
+		await alignmentSettings[1].$eval('label', setting => setting.click());
 
 		expect(await getEditedPostContent()).toMatchSnapshot();
 	});
 });
+
+// const popover = await page.$('.components-popover__content');
+// const alignmentWrapper = popover.$('.maxi-alignment-control__no-label');
+// const alignmentSettings = await alignmentWrapper.$$(
+// 	'.components-radio-control__option'
+// );

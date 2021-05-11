116183bd71a28065354e372da3bae4abe27c9d4d
Josh_soporte <50477222+Naaaaiix@users.noreply.github.com>
2021-05-11 10:14:46 +0200

    Fix SC palettes and backgrounds (#1398)
    * Fix palettes for SC
    
    * Fix background

--- src/components/maxi-block/index.js
+++ src/components/maxi-block/index.js
@@ -119,6 +119,7 @@ const MaxiBlock = forwardRef((props, ref) => {
 		disableBackground = false,
 		isSave = false,
 		classes: customClasses,
+		paletteClasses,
 		...extraProps
 	} = props;
 
@@ -154,7 +155,8 @@ const MaxiBlock = forwardRef((props, ref) => {
 		uniqueID,
 		className,
 		displayValue === 'none' && 'maxi-block-display-none',
-		customClasses
+		customClasses,
+		paletteClasses
 	);
 
 	const blockProps = {
@@ -172,7 +174,7 @@ const MaxiBlock = forwardRef((props, ref) => {
 					key={`maxi-block-${uniqueID}`}
 					uniqueID={uniqueID}
 					background={background}
-					disableBackground={disableBackground}
+					disableBackground={!disableBackground}
 					isSave={isSave}
 					{...blockProps}
 				>

9a0f844bffb022768d585075581bb7b7c1bb9d12
javad sameei <mirjavadsamiee@gmail.com>
2021-05-11 19:22:53 +0430

    Some fixees

--- src/components/divider-control/defaults.js
+++ src/components/divider-control/defaults.js
@@ -1,34 +1,27 @@
 export const dividerSolidHorizontal = {
 	'divider-border-style': 'solid',
-	'divider-border-radius': 20,
 };
 
 export const dividerDottedHorizontal = {
 	'divider-border-style': 'dotted',
-	'divider-border-radius': 20,
 };
 
 export const dividerDashedHorizontal = {
 	'divider-border-style': 'dashed',
-	'divider-border-radius': 20,
 };
 
 export const dividerSolidVertical = {
 	'divider-border-style': 'solid',
-	'divider-border-radius': 20,
 };
 
 export const dividerDottedVertical = {
 	'divider-border-style': 'dotted',
-	'divider-border-radius': 20,
 };
 
 export const dividerDashedVertical = {
 	'divider-border-style': 'dashed',
-	'divider-border-radius': 20,
 };
 
 export const dividerNone = {
 	'divider-border-style': 'none',
-	'divider-border-radius': 20,
 };

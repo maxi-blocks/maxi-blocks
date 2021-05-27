df0f34d1d3c21684df766009cce4ca6290b8781d
Josh <jjuancap@gmail.com>
2021-05-27 12:35:58 +0200

    Install and config @prettier/plugin-php Clean and organize php files

---
+++ core/class-maxi-page-template.php
@@ -0,0 +1,114 @@
+<?php
+
+/**
+ * Maxi Blocks styles API
+ */
+
+if (!defined('ABSPATH')) {
+	exit;
+}
+
+if (!class_exists('MaxiBlocks_PageTemplate')) :
+	class MaxiBlocks_PageTemplate {
+		/**
+		 * This plugin's instance.
+		 *
+		 * @var MaxiBlocks_PageTemplate
+		 */
+		private static $instance;
+
+		/**
+		 * Registers the plugin.
+		 */
+		public static function get_instance() {
+			if ( null == self::$instance ) {
+				self::$instance = new MaxiBlocks_PageTemplate();
+			}
+			return self::$instance;
+		}
+		/**
+		 * Variables
+		 */
+		protected $templates;
+
+		/**
+		 * Constructor.
+		 */
+		private function __construct() {
+			$this->templates = array();
+
+			add_filter(
+				'theme_page_templates', array( $this, 'add_new_template' )
+			);
+
+			add_filter(
+				'wp_insert_post_data',
+				array( $this, 'register_templates' )
+			);
+
+			add_filter(
+				'template_include',
+				array( $this, 'view_template')
+			);
+
+			$this->templates = array(
+				'full-width-template.php' => 'Maxi Full Width Template',
+			);
+
+		}
+
+		public function add_new_template( $posts_templates ) {
+			$posts_templates = array_merge( $posts_templates, $this->templates );
+			return $posts_templates;
+		}
+
+		public function register_templates( $atts ) {
+			$cache_key = 'page_templates-' . md5( get_theme_root() . '/' . get_stylesheet() );
+
+			$templates = wp_get_theme()->get_page_templates();
+			if ( empty( $templates ) ) {
+				$templates = array();
+			}
+
+			wp_cache_delete( $cache_key , 'themes');
+
+			$templates = array_merge( $templates, $this->templates );
+
+			wp_cache_add( $cache_key, $templates, 'themes', 1800 );
+
+			return $atts;
+		}
+
+		public function view_template( $template ) {
+			if ( is_search() ) {
+				return $template;
+			}
+			global $post;
+
+			if ( ! $post ) {
+				return $template;
+			}
+
+			if ( ! isset( $this->templates[get_post_meta(
+				$post->ID, '_wp_page_template', true
+			)] ) ) {
+				return $template;
+			}
+
+			$filepath = apply_filters( 'page_template_plugin_dir_path', MAXI_PLUGIN_DIR_PATH . 'templates/' );
+
+			$file =  $filepath . get_post_meta(
+				$post->ID, '_wp_page_template', true
+			);
+
+			if ( file_exists( $file ) ) {
+				return $file;
+			} else {
+				echo $file;
+			}
+
+			return $template;
+		}
+	}
+
+endif;
--- plugin.php
+++ plugin.php
@@ -13,90 +13,77 @@
 
 // Exit if accessed directly.
 if (!defined('ABSPATH')) {
-	exit;
+	exit();
 }
 
-define( 'MAXI_PLUGIN_DIR_PATH', plugin_dir_path(__FILE__ ) );
+define('MAXI_PLUGIN_DIR_PATH', plugin_dir_path(__FILE__));
+define('MAXI_PLUGIN_DIR_FILE', __FILE__);
 
-function create_block_test_maxi_blocks_block_init() {
-	$dir = dirname(__FILE__);
-
-	$script_asset_path = "$dir/build/index.asset.php";
-	if (!file_exists($script_asset_path)) {
-		throw new Error(
-			'You need to run `npm start` or `npm run build` for the "create-block/test-maxi-blocks" block first.'
-		);
-	}
-	$index_js     = 'build/index.js';
-	$script_asset = require($script_asset_path);
-	wp_register_script(
-		'create-block-test-maxi-blocks-block-editor',
-		plugins_url($index_js, __FILE__),
-		$script_asset['dependencies'],
-		$script_asset['version']
-	);
-
-	$editor_css = 'build/index.css';
-	wp_register_style(
-		'create-block-test-maxi-blocks-block-editor',
-		plugins_url($editor_css, __FILE__),
-		array(),
-		filemtime("$dir/$editor_css")
-	);
-
-	$style_css = 'build/style-index.css';
-	wp_register_style(
-		'create-block-test-maxi-blocks-block',
-		plugins_url($style_css, __FILE__),
-		array(),
-		filemtime("$dir/$style_css")
-	);
-
-	register_block_type('create-block/test-maxi-blocks', array(
-		'editor_script' => 'create-block-test-maxi-blocks-block-editor',
-		'editor_style'  => 'create-block-test-maxi-blocks-block-editor',
-		'style'         => 'create-block-test-maxi-blocks-block',
-	));
-}
-add_action('init', 'create_block_test_maxi_blocks_block_init');
-
-function maxi_blocks_add_db_table() {
-	global $wpdb;
-	$db_table_name = $wpdb->prefix . 'maxi_blocks_general';  // table name
-	$charset_collate = $wpdb->get_charset_collate();
-
-	//Check to see if the table exists already, if not, then create it
-	if ($wpdb->get_var("show tables like '$db_table_name'") != $db_table_name) {
-		$sql = "CREATE TABLE $db_table_name (
-                id varchar(128) NOT NULL,
-                object longtext NOT NULL,
-                UNIQUE (id)
-        ) $charset_collate;";
-
-		require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
-		dbDelta($sql);
-	}
+//======================================================================
+// MaxiBlocks Core
+//======================================================================
+require_once MAXI_PLUGIN_DIR_PATH . 'core/class-maxi-core.php';
+if (class_exists('MaxiBlocks_Core')) {
+	MaxiBlocks_Core::register();
 }
 
+//======================================================================
+// MaxiBlocks DB
+//======================================================================
+require_once MAXI_PLUGIN_DIR_PATH . 'core/class-maxi-db.php';
+if (class_exists('MaxiBlocks_DB')) {
+	MaxiBlocks_DB::register();
+}
 
-register_activation_hook(__FILE__, 'maxi_blocks_add_db_table');
+//======================================================================
+// MaxiBlocks Blocks
+//======================================================================
+require_once MAXI_PLUGIN_DIR_PATH . 'core/class-maxi-blocks.php';
+if (class_exists('MaxiBlocks_Blocks')) {
+	MaxiBlocks_Blocks::register();
+}
+
+//======================================================================
+// MaxiBlocks Styles
+//======================================================================
+require_once MAXI_PLUGIN_DIR_PATH . 'core/class-maxi-styles.php';
+if (class_exists('MaxiBlocks_Styles')) {
+	MaxiBlocks_Styles::register();
+}
+
+//======================================================================
+// MaxiBlocks Style Cards
+//======================================================================
+require_once MAXI_PLUGIN_DIR_PATH . 'core/class-maxi-style-cards.php';
+if (class_exists('MaxiBlocks_StyleCards')) {
+	MaxiBlocks_StyleCards::register();
+}
+
+//======================================================================
+// MaxiBlocks Image Crop
+//======================================================================
+require_once MAXI_PLUGIN_DIR_PATH . 'core/class-maxi-image-crop.php';
+if (class_exists('MaxiBlocks_ImageCrop')) {
+	MaxiBlocks_ImageCrop::register();
+}
+
+//======================================================================
+// MaxiBlocks API
+//======================================================================
+require_once MAXI_PLUGIN_DIR_PATH . 'core/class-maxi-api.php';
+if (class_exists('MaxiBlocks_API')) {
+	MaxiBlocks_API::register();
+}
+
+//======================================================================
+// MaxiBlocks Page Template
+//======================================================================
+require_once MAXI_PLUGIN_DIR_PATH . 'core/class-maxi-page-template.php';
+if (class_exists('MaxiBlocks_PageTemplate')) {
+	add_action( 'plugins_loaded', array( 'MaxiBlocks_PageTemplate', 'get_instance' ) );
+}
 
 /**
- * Block Initializer.
+ * TODO: Old init.php file. Please, delete these lines of comment and require onces this file has been removed
  */
 require_once plugin_dir_path(__FILE__) . 'src/init.php';
-
-
-//======================================================================
-// SETUP PAGE CONTENT DEPENDING ON CUSTOMIZER
-//======================================================================
-
-require_once plugin_dir_path(__FILE__) . 'page-content-setting/dynamic_content.php';
-
-//======================================================================
-// STYLING API
-//======================================================================
-require_once plugin_dir_path(__FILE__) . 'API/class-maxi-blocks-api.php';
-
-require_once plugin_dir_path(__FILE__) . 'templates/class-maxi-blocks-page-template.php';
-

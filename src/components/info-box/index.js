81f22ac75af86bf7233c0a4eb0f381b27f94bd05
Javad <65819265+javadsamiee@users.noreply.github.com>
2021-06-17 11:56:24 +0430

    Issue 1637 (#1666)
    * Show message for arrow control
    
    * Minor fix

--- src/components/info-box/index.js
+++ src/components/info-box/index.js
@@ -1,17 +1,53 @@
 /**
+ * WordPress dependencies
+ */
+import { dispatch } from '@wordpress/data';
+
+/**
+ * Internal dependencies
+ */
+import openSidebar from '../../extensions/dom';
+
+/**
  * External dependencies
  */
 import classnames from 'classnames';
+import { isEmpty } from 'lodash';
 
 /**
  * Styles
  */
 import './editor.scss';
 
-const InfoBox = ({ className, message }) => {
+const InfoBox = ({ className, message, links }) => {
 	const classes = classnames('maxi-warning-box', className);
 
-	return <div className={classes}>{message}</div>;
+	const { openGeneralSidebar } = dispatch('core/edit-post');
+
+	return (
+		<div className={classes}>
+			{message}
+			{links && (
+				<div className='maxi-warning-box__links'>
+					{links.map(item => (
+						<a
+							onClick={() => {
+								if (!isEmpty(item.panel))
+									openGeneralSidebar('edit-post/block').then(
+										() => openSidebar(item.panel)
+									);
+
+								if (!isEmpty(item.href))
+									window.open(item.href, '_blank');
+							}}
+						>
+							{item.title}
+						</a>
+					))}
+				</div>
+			)}
+		</div>
+	);
 };
 
 export default InfoBox;

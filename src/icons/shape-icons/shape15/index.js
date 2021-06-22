591ad1990cfa1ba8e860b322af7e33284ad92300
javad sameei <mirjavadsamiee@gmail.com>
2021-06-22 11:43:48 +0430

    Change default shapes

--- src/icons/shape-icons/shape15/index.js
+++ src/icons/shape-icons/shape15/index.js
@@ -1,9 +1,5 @@
 const shape15 = `
-<svg xmlns="http://www.w3.org/2000/svg" x="0" y="0" version="1.1" viewBox="0 0 36.1 36.1" xml:space="preserve">
-	<g>
-		<path d="M34.3 26.9L16.5 9.2H1.8V27h32.5z" />
-	</g>
-</svg>
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 36.1 36.1"><path d="M18.4 1.1c-6 .1-8.8-.2-8.5 0v17.1H1.3L18.1 35l16.7-16.8h-7.4V1.1h-9z" fill="#081219"/></svg>
 `;
 
 export default shape15;

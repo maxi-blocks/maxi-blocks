24736fc23d2d5fabf1ffeb67c26ab6e7263f450c
Alejandro <alejandro9kgm@gmail.com>
2021-06-23 11:34:42 +0200

    SelectControl test

---
+++ e2e-tests/components/select-control/index.spec.js
@@ -0,0 +1,27 @@
+/**
+ * WordPress dependencies
+ */
+import { createNewPost, insertBlock } from '@wordpress/e2e-test-utils';
+/**
+ * Internal dependencies
+ */
+import { getBlockAttributes, openSidebar } from '../../utils';
+
+describe('SelectControl', () => {
+	it('Check select control', async () => {
+		await createNewPost();
+		await insertBlock('Text Maxi');
+		const accordionPanel = await openSidebar(page, 'border');
+
+		const selector = await accordionPanel.$(
+			'.maxi-tabs-content .maxi-border-control .maxi-base-control__field select'
+		);
+		await selector.select('dotted');
+
+		const expectResult = 'dotted';
+		const attributes = await getBlockAttributes();
+		const style = attributes['border-style-general'];
+
+		expect(style).toStrictEqual(expectResult);
+	});
+});

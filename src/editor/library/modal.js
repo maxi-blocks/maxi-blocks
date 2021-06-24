f16e417d0737b6684c8e2304129af6df609e667b
javad sameei <mirjavadsamiee@gmail.com>
2021-06-24 18:48:54 +0430

    Add preview for loaded shapes

--- src/editor/library/modal.js
+++ src/editor/library/modal.js
@@ -8,19 +8,19 @@ import CloudLibrary from '.';
  * WordPress dependencies.
  */
 import { __ } from '@wordpress/i18n';
-import { Component } from '@wordpress/element';
+import { Component, RawHTML } from '@wordpress/element';
 import { Button } from '@wordpress/components';
-
-/**
- * Icons.
- */
-import { SCaddMore, toolbarReplaceImage } from '../../icons';
+import { select, dispatch } from '@wordpress/data';
 
 /**
  * Internal dependencies
  */
-
 import { Icon } from '../../components';
+
+/**
+ * Icons
+ */
+import { SCaddMore, toolbarReplaceImage } from '../../icons';
 
 class MaxiModal extends Component {
 	state = {
@@ -32,76 +32,103 @@ class MaxiModal extends Component {
 
 		const { type, clientId, empty, style, layerId } = this.props;
 
+		const { getBlockAttributes, getSelectedBlockClientId } =
+			select('core/block-editor');
+
+		const attributes = getBlockAttributes(getSelectedBlockClientId());
+
 		const onClick = () => {
 			this.setState({
 				isOpen: !isOpen,
 			});
-			wp.data
-				.dispatch('core/block-editor')
-				.updateBlockAttributes(clientId, {
-					openFirstTime: !isOpen,
-				});
+
+			dispatch('core/block-editor').updateBlockAttributes(clientId, {
+				openFirstTime: !isOpen,
+			});
 		};
 
 		return (
-			<>
-				{/* Launch the layout modal window */}
-				{type === 'patterns' && (
-					<Button
-						key={`maxi-block-library__modal-button--${clientId}`}
-						isPrimary
-						className='maxi-block-library__modal-button'
-						onClick={onClick}
-					>
-						{__('Launch the Library', 'maxi-blocks')}
-					</Button>
-				)}
-				{type === 'sc' && (
-					<Button
-						className='maxi-style-cards__sc__more-sc--add-more'
-						onClick={onClick}
-					>
-						<Icon icon={SCaddMore} />
-					</Button>
-				)}
-				{type === 'svg' && empty && (
-					<>
-						<div className='maxi-svg-icon-block__placeholder'>
-							<Button
-								isPrimary
-								key={`maxi-block-library__modal-button--${clientId}`}
-								className='maxi-block-library__modal-button'
-								onClick={onClick}
-							>
-								{__('Select SVG Icon', 'maxi-blocks')}
-							</Button>
+			<div className='maxi-library-modal__action-section'>
+				<div className='maxi-library-modal__action-section__buttons'>
+					{type === 'patterns' && (
+						<Button
+							key={`maxi-block-library__modal-button--${clientId}`}
+							isPrimary
+							className='maxi-block-library__modal-button'
+							onClick={onClick}
+						>
+							{__('Launch the Library', 'maxi-blocks')}
+						</Button>
+					)}
+					{type === 'sc' && (
+						<Button
+							className='maxi-style-cards__sc__more-sc--add-more'
+							onClick={onClick}
+						>
+							<Icon icon={SCaddMore} />
+						</Button>
+					)}
+					{type === 'svg' && empty && (
+						<>
+							<div className='maxi-svg-icon-block__placeholder'>
+								<Button
+									isPrimary
+									key={`maxi-block-library__modal-button--${clientId}`}
+									className='maxi-block-library__modal-button'
+									onClick={onClick}
+								>
+									{__('Select SVG Icon', 'maxi-blocks')}
+								</Button>
+							</div>
+						</>
+					)}
+					{type === 'svg' && !empty && (
+						<Button
+							className='maxi-svg-icon-block__replace-icon'
+							onClick={onClick}
+							icon={toolbarReplaceImage}
+						/>
+					)}
+					{type.includes('shape') && (
+						<Button
+							className='maxi-svg-defaults__load-library'
+							onClick={onClick}
+						>
+							{attributes['background-svg-SVGElement'] &&
+							attributes['background-svg-SVGCurrentElement'] ===
+								''
+								? __('Replace Shape From Cloud', 'maxi-blocks')
+								: __('Load Shape Library', 'maxi-blocks')}
+						</Button>
+					)}
+					{isOpen && (
+						<CloudLibrary
+							layerId={layerId}
+							cloudType={type}
+							onClose={onClick}
+							blockStyle={style}
+						/>
+					)}
+				</div>
+				{attributes &&
+					type === 'bg-shape' &&
+					attributes['background-svg-SVGElement'] &&
+					attributes['background-svg-SVGCurrentElement'] === '' && (
+						<div className='maxi-library-modal__action-section__preview'>
+							<RawHTML>
+								{attributes['background-svg-SVGElement']}
+							</RawHTML>
 						</div>
-					</>
-				)}
-				{type === 'svg' && !empty && (
-					<Button
-						className='maxi-svg-icon-block__replace-icon'
-						onClick={onClick}
-						icon={toolbarReplaceImage}
-					/>
-				)}
-				{type.includes('shape') && (
-					<Button
-						className='maxi-svg-defaults__load-library'
-						onClick={onClick}
-					>
-						{__('Load Shape Library', 'maxi-blocks')}
-					</Button>
-				)}
-				{isOpen && (
-					<CloudLibrary
-						layerId={layerId}
-						cloudType={type}
-						onClose={onClick}
-						blockStyle={style}
-					/>
-				)}
-			</>
+					)}
+				{attributes &&
+					type === 'image-shape' &&
+					attributes.SVGElement &&
+					attributes.SVGCurrentElement === '' && (
+						<div className='maxi-library-modal__action-section__preview'>
+							<RawHTML>{attributes.SVGElement}</RawHTML>
+						</div>
+					)}
+			</div>
 		);
 	}
 }

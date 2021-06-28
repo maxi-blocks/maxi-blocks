04a62088c6fc04951e2d0a0745076bf224601335
Alejandro9kgm <58038966+Alejandro9kgm@users.noreply.github.com>
2021-06-28 10:28:27 +0200

    Dropdown test (#1717)

---
+++ e2e-tests/components/dropdown/index.spec.js
@@ -0,0 +1,38 @@
+/**
+ * WordPress dependencies
+ */
+import { createNewPost, insertBlock } from '@wordpress/e2e-test-utils';
+/**
+ * Internal dependencies
+ */
+import { getBlockAttributes, openAdvancedSidebar } from '../../utils';
+
+describe('Dropdown', () => {
+	it('Check dropdown', async () => {
+		await createNewPost();
+		await insertBlock('Container Maxi');
+		await page.$eval('.maxi-container-block', select => select.focus());
+		const accordionPanel = await openAdvancedSidebar(page, 'shape divider');
+
+		await accordionPanel.$$eval(
+			'.maxi-shapedividercontrol .maxi-base-control label',
+			click => click[3].click()
+		);
+
+		await accordionPanel.$eval(
+			'.maxi-dropdown.maxi-shapedividercontrol__shape-selector div',
+			modal => modal.click()
+		);
+
+		await page.$$eval(
+			'.maxi-shapedividercontrol__shape-list label',
+			click => click[1].click()
+		);
+
+		const shapeStyle = 'waves-top';
+		const shapeStyleAttribute = await getBlockAttributes();
+		const style = shapeStyleAttribute['shape-divider-top-shape-style'];
+
+		expect(style).toStrictEqual(shapeStyle);
+	});
+});

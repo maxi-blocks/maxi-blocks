4e38f7440b2d48304df633373978f1a6b3c4caff
Javad <65819265+javadsamiee@users.noreply.github.com>
2021-06-29 13:00:45 +0430

    New issue 1676 (#1715)
    * Restyle load button
    
    * Change default shapes
    
    * Fix fill color for load shape in the first time
    
    * Fix image shape for loaded shapes
    
    * Fix loaded shapes for background layers
    
    * Some fixes
    
    * Fix palette and fill color
    
    * Change size default unit
    
    * Fix test
    
    * Fix the custom color and the fill
    
    * Add preview for loaded shapes
    
    * Fix replace image with cloud shape
    
    * Fix image shape for complex shapes
    
    * Revert to %
    
    * Change default percent value to 999
    
    * Fix tests

--- src/icons/shape-icons/shape13/index.js
+++ src/icons/shape-icons/shape13/index.js
@@ -1,9 +1,5 @@
 const shape13 = `
-<svg xmlns="http://www.w3.org/2000/svg" x="0" y="0" version="1.1" viewBox="0 0 36.1 36.1" xml:space="preserve">
-	<g>
-		<path d="M19.6 8.6L.8 27.4h34.5V8.6H19.6z" />
-	</g>
-</svg>
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 36.1 36.1"><path d="M35 18.3V9.8H17.8V1.3L1.1 18l16.7 16.7v-7.4H35v-9z" fill="#081219"/></svg>
 `;
 
 export default shape13;

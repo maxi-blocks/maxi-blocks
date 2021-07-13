422c3eecbc2fb2343708205f294c7f00ea19c1a2
Javad <65819265+javadsamiee@users.noreply.github.com>
2021-07-13 13:27:24 +0430

    Issue 1684 (#1779)
    * Fix palette for color layer
    
    * Minor fix
    
    * Fix palette for background shape
    
    * Fix shape preview
    
    * Fix tests
    
    * Fix unit test
    
    * Test for color and shape simple background
    
    * Test for layers and some fixes
    
    * Some minor fixes

--- src/components/background-control/colorLayer.js
+++ src/components/background-control/colorLayer.js
@@ -19,8 +19,7 @@ import { cloneDeep } from 'lodash';
  * Component
  */
 const ColorLayer = props => {
-	const { onChange, disableClipPath, isHover, prefix, clientId, type } =
-		props;
+	const { onChange, disableClipPath, isHover, prefix, clientId } = props;
 
 	const colorOptions = cloneDeep(props.colorOptions);
 
@@ -91,7 +90,6 @@ const ColorLayer = props => {
 						)]: paletteStatus,
 					});
 				}}
-				disablePalette={type === 'layer'}
 				globalProps={{
 					target: 'background-color-global',
 					type: 'button',

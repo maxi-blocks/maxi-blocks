78d55261de4e850044f5aa2e887297e5cf52ded3
Josh <jjuancap@gmail.com>
2021-07-22 17:06:58 +0200

    Fixes #1833

--- src/editor/library/container.js
+++ src/editor/library/container.js
@@ -106,12 +106,8 @@ const LibraryContainer = props => {
 		selectedSCValue,
 		clientId,
 		isValidTemplate,
-		currentAttributes,
 	} = useSelect(select => {
-		const { getSelectedBlockClientId, isValidTemplate, getBlock } =
-			select('core/block-editor');
-		const clientId = getSelectedBlockClientId();
-		const currentAttributes = getBlock(clientId).attributes;
+		const { isValidTemplate } = select('core/block-editor');
 
 		const { receiveMaxiStyleCards, receiveMaxiSelectedStyleCard } = select(
 			'maxiBlocks/style-cards'
@@ -126,7 +122,6 @@ const LibraryContainer = props => {
 			selectedSCValue,
 			clientId,
 			isValidTemplate,
-			currentAttributes,
 		};
 	});
 
@@ -279,8 +274,7 @@ const LibraryContainer = props => {
 
 		const finalSvgCode = svgAttributesReplacer(
 			blockStyle,
-			svgCode,
-			currentAttributes
+			svgCode
 		).replaceAll(replaceIt, newSvgClass);
 
 		if (isValidTemplate(finalSvgCode)) {
@@ -291,11 +285,7 @@ const LibraryContainer = props => {
 
 	/** SVG Icons Results */
 	const svgResults = ({ hit }) => {
-		const newContent = svgAttributesReplacer(
-			blockStyle,
-			hit.svg_code,
-			currentAttributes
-		);
+		const newContent = svgAttributesReplacer(blockStyle, hit.svg_code);
 
 		return (
 			<MasonryItem

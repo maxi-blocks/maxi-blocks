b37b21485e74c20dd640522ffad1d0183d7cb0f2
Alejandro <alejandro9kgm@gmail.com>
2021-07-22 11:38:05 +0200

    Placeholder-test

---
+++ e2e-tests/components/placeholder/index.spec.js
@@ -0,0 +1,60 @@
+/**
+ * WordPress dependencies
+ */
+import { createNewPost, insertBlock } from '@wordpress/e2e-test-utils';
+/**
+ * Interactive dependencies
+ */
+import { getBlockAttributes, openSidebar } from '../../utils';
+
+describe('ParallaxControl', () => {
+	it('Test the parallax control', async () => {
+		await createNewPost();
+		await page.$eval('.maxi-toolbar-layout button', button =>
+			button.click()
+		);
+
+		await page.$$eval(
+			'.maxi-responsive-selector .action-buttons__button',
+			button => button[0].click()
+		);
+
+		await page.$eval(
+			'.maxi-library-modal .components-modal__header button',
+			closeModal => closeModal.click()
+		);
+
+		// label
+
+		const placeholderLabel = await page.$eval(
+			'.maxi-placeholder .maxi-placeholder__label',
+			label => label.innerHTML
+		);
+		await page.waitForTimeout(500);
+		expect(placeholderLabel).toMatchSnapshot();
+
+		// placeholder
+
+		const placeholderInstructions = await page.$eval(
+			'.maxi-placeholder .maxi-placeholder__instructions',
+			instructions => instructions.innerHTML
+		);
+		await page.waitForTimeout(500);
+		expect(placeholderInstructions).toMatchSnapshot();
+
+		// Launch the Library
+		await page.waitForTimeout(500);
+		await page.$eval(
+			'.maxi-placeholder .maxi-placeholder__fieldset button',
+			button => button.click()
+		);
+
+		const library = await page.$eval(
+			'.components-modal__content .components-modal__header-heading-container h1',
+			modal => modal.innerHTML
+		);
+
+		await page.waitForTimeout(500);
+		expect(library).toMatchSnapshot();
+	});
+});

6ea59d6dbc2d5b7a86a10eaf1ae6187a45c5d849
javad sameei <mirjavadsamiee@gmail.com>
2021-07-27 18:02:27 +0430

    Fix tests

--- e2e-tests/blocks/svg-icon-maxi/index.spec.js
+++ e2e-tests/blocks/svg-icon-maxi/index.spec.js
@@ -17,10 +17,15 @@ describe('Svg Icon Maxi', () => {
 		await page.waitForSelector('.ais-SearchBox-input');
 		const modalSearcher = await modal.$('.ais-SearchBox-input');
 		await modalSearcher.focus();
-		await page.keyboard.type('Star');
+		await page.keyboard.type('omelette');
 		await page.waitForTimeout(1000);
-		await modal.$eval('.maxi-cloud-masonry-card__button', button =>
-			button.click()
+		await page.waitForSelector('.omelette-maxi-svg');
+		await page.waitForSelector(
+			'.maxi-cloud-masonry-card__svg-container__button'
+		);
+		await modal.$eval(
+			'.maxi-cloud-masonry-card__svg-container__button',
+			button => button.click()
 		);
 
 		expect(await getEditedPostContent()).toMatchSnapshot();

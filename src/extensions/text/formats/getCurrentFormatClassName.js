982f84dd96731fd6abdf4bd9b92e9606d1460e29
Josh <jjuancap@gmail.com>
2021-08-06 19:36:14 +0200

    Add `/artifacts` to .gitignore Update block tests to WP 5.8 Fix `onSplit` Text Maxi issue with Custom Formats API Fix error on `openPreviewPage` e2e util

--- src/extensions/text/formats/getCurrentFormatClassName.js
+++ src/extensions/text/formats/getCurrentFormatClassName.js
@@ -4,16 +4,31 @@
 import { getActiveFormat } from '@wordpress/rich-text';
 
 /**
+ * External dependencies
+ */
+import { isEmpty } from 'lodash';
+
+/**
  * Retrieves the current className of the 'maxi
  *
- * @param {Object} formatValue  		RichText format value
- * @param {boolean} isHover 			Is the requested typography under hover state
- *
+ * @param {Object}  formatValue RichText format value
+ * @param {boolean} isHover     Is the requested typography under hover state
  * @returns {string} Current className for Maxi Custom format
  */
 const getCurrentFormatClassName = (formatValue, isHover = false) => {
+	let isWholeContent = false;
+
+	if (!isEmpty(formatValue)) {
+		const { start, end } = formatValue;
+
+		isWholeContent = start === end;
+	}
+
 	const formatOptions = getActiveFormat(
-		formatValue,
+		{
+			...formatValue,
+			...(isWholeContent && { start: 1, end: 1 }),
+		},
 		!isHover ? 'maxi-blocks/text-custom' : 'maxi-blocks/text-custom-hover'
 	);
 

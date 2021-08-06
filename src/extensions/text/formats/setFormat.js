063f41d93f9eb88ef020651fa389fbe13ee830c0
Josh <jjuancap@gmail.com>
2021-08-06 19:36:14 +0200

    Add `/artifacts` to .gitignore Update block tests to WP 5.8 Fix `onSplit` Text Maxi issue with Custom Formats API Fix error on `openPreviewPage` e2e util

--- src/extensions/text/formats/setFormat.js
+++ src/extensions/text/formats/setFormat.js
@@ -19,15 +19,14 @@ import { isNil } from 'lodash';
 
 /**
  *
- * @param {Object} 	[$0]					Optional named arguments.
- * @param {Object} 	[$0.formatValue]		RichText format value
- * @param {Object} 	[$0.typography]			MaxiBlocks typography
- * @param {boolean} [$0.isList]				Text Maxi block has list mode active
- * @param {Object}	[$0.value]				Requested values to implement
- * 											on typography object
- * @param {string} 	[$0.breakpoint]			Device type breakpoint
- * @param {boolean}	[$0.isHover]			Is the requested typography under hover state
- *
+ * @param {Object}  [$0]             Optional named arguments.
+ * @param {Object}  [$0.formatValue] RichText format value
+ * @param {Object}  [$0.typography]  MaxiBlocks typography
+ * @param {boolean} [$0.isList]      Text Maxi block has list mode active
+ * @param {Object}  [$0.value]       Requested values to implement
+ *                                   on typography object
+ * @param {string}  [$0.breakpoint]  Device type breakpoint
+ * @param {boolean} [$0.isHover]     Is the requested typography under hover state
  * @returns {Object} Formatted object
  */
 const setFormat = ({
@@ -120,7 +119,11 @@ const setFormat = ({
 			isList,
 		});
 
-		return { ...newTypography, content: newContent };
+		return {
+			...newTypography,
+			content: newContent,
+			...(returnFormatValue && { formatValue: newFormatValue }),
+		};
 	}
 
 	// Ensures the format changes are saved as undo entity on historical records

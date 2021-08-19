99ae4d07945c9799fd6804ba2392c5a39eac9d36
Alejandro <alejandro9kgm@gmail.com>
2021-08-19 14:06:56 +0200

    minor fix

--- e2e-tests/blocks/row-maxi/index.spec.js
+++ e2e-tests/blocks/row-maxi/index.spec.js
@@ -17,28 +17,4 @@ describe('Row Maxi', () => {
 		);
 		expect(await getEditedPostContent()).toMatchSnapshot();
 	});
-
-	it('Row Maxi does not break in a group', async () => {
-		await createNewPost();
-		await insertBlock('Group Maxi');
-
-		const addBlock = await page.$(
-			'.maxi-group-block__group .maxi-block-placeholder__button'
-		);
-
-		await addBlock.click();
-		await page.waitForTimeout(150);
-		await page.keyboard.type('Row Maxi', { delay: 100 });
-
-		await page.$eval(
-			'.block-editor-inserter__quick-inserter-results button',
-			button => button.click()
-		);
-
-		await page.$$eval('.maxi-row-block__template button', button =>
-			button[1].click()
-		);
-
-		expect(await getEditedPostContent()).toMatchSnapshot();
-	});
 });

6cc409c54341346037fa7c38e4121038b1b1ac97
javad sameei <mirjavadsamiee@gmail.com>
2021-08-25 12:58:21 +0430

    Fix shape preview on layer and remove shape form hover layre

--- src/extensions/styles/setHoverAttributes.js
+++ src/extensions/styles/setHoverAttributes.js
@@ -9,7 +9,12 @@
 const setHoverAttributes = (attributes, hoverAttributes) => {
 	Object.entries(attributes).forEach(([key, val]) => {
 		if (!hoverAttributes[`${key}-hover`])
-			hoverAttributes[`${key}-hover`] = val;
+			hoverAttributes[`${key}-hover`] =
+				key === 'background-layers'
+					? hoverAttributes['background-layers']?.filter(
+							item => item.type !== 'shape'
+					  )
+					: val;
 	});
 
 	return { ...hoverAttributes };

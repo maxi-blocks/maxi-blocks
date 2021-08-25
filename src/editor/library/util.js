027b5739987ff051c6fbcbf700a4e20520de45a8
javad sameei <mirjavadsamiee@gmail.com>
2021-08-25 18:05:44 +0430

    Fix Fullwidth shape

--- src/editor/library/util.js
+++ src/editor/library/util.js
@@ -24,6 +24,31 @@ export const rgbToHex = color => {
 		: '';
 };
 
+export const fitSvg = svgCode => {
+	const template = document.createElement('div');
+
+	template.setAttribute('id', 'maxi-temporary-elem');
+	template.innerHTML = svgCode.trim();
+	document.querySelector('body').append(template);
+
+	const bbox = document.querySelector('#maxi-temporary-elem svg').getBBox();
+
+	document
+		.querySelector('#maxi-temporary-elem svg')
+		.setAttribute(
+			'viewBox',
+			`${bbox.x}, ${bbox.y}, ${bbox.width}, ${bbox.height}`
+		);
+
+	const newSvgCode = document.querySelector(
+		'#maxi-temporary-elem svg'
+	).outerHTML;
+
+	document.querySelector('#maxi-temporary-elem').remove();
+
+	return newSvgCode;
+};
+
 export const placeholderImage = async () => {
 	const ajaxurl = wp.ajax.settings.url;
 	try {

924682abe5eaecdec237d2c51e96aa5a67241361
Alejandro <alejandro9kgm@gmail.com>
2021-08-26 13:49:05 +0200

    changeResponsive

--- e2e-tests/components/border-control/index.spec.js
+++ e2e-tests/components/border-control/index.spec.js
@@ -69,59 +69,57 @@ describe('BorderControl', () => {
 		expect(style).toStrictEqual(expectResult);
 	});
 
-	it.only('Check Responsive border control', async () => {
-		/* const responsiveButton = await page.$$(
-			'.maxi-responsive-selector button'
-		); */
-		const attributes = await changeResponsive();
-
+	it('Check Responsive border control', async () => {
 		// Dotted bottom enabled
-		await page.$eval(
-			'.edit-post-header .edit-post-header__toolbar .maxi-toolbar-layout button',
-			button => button.click()
-		);
-
-		await responsiveButton[5].click();
+		await changeResponsive(page, 's');
 		await page.$eval('.maxi-text-block', block => block.focus());
 
-		const dottedButton = await page.$$eval(
-			'.maxi-tabs-content .maxi-default-styles-control button',
+		const dottedButton = await page.$eval(
+			'.maxi-tabs-content .maxi-border-control__type .maxi-select-control__input',
 			button => button.selectedOptions[0].innerHTML
 		);
 
-		expect(dottedButton).toMatchSnapshot();
+		const responsiveResult = 'Dotted';
+		expect(dottedButton).toStrictEqual(responsiveResult);
 
 		// responsive S
 		const accordionPanel = await openSidebar(page, 'border');
+		await changeResponsive(page, 's');
 
 		const selector = await accordionPanel.$(
 			'.maxi-tabs-content .maxi-border-control .maxi-base-control__field select'
 		);
 		await selector.select('dashed');
 
-		const responsiveResult = 'dashed';
-		const responsiveAttributes = await getBlockAttributes();
-		const responsiveStyle = responsiveAttributes['border-style-s'];
+		const responsiveSOption = await page.$eval(
+			'.maxi-tabs-content .maxi-border-control__type .maxi-select-control__input',
+			selectedStyle => selectedStyle.selectedOptions[0].innerHTML
+		);
 
-		expect(responsiveStyle).toStrictEqual(responsiveResult);
+		const responsiveSResult = 'Dashed';
+		expect(responsiveSOption).toStrictEqual(responsiveSResult);
 
 		// responsive XS
-		await responsiveButton[6].click();
+		await changeResponsive(page, 'xs');
 
-		const responsiveXsResult = 'dashed';
-		const responsiveXsAttributes = await getBlockAttributes();
-		const responsiveXsStyle = responsiveXsAttributes['border-style-s'];
+		const responsiveXsOption = await page.$eval(
+			'.maxi-tabs-content .maxi-border-control__type .maxi-select-control__input',
+			selectedStyle => selectedStyle.selectedOptions[0].innerHTML
+		);
 
-		expect(responsiveXsStyle).toStrictEqual(responsiveXsResult);
+		const responsiveXsResult = 'Dashed';
+		expect(responsiveXsOption).toStrictEqual(responsiveXsResult);
 
 		// responsive M
-		await responsiveButton[4].click();
+		await changeResponsive(page, 'm');
 
-		const responsiveMResult = 'dotted';
-		const responsiveMAttributes = await getBlockAttributes();
-		const responsiveMStyle = responsiveMAttributes['border-style-general'];
+		const responsiveMOption = await page.$eval(
+			'.maxi-tabs-content .maxi-border-control__type .maxi-select-control__input',
+			selectedStyle => selectedStyle.selectedOptions[0].innerHTML
+		);
 
-		expect(responsiveMStyle).toStrictEqual(responsiveMResult);
+		const responsiveMResult = 'Dotted';
+		expect(responsiveMOption).toStrictEqual(responsiveMResult);
 	});
 
 	it('Check hover values kept after setting normal border to none', async () => {
--- e2e-tests/utils/changeResponsive.js
+++ e2e-tests/utils/changeResponsive.js
@@ -1,31 +1,21 @@
-/**
- * WordPress dependencies
- */
-import {
-	ensureSidebarOpened,
-	openDocumentSettingsSidebar,
-} from '@wordpress/e2e-test-utils';
-
-const changeResponsive = async page => {
-	await openDocumentSettingsSidebar();
-	await ensureSidebarOpened();
-
+const changeResponsive = async (page, size) => {
 	await page.$eval(
 		'.edit-post-header .edit-post-header__toolbar .maxi-toolbar-layout button',
 		button => button.click()
 	);
-	/// /////////////////////////////////////
-	const responsive = [base, xxl, l, m, s, xs, cl, sc];
 
-	for (let i = 0; i < responsive.length; i++) {
-		await page.$$('.maxi-responsive-selector button');
-		await responsive[xxl].click();
-	}
+	const responsive = ['base', 'xxl', 'xl', 'l', 'm', 's', 'xs'];
+	const sizeIndex = responsive.indexOf(size);
 
-	/// //////////////////////////////////
+	await page.$$eval(
+		'.maxi-responsive-selector button',
+		(buttons, _sizeIndex) => buttons[_sizeIndex].click(),
+		sizeIndex
+	);
 
-	const responsiveButtons = await page.$$('.maxi-responsive-selector button');
-
-	const responsive = responsiveButtons[(Base, XXL, L, M, S, XS, CL, SC)];
+	await page.$eval('.maxi-responsive-selector span', closeButton =>
+		closeButton.click()
+	);
 };
+
 export default changeResponsive;

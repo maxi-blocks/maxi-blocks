b7a383a871c363774882903e563281111dc3c726
Alejandro9kgm <58038966+Alejandro9kgm@users.noreply.github.com>
2021-08-27 09:52:47 +0200

    add responsive test to border-control (#2055)
    * add test
    
    * changeResponsive
    
    * changeResponsive
    
    * minor fix

--- e2e-tests/utils/index.js
+++ e2e-tests/utils/index.js
@@ -3,3 +3,4 @@ export { default as getBlockAttributes } from './getBlockAttributes';
 export { default as openSidebar } from './openSidebar';
 export { default as openPreviewPage } from './openPreviewPage';
 export { default as openAdvancedSidebar } from './openAdvancedSidebar';
+export { default as changeResponsive } from './changeResponsive';

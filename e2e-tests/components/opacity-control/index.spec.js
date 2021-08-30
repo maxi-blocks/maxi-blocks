bd9638661025fa6a8a31c8433393e1be10349c40
Alejandro <alejandro9kgm@gmail.com>
2021-08-30 09:59:35 +0200

    add test

--- e2e-tests/components/opacity-control/index.spec.js
+++ e2e-tests/components/opacity-control/index.spec.js
@@ -9,7 +9,12 @@ import {
 /**
  * Internal dependencies
  */
-import { getBlockAttributes, openAdvancedSidebar } from '../../utils';
+import {
+	getBlockAttributes,
+	openAdvancedSidebar,
+	changeResponsive,
+	openSidebar,
+} from '../../utils';
 
 describe('OpacityControl', () => {
 	it('Checking the opacity control', async () => {
@@ -31,5 +36,64 @@ describe('OpacityControl', () => {
 		const expectResult = 0.19;
 
 		expect(opacity).toStrictEqual(expectResult);
+	});
+
+	it('Check Responsive font level control', async () => {
+		await createNewPost();
+		await insertBlock('Text Maxi');
+		await page.keyboard.type('Testing Text Maxi');
+		const accordionPanel = await openSidebar(page, 'typography');
+
+		const input = await accordionPanel.$(
+			'.maxi-typography-control .maxi-color-palette-control .maxi-advanced-number-control input'
+		);
+
+		await input.focus();
+		await page.keyboard.type('80');
+		await changeResponsive(page, 's');
+		debugger;
+		const fontLevel = await page.$eval(
+			'.maxi-typography-control .maxi-color-palette-control .maxi-advanced-number-control input',
+			button => button.value
+		);
+
+		expect(fontLevel).toStrictEqual('80');
+
+		// responsive S
+		await page.waitForTimeout(100);
+		await changeResponsive(page, 's');
+
+		await input.focus();
+		await pressKeyTimes('Backspace', '2');
+		await page.keyboard.type('55', { delay: 100 });
+
+		const responsiveSOption = await page.$eval(
+			'.maxi-typography-control .maxi-color-palette-control .maxi-advanced-number-control input',
+			selectedStyle => selectedStyle.value
+		);
+
+		expect(responsiveSOption).toStrictEqual('55');
+
+		// responsive XS
+		await page.waitForTimeout(100);
+		await changeResponsive(page, 'xs');
+
+		const responsiveXsOption = await page.$eval(
+			'.maxi-typography-control .maxi-color-palette-control .maxi-advanced-number-control input',
+			selectedStyle => selectedStyle.value
+		);
+
+		expect(responsiveXsOption).toStrictEqual('55');
+
+		// responsive M
+		await page.waitForTimeout(100);
+		await changeResponsive(page, 'm');
+
+		const responsiveMOption = await page.$eval(
+			'.maxi-typography-control .maxi-color-palette-control .maxi-advanced-number-control input',
+			selectedStyle => selectedStyle.value
+		);
+
+		expect(responsiveMOption).toStrictEqual('80');
 	});
 });

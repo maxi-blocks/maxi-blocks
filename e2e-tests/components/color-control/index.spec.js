334fc9628a44e724ceca97dc045a7fc5d295fe25
Alejandro9kgm <58038966+Alejandro9kgm@users.noreply.github.com>
2021-08-30 13:17:14 +0200

    Add responsive test to color-control (#2071)
    * add test
    
    * update test
    
    * minor fix
    
    * minor fix

--- e2e-tests/components/color-control/index.spec.js
+++ e2e-tests/components/color-control/index.spec.js
@@ -5,7 +5,7 @@ import { createNewPost, insertBlock } from '@wordpress/e2e-test-utils';
 /**
  * Internal dependencies
  */
-import { getBlockAttributes, openSidebar } from '../../utils';
+import { getBlockAttributes, openSidebar, changeResponsive } from '../../utils';
 
 describe('ColorControl', () => {
 	it('Checking the color control', async () => {
@@ -25,8 +25,63 @@ describe('ColorControl', () => {
 
 		const attributes = await getBlockAttributes();
 		const backgroundColor = attributes['background-palette-color'];
-		const expectedResult = 4;
 
-		expect(backgroundColor).toStrictEqual(expectedResult);
+		expect(backgroundColor).toStrictEqual(4);
+	});
+
+	it('Check Responsive color control', async () => {
+		const dataItem = await page.$eval(
+			'.maxi-typography-control .maxi-color-palette-control .maxi-color-palette-control__palette-label .maxi-sc-color-palette__box--active',
+			select => select.getAttribute('data-item')
+		);
+
+		await changeResponsive(page, 's');
+		await page.$eval('.maxi-text-block', block => block.focus());
+
+		const backgroundColor = await dataItem;
+		expect(backgroundColor).toStrictEqual('3');
+
+		// responsive S
+		await changeResponsive(page, 's');
+		await openSidebar(page, 'typography');
+
+		await page.$$eval(
+			'.maxi-typography-control .maxi-color-palette-control .maxi-color-palette-control__palette-label .maxi-sc-color-palette div',
+			select => select[4].click()
+		);
+		const dataItemS = await page.$eval(
+			'.maxi-typography-control .maxi-color-palette-control .maxi-color-palette-control__palette-label .maxi-sc-color-palette__box--active',
+			select => select.getAttribute('data-item')
+		);
+		const responsiveSOption = await dataItemS;
+
+		expect(responsiveSOption).toStrictEqual('5');
+
+		const attributes = await getBlockAttributes();
+		const color = attributes['palette-color-s'];
+
+		expect(color).toStrictEqual(5);
+
+		// responsive XS
+		await changeResponsive(page, 'xs');
+
+		const dataItemXs = await page.$eval(
+			'.maxi-typography-control .maxi-color-palette-control .maxi-color-palette-control__palette-label .maxi-sc-color-palette__box--active',
+			select => select.getAttribute('data-item')
+		);
+		const responsiveXsOption = await dataItemXs;
+
+		expect(responsiveXsOption).toStrictEqual('5');
+
+		// responsive M
+		await changeResponsive(page, 'm');
+
+		const dataItemM = await page.$eval(
+			'.maxi-typography-control .maxi-color-palette-control .maxi-color-palette-control__palette-label .maxi-sc-color-palette__box--active',
+			select => select.getAttribute('data-item')
+		);
+		const responsiveMOption = await dataItemM;
+
+		expect(responsiveMOption).toStrictEqual('3');
 	});
 });

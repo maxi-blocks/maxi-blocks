dd4c0eab0df9b545020a1162aa1ffdfcc4f63f13
Alejandro <alejandro9kgm@gmail.com>
2021-08-31 09:26:28 +0200

    add test

--- e2e-tests/components/full-size-control/index.spec.js
+++ e2e-tests/components/full-size-control/index.spec.js
@@ -1,11 +1,15 @@
 /**
  * WordPress dependencies
  */
-import { createNewPost, insertBlock } from '@wordpress/e2e-test-utils';
+import {
+	createNewPost,
+	insertBlock,
+	pressKeyTimes,
+} from '@wordpress/e2e-test-utils';
 /**
  * Internal dependencies
  */
-import { getBlockAttributes, openSidebar } from '../../utils';
+import { getBlockAttributes, openSidebar, changeResponsive } from '../../utils';
 
 describe('FullSizeControl', () => {
 	it('Checking the full size control', async () => {
@@ -23,5 +27,62 @@ describe('FullSizeControl', () => {
 		const width = expectAttributes.fullWidth;
 
 		expect(width).toStrictEqual(expectResult);
+	});
+
+	it('Check Responsive full size control', async () => {
+		await createNewPost();
+		await insertBlock('Text Maxi');
+		const accordionPanel = await openSidebar(page, 'width height');
+
+		const inputs = await accordionPanel.$$(
+			'.maxi-full-size-control .maxi-advanced-number-control .maxi-advanced-number-control__value'
+		);
+
+		await inputs[0].focus();
+		await page.keyboard.type('330', { delay: 100 });
+
+		const generalHeight = await accordionPanel.$eval(
+			'.maxi-full-size-control .maxi-advanced-number-control .maxi-advanced-number-control__value',
+			button => button.value
+		);
+
+		expect(generalHeight).toStrictEqual('330');
+		await changeResponsive(page, 's');
+
+		// responsive S
+		await changeResponsive(page, 's');
+
+		await inputs[0].focus();
+		await pressKeyTimes('Backspace', '2');
+		await page.keyboard.type('99', { delay: 100 });
+
+		const heightS = await accordionPanel.$eval(
+			'.maxi-full-size-control .maxi-advanced-number-control .maxi-advanced-number-control__value',
+			button => button.value
+		);
+		expect(heightS).toStrictEqual('399');
+
+		const attributes = await getBlockAttributes();
+		const sHeight = attributes['height-s'];
+
+		expect(sHeight).toStrictEqual(399);
+
+		// responsive XS
+		await changeResponsive(page, 'xs');
+
+		const heightXs = await accordionPanel.$eval(
+			'.maxi-full-size-control .maxi-advanced-number-control .maxi-advanced-number-control__value',
+			button => button.value
+		);
+		expect(heightXs).toStrictEqual('399');
+
+		// responsive M
+		await changeResponsive(page, 'm');
+
+		const heightM = await accordionPanel.$eval(
+			'.maxi-full-size-control .maxi-advanced-number-control .maxi-advanced-number-control__value',
+			button => button.value
+		);
+		expect(heightM).toStrictEqual('330');
 	});
 });

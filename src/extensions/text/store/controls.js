51fbe86c930d633ff737f1ba01037dedaf3a3d58
Josh <jjuancap@gmail.com>
2021-09-03 12:11:08 +0200

    Ensures font family weights on frontend

--- src/extensions/text/store/controls.js
+++ src/extensions/text/store/controls.js
@@ -5,25 +5,19 @@ import apiFetch from '@wordpress/api-fetch';
 import { select } from '@wordpress/data';
 
 /**
- * Internal dependencies
- */
-import frontendStyleGenerator from '../frontendStyleGenerator';
-
-/**
  * Controls
  */
 const controls = {
-	async SAVE_STYLES({ isUpdate, styles }) {
+	async SAVE_FONTS({ isUpdate, fonts }) {
 		const id = select('core/editor').getCurrentPostId();
-		const parsedStyles = frontendStyleGenerator(styles);
 
 		await apiFetch({
-			path: '/maxi-blocks/v1.0/post',
+			path: '/maxi-blocks/v1.0/post/fonts',
 			method: 'POST',
 			data: {
 				id,
 				meta: JSON.stringify({
-					styles: parsedStyles,
+					fonts,
 				}),
 				update: isUpdate,
 			},

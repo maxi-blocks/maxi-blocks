970ec0b02f82c44f7e7a0f0f514fd2d1a7c32bfb
Alejandro <alejandro9kgm@gmail.com>
2021-09-08 12:08:05 +0200

    add test

--- e2e-tests/components/column-pattern/index.spec.js
+++ e2e-tests/components/column-pattern/index.spec.js
@@ -5,7 +5,7 @@ import { createNewPost, insertBlock } from '@wordpress/e2e-test-utils';
 /**
  * Internal dependencies
  */
-import { getBlockAttributes, openSidebar } from '../../utils';
+import { getBlockAttributes, openSidebar, changeResponsive } from '../../utils';
 
 describe('ColumnPattern', () => {
 	it('Check column pattern', async () => {
@@ -43,5 +43,62 @@ describe('ColumnPattern', () => {
 		const expectGap = true;
 
 		expect(gapAttribute).toStrictEqual(expectGap);
+	});
+	it('Check Responsive columnSize control', async () => {
+		await createNewPost();
+		await insertBlock('Container Maxi');
+		await page.$eval('.maxi-container-block', container =>
+			container.focus()
+		);
+		await openSidebar(page, 'width height');
+
+		// general
+		const heightInput = await page.$$(
+			'.maxi-full-size-control .maxi-advanced-number-control input'
+		);
+
+		await heightInput[0].focus();
+		await page.keyboard.type('50');
+		const heightValue = await page.$$eval(
+			'.maxi-full-size-control .maxi-advanced-number-control input',
+			input => input[0].value
+		);
+
+		expect(heightValue).toStrictEqual('50');
+
+		// responsive S
+		await changeResponsive(page, 's');
+		await heightInput[0].focus();
+		await page.keyboard.type('7');
+
+		const responsiveSOption = await page.$$eval(
+			'.maxi-full-size-control .maxi-advanced-number-control input',
+			input => input[0].value
+		);
+
+		expect(responsiveSOption).toStrictEqual('507');
+
+		const attributes = await getBlockAttributes();
+		const containerHeight = attributes['container-height-s'];
+
+		expect(containerHeight).toStrictEqual(507);
+
+		// responsive XS
+		await changeResponsive(page, 'xs');
+		const responsiveXsOption = await page.$$eval(
+			'.maxi-full-size-control .maxi-advanced-number-control input',
+			input => input[0].value
+		);
+
+		expect(responsiveXsOption).toStrictEqual('507');
+
+		// responsive M
+		await changeResponsive(page, 'm');
+		const responsiveMOption = await page.$$eval(
+			'.maxi-full-size-control .maxi-advanced-number-control input',
+			input => input[0].value
+		);
+
+		expect(responsiveMOption).toStrictEqual('50');
 	});
 });

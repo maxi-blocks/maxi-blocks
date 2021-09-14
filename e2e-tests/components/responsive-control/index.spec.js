616030d48424269dd3ac53ce27cc25302ac4fb68
Alejandro9kgm <58038966+Alejandro9kgm@users.noreply.github.com>
2021-09-14 11:52:34 +0200

    Responsive test to responsive-control (#2120)
    * add test
    
    * minor fix
    
    * minor fix
    
    * minor fix

--- e2e-tests/components/responsive-control/index.spec.js
+++ e2e-tests/components/responsive-control/index.spec.js
@@ -9,7 +9,11 @@ import {
 /**
  * Internal dependencies
  */
-import { getBlockAttributes, openAdvancedSidebar } from '../../utils';
+import {
+	getBlockAttributes,
+	openAdvancedSidebar,
+	changeResponsive,
+} from '../../utils';
 
 describe('ResponsiveControl', () => {
 	it('Test the responsive control', async () => {
@@ -41,5 +45,53 @@ describe('ResponsiveControl', () => {
 		const expectValue = 1000;
 
 		expect(breakpoint).toStrictEqual(expectValue);
+	});
+
+	it('Check Responsive to responsive control', async () => {
+		const input = await page.$('.maxi-responsive-control input');
+
+		const breakpoint = await page.$eval(
+			'.maxi-responsive-control input',
+			button => button.value
+		);
+
+		expect(breakpoint).toStrictEqual('1000');
+
+		// responsive S
+		await changeResponsive(page, 's');
+		await input.focus();
+		await pressKeyTimes('Backspace', '4');
+		await page.keyboard.type('855');
+		const breakpointS = await page.$eval(
+			'.maxi-responsive-control input',
+			button => button.value
+		);
+
+		expect(breakpointS).toStrictEqual('855');
+
+		const attributes = await getBlockAttributes();
+		const breakpoints = attributes['breakpoints-s'];
+
+		expect(breakpoints).toStrictEqual(855);
+
+		// responsive XS
+		await changeResponsive(page, 'xs');
+
+		const breakpointXs = await page.$eval(
+			'.maxi-responsive-control input',
+			button => button.value
+		);
+
+		expect(breakpointXs).toStrictEqual('480');
+
+		// responsive M
+		await changeResponsive(page, 'm');
+
+		const breakpointM = await page.$eval(
+			'.maxi-responsive-control input',
+			button => button.value
+		);
+
+		expect(breakpointM).toStrictEqual('1024');
 	});
 });

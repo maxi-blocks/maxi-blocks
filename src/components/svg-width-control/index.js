483a0eed828eae731d22b861be94da8d94f74155
Svitlana <2401618+elzadj@users.noreply.github.com>
2021-09-17 12:46:51 +0300

    Add Button Icon Hover sidebar settings

--- src/components/svg-width-control/index.js
+++ src/components/svg-width-control/index.js
@@ -16,9 +16,10 @@ import AdvancedNumberControl from '../advanced-number-control';
  * Component
  */
 const SvgWidthControl = props => {
-	const { onChange, breakpoint, prefix } = props;
+	const { onChange, breakpoint, prefix, isHover } = props;
 
-	const width = props[`${prefix}width-${breakpoint}`];
+	const width =
+		props[`${prefix}width-${breakpoint}${isHover ? '-hover' : ''}`];
 	const defaultWidth = getDefaultAttribute(`${prefix}width-${breakpoint}`);
 	const widthUnit = getLastBreakpointAttribute(
 		`${prefix}width-unit`,
@@ -39,14 +40,16 @@ const SvgWidthControl = props => {
 						? getLastBreakpointAttribute(
 								`${prefix}width`,
 								breakpoint,
+								isHover,
 								props
 						  )
 						: null
 				}
 				onChangeValue={val => {
 					onChange({
-						[`${prefix}width-${breakpoint}`]:
-							val !== undefined && val !== '' ? val : '',
+						[`${prefix}width-${breakpoint}${
+							isHover ? '-hover' : ''
+						}`]: val !== undefined && val !== '' ? val : '',
 					});
 				}}
 				enableUnit
@@ -54,7 +57,9 @@ const SvgWidthControl = props => {
 				allowedUnits={['px', 'em', 'vw', '%']}
 				onChangeUnit={val => {
 					onChange({
-						[`${prefix}width-unit-${breakpoint}`]: val,
+						[`${prefix}width-unit-${breakpoint}${
+							isHover ? '-hover' : ''
+						}`]: val,
 					});
 				}}
 				min={10}
@@ -62,11 +67,16 @@ const SvgWidthControl = props => {
 				step={1}
 				onReset={() =>
 					onChange({
-						[`${prefix}width-${breakpoint}`]: defaultWidth,
-						[`${prefix}width-unit-${breakpoint}`]: defaultWidthUnit,
+						[`${prefix}width-${breakpoint}${
+							isHover ? '-hover' : ''
+						}`]: defaultWidth,
+						[`${prefix}width-unit-${breakpoint}${
+							isHover ? '-hover' : ''
+						}`]: defaultWidthUnit,
 					})
 				}
 				initialPosition={defaultWidth}
+				isHover={isHover}
 			/>
 		</>
 	);
---
+++ src/extensions/styles/defaults/iconBorderHover.js
@@ -0,0 +1,51 @@
+import { border, borderWidth, borderRadius } from './border';
+
+export const iconBorder = (() => {
+	const response = {};
+
+	Object.keys(border).forEach(key => {
+		const newKey = `icon-${key}`;
+		const value = { ...border[key] };
+
+		response[newKey] = value;
+	});
+
+	return response;
+})();
+
+export const iconBorderWidth = (() => {
+	const response = {};
+
+	Object.keys(borderWidth).forEach(key => {
+		const newKey = `icon-${key}`;
+		const value = { ...borderWidth[key] };
+
+		if ('default' in value) delete value.default;
+
+		response[newKey] = value;
+	});
+
+	return response;
+})();
+
+export const iconBorderRadius = (() => {
+	let response = {};
+
+	Object.keys(borderRadius).forEach(key => {
+		const newKey = `icon-${key}`;
+		const value = { ...borderRadius[key] };
+
+		if ('default' in value) delete value.default;
+
+		response[newKey] = value;
+	});
+
+	response = {
+		...response,
+		'icon-border-unit-radius-general': {
+			type: 'string',
+			default: 'px',
+		},
+	};
+	return response;
+})();

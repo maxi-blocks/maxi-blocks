4a4eb51be45fa0180bd6dca9500b8e0f383f1c3d
Svitlana <2401618+elzadj@users.noreply.github.com>
2021-09-19 19:22:20 +0300

    Add button Icon hover

--- src/extensions/styles/helpers/getIconStyles.js
+++ src/extensions/styles/helpers/getIconStyles.js
@@ -10,7 +10,6 @@ import { isNil } from 'lodash';
 
 const getIconStyles = (
 	obj,
-	target,
 	parentBlockStyle,
 	isIconInherit = true,
 	isHover = false
@@ -20,30 +19,27 @@ const getIconStyles = (
 		general: {},
 	};
 
-	if (target === 'svg') {
-		if (
-			!obj[`icon-palette-color-status${isHover ? '-hover' : ''}`] &&
-			!isNil(obj[`icon-color${isHover ? '-hover' : ''}`])
-		) {
-			response.general.fill = 'none';
-			response.general.stroke =
-				obj[`icon-color${isHover ? '-hover' : ''}`];
-		} else if (
-			obj[`icon-palette-color-status${isHover ? '-hover' : ''}`] &&
-			obj[`icon-palette-color${isHover ? '-hover' : ''}`]
-		) {
-			response.general.fill = 'none';
-			response.general.stroke = getColorRGBAString({
-				firstVar: `color-${
-					obj[`icon-palette-color${isHover ? '-hover' : ''}`]
-				}`,
-				opacity: obj[`icon-palette-opacity${isHover ? '-hover' : ''}`],
-				blockStyle: parentBlockStyle,
-			});
-		}
+	if (
+		!obj[`icon-palette-color-status${isHover ? '-hover' : ''}`] &&
+		!isNil(obj[`icon-color${isHover ? '-hover' : ''}`])
+	) {
+		response.general.fill = 'none';
+		response.general.stroke = obj[`icon-color${isHover ? '-hover' : ''}`];
+	} else if (
+		obj[`icon-palette-color-status${isHover ? '-hover' : ''}`] &&
+		obj[`icon-palette-color${isHover ? '-hover' : ''}`]
+	) {
+		response.general.fill = 'none';
+		response.general.stroke = getColorRGBAString({
+			firstVar: `color-${
+				obj[`icon-palette-color${isHover ? '-hover' : ''}`]
+			}`,
+			opacity: obj[`icon-palette-opacity${isHover ? '-hover' : ''}`],
+			blockStyle: parentBlockStyle,
+		});
 	}
 
-	if (target === 'svg' && isIconInherit) {
+	if (isIconInherit) {
 		if (!obj['palette-color-status-general'] && obj['color-general']) {
 			response.general.fill = 'none';
 			response.general.stroke = obj['color-general'];

feea2d6b02986234a8ad5988c9b8f0ffa178c87d
Alejandro <alejandro9kgm@gmail.com>
2021-09-29 10:29:11 +0200

    add test

--- e2e-tests/blocks/column-maxi/index.spec.js
+++ e2e-tests/blocks/column-maxi/index.spec.js
@@ -5,7 +5,12 @@ import {
 	createNewPost,
 	insertBlock,
 	getEditedPostContent,
+	pressKeyTimes,
 } from '@wordpress/e2e-test-utils';
+/**
+ * Internal dependencies
+ */
+import { getBlockAttributes, openSidebar } from '../../utils';
 
 describe('Column Maxi', () => {
 	it('Column Maxi does not break', async () => {
@@ -16,5 +21,34 @@ describe('Column Maxi', () => {
 			button[0].click()
 		);
 		expect(await getEditedPostContent()).toMatchSnapshot();
+	});
+
+	it('check column settings', async () => {
+		await page.$eval(
+			'.block-editor-block-list__layout .block-editor-inserter',
+			select => select.click()
+		);
+		await openSidebar(page, 'column settings');
+
+		await page.$eval(
+			'.maxi-advanced-number-control .maxi-advanced-number-control__value',
+			input => input.focus()
+		);
+
+		await pressKeyTimes('Backspace', '3');
+		await page.keyboard.type('50');
+
+		const attributes = await getBlockAttributes();
+		const columnSize = attributes['column-size-general'];
+
+		expect(columnSize).toStrictEqual(50);
+
+		const selector = await page.$(
+			'.maxi-accordion-control__item__panel .maxi-base-control__field select'
+		);
+		await selector.select('center');
+
+		const verticalAttributes = await getBlockAttributes();
+		expect(verticalAttributes.verticalAlign).toStrictEqual('center');
 	});
 });

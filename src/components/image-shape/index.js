4911fcd9cd404d52036ddff7e36dacf80de3b85a
Svitlana <2401618+elzadj@users.noreply.github.com>
2021-10-07 17:05:08 +0300

    Add Image - shape dcale and position

--- src/components/image-shape/index.js
+++ src/components/image-shape/index.js
@@ -2,41 +2,33 @@
  * WordPress dependencies
  */
 import { __ } from '@wordpress/i18n';
-import {
-	getDefaultAttribute,
-	getLastBreakpointAttribute,
-} from '../../extensions/styles';
-
-/**
- * External dependencies
- */
-import { isNil } from 'lodash';
 
 /**
  * Internal dependencies
  */
-import { SelectControl } from '../../components';
+import { SelectControl, AdvancedNumberControl } from '../../components';
 import MaxiModal from '../../editor/library/modal';
 
 /**
  * ImageShape
  */
 const ImageShape = props => {
-	const { onChange, onSelect, onRemove, breakpoint, icon } = props;
+	const { onChange, breakpoint, icon } = props;
 
 	const shapeSize = props[`image-shape-size-${breakpoint}`];
+	const shapeScale = props[`image-shape-scale-${breakpoint}`];
+	const shapePosition = props[`image-shape-position-${breakpoint}`];
 
 	let newIcon = icon;
 
 	const changeIcon = (attr, value) => {
-		console.log(`value ${value}`);
 		switch (attr) {
 			case 'size': {
 				const oldPreserveAspectRatio = icon
 					.split('preserveaspectratio="')
 					.pop()
 					.split('"')[0];
-				let newPreserveAspectRatio = '';
+				let newPreserveAspectRatio;
 				value === 'fill'
 					? (newPreserveAspectRatio = `${oldPreserveAspectRatio} slice`)
 					: (newPreserveAspectRatio = `${oldPreserveAspectRatio} meet`);
@@ -45,6 +37,32 @@ const ImageShape = props => {
 					.replaceAll(' meet', '')
 					.replaceAll(' slice', '')
 					.replace(oldPreserveAspectRatio, newPreserveAspectRatio);
+				return newIcon;
+			}
+			case 'scale': {
+				const oldViewBox = icon.split('viewBox="').pop().split('"')[0];
+				const newViewBoxValues = [0, 0, 36.1 / value, 36.1 / value];
+				const newViewBox = newViewBoxValues.join(' ');
+				newIcon = icon.replace(oldViewBox, newViewBox);
+				return newIcon;
+			}
+			case 'position': {
+				const oldPreserveAspectRatio = icon
+					.split('preserveaspectratio="')
+					.pop()
+					.split('"')[0];
+				let newPreserveAspectRatio;
+				if (oldPreserveAspectRatio.includes('slice'))
+					newPreserveAspectRatio = ' slice';
+				if (oldPreserveAspectRatio.includes('meet'))
+					newPreserveAspectRatio = ' meet';
+
+				newPreserveAspectRatio = value + newPreserveAspectRatio;
+
+				newIcon = icon.replace(
+					oldPreserveAspectRatio,
+					newPreserveAspectRatio
+				);
 				return newIcon;
 			}
 			default:
@@ -56,33 +74,108 @@ const ImageShape = props => {
 		<>
 			<MaxiModal
 				type='image-shape'
-				onSelect={obj => onSelect(obj)}
+				onSelect={obj => onChange(obj)}
 				onRemove={obj => {
-					onRemove(obj);
+					onChange(obj);
 				}}
 				icon={icon}
 			/>
-			{!isNil(icon) && (
-				<SelectControl
-					label={__('Size', 'maxi-blocks')}
-					value={shapeSize || ''}
-					options={[
-						{
-							label: __('Fit', 'maxi-blocks'),
-							value: '',
-						},
-						{
-							label: __('Fill', 'maxi-blocks'),
-							value: 'fill',
-						},
-					]}
-					onChange={shapeSize =>
-						onChange({
-							[`image-shape-size-${breakpoint}`]: shapeSize,
-							SVGElement: changeIcon('size', shapeSize),
-						})
-					}
-				/>
+			{icon && (
+				<>
+					<SelectControl
+						label={__('Size', 'maxi-blocks')}
+						value={shapeSize || ''}
+						options={[
+							{
+								label: __('Fit', 'maxi-blocks'),
+								value: '',
+							},
+							{
+								label: __('Fill', 'maxi-blocks'),
+								value: 'fill',
+							},
+						]}
+						onChange={shapeSize =>
+							onChange({
+								[`image-shape-size-${breakpoint}`]: shapeSize,
+								SVGElement: changeIcon('size', shapeSize),
+							})
+						}
+					/>
+					<AdvancedNumberControl
+						label={__('Scale', 'maxi-blocks')}
+						value={shapeScale || 1}
+						min={0}
+						max={5}
+						step={0.01}
+						initialPosition={1}
+						onChangeValue={val => {
+							onChange({
+								[`image-shape-scale-${breakpoint}`]:
+									val !== undefined && val !== '' ? val : '',
+								SVGElement: changeIcon('scale', val),
+							});
+						}}
+						onReset={() =>
+							onChange({
+								[`image-shape-scale-${breakpoint}`]: 1,
+								SVGElement: changeIcon('scale', 1),
+							})
+						}
+					/>
+					<SelectControl
+						label={__('Position', 'maxi-blocks')}
+						value={shapePosition || 'xMidYMid'}
+						options={[
+							{
+								label: __('Center Center', 'maxi-blocks'),
+								value: 'xMidYMid',
+							},
+							{
+								label: __('Left Center', 'maxi-blocks'),
+								value: 'xMinYMid',
+							},
+							{
+								label: __('Right Center', 'maxi-blocks'),
+								value: 'xMaxYMid',
+							},
+							{
+								label: __('Center Top', 'maxi-blocks'),
+								value: 'xMidYMax',
+							},
+							{
+								label: __('Center Bottom', 'maxi-blocks'),
+								value: 'xMidYMin',
+							},
+							{
+								label: __('Left Bottom', 'maxi-blocks'),
+								value: 'xMinYMin',
+							},
+							{
+								label: __('Right Bottom', 'maxi-blocks'),
+								value: 'xMaxYMin',
+							},
+							{
+								label: __('Left Top', 'maxi-blocks'),
+								value: 'xMinYMax',
+							},
+							{
+								label: __('Right Top', 'maxi-blocks'),
+								value: 'xMaxYMax',
+							},
+						]}
+						onChange={shapePosition =>
+							onChange({
+								[`image-shape-position-${breakpoint}`]:
+									shapePosition,
+								SVGElement: changeIcon(
+									'position',
+									shapePosition
+								),
+							})
+						}
+					/>
+				</>
 			)}
 		</>
 	);
--- src/extensions/styles/defaults/imageShape.js
+++ src/extensions/styles/defaults/imageShape.js
@@ -21,6 +21,50 @@ const imageShape = {
 	'image-shape-size-xs': {
 		type: 'string',
 	},
+	'image-shape-scale-general': {
+		type: 'number',
+		default: 1,
+	},
+	'image-shape-scale-xxl': {
+		type: 'number',
+	},
+	'image-shape-scale-xl': {
+		type: 'number',
+	},
+	'image-shape-scale-l': {
+		type: 'number',
+	},
+	'image-shape-scale-m': {
+		type: 'number',
+	},
+	'image-shape-scale-s': {
+		type: 'number',
+	},
+	'image-shape-scale-xs': {
+		type: 'number',
+	},
+	'image-shape-position-general': {
+		type: 'string',
+		default: '',
+	},
+	'image-shape-position-xxl': {
+		type: 'string',
+	},
+	'image-shape-position-xl': {
+		type: 'string',
+	},
+	'image-shape-position-l': {
+		type: 'string',
+	},
+	'image-shape-position-m': {
+		type: 'string',
+	},
+	'image-shape-position-s': {
+		type: 'string',
+	},
+	'image-shape-position-xs': {
+		type: 'string',
+	},
 };
 
 export default imageShape;

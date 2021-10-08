ec86e59f83851e9fc22485ff7b34de6a046839ef
Svitlana <2401618+elzadj@users.noreply.github.com>
2021-10-08 15:54:32 +0300

    Add flip for Image - Shape

--- src/extensions/styles/helpers/getImageShapeStyles.js
+++ src/extensions/styles/helpers/getImageShapeStyles.js
@@ -6,7 +6,7 @@ const breakpoints = ['general', 'xxl', 'xl', 'l', 'm', 's', 'xs'];
 /**
  * External dependencies
  */
-import { isNumber, isString, isEmpty } from 'lodash';
+import { isNumber, isEmpty } from 'lodash';
 
 /**
  * Internal dependencies
@@ -34,6 +34,18 @@ const getImageShapeStyles = (target = 'svg', obj) => {
 			obj
 		);
 
+		const flipX = getLastBreakpointAttribute(
+			'image-shape-flip-x',
+			breakpoint,
+			obj
+		);
+
+		const flipY = getLastBreakpointAttribute(
+			'image-shape-flip-y',
+			breakpoint,
+			obj
+		);
+
 		if (isNumber(scale)) {
 			if (target === 'svg') transformString += `scale(${scale / 100}) `;
 			if (target === 'image') transformString += `scale(${100 / scale}) `;
@@ -42,6 +54,14 @@ const getImageShapeStyles = (target = 'svg', obj) => {
 		if (isNumber(rotate)) {
 			if (target === 'svg') transformString += `rotate(${rotate}deg) `;
 			if (target === 'image') transformString += `rotate(-${rotate}deg) `;
+		}
+
+		if (flipX) {
+			transformString += 'scaleX(-1) ';
+		}
+
+		if (flipY) {
+			transformString += 'scaleY(-1) ';
 		}
 
 		const transformObj = {

d721d4367915304398da2fa7e7982e1f11e7420f
Alejandro9kgm <alejandro9kgm@gmail.com>
2021-11-08 10:47:01 +0100

    update test

--- e2e-tests/components/loader-control/index.spec.js
+++ e2e-tests/components/loader-control/index.spec.js
@@ -1,73 +1,47 @@
 /**
  * WordPress dependencies
  */
-import { createNewPost, insertBlock } from '@wordpress/e2e-test-utils';
+import {
+	createNewPost,
+	insertBlock,
+	pressKeyTimes,
+} from '@wordpress/e2e-test-utils';
 /**
  * Interactive dependencies
  */
-import { getBlockAttributes, openSidebarTab, modalMock } from '../../utils';
+import {
+	getBlockAttributes,
+	openSidebarTab,
+	addBackgroundLayer,
+} from '../../utils';
 // this test will be corrected with the update of the background-control tests
 describe('LoaderControl', () => {
 	it('Check loader control', async () => {
 		await createNewPost();
 		await insertBlock('Group Maxi');
-		const accordionPanel = await openSidebarTab(
-			page,
-			'style',
-			'background'
+		await openSidebarTab(page, 'style', 'background layer');
+		await addBackgroundLayer(page, 'color');
+
+		// change color
+		await page.$$eval(
+			'.maxi-background-layer__content .maxi-sc-color-palette__box',
+			colorPalette => colorPalette[4].click()
 		);
-
-		// add color layer
-		await accordionPanel.$$eval(
-			'.maxi-tabs-content .maxi-background-control .maxi-base-control label',
-			selectLayers => selectLayers[1].click()
-		);
-
-		const selectLayer = await accordionPanel.$(
-			'.maxi-tabs-content .maxi-background-control .maxi-loader-control .maxi-base-control__field select'
-		);
-
-		const addNewLayer = await accordionPanel.$(
-			'.maxi-tabs-content .maxi-background-control .maxi-loader-control button'
-		);
-
-		// add layers
-		await selectLayer.select('color');
-		await addNewLayer.click();
-
-		await selectLayer.select('image');
-		await addNewLayer.click();
-
-		await selectLayer.select('video');
-		await addNewLayer.click();
-		await accordionPanel.$$eval(
-			'.maxi-background-layers_options .maxi-background-layer span',
-			select => select[12].click()
-		);
-
+		// opacity
 		await page.$eval(
-			'.maxi-background-layer__content .maxi-text-control input',
-			select => select.focus()
+			'.maxi-background-control .maxi-advanced-number-control input',
+			opacity => opacity.focus()
 		);
-		await page.keyboard.type('https://youtu.be/hM7Eh0gGNKA');
-
-		await selectLayer.select('gradient');
-		await addNewLayer.click();
-
-		debugger;
-		await selectLayer.select('shape');
-		await addNewLayer.click();
-		await accordionPanel.$$eval(
-			'.maxi-background-layers_options .maxi-background-layer span',
-			select => select[24].click()
+		await pressKeyTimes('Backspace', '3');
+		await page.keyboard.type('45');
+		// clip-path
+		await page.$eval(
+			'.maxi-clip-path-control .maxi-toggle-switch__toggle input',
+			input => input.click()
 		);
-
-		/* await accordionPanel.$$eval(
-			'.maxi-settingstab-control .maxi-library-modal__action-section__buttons button',
-			click => click[0].click()
-		); */
-
-		await modalMock(page, { type: 'background-layers' });
+		await page.$$eval('.clip-path-defaults button', buttons =>
+			buttons[3].click()
+		);
 
 		const expectBackgroundLayers = await getBlockAttributes();
 		const allLayers = expectBackgroundLayers['background-layers'];

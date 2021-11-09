aa205f68b683acddedad7e0db08ff3c675d159c7
Alejandro9kgm <alejandro9kgm@gmail.com>
2021-11-09 12:07:08 +0100

    update loader-control test

--- e2e-tests/components/loader-control/index.spec.js
+++ e2e-tests/components/loader-control/index.spec.js
@@ -1,11 +1,7 @@
 /**
  * WordPress dependencies
  */
-import {
-	createNewPost,
-	insertBlock,
-	pressKeyTimes,
-} from '@wordpress/e2e-test-utils';
+import { createNewPost, insertBlock } from '@wordpress/e2e-test-utils';
 /**
  * Interactive dependencies
  */
@@ -20,32 +16,30 @@ describe('LoaderControl', () => {
 		await createNewPost();
 		await insertBlock('Group Maxi');
 		await openSidebarTab(page, 'style', 'background layer');
+
 		await addBackgroundLayer(page, 'color');
+		const backgroundLayersColor = await getBlockAttributes();
+		const colorLayers = backgroundLayersColor['background-layers'];
+		expect(colorLayers).toMatchSnapshot();
 
-		// change color
-		await page.$$eval(
-			'.maxi-background-layer__content .maxi-sc-color-palette__box',
-			colorPalette => colorPalette[4].click()
-		);
-		// opacity
-		await page.$eval(
-			'.maxi-background-control .maxi-advanced-number-control input',
-			opacity => opacity.focus()
-		);
-		await pressKeyTimes('Backspace', '3');
-		await page.keyboard.type('45');
-		// clip-path
-		await page.$eval(
-			'.maxi-clip-path-control .maxi-toggle-switch__toggle input',
-			input => input.click()
-		);
-		await page.$$eval('.clip-path-defaults button', buttons =>
-			buttons[3].click()
-		);
+		await addBackgroundLayer(page, 'image');
+		const backgroundLayersImage = await getBlockAttributes();
+		const imageLayers = backgroundLayersImage['background-layers'];
+		expect(imageLayers).toMatchSnapshot();
 
-		const expectBackgroundLayers = await getBlockAttributes();
-		const allLayers = expectBackgroundLayers['background-layers'];
+		await addBackgroundLayer(page, 'video');
+		const backgroundLayersVideo = await getBlockAttributes();
+		const videoLayers = backgroundLayersVideo['background-layers'];
+		expect(videoLayers).toMatchSnapshot();
 
-		expect(allLayers).toMatchSnapshot();
+		await addBackgroundLayer(page, 'gradient');
+		const backgroundLayersGradient = await getBlockAttributes();
+		const gradientLayers = backgroundLayersGradient['background-layers'];
+		expect(gradientLayers).toMatchSnapshot();
+
+		await addBackgroundLayer(page, 'shape');
+		const backgroundLayersShape = await getBlockAttributes();
+		const shapeLayers = backgroundLayersShape['background-layers'];
+		expect(shapeLayers).toMatchSnapshot();
 	});
 });

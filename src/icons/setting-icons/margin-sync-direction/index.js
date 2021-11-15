5130be3d2a7a1a5ffc634dac04268aa1e81510e2
Javad <65819265+javadsamiee@users.noreply.github.com>
2021-11-15 18:00:45 +0330

    Issue 2245 (#2322)
    * Implement #2245 structure
    
    * Some fix
    
    * Some minor fix
    
    * Add padding and margin icons
    
    * Fix Transition duration min value
    
    * Minor fix
    
    * Fix group button when they have label
    
    * Fix lables
    
    * Fix active color blue for icons and remove default axis style
    
    * Minor style fix
    
    * Replace checkbox with ToggleSwitch
    
    * Fix labels
    
    * Fix accessibility
    
    * Fix active blue
    
    * Minor fix for ButtonGroupControl bottom spacing
    
    * Some fix
    
    * Hide number controller when value is Auto
    
    * Fix space of button shortcuts
    
    * Fix axisControl rangeSider class fix
    
    Co-authored-by: Josh <jjuancap@gmail.com>

---
+++ src/icons/setting-icons/margin-sync-direction/index.js
@@ -0,0 +1,28 @@
+/**
+ * WordPress dependencies
+ */
+import { SVG, Path } from '@wordpress/primitives';
+
+const marginSyncDirection = (
+	<SVG xmlns='http://www.w3.org/2000/svg' viewBox='0 0 19.5 19.5'>
+		<Path
+			fill='none'
+			stroke='#081219'
+			strokeLinecap='round'
+			strokeLinejoin='round'
+			strokeWidth='1.5'
+			d='M12.932 15.759H6.889m8.87-9.161v6.142m-8.87-9h6.043m-9.191 9V6.602'
+		/>
+		<Path
+			fill='none'
+			stroke='#081219'
+			strokeDasharray='3 3 3 3 3'
+			strokeLinecap='round'
+			strokeLinejoin='round'
+			strokeWidth='1.5'
+			d='M18.75 18.75h-18v-18h18Z'
+		/>
+	</SVG>
+);
+
+export default marginSyncDirection;

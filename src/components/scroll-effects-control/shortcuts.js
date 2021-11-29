3adc7dc54350b09c5f2cc65f12b9b90a15c9081d
Svitlana <2401618+elzadj@users.noreply.github.com>
2021-11-29 17:56:18 +0200

    Add local shortcut

--- src/components/scroll-effects-control/shortcuts.js
+++ src/components/scroll-effects-control/shortcuts.js
@@ -30,3 +30,19 @@ export const shortcut1 = {
 };
 export const shortcut2 = {};
 export const shortcut3 = {};
+
+export const rotate = {
+	shortcut1: {
+		'motion-delay-rotate-general': 0,
+		'motion-easing-rotate-general': 'ease',
+		'motion-rotate-end-rotate-general': 360,
+		'motion-rotate-mid-rotate-general': 180,
+		'motion-rotate-start-rotate-general': 0,
+		'motion-speed-rotate-general': 500,
+		'motion-status-reverse-rotate-general': true,
+		'motion-status-rotate-general': true,
+		'motion-viewport-bottom-rotate-general': 'bottom',
+		'motion-viewport-middle-rotate-general': 'mid',
+		'motion-viewport-top-rotate-general': 'top',
+	},
+};

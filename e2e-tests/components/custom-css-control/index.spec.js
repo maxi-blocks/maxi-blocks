84b8beaee98419b8809b3871159ea206ccb19d53
Alejandro9kgm <alejandro9kgm@gmail.com>
2021-12-09 09:33:55 +0100

    minor fix

--- e2e-tests/components/custom-css-control/index.spec.js
+++ e2e-tests/components/custom-css-control/index.spec.js
@@ -12,26 +12,7 @@ describe('Custom-Css-Control', () => {
 	it('Checking the custom-css control in Group Maxi', async () => {
 		await createNewPost();
 		await insertBlock('Group Maxi');
-
 		await addCustomCSS(page);
-
-		/* expect(cssValidate).toStrictEqual('Valid');
-		expect(css).toMatchSnapshot();
-
-		const cssHover = await page.$$eval(
-			'.maxi-additional__css .w-tc-editor textarea',
-			content => content[1].value
-		);
-
-		const cssHoverValidate = await page.$eval(
-			'#maxi-additional__css-error-text__group_on_hover',
-			valid => valid.innerText
-		);
-
-		await page.waitForTimeout(1000);
-		expect(cssHoverValidate).toStrictEqual('Valid');
-
-		expect(cssHover).toMatchSnapshot(); */
 
 		expect(await getBlockStyle(page)).toMatchSnapshot();
 	});

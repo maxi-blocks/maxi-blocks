336e44eaf4a28f3d40381a3342ebbda952fcd095
Svitlana <2401618+elzadj@users.noreply.github.com>
2021-12-16 11:24:25 +0200

    For tests

--- src/components/typography-control/index.js
+++ src/components/typography-control/index.js
@@ -749,10 +749,10 @@ const TypographyControl = withFormatValue(props => {
 			<ResponsiveTabsControl
 				className='maxi-typography-control__text-options-tabs'
 				breakpoint={breakpoint}
-				active={
-					!styleCards &&
-					getActiveAttributes(typography, 'breakpoints')
-				}
+				// active={
+				// 	!styleCards &&
+				// 	getActiveAttributes(typography, 'breakpoints')
+				// }
 			>
 				<TextOptions
 					getValue={getValue}
@@ -894,7 +894,7 @@ const TypographyControl = withFormatValue(props => {
 					isHover={isHover}
 					clientId={clientId}
 					getOpacityValue={getOpacityValue}
-					active={getActiveAttributes(typography, 'link')}
+					// active={getActiveAttributes(typography, 'link')}
 				/>
 			)}
 		</div>

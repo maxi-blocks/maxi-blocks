b2d2801e836c69ed7154a598216d1ddb32b08795
Josh <jjuancap@gmail.com>
2021-12-16 20:37:19 +0100

    Fixes Text Maxi tests

--- src/blocks/text-maxi/edit.js
+++ src/blocks/text-maxi/edit.js
@@ -2,7 +2,6 @@
 /**
  * WordPress dependencies
  */
-import { __ } from '@wordpress/i18n';
 import { compose } from '@wordpress/compose';
 import { createBlock } from '@wordpress/blocks';
 import { withSelect, dispatch } from '@wordpress/data';
@@ -156,6 +155,7 @@ class edit extends MaxiBlockComponent {
 						onReplace={onReplace}
 						onMerge={forward => onMerge(this.props, forward)}
 						__unstableEmbedURLOnPaste
+						preserveWhiteSpace
 					>
 						{onChangeRichText}
 					</RichText>

df9e9e6d53c4d5cf7777f921b5d8c071d671b118
Josh <jjuancap@gmail.com>
2021-12-16 20:37:19 +0100

    Fixes Text Maxi tests

--- e2e-tests/utils/index.js
+++ e2e-tests/utils/index.js
@@ -10,3 +10,4 @@ export { default as openPreviewPage } from './openPreviewPage';
 export { default as openSidebarTab } from './openSidebarTab';
 export { default as removeBackgroundLayers } from './removeBackgroundLayers';
 export { default as changeResponsive } from './changeResponsive';
+export { default as setAttributes } from './setAttributes';

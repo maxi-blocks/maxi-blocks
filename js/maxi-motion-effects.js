672596dac661022bd8fcbe8d25592803f72d7008
Svitlana <2401618+elzadj@users.noreply.github.com>
2021-12-23 14:57:00 +0200

    Fix bugs in number counter effects

--- js/maxi-motion-effects.js
+++ js/maxi-motion-effects.js
@@ -192,7 +192,6 @@ class Parallax {
 const motions = () => {
 	const motionElements = document.querySelectorAll('.maxi-motion-effect');
 	motionElements.forEach(elem => {
-		console.log(maxi_custom_data.custom_data);
 		if (!maxi_custom_data.custom_data) return;
 
 		const motionID = elem.id;
@@ -203,86 +202,6 @@ const motions = () => {
 				: null;
 
 		if (motionData !== null) {
-			// Number Counter
-			if ('number-counter-status' in motionData) {
-				const numberCounterElem = document.querySelector(
-					`#${motionID} .maxi-number-counter__box`
-				);
-				const numberCounterElemText = document.querySelector(
-					`#${motionID} .maxi-number-counter__box .maxi-number-counter__box__text`
-				);
-				const numberCounterElemCircle = document.querySelector(
-					`#${motionID} .maxi-number-counter__box .maxi-number-counter__box__circle`
-				);
-
-				const radius = 90;
-				const circumference = 2 * Math.PI * radius;
-				const {
-					'number-counter-start': motionCounterStart,
-					'number-counter-end': motionCounterEnd,
-					'number-counter-duration': motionCounterDuration,
-					'number-counter-percentage-sign-status': usePercentage,
-					'number-counter-start-animation': startAnimation,
-				} = motionData;
-				const startCountValue = Math.ceil(
-					(motionCounterStart * 360) / 100
-				);
-				const endCountValue = Math.ceil((motionCounterEnd * 360) / 100);
-
-				const frameDuration =
-					(1 /
-						((endCountValue - startCountValue) /
-							motionCounterDuration)) *
-					1000;
-
-				let count = startCountValue;
-
-				const startCounter = () => {
-					const interval = setInterval(() => {
-						count += 1;
-
-						if (count >= endCountValue) {
-							count = endCountValue;
-							clearInterval(interval);
-						}
-
-						let newInnerHTML = `${parseInt((count / 360) * 100)}`;
-
-						if (usePercentage) {
-							const percentageNode =
-								numberCounterElemText.nodeName === 'SPAN'
-									? '<sup>%</sup>'
-									: '<tspan baselineShift="super">%</tspan>';
-
-							newInnerHTML += percentageNode;
-						}
-
-						numberCounterElemText.innerHTML = newInnerHTML;
-
-						numberCounterElemCircle &&
-							numberCounterElemCircle.setAttribute(
-								'stroke-dasharray',
-								`${parseInt(
-									(count / 360) * circumference
-								)} ${circumference}`
-							);
-					}, frameDuration);
-				};
-
-				if (startAnimation === 'view-scroll') {
-					// eslint-disable-next-line no-unused-vars, no-undef
-					const waypoint = new Waypoint({
-						element: numberCounterElem,
-						handler() {
-							startCounter();
-						},
-						offset: '100%',
-					});
-				} else {
-					startCounter();
-				}
-			}
-
 			// Hover
 			if (
 				'hover-basic-effect-type' in motionData &&

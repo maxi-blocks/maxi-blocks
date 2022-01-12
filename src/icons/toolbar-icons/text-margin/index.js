3a147615055eeaf745de90358599928c47d71190
Rustam Samsonyan <83059229+Rustam198804@users.noreply.github.com>
2022-01-12 14:08:54 +0400

    Fix svg codes

--- src/icons/toolbar-icons/text-margin/index.js
+++ src/icons/toolbar-icons/text-margin/index.js
@@ -4,48 +4,22 @@
 import { SVG, Path } from '@wordpress/primitives';
 
 const toolbarTextMargin = (
-	<SVG width='16.09' height='16' viewBox='0 0 16.09 16'>
-		<g id='Group_962' data-name='Group 962' transform='translate(0 0)'>
-			<g
-				id='Group_874'
-				data-name='Group 874'
-				transform='translate(0 0)'
-				clipPath='url(#clip-path)'
-			>
+	<SVG width={16.09} height={16} viewBox='0 0 16.09 16'>
+		<g data-name='Group 962'>
+			<g data-name='Group 874'>
 				<Path
-					d='M12.236,13.936l2.171,2.714,2.171-2.714'
+					d='M12.236 13.936l2.171 2.714 2.171-2.714'
 					transform='translate(-1.166 -1.328)'
-					strokeLinecap='round'
-					strokeLinejoin='round'
-					strokeWidth='1.5'
 				/>
-				<line
-					y2='5.037'
-					transform='translate(13.241 10.285)'
-					strokeLinecap='round'
-					strokeLinejoin='round'
-					strokeWidth='1.5'
-				/>
+				<Path transform='translate(13.241 10.285)' d='M0 0L0 5.037' />
 				<Path
-					d='M.75,13.722l4.327-10.7L9.4,13.722m-7-4.091H7.594'
-					transform='translate(-0.071 -0.288)'
-					strokeLinecap='round'
-					strokeLinejoin='round'
-					strokeWidth='1.5'
+					d='M.75 13.722l4.327-10.7 4.323 10.7m-7-4.091h5.194'
+					transform='translate(-.071 -.288)'
 				/>
-				<line
-					y2='4.914'
-					transform='translate(13.241 0.679)'
-					strokeLinecap='round'
-					strokeLinejoin='round'
-					strokeWidth='1.5'
-				/>
+				<Path transform='translate(13.241 .679)' d='M0 0L0 4.914' />
 				<Path
-					d='M12.236,3.464,14.408.75l2.171,2.714'
-					transform='translate(-1.166 -0.071)'
-					strokeLinecap='round'
-					strokeLinejoin='round'
-					strokeWidth='1.5'
+					d='M12.236 3.464L14.408.75l2.171 2.714'
+					transform='translate(-1.166 -.071)'
 				/>
 			</g>
 		</g>

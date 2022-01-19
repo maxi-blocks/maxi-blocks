b4eb390c4f47be4a66de67b76170f06feb873ef0
Josh <jjuancap@gmail.com>
2022-01-19 12:17:56 +0100

    Test for SC values on github

--- src/extensions/style-cards/getPaletteColor.js
+++ src/extensions/style-cards/getPaletteColor.js
@@ -4,17 +4,38 @@
 import { getBlockStyle } from '../styles';
 import getActiveStyleCard from './getActiveStyleCard';
 
+import { isEmpty } from 'lodash';
+
 const getPaletteColor = ({ clientId, color, blockStyle }) => {
 	const SCValue = getActiveStyleCard().value;
 	const parentBlockStyle = blockStyle ?? getBlockStyle(clientId);
 
-	return (
-		SCValue?.[parentBlockStyle].styleCard?.color?.[color] ||
-		SCValue?.[parentBlockStyle].defaultStyleCard?.color?.[color] ||
-		getComputedStyle(document.documentElement).getPropertyValue(
-			`--maxi-${parentBlockStyle}-color-${color}`
+	let string = '';
+	if (SCValue) string += 'SCValues doesnt exist || ';
+	if (SCValue?.[parentBlockStyle].defaultStyleCard?.color?.[color])
+		string += 'colour on SCValues doesnt exist || ';
+	if (
+		isEmpty(
+			getComputedStyle(document.documentElement).getPropertyValue(
+				`--maxi-${parentBlockStyle}-color-${color}`
+			)
 		)
-	);
+	) {
+		string += 'colour variable on root is empty and may not exist: ';
+		string += document.querySelector(
+			'#maxi-blocks-sc-vars-inline-css'
+		).innerText;
+	}
+
+	return string;
+
+	// return (
+	// 	SCValue?.[parentBlockStyle].styleCard?.color?.[color] ||
+	// 	SCValue?.[parentBlockStyle].defaultStyleCard?.color?.[color] ||
+	// 	getComputedStyle(document.documentElement).getPropertyValue(
+	// 		`--maxi-${parentBlockStyle}-color-${color}`
+	// 	)
+	// );
 };
 
 export default getPaletteColor;

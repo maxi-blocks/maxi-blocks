8ab7472a5c9291e3959970fa63c7b652d713ceeb
Svitlana <2401618+elzadj@users.noreply.github.com>
2022-01-21 14:23:24 +0200

    Fix uninstall php

--- uninstall.php
+++ uninstall.php
@@ -3,6 +3,6 @@
 }
 
 global $wpdb;
-$wpdb->query("SELECT CONCAT('DROP TABLE IF EXISTS `', TABLE_NAME,'`;') 
-FROM INFORMATION_SCHEMA.TABLES 
-WHERE TABLE_NAME LIKE 'maxi_blocks_';");
+$wpdb->query("DROP TABLE IF EXISTS {$wpdb->prefix}maxi_blocks_general");
+$wpdb->query("DROP TABLE IF EXISTS {$wpdb->prefix}maxi_blocks_custom_data");
+$wpdb->query("DROP TABLE IF EXISTS {$wpdb->prefix}maxi_blocks_styles");

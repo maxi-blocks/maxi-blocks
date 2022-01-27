acbcc8ee4fd715113f4dc4563e8ed1aad3ca552d
bilal <bilal89dif@gmail.com>
2022-01-27 13:15:23 +0100

    minor fixes

--- src/components/border-control/defaults.js
+++ src/components/border-control/defaults.js
@@ -5,15 +5,17 @@ import {
 
 import { prefixAttributesCreator } from '../../extensions/styles';
 
-const getDefaultBorderWidth = (
+const getBorderDefault = (
 	prefix,
-	{ BorderTopWidth, BorderRightWidth, BorderBottomWidth, BorderLeftWidth }
+	{ borderTopWidth, borderRightWidth, borderBottomWidth, borderLeftWidth }
 ) => {
 	return {
-		[`${prefix}border-top-width`]: BorderTopWidth || 2,
-		[`${prefix}border-right-width`]: BorderRightWidth || 2,
-		[`${prefix}border-bottom-width`]: BorderBottomWidth || 2,
-		[`${prefix}border-left-width`]: BorderLeftWidth || 2,
+		[`${prefix}border-sync-width`]: 'all',
+		[`${prefix}border-unit-width`]: 'px',
+		[`${prefix}border-top-width`]: borderTopWidth || 2,
+		[`${prefix}border-right-width`]: borderRightWidth || 2,
+		[`${prefix}border-bottom-width`]: borderBottomWidth || 2,
+		[`${prefix}border-left-width`]: borderLeftWidth || 2,
 	};
 };
 
@@ -67,9 +69,7 @@ export const borderSolid = (prefix, defaultWidthValue) => {
 	let response = {};
 	response = {
 		[`${prefix}border-style`]: 'solid',
-		[`${prefix}border-sync-width`]: 'all',
-		[`${prefix}border-unit-width`]: 'px',
-		...getDefaultBorderWidth(prefix, defaultWidthValue),
+		...getBorderDefault(prefix, defaultWidthValue),
 	};
 	return response;
 };
@@ -78,9 +78,7 @@ export const borderDashed = (prefix, defaultWidthValue) => {
 	let response = {};
 	response = {
 		[`${prefix}border-style`]: 'dashed',
-		[`${prefix}border-sync-width`]: 'all',
-		[`${prefix}border-unit-width`]: 'px',
-		...getDefaultBorderWidth(prefix, defaultWidthValue),
+		...getBorderDefault(prefix, defaultWidthValue),
 	};
 	return response;
 };
@@ -89,9 +87,7 @@ export const borderDotted = (prefix, defaultWidthValue) => {
 	let response = {};
 	response = {
 		[`${prefix}border-style`]: 'dotted',
-		[`${prefix}border-sync-width`]: 'all',
-		[`${prefix}border-unit-width`]: 'px',
-		...getDefaultBorderWidth(prefix, defaultWidthValue),
+		...getBorderDefault(prefix, defaultWidthValue),
 	};
 	return response;
 };

f9e6d0c6a92a4eb68b95e6fb0749c259197b8aaf
Rustam Samsonyan <83059229+Rustam198804@users.noreply.github.com>
2022-02-08 17:58:06 +0400

    Change icons for SC and cloud lib

--- src/icons/general-icons/style-cards-menu/index.js
+++ src/icons/general-icons/style-cards-menu/index.js
@@ -4,78 +4,88 @@
 import { SVG, Path } from '@wordpress/primitives';
 
 const styleCardMenu = (
-	<SVG width={19.13} height={20.036} viewBox='0 0 19.13 20.036'>
-		<g data-name='Group 1011' clipPath='url(#a)'>
-			<Path
-				d='M8.586.75l-.094 9.608 7.276 1.337zM4.7 10.98c.062-.031 3.793-.622 3.793-.622L8.586.75z'
-				fill='#ff4a17'
-			/>
-			<Path
-				d='M8.586.75l-.094 9.608 7.276 1.337L8.586.75M4.7 10.98c.062-.031 3.793-.622 3.793-.622L8.586.75z'
-				fill='none'
-				stroke='#081219'
-				strokeLinecap='round'
-				strokeLinejoin='round'
-				strokeWidth={1.5}
-			/>
-			<Path
-				d='M15.457 3.02a1.5 1.5 0 00-.684.342c-.156.093-.28.249-.435.342-.529-.466-.871-.715-1.089-.715'
-				fill='none'
-				stroke='#081219'
-				strokeLinecap='round'
-				strokeLinejoin='round'
-				strokeWidth={1.5}
-			/>
-			<Path
-				d='M18.38 5.569a1.351 1.351 0 00-.684.373 4.578 4.578 0 00-.4.342c-.5-.5-.871-.746-1.089-.746'
-				fill='none'
-				stroke='#081219'
-				strokeLinecap='round'
-				strokeLinejoin='round'
-				strokeWidth={1.5}
-			/>
-			<Path
-				d='M8.492 10.358v4.011l4.819.715-.5 1.586a7.429 7.429 0 01-1.493 2.581'
-				fill='none'
-				stroke='#081219'
-				strokeLinecap='round'
-				strokeLinejoin='round'
-				strokeWidth={1.5}
-			/>
-			<Path
-				d='M4.326 4.606c-.249-.031-.84.5-1.088.746-.5-.5-.871-.746-1.089-.746'
-				fill='none'
-				stroke='#081219'
-				strokeLinecap='round'
-				strokeLinejoin='round'
-				strokeWidth={1.5}
-			/>
-			<Path
-				data-name='Line 675'
-				transform='translate(8.492 .75)'
-				fill='none'
-				stroke='#081219'
-				strokeLinecap='round'
-				strokeLinejoin='round'
-				strokeWidth={1.5}
-				d='M0 9.608L0.093 0'
-			/>
-			<Path
-				d='M5.632 19.126A8.638 8.638 0 014.3 16.7l-.5-1.648 4.7-.684v2.114'
-				fill='none'
-				stroke='#081219'
-				strokeLinecap='round'
-				strokeLinejoin='round'
-				strokeWidth={1.5}
-			/>
-			<Path
-				d='M.75 19.188A1.674 1.674 0 001.527 19a2.15 2.15 0 00.591-.466 1.617 1.617 0 002.7 0 1.715 1.715 0 002.674 0 1.779 1.779 0 002.736 0c.716 1.057 1.866.933 2.706.031a1.707 1.707 0 001.368.653 1.856 1.856 0 001.368-.653 1.689 1.689 0 002.7 0'
-				fill='none'
-				stroke='#081219'
-				strokeLinecap='round'
-				strokeLinejoin='round'
-				strokeWidth={1.5}
-			/>
+	<SVG width={18.539} height={19.43} viewBox='0 0 18.539 19.43'>
+		<g>
+			<g>
+				<Path
+					id='styleCardMenu_a'
+					d='M8.307.6l-.092 9.449 7.156 1.315zM4.484 10.661c.061-.031 3.731-.612 3.731-.612L8.307.6z'
+					fill='#ff4a17'
+				/>
+				<Path
+					id='styleCardMenu_b'
+					d='M8.307.6l-.092 9.449 7.156 1.315L8.307.6M4.484 10.661c.061-.031 3.731-.612 3.731-.612L8.307.6z'
+					fill='none'
+					stroke='#081219'
+					strokeLinecap='round'
+					strokeLinejoin='round'
+					strokeWidth={1.2}
+				/>
+				<Path
+					id='styleCardMenu_b'
+					d='M15.064 2.832a1.475 1.475 0 00-.673.336c-.153.092-.275.245-.428.337-.52-.459-.856-.7-1.07-.7'
+					fill='none'
+					stroke='#081219'
+					strokeLinecap='round'
+					strokeLinejoin='round'
+					strokeWidth={1.2}
+				/>
+				<Path
+					id='styleCardMenu_b'
+					d='M17.939 5.34a1.327 1.327 0 00-.673.367 6.348 6.348 0 00-.4.336c-.49-.489-.857-.734-1.071-.734'
+					fill='none'
+					stroke='#081219'
+					strokeLinecap='round'
+					strokeLinejoin='round'
+					strokeWidth={1.2}
+				/>
+				<Path
+					id='styleCardMenu_b'
+					d='M8.215 10.049v3.945l4.74.7-.49 1.56A7.3 7.3 0 0111 18.8'
+					fill='none'
+					stroke='#081219'
+					strokeLinecap='round'
+					strokeLinejoin='round'
+					strokeWidth={1.2}
+				/>
+				<Path
+					id='styleCardMenu_b'
+					d='M4.117 4.392c-.245-.031-.826.489-1.07.734-.49-.489-.857-.734-1.071-.734'
+					fill='none'
+					stroke='#081219'
+					strokeLinecap='round'
+					strokeLinejoin='round'
+					strokeWidth={1.2}
+				/>
+				<Path
+					id='styleCardMenu_b'
+					transform='translate(8.215 .6)'
+					fill='none'
+					stroke='#081219'
+					strokeLinecap='round'
+					strokeLinejoin='round'
+					strokeWidth={1.2}
+					d='M0 9.449L0.092 0'
+				/>
+				<Path
+					id='styleCardMenu_b'
+					d='M5.4 18.673a8.492 8.492 0 01-1.315-2.385L3.6 14.667l4.617-.673v2.08'
+					fill='none'
+					stroke='#081219'
+					strokeLinecap='round'
+					strokeLinejoin='round'
+					strokeWidth={1.2}
+				/>
+				<Path
+					id='styleCardMenu_b'
+					d='M.6 18.734a1.665 1.665 0 00.765-.183 2.168 2.168 0 00.581-.459 1.591 1.591 0 002.66 0 1.687 1.687 0 002.63 0 1.751 1.751 0 002.691 0 1.585 1.585 0 002.661.03 1.676 1.676 0 001.345.643 1.826 1.826 0 001.346-.643 1.661 1.661 0 002.66 0'
+					fill='none'
+					stroke='#081219'
+					strokeLinecap='round'
+					strokeLinejoin='round'
+					strokeWidth={1.2}
+				/>
+			</g>
 		</g>
 	</SVG>
 );

43ca5d4d558954bea987cda7a84fc133f6637e9d
Josh <jjuancap@gmail.com>
2022-02-09 11:56:06 +0100

    Fix test (again, lol!)

--- e2e-tests/blocks/text-maxi/index.spec.js
+++ e2e-tests/blocks/text-maxi/index.spec.js
@@ -225,7 +225,7 @@ describe('TextMaxi', () => {
 		expect(await getBlockStyle(page)).toMatchSnapshot();
 	});
 
-	it('Test Text Maxi toolbar Link in whole content, and being modifiable from the end', async () => {
+	it.only('Test Text Maxi toolbar Link in whole content, and being modifiable from the end', async () => {
 		await page.keyboard.type('Test Text Maxi', { delay: 100 });
 		await page.waitForTimeout(150);
 		await page.$eval('.toolbar-item__text-link', button => button.click());
@@ -254,7 +254,7 @@ describe('TextMaxi', () => {
 				await page.$eval('.toolbar-item__text-link', button =>
 					button.click()
 				);
-				await page.waitForTimeout(150);
+				await page.waitForTimeout(250);
 
 				await page.waitForSelector('a.components-external-link');
 			});

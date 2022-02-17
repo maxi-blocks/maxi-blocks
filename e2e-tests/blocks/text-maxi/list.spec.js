344164bfae4504e6302b0d07e39a5267a101520c
Alejandro9kgm <alejandro9kgm@gmail.com>
2022-02-17 10:07:00 +0100

    update list test

--- e2e-tests/blocks/text-maxi/list.spec.js
+++ e2e-tests/blocks/text-maxi/list.spec.js
@@ -2,7 +2,11 @@
 /**
  * WordPress dependencies
  */
-import { createNewPost, insertBlock } from '@wordpress/e2e-test-utils';
+import {
+	createNewPost,
+	insertBlock,
+	pressKeyWithModifier,
+} from '@wordpress/e2e-test-utils';
 
 /**
  * Internal dependencies
@@ -12,10 +16,11 @@ import {
 	openSidebarTab,
 	editAdvancedNumberControl,
 	getAttributes,
+	modalMock,
 } from '../../utils';
 
-describe('TextMaxi', () => {
-	it('Writes a sentence on Text Maxi', async () => {
+describe('List in Text-maxi', () => {
+	it('Use list in Text Maxi Organized', async () => {
 		await createNewPost();
 		await insertBlock('Text Maxi');
 		await page.keyboard.type('Testing Text Maxi List', { delay: 100 });
@@ -105,19 +110,6 @@ describe('TextMaxi', () => {
 			'sub'
 		);
 
-		// Type of list
-		const listType = await page.$(
-			'.maxi-image-inspector__list-type select'
-		);
-		await listType.select('ul');
-
-		expect(await getAttributes('typeOfList')).toStrictEqual('ul');
-
-		const listTypeOrganized = await page.$(
-			'.maxi-image-inspector__list-type select'
-		);
-		await listTypeOrganized.select('ol');
-
 		// Style
 		const style = await page.$$('.maxi-image-inspector__list-style select');
 		await style[1].select('armenian');
@@ -130,7 +122,94 @@ describe('TextMaxi', () => {
 			instance: await page.$('.maxi-image-inspector__list-start '),
 			newNumber: '78',
 		});
+
 		expect(await getAttributes('listStart')).toStrictEqual(78);
+
+		await editAdvancedNumberControl({
+			page,
+			instance: await page.$('.maxi-image-inspector__list-start '),
+			newNumber: '-44',
+		});
+
+		expect(await getAttributes('listStart')).toStrictEqual(4);
+
 		// Reverse order button
+		await page.$eval('.maxi-image-inspector__list-reverse input', input =>
+			input.click()
+		);
+
+		expect(await getBlockStyle(page)).toMatchSnapshot();
+
+		// Reverse order button
+		await page.$eval('.maxi-image-inspector__list-reverse input', input =>
+			input.click()
+		);
+
+		expect(await getBlockStyle(page)).toMatchSnapshot();
+	});
+
+	it('Use list in Text Maxi Unorganized', async () => {
+		// Select
+		// Type of list
+		const listType = await page.$(
+			'.maxi-image-inspector__list-type select'
+		);
+		await listType.select('ul');
+
+		expect(await getAttributes('typeOfList')).toStrictEqual('ul');
+
+		// style
+		const style = await page.$$('.maxi-image-inspector__list-style select');
+		await style[1].select('circle');
+
+		expect(await getBlockStyle(page)).toMatchSnapshot();
+
+		// Style custom
+		const styleCustom = await page.$$(
+			'.maxi-image-inspector__list-style select'
+		);
+		await styleCustom[1].select('custom');
+
+		await page.waitForTimeout(150);
+
+		const source = await page.$(
+			'.maxi-image-inspector__list-source-selector select'
+		);
+
+		await source.select('text');
+		await page.waitForTimeout(150);
+
+		await page.$eval(
+			'.maxi-image-inspector__list-source-text input',
+			input => input.focus()
+		);
+
+		await pressKeyWithModifier('primary', 'a');
+		await page.keyboard.type('test');
+
+		expect(await getAttributes('listStyleCustom')).toStrictEqual('test');
+
+		// source URL
+		/* await source.select('url');
+		await page.$eval(
+			'.maxi-image-inspector__list-source-text input',
+			input => input.focus()
+		);
+		await pressKeyWithModifier('primary', 'a');
+		await page.keyboard.type('http://placekitten.com/20/20');
+		await page.waitForTimeout(500);
+		debugger; 
+
+		 expect(await getAttributes('listStyleCustom')).toStrictEqual(
+			'http://placekitten.com/20/20'
+		); */
+
+		// source Icon
+
+		await source.select('icon');
+
+		await modalMock(page, { type: 'list-svg' });
+
+		expect(await getBlockStyle(page)).toMatchSnapshot();
 	});
 });

b73dd8afcb7608770608fbddd8821e74279f0a2e
Alejandro9kgm <alejandro9kgm@gmail.com>
2022-03-03 15:15:36 +0100

    update test

--- e2e-tests/blocks/Divider-maxi/index.spec.js
+++ e2e-tests/blocks/Divider-maxi/index.spec.js
@@ -6,7 +6,7 @@ import {
 	insertBlock,
 	getEditedPostContent,
 } from '@wordpress/e2e-test-utils';
-import { getBlockStyle } from '../../utils';
+import { getBlockStyle, openSidebarTab, changeResponsive } from '../../utils';
 
 describe('Divider Maxi', () => {
 	it('Divider Maxi does not break', async () => {
@@ -16,5 +16,68 @@ describe('Divider Maxi', () => {
 		expect(await getEditedPostContent()).toMatchSnapshot();
 
 		expect(await getBlockStyle(page)).toMatchSnapshot();
+	});
+
+	it('Check Divider alignment', async () => {
+		const accordionPanel = await openSidebarTab(page, 'style', 'alignment');
+
+		const alignmentSelectors = await accordionPanel.$$(
+			'.maxi-base-control select'
+		);
+
+		await alignmentSelectors[0].select('vertical');
+		await page.waitForTimeout(150);
+		await alignmentSelectors[1].select('flex-start');
+		await page.waitForTimeout(150);
+		await alignmentSelectors[2].select('flex-start');
+
+		expect(await getBlockStyle(page)).toMatchSnapshot();
+		debugger;
+		// responsive
+		// responsive S
+		await changeResponsive(page, 's');
+
+		await alignmentSelectors[0].select('horizontal');
+		await page.waitForTimeout(150);
+		await alignmentSelectors[1].select('flex-end');
+		await page.waitForTimeout(150);
+		await alignmentSelectors[2].select('flex-end');
+
+		// responsive XS
+
+		const alignmentOrientation = await accordionPanel.$$eval(
+			'.maxi-base-control select',
+			select => select[0].value
+		);
+		const alignmentVertical = await accordionPanel.$$eval(
+			'.maxi-base-control select',
+			select => select[1].value
+		);
+		const alignmentHorizontal = await accordionPanel.$$eval(
+			'.maxi-base-control select',
+			select => select[2].value
+		);
+
+		expect(alignmentOrientation).toBe('horizontal');
+		expect(alignmentVertical).toBe('flex-end');
+		expect(alignmentHorizontal).toBe('flex-end');
+
+		// responsive M
+		const alignmentMOrientation = await accordionPanel.$$eval(
+			'.maxi-base-control select',
+			select => select[0].value
+		);
+		const alignmentMVertical = await accordionPanel.$$eval(
+			'.maxi-base-control select',
+			select => select[1].value
+		);
+		const alignmentMHorizontal = await accordionPanel.$$eval(
+			'.maxi-base-control select',
+			select => select[2].value
+		);
+
+		expect(alignmentMOrientation).toBe('horizontal');
+		expect(alignmentMVertical).toBe('flex-end');
+		expect(alignmentMHorizontal).toBe('flex-end');
 	});
 });

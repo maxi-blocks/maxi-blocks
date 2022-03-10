144ee2bcad4909428bf6a7c58753351ab64a5594
Alejandro9kgm <alejandro9kgm@gmail.com>
2022-03-10 15:51:22 +0100

    Add icon test

--- e2e-tests/svg/index.spec.js
+++ e2e-tests/svg/index.spec.js
@@ -1,10 +1,14 @@
+/* eslint-disable no-await-in-loop */
+
 import { createNewPost, insertBlock } from '@wordpress/e2e-test-utils';
-import { setAttributes, svgFetch } from '../utils';
+import { setAttributes, svgFetch, openPreviewPage } from '../utils';
+import parse from 'html-react-parser';
 
 const checkSVGGroup = async (page, fetchPage = 1) => {
 	await createNewPost();
-
-	debugger;
+	let checker = true;
+	const checkRefresh = true;
+	const svgHtml = [];
 
 	const svgGroup = await svgFetch(page, fetchPage);
 
@@ -12,6 +16,7 @@ const checkSVGGroup = async (page, fetchPage = 1) => {
 		const svg = svgGroup[i];
 
 		await insertBlock('SVG Icon Maxi');
+
 		// Close model opened automatically by the block
 		await page.waitForSelector(
 			'.components-modal__content .components-modal__header button'
@@ -21,12 +26,65 @@ const checkSVGGroup = async (page, fetchPage = 1) => {
 			svg => svg.click()
 		);
 
-		await page.waitForTimeout(100);
-
 		await setAttributes(page, { content: svg });
+
+		const svgBaseHtml = await page.$$eval(
+			'.block-editor-block-list__layout .maxi-svg-icon-block__icon svg',
+			(svg, _i) => svg[_i].outerHTML,
+			i
+		);
+		svgHtml.push(svgBaseHtml);
 	}
 
-	debugger;
+	// FrontEnd
+	const previewPage = await openPreviewPage(page);
+	await previewPage.waitForSelector('.entry-content');
+
+	for (let e = 0; e < svgGroup.length; e += 1) {
+		const previewIcon = await previewPage.$$eval(
+			'.wp-block-post-content .maxi-svg-icon-block__icon svg',
+			(svg, _e) => svg[_e].outerHTML,
+			e
+		);
+
+		const frontEndHtml = parse(previewIcon);
+		const previewIconHtml = parse(svgHtml[e]);
+
+		if (previewIconHtml === frontEndHtml) {
+			checker = true;
+			break;
+		}
+	}
+	if (!checker) return false;
+
+	// Reload
+	await page.waitForTimeout(250);
+
+	await previewPage.close();
+
+	await page.waitForTimeout(150);
+
+	await page.reload();
+	await page.waitForTimeout(150);
+
+	for (let i = 0; i < svgGroup.length; i += 1) {
+		const svgRefresh = await page.$$eval(
+			'.block-editor-block-list__layout .maxi-svg-icon-block__icon svg',
+			(svg, _i) => svg[_i].outerHTML,
+			i
+		);
+
+		const frontEndHtml = parse(svgRefresh);
+		const previewIconHtml = parse(svgHtml[i]);
+
+		if (previewIconHtml === frontEndHtml) {
+			checker = true;
+			break;
+		}
+	}
+	if (!checkRefresh) return false;
+
+	return true;
 };
 
 describe('SVG checker', () => {
@@ -34,9 +92,12 @@ describe('SVG checker', () => {
 		let i = 1;
 
 		do {
-			await checkSVGGroup(page, i);
+			const test = await checkSVGGroup(page, i);
+			await page.waitForTimeout(250);
+			await expect(test).toBeTruthy();
 
 			i += 1;
+			await page.waitForTimeout(50);
 		} while (i <= 3);
 	}, 999999999);
 });

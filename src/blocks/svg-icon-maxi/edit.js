37a565a17c9ff4559e0d0fc629fae4ff2d50f344
Fitsum Abebe <engfitsum@gmail.com>
2022-03-11 20:43:58 +0300

    small bug fix on force responsive

--- src/blocks/svg-icon-maxi/edit.js
+++ src/blocks/svg-icon-maxi/edit.js
@@ -178,15 +178,7 @@ class edit extends MaxiBlockComponent {
 							resizableObject={this.resizableObject}
 							isOverflowHidden={getIsOverflowHidden()}
 							lockAspectRatio
-							maxWidth={
-								getLastBreakpointAttribute({
-									target: 'svg-responsive',
-									breakpoint: deviceType,
-									attributes,
-								})
-									? '100%'
-									: null
-							}
+							maxWidth='100%'
 							size={{
 								width: `${getLastBreakpointAttribute(
 									'svg-width',

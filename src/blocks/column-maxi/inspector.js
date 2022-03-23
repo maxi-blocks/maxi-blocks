7f39459d1f59f8caad0814ac28dc4072023189b6
Josh <50477222+Naaaaiix@users.noreply.github.com>
2022-03-23 11:44:27 +0100

    Implement `force aspect ratio` (#2747)
    * Implement `force aspect ratio`
    Add Column Maxi size support
    
    * Fix test
    
    * Fix test

--- src/blocks/column-maxi/inspector.js
+++ src/blocks/column-maxi/inspector.js
@@ -202,6 +202,12 @@ const Inspector = props => {
 										...inspectorTabs.boxShadow({
 											props,
 										}),
+										...inspectorTabs.size({
+											props,
+											block: true,
+											hideWidth: true,
+											hideMaxWidth: true,
+										}),
 										...inspectorTabs.marginPadding({
 											props,
 										}),

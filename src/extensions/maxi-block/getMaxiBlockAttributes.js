dab8b851da7c0f1901da29e57934332c07162624
Josh <50477222+Naaaaiix@users.noreply.github.com>
2022-03-28 13:37:57 +0200

    Issue 2699/2475/2534 - New block inserter (#2746)
    * Fixes #2699
    Replaces BlockPlaceholder for BlockInserter
    Add new WrapperBlockInserter as a Popover
    Update wrapper blocks with new BlockInserter on MaxiBlock component
    Clean Toolbar boundaryElement request
    Abstract `hasInnerBlocks` to withMaxiProps
    
    * Remove unnecessary CSS classes on MaxiBlock
    Fix tests
    
    * Minor fix
    
    * Minor fix
    
    * Fix tests
    
    * Remove obsolete test

--- src/extensions/maxi-block/getMaxiBlockAttributes.js
+++ src/extensions/maxi-block/getMaxiBlockAttributes.js
@@ -9,7 +9,7 @@ import { getLastBreakpointAttribute, getGroupAttributes } from '../styles';
 import { isEmpty } from 'lodash';
 
 const getMaxiBlockAttributes = props => {
-	const { name, deviceType, attributes, clientId } = props;
+	const { name, deviceType, attributes, clientId, hasInnerBlocks } = props;
 	const {
 		blockStyle,
 		extraClassName,
@@ -162,6 +162,7 @@ const getMaxiBlockAttributes = props => {
 		motion,
 		background,
 		hasLink,
+		hasInnerBlocks,
 		...scroll,
 	};
 };

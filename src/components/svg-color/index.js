77983e300c72a36c4b433c211f1e2d10d4ea26cc
Svitlana <2401618+elzadj@users.noreply.github.com>
2022-04-06 12:14:07 +0300

    Add normal-hover tabs

--- src/components/svg-color/index.js
+++ src/components/svg-color/index.js
@@ -1,55 +1,100 @@
 /**
+ * WordPress dependencies
+ */
+import { __ } from '@wordpress/i18n';
+
+/**
  * Internal dependencies
  */
 import ColorControl from '../color-control';
-import { getDefaultAttribute } from '../../extensions/styles';
+import SettingTabsControl from '../setting-tabs-control';
 
 /**
  * SvgColor
  */
 const SvgColor = props => {
-	const { type, label, onChange } = props;
+	const { type, label, onChange, isHover = false } = props;
 
 	return (
-		<>
-			{type === 'line' ? (
-				<ColorControl
-					label={label}
-					className='maxi-color-control__SVG-line-color'
-					color={props['svg-line-color']}
-					prefix='svg-line-'
-					paletteColor={props['svg-line-palette-color']}
-					paletteStatus={props['svg-line-palette-status']}
-					onChange={({ color, paletteColor, paletteStatus }) => {
-						onChange({
-							'svg-line-color': color,
-							'svg-line-palette-color': paletteColor,
-							'svg-line-palette-status': paletteStatus,
-						});
-					}}
-					globalProps={{ target: 'line', type: 'icon' }}
-					disableOpacity
-				/>
-			) : (
-				<ColorControl
-					label={label}
-					className='maxi-color-control__SVG-fill-color'
-					color={props['svg-fill-color']}
-					prefix='svg-fill-'
-					paletteColor={props['svg-fill-palette-color']}
-					paletteStatus={props['svg-fill-palette-status']}
-					onChange={({ color, paletteColor, paletteStatus }) => {
-						onChange({
-							'svg-fill-color': color,
-							'svg-fill-palette-color': paletteColor,
-							'svg-fill-palette-status': paletteStatus,
-						});
-					}}
-					globalProps={{ target: 'fill', type: 'icon' }}
-					disableOpacity
-				/>
-			)}
-		</>
+		<SettingTabsControl
+			items={[
+				{
+					label: __('Normal state', 'maxi-blocks'),
+					content: (
+						<>
+							{type === 'line' ? (
+								<ColorControl
+									label={label}
+									isHover={isHover}
+									className='maxi-color-control__SVG-line-color'
+									color={props['svg-line-color']}
+									prefix='svg-line-'
+									paletteColor={
+										props['svg-line-palette-color']
+									}
+									paletteStatus={
+										props['svg-line-palette-status']
+									}
+									onChange={({
+										color,
+										paletteColor,
+										paletteStatus,
+									}) => {
+										onChange({
+											'svg-line-color': color,
+											'svg-line-palette-color':
+												paletteColor,
+											'svg-line-palette-status':
+												paletteStatus,
+										});
+									}}
+									globalProps={{
+										target: 'line',
+										type: 'icon',
+									}}
+									disableOpacity
+								/>
+							) : (
+								<ColorControl
+									label={label}
+									className='maxi-color-control__SVG-fill-color'
+									color={props['svg-fill-color']}
+									prefix='svg-fill-'
+									paletteColor={
+										props['svg-fill-palette-color']
+									}
+									paletteStatus={
+										props['svg-fill-palette-status']
+									}
+									onChange={({
+										color,
+										paletteColor,
+										paletteStatus,
+									}) => {
+										onChange({
+											'svg-fill-color': color,
+											'svg-fill-palette-color':
+												paletteColor,
+											'svg-fill-palette-status':
+												paletteStatus,
+										});
+									}}
+									globalProps={{
+										target: 'fill',
+										type: 'icon',
+									}}
+									disableOpacity
+								/>
+							)}
+						</>
+					),
+				},
+				{
+					label: __('Hover state', 'maxi-blocks'),
+					content: <>test</>,
+				},
+			]}
+		/>
 	);
 };
 

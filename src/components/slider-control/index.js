3d7616c0240467691f61d15392e970f961948bf3
TeksPeks <53875005+TeksPeks@users.noreply.github.com>
2022-04-19 16:01:07 +0300

    SliderControl

---
+++ src/components/slider-control/index.js
@@ -0,0 +1,36 @@
+import { __ } from '@wordpress/i18n';
+import classnames from 'classnames';
+import AdvancedNumberControl from '../advanced-number-control';
+import ToggleSwitch from '../toggle-switch';
+
+const SliderControl = props => {
+	const { className, attributes, maxiSetAttributes } = props;
+
+	const { numberOfSlides, isVertical } = attributes;
+
+	const classes = classnames('maxi-slider-control', className);
+
+	return (
+		<div className={classes}>
+			<ToggleSwitch
+				label={__('Vertical view', 'maxi-blocks')}
+				selected={isVertical}
+				onChange={val => {
+					maxiSetAttributes({ isVertical: val });
+				}}
+			/>
+			<AdvancedNumberControl
+				label='Number of slides'
+				value={numberOfSlides}
+				onChangeValue={val =>
+					maxiSetAttributes({ numberOfSlides: val })
+				}
+				min={0}
+				max={10}
+				className={classes}
+			/>
+		</div>
+	);
+};
+
+export default SliderControl;

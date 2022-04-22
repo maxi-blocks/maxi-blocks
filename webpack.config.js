49094183962b241f6e1f32fb82de4078e87cd0e9
Svitlana <2401618+elzadj@users.noreply.github.com>
2022-04-22 17:18:55 +0300

    Issue 2756 dependencies css (#2892)
    * Add MiniCssExtractPlugin
    
    * Remove comments from built css
    
    * Update webpack.config.js
    
    Co-authored-by: Josh <50477222+Naaaaiix@users.noreply.github.com>
    
    Co-authored-by: Josh <jjuancap@gmail.com>
    Co-authored-by: Josh <50477222+Naaaaiix@users.noreply.github.com>

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,33 @@
  */
 const defaultConfig = require('@wordpress/scripts/config/webpack.config');
 
+/**
+ * External Dependencies
+ */
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const { basename, dirname } = require('path');
+
+const isProduction = process.env.NODE_ENV === 'production';
+
 module.exports = {
 	...defaultConfig,
+	optimization: {
+		...defaultConfig.optimization,
+		minimizer: [
+			new CssMinimizerPlugin({
+				minimizerOptions: {
+					preset: [
+						'default',
+						{
+							discardComments: { removeAll: true },
+						},
+					],
+				},
+			}),
+			...defaultConfig.optimization.minimizer,
+		],
+	},
 	resolve: {
 		...defaultConfig.resolve,
 		fallback: { ...defaultConfig.resolve.fallback, https: false },

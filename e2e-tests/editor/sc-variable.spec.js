f2cd88c800fb619bd1647098e82223d264a27078
Bilal <bilal89dif@gmail.com>
2022-04-23 17:35:20 +0200

    fix test

--- e2e-tests/editor/sc-variable.spec.js
+++ e2e-tests/editor/sc-variable.spec.js
@@ -19,13 +19,13 @@ describe('sc-variable', () => {
 		await createNewPost();
 		await insertBlock('Divider Maxi');
 
-		await page.waitForTimeout(1000);
+		await page.waitForTimeout(5000);
 		await saveDraft();
-		await page.waitForTimeout(1000);
+		await page.waitForTimeout(5000);
 
 		await page.evaluate(() => window.location.reload());
 
-		await page.waitForTimeout(500);
+		await page.waitForTimeout(5000);
 		await page.waitForSelector('#maxi-blocks-sc-vars-inline-css');
 
 		const scVariable = await page.$eval(

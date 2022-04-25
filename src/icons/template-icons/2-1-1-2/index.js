da9556f52994a20a48ca9eb151bc4277b1120c4e
Marko64 <marko.niksicafe@gmail.com>
2022-04-25 11:31:26 +0200

    issue-2904 SVG attribute errors
    The only place I could it is the "twoOneOnetwo" icon. Is that what you were getting the warning for or something else?

--- src/icons/template-icons/2-1-1-2/index.js
+++ src/icons/template-icons/2-1-1-2/index.js
@@ -4,7 +4,7 @@
 import { SVG, Path } from '@wordpress/primitives';
 
 const twoOneOneTwo = (
-	<SVG xmlns="http://www.w3.org/2000/svg" viewBox="0 0 112.35 37" fill="none" stroke="#323c47" stroke-linejoin="round" stroke-width="2">
+	<SVG xmlns="http://www.w3.org/2000/svg" viewBox="0 0 112.35 37" fill="none" stroke="#323c47" strokeLinejoin="round" strokeWidth="2">
 		<Path d="M1 1h110.35v8.08H1zm0 26.92h110.35V36H1zm0-13.39h52.29v6.94H1zm58.06 0h52.29v6.94H59.06z"/>
 	</SVG>
 );

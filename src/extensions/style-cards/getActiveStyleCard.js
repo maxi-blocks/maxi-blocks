3d8b1a44dd69e15abce72ab912c812c2ad743b41
Andrew <46112160+Fybex@users.noreply.github.com>
2022-04-25 13:12:49 +0300

    Fix unit test problem

--- src/extensions/style-cards/getActiveStyleCard.js
+++ src/extensions/style-cards/getActiveStyleCard.js
@@ -9,13 +9,14 @@ import { select } from '@wordpress/data';
 import { forIn } from 'lodash';
 
 const getActiveStyleCard = styleCards => {
-	const { receiveMaxiStyleCards } = select('maxiBlocks/style-cards');
-
 	let SC;
 
 	if (!styleCards)
 		// Needs a delay, if not Redux returns error 3
-		SC = setTimeout(() => receiveMaxiStyleCards(), 150);
+		SC = setTimeout(
+			() => select('maxiBlocks/style-cards').receiveMaxiStyleCards(),
+			150
+		);
 	else SC = styleCards;
 
 	let styleCardActive;

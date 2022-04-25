07a3172ac76608b4cc209159b60c4ad3e5375148
Alejandro9kgm <58038966+Alejandro9kgm@users.noreply.github.com>
2022-04-25 14:11:31 +0200

    Update index.spec.js

--- e2e-tests/components/responsive-control/index.spec.js
+++ e2e-tests/components/responsive-control/index.spec.js
@@ -27,43 +27,6 @@ describe('ResponsiveControl', () => {
 
 		expect(await getAttributes('breakpoints-xs')).toStrictEqual(450);
 
-		await openSidebarTab(page, 'style', 'alignment');
-		await changeResponsive(page, 'xxl');
-
-		const alignmentButtons = await page.$$(
-			'.maxi-tabs-control__full-width .maxi-settingstab-control_has-border-left-right button'
-		);
-
-		await alignmentButtons[0].click();
-
-		expect(await getAttributes('alignment-xxl')).toStrictEqual('left');
-
-		await changeResponsive(page, 'xl');
-		await alignmentButtons[2].click();
-
-		expect(await getAttributes('alignment-xl')).toStrictEqual('right');
-
-		await changeResponsive(page, 'l');
-		await alignmentButtons[1].click();
-
-		expect(await getAttributes('alignment-l')).toStrictEqual('center');
-
-		await changeResponsive(page, 'm');
-		await alignmentButtons[0].click();
-
-		expect(await getAttributes('alignment-m')).toStrictEqual('left');
-
-		await changeResponsive(page, 's');
-		await alignmentButtons[2].click();
-
-		expect(await getAttributes('alignment-s')).toStrictEqual('right');
-
-		await changeResponsive(page, 'xs');
-		await alignmentButtons[1].click();
-
-		expect(await getAttributes('alignment-xs')).toStrictEqual('center');
-	});
-
 	/* it('Check Responsive to responsive control', async () => {
 		const input = await page.$('.maxi-responsive-control input');
 

9aca1f846d9cecea6dbd78360a05c33b8bf3f2e6
Bilal <bilal89dif@gmail.com>
2022-04-27 23:47:46 +0200

    remove console

--- src/extensions/styles/helpers/getNumberCounterStyles.js
+++ src/extensions/styles/helpers/getNumberCounterStyles.js
@@ -8,7 +8,6 @@ import getPaletteAttributes from '../getPaletteAttributes';
  * External dependencies
  */
 import { isNil } from 'lodash';
-import { getPropsFromChildren } from '../../indicators';
 
 const breakpoints = ['general', 'xxl', 'xl', 'l', 'm', 's', 'xs'];
 
@@ -93,8 +92,6 @@ const getTextStyles = (obj, blockStyle) => {
 	};
 
 	breakpoints.forEach(breakpoint => {
-		console.log(getColor(breakpoint));
-
 		response[breakpoint] = {
 			...(!isNil(obj[`number-counter-title-font-size-${breakpoint}`]) && {
 				'font-size': `${
@@ -112,7 +109,6 @@ const getTextStyles = (obj, blockStyle) => {
 		};
 	});
 
-	console.log(response);
 	return { numberCounterText: response };
 };
 

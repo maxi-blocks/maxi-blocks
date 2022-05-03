ad2f55e9b237706ac9d591b2d69e192874d6e71d
Marko64 <marko.niksicafe@gmail.com>
2022-05-03 15:58:26 +0200

    New block icons

--- src/icons/block-icons/group-icon/index.js
+++ src/icons/block-icons/group-icon/index.js
@@ -4,41 +4,15 @@
 import { SVG, Path, Circle } from '@wordpress/primitives';
 
 const group = (
-	<SVG preserveAspectRatio='none' width={24} height={24} viewBox='0 0 24 24'>
-		<defs>
-			<Path
-				id='container__a'
-				stroke='#ff4a17'
-				strokeWidth={1.5}
-				strokeLinejoin='round'
-				strokeLinecap='round'
-				fill='none'
-				d='M12.4 18.9h3M1.4 1.2h13.5v14.3H1.4z'
-			/>
-			<Path
-				id='container__b'
-				stroke='#ff4a17'
-				strokeWidth={1.5}
-				strokeLinejoin='round'
-				strokeLinecap='round'
-				fill='none'
-				d='M9 8.4h13.5v14.3H9zM18.3 18.9h1.9'
-			/>
-			<Circle
-				id='container__c'
-				stroke='#ff4a17'
-				strokeWidth={1.5}
-				strokeLinejoin='round'
-				strokeLinecap='round'
-				fill='none'
-				cx={6}
-				cy={5.4}
-				r={2.1}
-			/>
-		</defs>
-		<use xlinkHref='#container__a' />
-		<use xlinkHref='#container__b' />
-		<use xlinkHref='#container__c' />
+	<SVG viewBox='0 0 24 24'>
+		<Path
+			fill='#ff4a17'
+			stroke='none'
+			strokeLinecap='round'
+			strokeLinejoin='round'
+			strokeWidth='0'
+			d='M15.6 18.1h-3.1a.94.94 0 0 0-1 1 .94.94 0 0 0 1 1h3.1a.94.94 0 0 0 1-1c0-.6-.5-1-1-1zM23 7.4h-6.9V1a.94.94 0 0 0-1-1H1a1.08 1.08 0 0 0-1 1v14.6a.94.94 0 0 0 1 1h6.9V23a.94.94 0 0 0 1 1h14a.94.94 0 0 0 1-1V8.4c.1-.6-.4-1-.9-1zM2 14.6V2h12v5.3H8.9a.94.94 0 0 0-1 1v6.2H2zm12.1-5.2v5.2H9.9V9.4h4.2zM22 22H10v-5.3h5.1a.94.94 0 0 0 1-1V9.4H22V22z"/><path d="M18.6 20.1h2a.94.94 0 0 0 1-1 .94.94 0 0 0-1-1h-2a.94.94 0 0 0-1 1 .94.94 0 0 0 1 1zM9 5.3c0-1.7-1.4-3.1-3.2-3.1S2.6 3.6 2.6 5.3 4 8.4 5.8 8.4C7.6 8.5 9 7.1 9 5.3zM5.8 6.5c-.6 0-1.2-.5-1.2-1.2s.5-1.1 1.2-1.1S7 4.7 7 5.3s-.5 1.2-1.2 1.2z'
+		/>
 	</SVG>
 );
 

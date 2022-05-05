1d3e602e5b6cefc5d0a9720d4b0725826dabe15c
user <engfitsum@gmail.com>
2022-05-05 14:36:35 +0300

    fixed the arrow icon color issue

--- src/extensions/svg/setSVGContent.js
+++ src/extensions/svg/setSVGContent.js
@@ -1,8 +1,8 @@
 const setSVGContent = (content, color, type) => {
-	const fillRegExp = new RegExp(`${type}:([^none])([^\\}]+)`, 'g');
+	const fillRegExp = new RegExp(` ${type}:([^none])([^\\}]+)`, 'g');
 	const fillStr = `${type}:${color}`;
 
-	const fillRegExp2 = new RegExp(`${type}=[^-]([^none])([^\\"]+)`, 'g');
+	const fillRegExp2 = new RegExp(` ${type}=[^-]([^none])([^\\"]+)`, 'g');
 	const fillStr2 = ` ${type}="${color}`;
 
 	const newContent = content

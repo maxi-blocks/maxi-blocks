4e94d34e3dec3552dcd2f413175d2f2a41602d9e
Marko64 <marko.niksicafe@gmail.com>
2022-05-09 10:19:42 +0200

    Issue 2995 new block icons (#2999)
    * New block icons
    
    * Added column
    
    * Update index.js
    
    * Group icon error fixed
    
    * Image icon code fixed

--- src/icons/block-icons/row-icon/index.js
+++ src/icons/block-icons/row-icon/index.js
@@ -6,11 +6,11 @@ import { SVG, Path } from '@wordpress/primitives';
 const textIcon = (
 	<SVG xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'>
 		<Path
-			fill='none'
-			stroke='#ff4a17'
+			fill='#ff4a17'
+			stroke='none'
 			strokeLinecap='round'
 			strokeLinejoin='round'
-			d='M16.6 1.3H23v21.5h-6.4V1.3zM1 1.3h6.4v21.5H1V1.3zm7.9 0h6.4v21.5H8.9V1.3z'
+			d='M23 0h-5.4a.94.94 0 0 0-1 1v22a.94.94 0 0 0 1 1H23a.94.94 0 0 0 1-1V1c0-.5-.4-1-1-1zm-1 22h-3.4V2H22v20zM5.4 0H1C.4 0 0 .5 0 1v22a.94.94 0 0 0 1 1h4.4a.94.94 0 0 0 1-1V1c0-.5-.4-1-1-1zm-1 22H2V2h2.4v20zm9.3-22H9.3a.94.94 0 0 0-1 1v22a.94.94 0 0 0 1 1h4.4a.94.94 0 0 0 1-1V1c0-.5-.4-1-1-1zm-1 22h-2.4V2h2.4v20z'
 		/>
 	</SVG>
 );

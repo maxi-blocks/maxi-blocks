a23d9173bc7ada0ebedaf6dc78ea1a3a19c44dd1
Josh <jjuancap@gmail.com>
2022-05-09 16:25:15 +0200

    Pattern test implementation

--- e2e-tests/editor/pattern-test/index.spec.js
+++ e2e-tests/editor/pattern-test/index.spec.js
@@ -1,32 +1,27 @@
 /**
  * WordPress dependencies
  */
-import { createNewPost, setPostContent } from '@wordpress/e2e-test-utils';
+import {
+	createNewPost,
+	saveDraft,
+	getEditedPostContent,
+	setPostContent,
+} from '@wordpress/e2e-test-utils';
+
 /**
  * Internal dependencies
  */
-
-import { getBlockStyle, getEditedPostContent } from '../../utils';
 import basePattern from './pattern';
-
-// paste blocks
-/* const recovery = document
-	.querySelectorAll('.components-button.is-primary')
-	.forEach(e => {
-		if (e.innerHTML === 'Attempt Block Recovery') e.click();
-	}); */
 
 describe('Pattern', () => {
 	it('Checking pattern generate', async () => {
 		await createNewPost();
 		await setPostContent(basePattern);
-		await page.waitForTimeout(1000);
-		debugger;
-		await page.waitForTimeout(1000);
-		debugger;
+
+		await saveDraft();
 
 		expect(await getEditedPostContent()).toMatchSnapshot();
-		expect(await getBlockStyle(page)).toMatchSnapshot();
+		// expect(await getBlockStyle(page)).toMatchSnapshot();
 		// Checkeas frontend
 
 		// Reload + snapshot again (snapshot de conteniido(html) y de styles)

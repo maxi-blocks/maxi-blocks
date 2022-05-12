14eead8c1203c1eae0dafe5e5616e182635e17ff
user <engfitsum@gmail.com>
2022-05-12 23:08:39 +0300

    fixed setting tab indicator

--- src/blocks/divider-maxi/inspector.js
+++ src/blocks/divider-maxi/inspector.js
@@ -26,15 +26,7 @@ import { withMaxiInspector } from '../../extensions/inspector';
  * Inspector
  */
 const Inspector = props => {
-	const {
-		attributes,
-		deviceType,
-		maxiSetAttributes,
-		insertInlineStyles,
-		cleanInlineStyles,
-		inlineStylesTargets,
-		clientId,
-	} = props;
+	const { attributes, deviceType, maxiSetAttributes, clientId } = props;
 
 	return (
 		<InspectorControls>

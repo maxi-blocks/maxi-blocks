72971e883a474a82a22f661b33a35f905fc7035a
Andrew <46112160+Fybex@users.noreply.github.com>
2022-05-13 17:03:10 +0300

    Add check to onChangeInline existence

--- src/components/color-control/index.js
+++ src/components/color-control/index.js
@@ -40,7 +40,6 @@ const ColorControl = props => {
 		color,
 		defaultColorAttributes,
 		globalProps,
-		noColorPrefix,
 		onChangeInline,
 		onChange,
 		isHover,
@@ -152,7 +151,7 @@ const ColorControl = props => {
 				blockStyle,
 			})},${paletteOpacity || 1})`;
 
-			onChangeInline({ color: defaultColor });
+			onChangeInline && onChangeInline({ color: defaultColor });
 			onChange({
 				paletteStatus,
 				paletteColor,

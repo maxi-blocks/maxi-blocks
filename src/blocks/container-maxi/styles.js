75fc4e71831df6914756047ef363acac80cd20b4
Andrew Valenya <46112160+Fybex@users.noreply.github.com>
2022-05-13 15:06:17 +0300

    Issue 2985 - Prevent adding 'width' and 'max-width' styles if fullWidth (#3068)
    * Don't return width and max-width if fullWidth
    
    * Add fullWidth to getSizeStyles obj
    
    * Fix e2e
    
    * Fix e2e custom css

--- src/blocks/container-maxi/styles.js
+++ src/blocks/container-maxi/styles.js
@@ -33,6 +33,7 @@ const getNormalObject = props => {
 		}),
 		size: getSizeStyles({
 			...getGroupAttributes(props, 'size'),
+			fullWidth: props.blockFullWidth,
 		}),
 		boxShadow: getBoxShadowStyles({
 			obj: {

8de8c873791305c46ff362ac918e289939d13e68
Alejandro9kgm <alejandro9kgm@gmail.com>
2022-05-13 11:42:18 +0200

    add basic test

--- e2e-tests/components/toolbar/components/copy-paste/index.spec.js
+++ e2e-tests/components/toolbar/components/copy-paste/index.spec.js
@@ -6,17 +6,20 @@ import { createNewPost, insertBlock } from '@wordpress/e2e-test-utils';
 /**
  * Internal dependencies
  */
-import { getBlockAttributes } from '../../../../utils';
-import { block } from './utils';
+import {
+	getBlockAttributes,
+	openSidebarTab,
+	editAxisControl,
+	getAttributes,
+} from '../../../../utils';
 
 /**
  * External dependencies
  */
 import { isEqual } from 'lodash';
-import getGroupAttributes from '../../../../../src/extensions/styles/getGroupAttributes';
 
-describe.skip('CopyPaste from Toolbar', () => {
-	beforeEach(async () => {
+describe('CopyPaste from Toolbar', () => {
+	/* beforeEach(async () => {
 		await createNewPost();
 		await page.waitForTimeout(1000);
 
@@ -36,31 +39,94 @@ describe.skip('CopyPaste from Toolbar', () => {
 
 		// Set new block
 		await insertBlock('Text Maxi');
-	});
+	}); */
 
 	it('Should copy and paste bulk styles', async () => {
-		// Paste styles
-		await page.waitForSelector('.toolbar-item__copy-paste');
-		await page.$eval('.toolbar-item__copy-paste', button => button.click());
-		await page.waitForSelector('.toolbar-item__copy-paste__popover');
-		await page.$$eval('.toolbar-item__copy-paste__popover button', button =>
-			button[1].click()
+		await createNewPost();
+		await page.waitForTimeout(1000);
+		await insertBlock('Text Maxi');
+
+		// edit text maxi
+		const accordionPanel = await openSidebarTab(
+			page,
+			'advanced',
+			'position'
 		);
+		const selectPosition = await accordionPanel.$(
+			'.maxi-position-control .maxi-base-control__field select'
+		);
+		await selectPosition.select('relative');
 
-		// Compare attributes
-		const secondBlockAttr = await getBlockAttributes();
-		await page.waitForSelector('.maxi-text-block');
-		await page.$eval('.maxi-text-block', block => block.focus());
-		const firstBlockAttr = await getBlockAttributes();
-
-		['uniqueID', 'content', 'fullWidth'].forEach(attr => {
-			delete secondBlockAttr[attr];
-			delete firstBlockAttr[attr];
+		await editAxisControl({
+			page,
+			instance: await page.$('.maxi-position-control .maxi-axis-control'),
+			syncOption: 'all',
+			values: '56',
+			unit: '%',
 		});
 
-		await expect(isEqual(firstBlockAttr, secondBlockAttr)).toBeTruthy();
+		// open options
+		await page.$eval(
+			'.toolbar-wrapper .toolbar-item__more-settings button',
+			button => button.click()
+		);
+		await page.waitForTimeout(150);
+
+		// select copy/paste
+		await page.$eval(
+			'.components-popover__content .maxi-copypaste__copy-selector button',
+			button => button.click()
+		);
+		await page.waitForTimeout(150);
+
+		// select copy Style
+		await page.$eval(
+			'.components-popover__content .toolbar-item__copy-paste__popover button',
+			button => button.click()
+		);
+		await page.waitForTimeout(150);
+		await insertBlock('Text Maxi');
+
+		// open options
+		await page.$eval(
+			'.toolbar-wrapper .toolbar-item__more-settings button',
+			button => button.click()
+		);
+		await page.waitForTimeout(150);
+
+		// select copy/paste
+		await page.$eval(
+			'.components-popover__content .maxi-copypaste__copy-selector button',
+			button => button.click()
+		);
+		await page.waitForTimeout(150);
+
+		// select copy Style
+		await page.$$eval(
+			'.components-popover__content .toolbar-item__copy-paste__popover button',
+			button => button[1].click()
+		);
+		await page.waitForTimeout(150);
+
+		const expectPosition = {
+			'position-top-general': 56,
+			'position-bottom-general': 56,
+			'position-left-general': 56,
+			'position-right-general': 56,
+			'position-unit-general': '%',
+		};
+
+		const positionResult = await getAttributes([
+			'position-top-general',
+			'position-bottom-general',
+			'position-left-general',
+			'position-right-general',
+			'position-unit-general',
+		]);
+
+		expect(positionResult).toStrictEqual(expectPosition);
 	});
-	it('Should copy and paste styles with special paste', async () => {
+	/* it('Should copy and paste styles with special paste', async () => {
 		// Paste styles
 		await page.waitForSelector('.toolbar-item__copy-paste');
 		await page.$eval('.toolbar-item__copy-paste', button => button.click());
@@ -101,5 +167,5 @@ describe.skip('CopyPaste from Toolbar', () => {
 		await expect(
 			isEqual(firstBlockTypography, secondBlockTypography)
 		).toBeTruthy();
-	});
+	}); */
 });

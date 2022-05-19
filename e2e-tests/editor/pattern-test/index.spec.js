e85fb4b2bd84604ed704173a66e5c633a337e507
Alejandro9kgm <alejandro9kgm@gmail.com>
2022-05-19 11:15:06 +0200

    update

--- e2e-tests/editor/pattern-test/index.spec.js
+++ e2e-tests/editor/pattern-test/index.spec.js
@@ -6,6 +6,8 @@ import {
 	saveDraft,
 	getEditedPostContent,
 	setPostContent,
+	getBlockStyle,
+	openPreviewPage,
 } from '@wordpress/e2e-test-utils';
 
 /**
@@ -16,11 +18,25 @@ import basePattern from './pattern';
 describe('Pattern', () => {
 	it('Checking pattern generate', async () => {
 		await createNewPost();
-		await setPostContent(basePattern);
 
+		// add pattern from code editor
+		await setPostContent(basePattern);
+		await page.waitForTimeout(1500);
+
+		// save changes
 		await saveDraft();
 
+		const previewPage = await openPreviewPage(page);
+		await previewPage.waitForSelector('.entry-content');
+		await page.waitForTimeout(1500);
+
 		expect(await getEditedPostContent()).toMatchSnapshot();
+		/* const htmlPage = await page.$eval(
+			'.is-root-container.block-editor-block-list__layout',
+			div => div.outerHtml
+		);
+
+		expect(htmlPage).toMatchSnapshot(); */
 		// expect(await getBlockStyle(page)).toMatchSnapshot();
 		// Checkeas frontend
 

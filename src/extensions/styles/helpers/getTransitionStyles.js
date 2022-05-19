1d7698af8caf76bba08e3d583b1c4773005c8978
Andrew <46112160+Fybex@users.noreply.github.com>
2022-05-19 19:09:58 +0300

    Fix getTranstionStyles

--- src/extensions/styles/helpers/getTransitionStyles.js
+++ src/extensions/styles/helpers/getTransitionStyles.js
@@ -3,6 +3,7 @@
  */
 const breakpoints = ['general', 'xxl', 'xl', 'l', 'm', 's', 'xs'];
 
+import { isNil } from 'lodash';
 /**
  * Generates size styles object
  *
@@ -12,16 +13,17 @@ const getTransitionStyles = obj => {
 	const response = {};
 
 	breakpoints.forEach(breakpoint => {
-		if (obj[`transition-${breakpoint}`])
-			response[breakpoint] = {
-				'transition-duration': `${
-					obj[`transition-duration-${breakpoint}`]
-				}s`,
-				'transition-delay': `${
-					obj[`transition-delay-${breakpoint}`]
-				}s`,
-				'transition-timing-function': obj[`transition-timing-function-${breakpoint}`],
-			};
+		response[breakpoint] = {
+			'transition-duration':
+				!isNil(obj[`transition-duration-${breakpoint}`]) &&
+				`${obj[`transition-duration-${breakpoint}`]}s`,
+			'transition-delay':
+				!isNil(obj[`transition-delay-${breakpoint}`]) &&
+				`${obj[`transition-delay-${breakpoint}`]}s`,
+			'transition-timing-function':
+				!isNil(obj[`transition-timing-function-${breakpoint}`]) &&
+				obj[`transition-timing-function-${breakpoint}`],
+		};
 	});
 
 	return response;

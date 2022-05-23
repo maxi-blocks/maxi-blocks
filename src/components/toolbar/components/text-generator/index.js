83987081120775c141f8c34e50cc5f807a801290
Mr C <christiaan@yeahcan.com>
2022-05-23 12:33:08 +0100

    Update index.js

--- src/components/toolbar/components/text-generator/index.js
+++ src/components/toolbar/components/text-generator/index.js
@@ -87,7 +87,7 @@ const TextGenerator = props => {
 			position='right top'
 			renderToggle={({ isOpen, onToggle }) => (
 				<Button onClick={onToggle} text='Copy'>
-					{__('Ipsum text generator (Ipsum)', 'maxi-blocks')}
+					{__('Ipsum text generator', 'maxi-blocks')}
 				</Button>
 			)}
 			renderContent={() => (

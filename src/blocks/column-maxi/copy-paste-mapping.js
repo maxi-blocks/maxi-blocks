469bbfe3f50a399452992d4c5925585cd2da3640
user <engfitsum@gmail.com>
2022-05-23 15:55:28 +0300

    implemented for column maxi

--- src/blocks/column-maxi/copy-paste-mapping.js
+++ src/blocks/column-maxi/copy-paste-mapping.js
@@ -1,54 +1,62 @@
 const copyPasteMapping = {
 	settings: {
-		blockSpecific: {
-			fullWidth: 'Full width',
-			verticalAlign: 'Vertical align',
-			customLabel: 'Custom label',
-			anchorLink: {
-				label: 'Anchor',
-				value: ['anchorLink', 'linkSettings'],
+		withBreakpoint: {
+			'column-settings': {
+				groupLabel: 'Column settings',
+				props: {
+					'column-fit-content': 'Fit content',
+					'column-size': 'Column size',
+					'justify-content': 'Vertical align',
+				},
 			},
-			extraClassName: 'Custom CSS classes',
 		},
 		withoutPrefix: {
+			blockBackground: 'Background',
 			border: {
 				groupLabel: 'Border',
 				props: {
 					border: 'Border',
 					borderWidth: 'Border width',
 					borderRadius: 'Border radius',
-				},
-			},
-			boxShadow: 'Box shadow',
-			blockBackground: 'Background',
-			columnSize: 'Column size',
-			size: 'Size',
-			margin: 'Margin',
-			padding: 'Padding',
-			breakpoints: 'Breakpoints',
-		},
-		withoutPrefixHover: {
-			borderHover: {
-				groupLabel: 'Border hover',
-				props: {
 					borderHover: 'Border hover',
 					borderWidthHover: 'Border width hover',
 					borderRadiusHover: 'Border radius hover',
 				},
 			},
-			boxShadowHover: 'Box shadow hover',
+			boxShadow: {
+				groupLabel: 'Box shadow',
+				props: {
+					boxShadow: 'Box shadow',
+					boxShadowHover: 'Box shadow hover',
+				},
+			},
+			size: 'Size',
+			'margin-padding': {
+				groupLabel: 'Margin/Padding',
+				props: { margin: 'Margin', padding: 'Padding' },
+			},
 		},
 	},
 	advanced: {
+		blockSpecific: {
+			extraClassName: 'Custom CSS classes',
+			anchorLink: {
+				label: 'Anchor',
+				value: ['anchorLink', 'linkSettings'],
+			},
+			relations: 'Interaction',
+		},
 		withoutPrefix: {
-			opacity: 'Opacity',
-			scroll: 'Scroll',
-			transform: 'Transform',
-			display: 'Display',
-			overflow: 'Overflow',
-			zIndex: 'z-Index',
+			breakpoints: 'Breakpoints',
 			customCss: 'Custom CSS',
-			flex: 'Flex',
+			scroll: 'Scroll effects',
+			transform: 'Transform',
+			transition: 'Hyperlink hover transition',
+			display: 'Show/hide block',
+			position: 'Position',
+			overflow: 'Overflow',
+			flex: 'Flexbox',
+			zIndex: 'Z-index',
 		},
 	},
 };

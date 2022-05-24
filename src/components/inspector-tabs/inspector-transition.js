31e7714407355947b525f2a8a3ae657eee7beb9a
Andrew <46112160+Fybex@users.noreply.github.com>
2022-05-24 17:51:58 +0300

    Transition inspector and control update

--- src/components/inspector-tabs/inspector-transition.js
+++ src/components/inspector-tabs/inspector-transition.js
@@ -9,27 +9,106 @@ import { __ } from '@wordpress/i18n';
 import TransitionControl from '../transition-control';
 import { getGroupAttributes } from '../../extensions/styles';
 import ResponsiveTabsControl from '../responsive-tabs-control';
+import SettingTabsControl from '../setting-tabs-control';
+import SelectControl from '../select-control';
+
+/**
+ * External dependencies
+ */
+import { isEmpty } from 'lodash';
 
 /**
  * Component
  */
+const TransitionControlWrapper = props => {
+	const { attributes, deviceType, maxiSetAttributes, type } = props;
+	const { transition } = attributes;
+
+	return (
+		<>
+			<SelectControl
+				label={__('Settings', 'maxi-blocks')}
+				value={attributes[`transition-${type}-selected`]}
+				options={[
+					{
+						label: __('Select setting', 'maxi-blocks'),
+						value: 'none',
+					},
+					...(transition[type] &&
+						Object.keys(transition[type]).map(name => ({
+							label: __(
+								name.charAt(0).toUpperCase() + name.slice(1),
+								'maxi-blocks'
+							),
+							value: name,
+						}))),
+				]}
+				onChange={val => {
+					maxiSetAttributes({
+						[`transition-${type}-selected`]: val,
+					});
+				}}
+			/>
+			{attributes[`transition-${type}-selected`] &&
+				attributes[`transition-${type}-selected`] !== 'none' && (
+					<ResponsiveTabsControl breakpoint={deviceType}>
+						<TransitionControl
+							{...getGroupAttributes(attributes, 'transition')}
+							onChange={obj => maxiSetAttributes(obj)}
+							breakpoint={deviceType}
+							type={type}
+						/>
+					</ResponsiveTabsControl>
+				)}
+		</>
+	);
+};
+
 const transition = ({
 	props,
 	label = __('Hyperlink hover transition', 'maxi-blocks'),
 }) => {
-	const { attributes, deviceType, maxiSetAttributes } = props;
+	const { attributes, deviceType } = props;
+	const { transition } = attributes;
+
+	const ignoreIndicator = [
+		'transition-block-selected',
+		'transition-canvas-selected',
+	];
 
 	return {
 		label,
-		content: (
-			<ResponsiveTabsControl breakpoint={deviceType}>
-				<TransitionControl
-					{...getGroupAttributes(attributes, 'transition')}
-					onChange={obj => maxiSetAttributes(obj)}
+		content:
+			!isEmpty(transition.block) && !isEmpty(transition.canvas) ? (
+				<SettingTabsControl
 					breakpoint={deviceType}
+					items={[
+						{
+							label: __('Block', 'maxi-blocks'),
+							content: (
+								<TransitionControlWrapper
+									type='block'
+									{...props}
+								/>
+							),
+							ignoreIndicator,
+						},
+						{
+							label: __('Canvas', 'maxi-blocks'),
+							content: (
+								<TransitionControlWrapper
+									type='canvas'
+									{...props}
+								/>
+							),
+							ignoreIndicator,
+						},
+					]}
 				/>
-			</ResponsiveTabsControl>
-		),
+			) : (
+				<TransitionControlWrapper type='block' {...props} />
+			),
+		ignoreIndicator,
 	};
 };
 
--- src/components/transition-control/index.js
+++ src/components/transition-control/index.js
@@ -22,107 +22,138 @@ import SelectControl from '../select-control';
  * Component
  */
 const TransitionControl = props => {
-	const { onChange, className, breakpoint } = props;
+	const { onChange, className, breakpoint, type } = props;
+	const selected = props[`transition-${type}-selected`] || 'none';
+
+	const onChangeTransition = obj => {
+		const newObj = {
+			transition: {
+				...props?.transition,
+				[type]: {
+					...(props?.transition?.[type] || []),
+					[selected]: {
+						...(props?.transition?.[type]?.[selected] || {}),
+						...obj,
+					},
+				},
+			},
+		};
+
+		onChange(newObj);
+
+		return newObj;
+	};
+
+	const getDefaultTransitionAttribute = prop => {
+		const defaultTransition = getDefaultAttribute('transition');
+
+		return getLastBreakpointAttribute({
+			target: prop,
+			breakpoint,
+			attributes: defaultTransition[type][selected],
+		});
+	};
+
+	const transitionObj = props.transition[type][selected];
 
 	const classes = classnames('maxi-transition-control', className);
 
 	return (
 		<>
-		<AdvancedNumberControl
-			label={__('Transition duration', 'maxi-blocks')}
-			className={classes}
-			defaultValue={getDefaultAttribute(
-				`transition-duration-${breakpoint}`
-			)}
-			value={getLastBreakpointAttribute({
-				target: 'transition-duration',
-				breakpoint,
-				attributes: props,
-			})}
-			onChangeValue={val => {
-				onChange({
-					[`transition-duration-${breakpoint}`]:
-						val !== undefined && val !== '' ? val : '',
-				});
-			}}
-			min={0}
-			max={5}
-			step={0.1}
-			initial={0.3}
-			onReset={() =>
-				onChange({
-					[`transition-duration-${breakpoint}`]: getDefaultAttribute(
-						`transition-duration-${breakpoint}`
-					),
-				})
-			}
-			initialPosition={getDefaultAttribute(
-				`transition-duration-${breakpoint}`
-			)}
-		/>
-		<AdvancedNumberControl
-			label={__('Transition delay', 'maxi-blocks')}
-			className={classes}
-			defaultValue={getDefaultAttribute(
-				`transition-delay-${breakpoint}`
-			)}
-			value={getLastBreakpointAttribute({
-				target: 'transition-delay',
-				breakpoint,
-				attributes: props,
-			})}
-			onChangeValue={val => {
-				onChange({
-					[`transition-delay-${breakpoint}`]:
-						val !== undefined && val !== '' ? val : '',
-				});
-			}}
-			min={0}
-			max={5}
-			step={0.1}
-			initial={0}
-			onReset={() =>
-				onChange({
-					[`transition-delay-${breakpoint}`]: getDefaultAttribute(
-						`transition-delay-${breakpoint}`
-					),
-				})
-			}
-			initialPosition={getDefaultAttribute(
-				`transition-delay-${breakpoint}`
-			)}
-		/>
-		<SelectControl
-			label={__('Easing', 'maxi-blocks')}
-			className={classes}
-			value={getLastBreakpointAttribute({
-				target: 'easing',
-				breakpoint,
-				attributes: props,
-			})}
-			options={[
-				{ label: __('Ease', 'maxi-blocks'), value: 'ease' },
-				{ label: __('Ease-in', 'maxi-blocks'), value: 'ease-in' },
-				{ label: __('Ease-out', 'maxi-blocks'), value: 'ease-out' },
-				{ label: __('Ease-in-out', 'maxi-blocks'), value: 'ease-in-out' },
-				{ label: __('Linear', 'maxi-blocks'), value: 'linear' },
-			]}
-			onChange={val => {
-				onChange({
-					[`easing-${breakpoint}`]: val !== undefined && val !== ''
-						? val
-						: '',
-				});
-			}}
-			onReset={() =>
-				onChange({
-					[`easing-${breakpoint}`]: getDefaultAttribute(
-						`easing-${breakpoint}`
-					),
-				})
-			}
-			initialPosition={getDefaultAttribute(`easing-${breakpoint}`)}
-		/>
+			<AdvancedNumberControl
+				label={__('Transition duration', 'maxi-blocks')}
+				className={classes}
+				defaultValue={getDefaultTransitionAttribute(
+					'transition-duration'
+				)}
+				value={getLastBreakpointAttribute({
+					target: 'transition-duration',
+					breakpoint,
+					attributes: transitionObj,
+				})}
+				onChangeValue={val => {
+					onChangeTransition({
+						[`transition-duration-${breakpoint}`]:
+							val !== undefined && val !== '' ? val : '',
+					});
+				}}
+				min={0}
+				max={5}
+				step={0.1}
+				initial={0.3}
+				onReset={() =>
+					onChangeTransition({
+						[`transition-duration-${breakpoint}`]:
+							getDefaultTransitionAttribute(
+								'transition-duration'
+							),
+					})
+				}
+				initialPosition={getDefaultAttribute(
+					`transition-duration-${breakpoint}`
+				)}
+			/>
+			<AdvancedNumberControl
+				label={__('Transition delay', 'maxi-blocks')}
+				className={classes}
+				defaultValue={getDefaultTransitionAttribute('transition-delay')}
+				value={getLastBreakpointAttribute({
+					target: 'transition-delay',
+					breakpoint,
+					attributes: transitionObj,
+				})}
+				onChangeValue={val => {
+					onChangeTransition({
+						[`transition-delay-${breakpoint}`]:
+							val !== undefined && val !== '' ? val : '',
+					});
+				}}
+				min={0}
+				max={5}
+				step={0.1}
+				initial={0}
+				onReset={() =>
+					onChangeTransition({
+						[`transition-delay-${breakpoint}`]:
+							getDefaultTransitionAttribute('transition-delay'),
+					})
+				}
+				initialPosition={getDefaultAttribute(
+					`transition-delay-${breakpoint}`
+				)}
+			/>
+			<SelectControl
+				label={__('Easing', 'maxi-blocks')}
+				className={classes}
+				value={getLastBreakpointAttribute({
+					target: 'easing',
+					breakpoint,
+					attributes: transitionObj,
+				})}
+				options={[
+					{ label: __('Ease', 'maxi-blocks'), value: 'ease' },
+					{ label: __('Ease-in', 'maxi-blocks'), value: 'ease-in' },
+					{ label: __('Ease-out', 'maxi-blocks'), value: 'ease-out' },
+					{
+						label: __('Ease-in-out', 'maxi-blocks'),
+						value: 'ease-in-out',
+					},
+					{ label: __('Linear', 'maxi-blocks'), value: 'linear' },
+				]}
+				onChange={val => {
+					onChangeTransition({
+						[`easing-${breakpoint}`]:
+							val !== undefined && val !== '' ? val : '',
+					});
+				}}
+				onReset={() =>
+					onChangeTransition({
+						[`easing-${breakpoint}`]:
+							getDefaultTransitionAttribute('easing'),
+					})
+				}
+				initialPosition={getDefaultTransitionAttribute('easing')}
+			/>
 		</>
 	);
 };

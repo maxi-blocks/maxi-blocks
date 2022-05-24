f7079858b6d5030a26c5d4a3c44392ec27d18dbc
Andrew <46112160+Fybex@users.noreply.github.com>
2022-05-24 16:56:25 +0300

    Fix getTransitionStyles

--- src/extensions/styles/helpers/getTransitionStyles.js
+++ src/extensions/styles/helpers/getTransitionStyles.js
@@ -4,24 +4,85 @@
 const breakpoints = ['general', 'xxl', 'xl', 'l', 'm', 's', 'xs'];
 
 /**
+ * Internal dependencies
+ */
+import getLastBreakpointAttribute from '../getLastBreakpointAttribute';
+
+const properties = [
+	{
+		target: 'border',
+		property: 'border',
+	},
+	{
+		target: 'box shadow',
+		property: 'box-shadow',
+	},
+	{
+		target: 'button background',
+		property: 'background',
+	},
+];
+
+/**
  * Generates size styles object
  *
  * @param {Object} obj Block size properties
  */
-const getTransitionStyles = obj => {
-	const response = {};
+const getTransitionStyles = (obj, type, rawTargets) => {
+	if (!rawTargets) return null;
 
+	const { transition } = obj;
+	const targets = !Array.isArray(rawTargets) ? [rawTargets] : rawTargets;
+
+	const transitionObj = targets.map(target => {
+		const property = properties.find(
+			property => property.target === target
+		)?.property;
+
+		return {
+			...transition[type][target],
+			property,
+		};
+	});
+
+	const response = {};
 	breakpoints.forEach(breakpoint => {
-		if (obj[`transition-${breakpoint}`])
-			response[breakpoint] = {
-				'transition-duration': `${
-					obj[`transition-duration-${breakpoint}`]
-				}s`,
-				'transition-delay': `${
-					obj[`transition-delay-${breakpoint}`]
-				}s`,
-				'transition-timing-function': obj[`transition-timing-function-${breakpoint}`],
-			};
+		let transitionString = '';
+		transitionObj.forEach(obj => {
+			const transitionDuration = getLastBreakpointAttribute({
+				target: 'transition-duration',
+				breakpoint,
+				attributes: obj,
+			});
+
+			const transitionDelay = getLastBreakpointAttribute({
+				target: 'transition-delay',
+				breakpoint,
+				attributes: obj,
+			});
+
+			const transitionTimingFunction = getLastBreakpointAttribute({
+				target: 'easing',
+				breakpoint,
+				attributes: obj,
+			});
+
+			if (
+				transitionDuration ||
+				transitionDelay ||
+				transitionTimingFunction
+			) {
+				transitionString += `${transitionDuration}s ${transitionDelay}s ${transitionTimingFunction} ${
+					obj.property || ''
+				}, `;
+			}
+		});
+
+		transitionString = transitionString.replace(/,\s*$/, '');
+
+		response[breakpoint] = {
+			transition: transitionString,
+		};
 	});
 
 	return response;

df61c3504f731c064c9cf87fb6c831196a64c1a6
Andrew Valenya <fybexit@gmail.com>
2022-06-03 14:59:56 +0300

    InteractionBuilder frontend effects

--- js/maxi-relations.js
+++ js/maxi-relations.js
@@ -1,16 +1,51 @@
 // Relations
 
-const relations = () => {
-	const getCssStyles = css => {
-		let styles = '';
+const breakpoints = ['general', 'xxl', 'xl', 'l', 'm', 's', 'xs'];
 
-		Object.entries(css).forEach(([key, value]) => {
-			if (!value.breakpoint || window.innerWidth < value.breakpoint) {
-				styles += value.content;
+const relations = () => {
+	const getCssResponsiveObj = (css, effects) => {
+		let stylesObj = {};
+		let effectsObj = {};
+
+		Object.entries(css).forEach(([breakpoint, obj]) => {
+			console.log(breakpoint, breakpoints.includes(breakpoint), obj);
+			if (
+				breakpoints.includes(breakpoint) &&
+				(window.innerWidth < obj.breakpoint || !obj.breakpoint)
+			) {
+				stylesObj = {
+					...stylesObj,
+					...obj.styles,
+				};
+
+				effectsObj = {
+					...effectsObj,
+					...(effects[`transition-duration-${breakpoint}`] && {
+						'transition-duration':
+							effects[`transition-duration-${breakpoint}`],
+					}),
+					...(effects[`transition-delay-${breakpoint}`] && {
+						'transition-delay':
+							effects[`transition-delay-${breakpoint}`],
+					}),
+					...(effects[`transition-timing-function-${breakpoint}`] && {
+						'transition-timing-function':
+							effects[`transition-timing-function-${breakpoint}`],
+					}),
+				};
 			}
 		});
 
-		return styles;
+		return {
+			css: stylesObj,
+			effectsObj,
+		};
+	};
+
+	const toggleInlineStyles = (stylesObj, element, remove = false) => {
+		Object.entries(stylesObj).forEach(([key, value]) => {
+			element.style[key] = !remove ? value : '';
+		});
 	};
 
 	maxiRelations[0]?.map(item => {
@@ -19,21 +54,33 @@ const relations = () => {
 			`.${item.uniqueID} ${item.target ?? ''}`
 		);
 
-		const css = getCssStyles(item.css);
+		const { css, effectsObj } = getCssResponsiveObj(item.css, item.effects);
+
+		const transitionString = `all ${effectsObj['transition-duration']}s ${effectsObj['transition-delay']}s ${effectsObj['transition-timing-function']}`;
+
+		let timeout;
 
 		switch (item.action) {
 			case 'hover': {
 				triggerEl.addEventListener('mouseenter', () => {
-					targetEl.style = css;
+					clearTimeout(timeout);
+					targetEl.style.transition = transitionString;
+
+					toggleInlineStyles(css, targetEl);
 				});
 
 				triggerEl.addEventListener('mouseleave', () => {
-					targetEl.style = '';
+					toggleInlineStyles(css, targetEl, true);
+
+					timeout = setTimeout(() => {
+						targetEl.style.transition = '';
+					}, item.effects['transition-duration-general'] * 1000 + 10);
 				});
 			}
 			case 'click': {
 				triggerEl.addEventListener('click', () => {
-					targetEl.style = css;
+					targetEl.style.transition = transitionString;
+					toggleInlineStyles(css, targetEl);
 				});
 			}
 		}

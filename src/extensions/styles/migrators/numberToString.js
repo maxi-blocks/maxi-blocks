d8acb7959504a8fa632ee1e706924f967c2ee768
Andrew Valenya <46112160+Fybex@users.noreply.github.com>
2022-06-07 11:47:28 +0300

    Fix migrators

--- src/extensions/styles/migrators/numberToString.js
+++ src/extensions/styles/migrators/numberToString.js
@@ -8,7 +8,7 @@ import getGroupAttributes from '../getGroupAttributes';
 import { isFinite } from 'lodash';
 
 const fromNumberToStringMigrator = ({ attributes, save }) => {
-	const targets = ['padding', 'size', 'icon', 'svg', 'position'];
+	const targets = 'position';
 
 	return {
 		isEligible(blockAttributes) {
@@ -23,6 +23,159 @@ const fromNumberToStringMigrator = ({ attributes, save }) => {
 
 				return false;
 			});
+		},
+
+		attributes: {
+			...attributes,
+			'position-general': {
+				type: 'string',
+			},
+			'position-xxl': {
+				type: 'string',
+			},
+			'position-xl': {
+				type: 'string',
+			},
+			'position-l': {
+				type: 'string',
+			},
+			'position-m': {
+				type: 'string',
+			},
+			'position-s': {
+				type: 'string',
+			},
+			'position-xs': {
+				type: 'string',
+			},
+			'position-top-general': {
+				type: 'number',
+			},
+			'position-top-xxl': {
+				type: 'number',
+			},
+			'position-top-xl': {
+				type: 'number',
+			},
+			'position-top-l': {
+				type: 'number',
+			},
+			'position-top-m': {
+				type: 'number',
+			},
+			'position-top-s': {
+				type: 'number',
+			},
+			'position-top-xs': {
+				type: 'number',
+			},
+			'position-right-general': {
+				type: 'number',
+			},
+			'position-right-xxl': {
+				type: 'number',
+			},
+			'position-right-xl': {
+				type: 'number',
+			},
+			'position-right-l': {
+				type: 'number',
+			},
+			'position-right-m': {
+				type: 'number',
+			},
+			'position-right-s': {
+				type: 'number',
+			},
+			'position-right-xs': {
+				type: 'number',
+			},
+			'position-bottom-general': {
+				type: 'number',
+			},
+			'position-bottom-xxl': {
+				type: 'number',
+			},
+			'position-bottom-xl': {
+				type: 'number',
+			},
+			'position-bottom-l': {
+				type: 'number',
+			},
+			'position-bottom-m': {
+				type: 'number',
+			},
+			'position-bottom-s': {
+				type: 'number',
+			},
+			'position-bottom-xs': {
+				type: 'number',
+			},
+			'position-left-general': {
+				type: 'number',
+			},
+			'position-left-xxl': {
+				type: 'number',
+			},
+			'position-left-xl': {
+				type: 'number',
+			},
+			'position-left-l': {
+				type: 'number',
+			},
+			'position-left-m': {
+				type: 'number',
+			},
+			'position-left-s': {
+				type: 'number',
+			},
+			'position-left-xs': {
+				type: 'number',
+			},
+			'position-sync-general': {
+				type: 'string',
+				default: 'all',
+			},
+			'position-sync-xxl': {
+				type: 'string',
+			},
+			'position-sync-xl': {
+				type: 'string',
+			},
+			'position-sync-l': {
+				type: 'string',
+			},
+			'position-sync-m': {
+				type: 'string',
+			},
+			'position-sync-s': {
+				type: 'string',
+			},
+			'position-sync-xs': {
+				type: 'string',
+			},
+			'position-unit-general': {
+				type: 'string',
+				default: 'px',
+			},
+			'position-unit-xxl': {
+				type: 'string',
+			},
+			'position-unit-xl': {
+				type: 'string',
+			},
+			'position-unit-l': {
+				type: 'string',
+			},
+			'position-unit-m': {
+				type: 'string',
+			},
+			'position-unit-s': {
+				type: 'string',
+			},
+			'position-unit-xs': {
+				type: 'string',
+			},
 		},
 
 		migrate(oldAttributes) {

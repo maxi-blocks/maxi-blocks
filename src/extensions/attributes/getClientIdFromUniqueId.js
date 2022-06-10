83baa4b47455d6f7fb3412f182dad2db1169c3ae
Andrew Valenya <46112160+Fybex@users.noreply.github.com>
2022-06-10 15:42:30 +0300

    Search clientId without using DOM

--- src/extensions/attributes/getClientIdFromUniqueId.js
+++ src/extensions/attributes/getClientIdFromUniqueId.js
@@ -1,6 +1,27 @@
+import { select } from '@wordpress/data';
+
 const getClientIdFromUniqueId = uniqueID => {
-	const element = document.querySelector(`[uniqueId="${uniqueID}"]`);
-	const clientId = element ? element.getAttribute('data-block') : null;
+	const { getBlocks } = select('core/block-editor');
+
+	const findClientId = (blocks, uniqueID) => {
+		const block = blocks.find(block => {
+			if (block.attributes.uniqueID === uniqueID) {
+				return block.clientId;
+			}
+
+			if (block.innerBlocks.length) {
+				const foundClientId = findClientId(block.innerBlocks, uniqueID);
+				if (foundClientId) {
+					return foundClientId;
+				}
+			}
+		});
+
+		return block ? block.clientId : null;
+	};
+
+	const blocks = getBlocks();
+	const clientId = findClientId(blocks, uniqueID);
 
 	return clientId;
 };

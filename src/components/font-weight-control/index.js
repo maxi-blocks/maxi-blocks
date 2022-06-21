bab6fb4ffdfb0748a7b1e74c716f76b81f7c78f1
user <engfitsum@gmail.com>
2022-06-21 22:34:10 +0300

    fixed font-weight issue on NC

--- src/components/font-weight-control/index.js
+++ src/components/font-weight-control/index.js
@@ -3,7 +3,7 @@
  */
 import { __ } from '@wordpress/i18n';
 import SelectControl from '../select-control';
-import { getWeightOptions } from '../../components/typography-control/utils';
+import { getWeightOptions } from '../typography-control/utils';
 import { loadFonts } from '../../extensions/text/fonts';
 
 const FontWeightControl = props => {

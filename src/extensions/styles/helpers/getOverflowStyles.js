4aebb6816777b13e347bd6cdc84e81575278605f
Andrew Valenya <46112160+Fybex@users.noreply.github.com>
2022-06-23 11:08:40 +0300

    Minimize overflow styles

--- src/extensions/styles/helpers/getOverflowStyles.js
+++ src/extensions/styles/helpers/getOverflowStyles.js
@@ -1,3 +1,5 @@
+import getLastBreakpointAttribute from '../getLastBreakpointAttribute';
+
 /**
  * General
  */
@@ -11,16 +13,37 @@ const breakpoints = ['general', 'xxl', 'xl', 'l', 'm', 's', 'xs'];
 const getOverflowStyles = obj => {
 	const response = {};
 
-	breakpoints.forEach(breakpoint => {
-		response[breakpoint] = {
-			...(obj[`overflow-x-${breakpoint}`] && {
-				'overflow-x': obj[`overflow-x-${breakpoint}`],
-			}),
-			...(obj[`overflow-y-${breakpoint}`] && {
-				'overflow-y': obj[`overflow-y-${breakpoint}`],
-			}),
-		};
+	const isOverflowStylesNeeded = breakpoints.some(breakpoint => {
+		const overflowX = getLastBreakpointAttribute({
+			target: 'overflow-x',
+			breakpoint,
+			attributes: obj,
+		});
+
+		const overflowY = getLastBreakpointAttribute({
+			target: 'overflow-y',
+			breakpoint,
+			attributes: obj,
+		});
+
+		if (overflowX !== 'visible' || overflowY !== 'visible') {
+			return true;
+		}
+
+		return false;
 	});
+
+	if (isOverflowStylesNeeded)
+		breakpoints.forEach(breakpoint => {
+			response[breakpoint] = {
+				...(obj[`overflow-x-${breakpoint}`] && {
+					'overflow-x': obj[`overflow-x-${breakpoint}`],
+				}),
+				...(obj[`overflow-y-${breakpoint}`] && {
+					'overflow-y': obj[`overflow-y-${breakpoint}`],
+				}),
+			};
+		});
 
 	return response;
 };

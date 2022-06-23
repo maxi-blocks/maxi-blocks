ccb5136f50030eab0bb299235739a0297ae99043
TeksPeks <53875005+TeksPeks@users.noreply.github.com>
2022-06-23 15:50:58 +0300

    shape divider migrator

--- src/extensions/styles/migrators/blockMigrator.js
+++ src/extensions/styles/migrators/blockMigrator.js
@@ -8,13 +8,19 @@ import {
 	attributes as fromFullWidthNonToResponsiveAttributes,
 	migrate as fromFullWidthNonToResponsiveMigrator,
 } from './fullWidthNonToResponsive';
+import {
+	isEligible as shapeDividerIsEligible,
+	attributes as shapeDividerAttributes,
+	migrate as shapeDividerMigrator,
+} from './shapeDividerMigrator';
 
 const blockMigrator = ({ attributes, save, prefix, isContainer = false }) => {
 	return {
 		isEligible(blockAttributes) {
 			return (
 				positionIsEligible(blockAttributes, attributes) ||
-				fromFullWidthNonToResponsiveIsEligible(blockAttributes)
+				fromFullWidthNonToResponsiveIsEligible(blockAttributes) ||
+				shapeDividerIsEligible(blockAttributes)
 			);
 		},
 
@@ -22,6 +28,7 @@ const blockMigrator = ({ attributes, save, prefix, isContainer = false }) => {
 			...attributes,
 			...positionAttributes,
 			...fromFullWidthNonToResponsiveAttributes(isContainer),
+			...shapeDividerAttributes,
 		},
 
 		migrate(oldAttributes) {
@@ -29,6 +36,7 @@ const blockMigrator = ({ attributes, save, prefix, isContainer = false }) => {
 
 			positionMigrator(newAttributes, attributes);
 			fromFullWidthNonToResponsiveMigrator(newAttributes, prefix);
+			shapeDividerMigrator(newAttributes);
 
 			return newAttributes;
 		},

9686d4377b63d686d5e22a984a8d4cf0d9523b47
Andrew Valenya <46112160+Fybex@users.noreply.github.com>
2022-07-04 14:32:00 +0300

    Move on change func's inside of SvgColorControl

--- src/blocks/svg-icon-maxi/inspector.js
+++ src/blocks/svg-icon-maxi/inspector.js
@@ -134,6 +134,9 @@ const Inspector = props => {
 												maxiSetAttributes={
 													maxiSetAttributes
 												}
+												cleanInlineStyles={
+													cleanInlineStyles
+												}
 												onChangeInline={(
 													obj,
 													target
@@ -143,27 +146,6 @@ const Inspector = props => {
 														target,
 														isMultiplySelector: true,
 													});
-												}}
-												onChangeFill={obj => {
-													maxiSetAttributes(obj);
-													if (svgType !== 'Line')
-														cleanInlineStyles(
-															'[data-fill]'
-														);
-												}}
-												onChangeStroke={obj => {
-													maxiSetAttributes(obj);
-
-													if (svgType !== 'Shape')
-														cleanInlineStyles(
-															'[data-stroke]'
-														);
-												}}
-												onChangeHoverFill={obj => {
-													maxiSetAttributes(obj);
-												}}
-												onChangeHoverStroke={obj => {
-													maxiSetAttributes(obj);
 												}}
 												blockStyle={blockStyle}
 												content={attributes.content}

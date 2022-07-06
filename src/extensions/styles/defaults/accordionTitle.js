3fd34b89e8b487fd0019a807daf1c34f914e627d
TeksPeks <verstyukm@gmail.com>
2022-07-06 11:49:25 +0300

    title defaults

--- src/extensions/styles/defaults/accordionTitle.js
+++ src/extensions/styles/defaults/accordionTitle.js
@@ -2,10 +2,9 @@ import paletteAttributesCreator from '../paletteAttributesCreator';
 
 const accordionTitle = {
 	titleLevel: { type: 'string', default: 'h6' },
-	...paletteAttributesCreator({ prefix: 'title-', palette: 6 }),
+	...paletteAttributesCreator({ prefix: 'title-', palette: 5 }),
 	...paletteAttributesCreator({
 		prefix: 'title-background-',
-		palette: 1,
 	}),
 };
 

951d2255cee84f04176fe4152771720cca5b77ef
Andrew Valenya <46112160+Fybex@users.noreply.github.com>
2022-07-14 09:17:38 +0300

    Inspector fixes
    - move inspectorTabs.blockSettings outside of settings accordion
    - replace parentBlockStyle with blockStyle
    - remove changeSVGContent

--- src/blocks/map-maxi/inspector.js
+++ src/blocks/map-maxi/inspector.js
@@ -34,17 +34,19 @@ import * as inspectorTabs from '../../components/inspector-tabs';
 const Inspector = props => {
 	const {
 		attributes,
-		changeSVGContent,
 		deviceType,
 		maxiSetAttributes,
 		clientId,
-		parentBlockStyle,
+		blockStyle,
 		apiKey,
 	} = props;
 
 	return (
 		<InspectorControls>
 			{inspectorTabs.responsiveInfoBox({ props })}
+			{inspectorTabs.blockSettings({
+				props,
+			})}
 			<SettingTabsControl
 				target='sidebar-settings-tabs'
 				disablePadding
@@ -54,295 +56,278 @@ const Inspector = props => {
 					{
 						label: __('Settings', 'maxi-blocks'),
 						content: (
-							<>
-								{inspectorTabs.blockSettings({
-									props,
-								})}
-								<AccordionControl
-									isPrimary
-									items={[
-										{
-											label: __(
-												'Configure map',
-												'maxi-blocks'
-											),
-											content: (
-												<MapControl
-													{...getGroupAttributes(
-														attributes,
-														'map'
-													)}
-													onChange={obj =>
-														maxiSetAttributes(obj)
-													}
-													hasApiKey={!isEmpty(apiKey)}
-												/>
-											),
-										},
-										{
-											label: __(
-												'Map marker',
-												'maxi-blocks'
-											),
-											content: (
-												<MapMarkersControl
-													{...getGroupAttributes(
-														attributes,
-														['map', 'svg']
-													)}
-													onChange={obj =>
-														maxiSetAttributes(obj)
-													}
-													parentBlockStyle={
-														parentBlockStyle
-													}
-													changeSVGContent={
-														changeSVGContent
-													}
-													deviceType={deviceType}
-												/>
-											),
-										},
-										{
-											label: __(
-												'Marker pop-up text',
-												'maxi-blocks'
-											),
-											content: (
-												<SettingTabsControl
-													items={[
-														{
-															label: __(
-																'Title',
-																'maxi-blocks'
-															),
-															content: (
-																<>
-																	<span>
-																		Marker
-																		title
-																		text
-																	</span>
-																	<FontLevelControl
-																		{...getGroupAttributes(
+							<AccordionControl
+								isPrimary
+								items={[
+									{
+										label: __(
+											'Configure map',
+											'maxi-blocks'
+										),
+										content: (
+											<MapControl
+												{...getGroupAttributes(
+													attributes,
+													'map'
+												)}
+												onChange={obj =>
+													maxiSetAttributes(obj)
+												}
+												hasApiKey={!isEmpty(apiKey)}
+											/>
+										),
+									},
+									{
+										label: __('Map marker', 'maxi-blocks'),
+										content: (
+											<MapMarkersControl
+												{...getGroupAttributes(
+													attributes,
+													['map', 'svg']
+												)}
+												onChange={obj =>
+													maxiSetAttributes(obj)
+												}
+												blockStyle={blockStyle}
+												deviceType={deviceType}
+											/>
+										),
+									},
+									{
+										label: __(
+											'Marker pop-up text',
+											'maxi-blocks'
+										),
+										content: (
+											<SettingTabsControl
+												items={[
+													{
+														label: __(
+															'Title',
+															'maxi-blocks'
+														),
+														content: (
+															<>
+																<span>
+																	Marker title
+																	text
+																</span>
+																<FontLevelControl
+																	{...getGroupAttributes(
+																		attributes,
+																		'map'
+																	)}
+																	value={
+																		attributes[
+																			'map-marker-heading-level'
+																		]
+																	}
+																	onChange={obj => {
+																		maxiSetAttributes(
+																			{
+																				'map-marker-heading-level':
+																					obj.textLevel,
+																			}
+																		);
+																	}}
+																/>
+																<TypographyControl
+																	typography={{
+																		...getGroupAttributes(
 																			attributes,
-																			'map'
-																		)}
-																		value={
-																			attributes[
-																				'map-marker-heading-level'
-																			]
-																		}
-																		onChange={obj => {
-																			maxiSetAttributes(
-																				{
-																					'map-marker-heading-level':
-																						obj.textLevel,
-																				}
-																			);
-																		}}
-																	/>
-																	<TypographyControl
-																		typography={{
-																			...getGroupAttributes(
-																				attributes,
-																				'typography'
-																			),
-																		}}
-																		onChange={obj => {
-																			maxiSetAttributes(
-																				obj
-																			);
-																		}}
-																		textLevel={
-																			attributes[
-																				'map-marker-heading-level'
-																			]
-																		}
-																		hideAlignment
-																		clientId={
-																			clientId
-																		}
-																		blockStyle={
-																			parentBlockStyle
-																		}
-																		breakpoint={
-																			deviceType
-																		}
-																	/>
-																</>
-															),
-														},
-														{
-															label: __(
-																'Description',
-																'maxi-blocks'
-															),
-															content: (
-																<>
-																	<span>
-																		Marker
-																		description
-																		text
-																	</span>
-																	<TypographyControl
-																		typography={{
-																			...getGroupAttributes(
-																				attributes,
-																				'typography',
-																				false,
-																				'description-'
-																			),
-																		}}
-																		textLevel='p'
-																		prefix='description-'
-																		onChange={obj => {
-																			maxiSetAttributes(
-																				obj
-																			);
-																		}}
-																		hideAlignment
-																		clientId={
-																			clientId
-																		}
-																		blockStyle={
-																			parentBlockStyle
-																		}
-																		breakpoint={
-																			deviceType
-																		}
-																	/>
-																</>
-															),
-														},
-													]}
+																			'typography'
+																		),
+																	}}
+																	onChange={obj => {
+																		maxiSetAttributes(
+																			obj
+																		);
+																	}}
+																	textLevel={
+																		attributes[
+																			'map-marker-heading-level'
+																		]
+																	}
+																	hideAlignment
+																	clientId={
+																		clientId
+																	}
+																	blockStyle={
+																		blockStyle
+																	}
+																	breakpoint={
+																		deviceType
+																	}
+																	disableCustomFormats
+																/>
+															</>
+														),
+													},
+													{
+														label: __(
+															'Description',
+															'maxi-blocks'
+														),
+														content: (
+															<>
+																<span>
+																	Marker
+																	description
+																	text
+																</span>
+																<TypographyControl
+																	typography={{
+																		...getGroupAttributes(
+																			attributes,
+																			'typography',
+																			false,
+																			'description-'
+																		),
+																	}}
+																	textLevel='p'
+																	prefix='description-'
+																	onChange={obj => {
+																		maxiSetAttributes(
+																			obj
+																		);
+																	}}
+																	hideAlignment
+																	clientId={
+																		clientId
+																	}
+																	blockStyle={
+																		blockStyle
+																	}
+																	breakpoint={
+																		deviceType
+																	}
+																	disableCustomFormats
+																/>
+															</>
+														),
+													},
+												]}
+											/>
+										),
+									},
+									{
+										label: __(
+											'Marker pop-up',
+											'maxi-blocks'
+										),
+										content: (
+											<MapPopupsControl
+												onChange={obj =>
+													maxiSetAttributes(obj)
+												}
+												blockStyle={blockStyle}
+												deviceType={deviceType}
+												clientId={clientId}
+												attributes={attributes}
+											/>
+										),
+									},
+									...inspectorTabs.border({
+										props,
+									}),
+									...inspectorTabs.boxShadow({
+										props,
+									}),
+									...inspectorTabs.size({
+										props,
+										block: true,
+									}),
+									...inspectorTabs.marginPadding({
+										props,
+									}),
+									{
+										label: __(
+											'Map interaction',
+											'maxi-blocks'
+										),
+										content: (
+											<>
+												<ToggleSwitch
+													label={__(
+														'Map dragging',
+														'maxi-blocks'
+													)}
+													selected={
+														attributes[
+															'map-dragging'
+														]
+													}
+													onChange={() => {
+														maxiSetAttributes({
+															'map-dragging':
+																!attributes[
+																	'map-dragging'
+																],
+														});
+													}}
 												/>
-											),
-										},
-										{
-											label: __(
-												'Marker pop-up',
-												'maxi-blocks'
-											),
-											content: (
-												<MapPopupsControl
-													onChange={obj =>
-														maxiSetAttributes(obj)
+												<ToggleSwitch
+													label={__(
+														'Touch zoom',
+														'maxi-blocks'
+													)}
+													selected={
+														attributes[
+															'map-touch-zoom'
+														]
 													}
-													parentBlockStyle={
-														parentBlockStyle
-													}
-													deviceType={deviceType}
-													changeSVGContent={
-														changeSVGContent
-													}
-													clientId={clientId}
-													attributes={attributes}
+													onChange={() => {
+														maxiSetAttributes({
+															'map-touch-zoom':
+																!attributes[
+																	'map-touch-zoom'
+																],
+														});
+													}}
 												/>
-											),
-										},
-										...inspectorTabs.border({
-											props,
-										}),
-										...inspectorTabs.boxShadow({
-											props,
-										}),
-										...inspectorTabs.size({
-											props,
-											block: true,
-										}),
-										...inspectorTabs.marginPadding({
-											props,
-										}),
-										{
-											label: __(
-												'Map interaction',
-												'maxi-blocks'
-											),
-											content: (
-												<>
-													<ToggleSwitch
-														label={__(
-															'Map dragging',
-															'maxi-blocks'
-														)}
-														selected={
-															attributes[
-																'map-dragging'
-															]
-														}
-														onChange={() => {
-															maxiSetAttributes({
-																'map-dragging':
-																	!attributes[
-																		'map-dragging'
-																	],
-															});
-														}}
-													/>
-													<ToggleSwitch
-														label={__(
-															'Touch zoom',
-															'maxi-blocks'
-														)}
-														selected={
-															attributes[
-																'map-touch-zoom'
-															]
-														}
-														onChange={() => {
-															maxiSetAttributes({
-																'map-touch-zoom':
-																	!attributes[
-																		'map-touch-zoom'
-																	],
-															});
-														}}
-													/>
-													<ToggleSwitch
-														label={__(
-															'Double click zoom',
-															'maxi-blocks'
-														)}
-														selected={
-															attributes[
-																'map-double-click-zoom'
-															]
-														}
-														onChange={() => {
-															maxiSetAttributes({
-																'map-double-click-zoom':
-																	!attributes[
-																		'map-double-click-zoom'
-																	],
-															});
-														}}
-													/>
-													<ToggleSwitch
-														label={__(
-															'Scroll wheel zoom',
-															'maxi-blocks'
-														)}
-														selected={
-															attributes[
-																'map-scroll-wheel-zoom'
-															]
-														}
-														onChange={() => {
-															maxiSetAttributes({
-																'map-scroll-wheel-zoom':
-																	!attributes[
-																		'map-scroll-wheel-zoom'
-																	],
-															});
-														}}
-													/>
-												</>
-											),
-										},
-									]}
-								/>
-							</>
+												<ToggleSwitch
+													label={__(
+														'Double click zoom',
+														'maxi-blocks'
+													)}
+													selected={
+														attributes[
+															'map-double-click-zoom'
+														]
+													}
+													onChange={() => {
+														maxiSetAttributes({
+															'map-double-click-zoom':
+																!attributes[
+																	'map-double-click-zoom'
+																],
+														});
+													}}
+												/>
+												<ToggleSwitch
+													label={__(
+														'Scroll wheel zoom',
+														'maxi-blocks'
+													)}
+													selected={
+														attributes[
+															'map-scroll-wheel-zoom'
+														]
+													}
+													onChange={() => {
+														maxiSetAttributes({
+															'map-scroll-wheel-zoom':
+																!attributes[
+																	'map-scroll-wheel-zoom'
+																],
+														});
+													}}
+												/>
+											</>
+										),
+									},
+								]}
+							/>
 						),
 					},
 					{

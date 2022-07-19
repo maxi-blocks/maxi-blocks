7055a06c3a7aae3cf84b4dde9115c6c89a4a901b
Andrew Valenya <46112160+Fybex@users.noreply.github.com>
2022-07-19 15:39:22 +0300

    transitionMigrator

---
+++ src/extensions/styles/migrators/transitionMigrator.js
@@ -0,0 +1,29 @@
+const NEW_KEYS = ['icon colour', 'icon width', 'icon background'];
+
+const isEligible = blockAttributes => {
+	const { transition } = blockAttributes;
+
+	if (transition && transition.block.icon) {
+		return true;
+	}
+
+	return false;
+};
+
+const migrate = ({ newAttributes }) => {
+	const { transition } = newAttributes;
+
+	if (transition.block.icon) {
+		NEW_KEYS.forEach(key => {
+			transition.block[key] = {
+				...transition.block.icon,
+			};
+		});
+	}
+
+	delete transition.block.icon;
+
+	return newAttributes;
+};
+
+export default { isEligible, migrate };

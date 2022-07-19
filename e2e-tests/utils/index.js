47eef21152220db004742de6c8df57ae498fa9ec
TeksPeks <verstyukm@gmail.com>
2022-07-19 17:55:55 +0300

    global SC tests

--- e2e-tests/utils/index.js
+++ e2e-tests/utils/index.js
@@ -18,6 +18,6 @@ export { default as openPreviewPage } from './openPreviewPage';
 export { default as openSidebarTab } from './openSidebarTab';
 export { default as removeBackgroundLayers } from './removeBackgroundLayers';
 export { default as resettingAttributes } from './resettingAttributes';
-export { default as checkSCResult } from './checkSCResult';
+export { checkSCResult, receiveSelectedMaxiStyleCard } from './checkSCResult';
 export { default as changeResponsive } from './changeResponsive';
 export { default as setAttributes } from './setAttributes';

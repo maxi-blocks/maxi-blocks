5c2af8b27a1a0d3d0cd35a531031bf64cc874571
TeksPeks <verstyukm@gmail.com>
2022-07-20 18:03:48 +0300

    colour preset responsive

--- e2e-tests/editor/style-cards/colorPreset.spec.js
+++ e2e-tests/editor/style-cards/colorPreset.spec.js
@@ -7,7 +7,11 @@ import {
 	pressKeyWithModifier,
 } from '@wordpress/e2e-test-utils';
 
-import { getStyleCardEditor, checkSCResult } from '../../utils';
+import {
+	getStyleCardEditor,
+	checkSCResult,
+	changeResponsive,
+} from '../../utils';
 
 describe('StyleCards ColorPresets', () => {
 	it('Check Quick Pick Colour Presets', async () => {
@@ -18,6 +22,61 @@ describe('StyleCards ColorPresets', () => {
 			page,
 			accordion: 'color',
 		});
+
+		// ColorControl check palette-color
+		await page.$$eval(
+			'.maxi-accordion-control__item__panel .maxi-style-cards__quick-color-presets .maxi-style-cards__quick-color-presets__box',
+			buttons => buttons[3].click()
+		);
+
+		const colorInput = await page.$eval(
+			'.maxi-style-cards-control__sc__color-4-light input',
+			input => input.value
+		);
+
+		expect(colorInput).toStrictEqual('#FF4A17');
+
+		// ColorControl check custom-color
+		await page.$eval(
+			'.maxi-color-control .maxi-color-control__color input',
+			input => input.focus()
+		);
+
+		await pressKeyWithModifier('primary', 'a');
+		await page.keyboard.type('106D3C');
+
+		const customColor = await page.$eval(
+			'.maxi-color-control .maxi-color-control__color input',
+			input => input.value
+		);
+
+		expect(customColor).toStrictEqual('106D3C');
+
+		await page.waitForTimeout(150);
+		expect(await checkSCResult(page)).toMatchSnapshot();
+
+		// Test reset
+		await page.$eval(
+			'.maxi-accordion-control__item__panel button',
+			button => button.click()
+		);
+
+		expect(colorInput).toStrictEqual('#FF4A17');
+
+		await page.waitForTimeout(150);
+		expect(await checkSCResult(page)).toMatchSnapshot();
+	});
+
+	it('Should work on responsive', async () => {
+		await createNewPost();
+		await setBrowserViewport('large');
+
+		await getStyleCardEditor({
+			page,
+			accordion: 'color',
+		});
+
+		await changeResponsive(page, 'm');
 
 		// ColorControl check palette-color
 		await page.$$eval(

973071a031c13b46e49d30e5781e3e06cfad1d63
Andrew Valenya <46112160+Fybex@users.noreply.github.com>
2022-07-26 08:17:17 +0300

    container-maxi copy-paste

--- src/blocks/container-maxi/copy-paste-mapping.js
+++ src/blocks/container-maxi/copy-paste-mapping.js
@@ -1,122 +1,58 @@
+import { capitalize } from 'lodash';
+
+const shapeDividerCopyPasteGenerator = position => {
+	return {
+		[`${capitalize(position)} shape divider`]: {
+			group: {
+				'Divider status': `shape-divider-${position}-status`,
+				'Divider style': `shape-divider-${position}-shape-style`,
+				'Divider opacity': `shape-divider-${position}-opacity`,
+				'Divider color': {
+					props: [
+						`shape-divider-${position}-palette-color`,
+						`shape-divider-${position}-color`,
+						`shape-divider-${position}-palette-status`,
+					],
+				},
+				'Divider height': `shape-divider-${position}-height`,
+				'Divider height unit': `shape-divider-${position}-height-unit`,
+				'Divider scroll effect': `shape-divider-${position}-effects-status`,
+			},
+		},
+	};
+};
+
 const copyPasteMapping = {
-	_order: [
-		'Callout arrow',
-		'Top shape divider',
-		'Bottom shape divider',
-		'Background',
-		'Border',
-		'Box shadow',
-		'Size',
-		'Margin/Padding',
-	],
 	settings: {
-		withBreakpoint: {
-			arrow: {
-				groupLabel: 'Callout arrow',
-				props: {
-					'arrow-status': 'Show arrow',
-					'arrow-side': 'Arrow side',
-					'arrow-position': 'Arrow position',
-					'arrow-width': 'Arrow size',
-				},
+		'Callout arrow': {
+			group: {
+				'Show arrow': 'arrow-status',
+				'Arrow side': 'arrow-side',
+				'Arrow position': 'arrow-position',
+				'Arrow size': 'arrow-width',
 			},
+			hasBreakpoints: true,
 		},
-		blockSpecific: {
-			topShapeDivider: {
-				groupLabel: 'Top shape divider',
-				props: {
-					'shape-divider-top-status': 'Divider status',
-					'shape-divider-top-shape-style': 'Divider style',
-					'shape-divider-top-opacity': 'Divider opacity',
-					'shape-divider-top-color': {
-						label: 'Divider color',
-						props: [
-							'shape-divider-top-palette-color',
-							'shape-divider-top-color',
-							'shape-divider-top-palette-status',
-						],
-					},
-					'shape-divider-top-height': 'Divider height',
-					'shape-divider-top-height-unit': 'Divider height unit',
-					'shape-divider-top-effects-status': 'Divider scroll effect',
-				},
-			},
-			bottomShapeDivider: {
-				groupLabel: 'Bottom shape divider',
-				props: {
-					'shape-divider-bottom-status': 'Divider status',
-					'shape-divider-bottom-shape-style': 'Divider style',
-					'shape-divider-bottom-opacity': 'Divider opacity',
-					'shape-divider-bottom-color': {
-						label: 'Divider colour',
-						props: [
-							'shape-divider-bottom-palette-color',
-							'shape-divider-bottom-palette-status',
-							'shape-divider-bottom-color',
-						],
-					},
-					'shape-divider-bottom-height': 'Divider height',
-					'shape-divider-bottom-height-unit': 'Divider height unit',
-					'shape-divider-bottom-effects-status':
-						'Divider scroll effect',
-				},
-			},
-			size: {
-				groupLabel: 'Size',
-				props: {
-					blockFullWidth: 'Full width',
-					size: { label: 'Size', type: 'withoutPrefix' },
-				},
-			},
+		...shapeDividerCopyPasteGenerator('top'),
+		...shapeDividerCopyPasteGenerator('bottom'),
+		Background: {
+			template: 'blockBackground',
 		},
-		withoutPrefix: {
-			blockBackground: 'Background',
-			border: {
-				groupLabel: 'Border',
-				props: {
-					border: 'Border',
-					borderWidth: 'Border width',
-					borderRadius: 'Border radius',
-					borderHover: 'Border hover',
-					borderWidthHover: 'Border width hover',
-					borderRadiusHover: 'Border radius hover',
-				},
-			},
-			boxShadow: {
-				groupLabel: 'Box shadow',
-				props: {
-					boxShadow: 'Box shadow',
-					boxShadowHover: 'Box shadow hover',
-				},
-			},
-			'margin-padding': {
-				groupLabel: 'Margin/Padding',
-				props: { margin: 'Margin', padding: 'Padding' },
-			},
+		Border: {
+			template: 'border',
+		},
+		'Box shadow': {
+			template: 'boxShadow',
+		},
+		Size: {
+			template: 'size',
+		},
+		'Margin/Padding': {
+			template: 'marginPadding',
 		},
 	},
 	advanced: {
-		blockSpecific: {
-			extraClassName: 'Custom CSS classes',
-			anchorLink: {
-				label: 'Anchor',
-				value: ['anchorLink', 'linkSettings'],
-			},
-			relations: 'Interaction',
-		},
-		withoutPrefix: {
-			breakpoints: 'Breakpoints',
-			customCss: 'Custom CSS',
-			scroll: 'Scroll effects',
-			transform: 'Transform',
-			transition: 'Hyperlink hover transition',
-			display: 'Show/hide block',
-			opacity: 'Opacity',
-			position: 'Position',
-			overflow: 'Overflow',
-			flex: 'Flexbox',
-			zIndex: 'Z-index',
-		},
+		template: 'advanced',
 	},
 };
 

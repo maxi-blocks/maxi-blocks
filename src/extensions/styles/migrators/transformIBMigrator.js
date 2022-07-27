d2417a1c692d5f1836deea22e752de5b59f1a7ca
Josh <jjuancap@gmail.com>
2022-07-27 16:50:03 +0200

    Fixes minor bug

--- src/extensions/styles/migrators/transformIBMigrator.js
+++ src/extensions/styles/migrators/transformIBMigrator.js
@@ -1,10 +1,41 @@
 /**
+ * Internal dependencies
+ */
+import { selectorsButton } from '../../../blocks/button-maxi/custom-css';
+import { selectorsColumn } from '../../../blocks/column-maxi/custom-css';
+import { selectorsContainer } from '../../../blocks/container-maxi/custom-css';
+import { selectorsDivider } from '../../../blocks/divider-maxi/custom-css';
+import { selectorsGroup } from '../../../blocks/group-maxi/custom-css';
+import { selectorsImage } from '../../../blocks/image-maxi/custom-css';
+import { selectorsMap } from '../../../blocks/map-maxi/custom-css';
+import { selectorsNumberCounter } from '../../../blocks/number-counter-maxi/custom-css';
+import { selectorsRow } from '../../../blocks/row-maxi/custom-css';
+import { selectorsSvgIcon } from '../../../blocks/svg-icon-maxi/custom-css';
+import { selectorsText } from '../../../blocks/text-maxi/custom-css';
+import { selectorsVideo } from '../../../blocks/video-maxi/custom-css';
+
+/**
  * External dependencies
  */
-import { isEmpty } from 'lodash';
+import { isEmpty, findKey } from 'lodash';
 import { splitValueAndUnit } from '../utils';
 
 const breakpoints = ['general', 'xxl', 'xl', 'l', 'm', 's', 'xs'];
+
+const selectorsDictionary = {
+	'button-maxi': selectorsButton,
+	'column-maxi': selectorsColumn,
+	'container-maxi': selectorsContainer,
+	'divider-maxi': selectorsDivider,
+	'group-maxi': selectorsGroup,
+	'image-maxi': selectorsImage,
+	'map-maxi': selectorsMap,
+	'number-counter-maxi': selectorsNumberCounter,
+	'row-maxi': selectorsRow,
+	'svg-icon-maxi': selectorsSvgIcon,
+	'text-maxi': selectorsText,
+	'video-maxi': selectorsVideo,
+};
 
 const isEligible = blockAttributes => {
 	const { relations } = blockAttributes;
@@ -23,7 +54,7 @@ const isEligible = blockAttributes => {
 	return isOldStructure;
 };
 
-const getAttributesFromStyle = styles => {
+const getAttributesFromStyle = (styles, selector) => {
 	const result = {};
 
 	breakpoints.forEach(breakpoint => {
@@ -51,11 +82,14 @@ const getAttributesFromStyle = styles => {
 			if (['scale', 'rotate'].includes(type) || !isOriginWithUnit) {
 				result[`transform-${type}-${breakpoint}`] = {
 					...result[`transform-${type}-${breakpoint}`],
-					canvas: {
-						...result[`transform-${type}-${breakpoint}`]?.canvas,
+					[selector]: {
+						...result[`transform-${type}-${breakpoint}`]?.[
+							selector
+						],
 						normal: {
-							...result[`transform-${type}-${breakpoint}`]?.canvas
-								.normal,
+							...result[`transform-${type}-${breakpoint}`]?.[
+								selector
+							].normal,
 							[axis]: value,
 						},
 					},
@@ -66,11 +100,14 @@ const getAttributesFromStyle = styles => {
 
 				result[`transform-${type}-${breakpoint}`] = {
 					...result[`transform-${type}-${breakpoint}`],
-					canvas: {
-						...result[`transform-${type}-${breakpoint}`]?.canvas,
+					[selector]: {
+						...result[`transform-${type}-${breakpoint}`]?.[
+							selector
+						],
 						normal: {
-							...result[`transform-${type}-${breakpoint}`]?.canvas
-								.normal,
+							...result[`transform-${type}-${breakpoint}`]?.[
+								selector
+							].normal,
 							[axis]: num,
 							[`${axis}-unit`]: unit,
 						},
@@ -91,12 +128,21 @@ const migrate = ({ newAttributes }) => {
 	newRelations.forEach((relation, i) => {
 		if (relation.settings !== 'Transform') return;
 
-		const { attributes, css } = relation;
+		const { attributes, css, uniqueID } = relation;
+
+		// Gets the type of block to look for its selectors on the dictionary
+		const typeOfTarget = uniqueID.slice(0, uniqueID.lastIndexOf('-'));
+
+		// Returns the empty target selector, so the main one
+		const selector = findKey(
+			selectorsDictionary[typeOfTarget],
+			selector => selector.normal.target === ''
+		);
 
 		newRelations[i].attributes = {
 			...attributes,
-			...getAttributesFromStyle(css),
-			'transform-target': 'canvas',
+			...getAttributesFromStyle(css, selector),
+			'transform-target': selector,
 		};
 		newRelations[i].css = { '': { ...css } };
 	});

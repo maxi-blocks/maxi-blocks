92551fd6b15e40d4eaee31623419942f9bc75fb9
Myroslav <53875005+TeksPeks@users.noreply.github.com>
2022-07-28 10:38:35 +0300

    Style cards tests (#3502)
    * enable heading test
    
    * colour preset reset
    
    * global SC tests
    
    * export/import test
    
    * small improvements
    
    * check style card is deleted on all pages
    
    * add direction to typography style test util
    
    * headings tab responsive
    
    * button responsive test
    
    * colour preset responsive
    
    * paragraph responsive test
    
    * update snapshot
    
    * update snapshot after merge
    
    * move indent to addTypographyStyle
    
    * some improvements
    
    * fix colour reset tests
    
    * implement new responsive test strategy on button
    
    * add new responsive test on heading and paragraph
    
    * remove responsive test from colorPreset tab
    
    * update snapshot
    
    * add instance of typoraphyControl
    
    * change direction
    
    * blur input
    
    * getElementAttribute enhancement
    
    Co-authored-by: Josh <50477222+Naaaaiix@users.noreply.github.com>
    
    * Revert "getElementAttribute enhancement"
    
    This reverts commit 530fb407126901ccc4af974e1c5ffbfd50623ebb.
    
    Co-authored-by: Josh <50477222+Naaaaiix@users.noreply.github.com>

--- e2e-tests/utils/addTypographyStyle.js
+++ e2e-tests/utils/addTypographyStyle.js
@@ -1,6 +1,12 @@
 /**
  * WordPress dependencies
  */
+import { pressKeyWithModifier } from '@wordpress/e2e-test-utils';
+
+/**
+ * Internal dependencies
+ */
+import getElementAttribute from './getElementAttribute';
 
 const addTypographyStyle = async ({
 	instance,
@@ -9,32 +15,65 @@ const addTypographyStyle = async ({
 	transform,
 	style,
 	orientation,
+	direction,
+	indent,
 }) => {
+	const response = {};
 	// Weight, Transform, Style, Decoration
 	const weightSelector = await instance.$(
 		'.maxi-typography-control__weight .maxi-base-control__field select'
 	);
-	await weightSelector.select(weight);
+	if (weight) await weightSelector.select(weight);
+	response.weight = await getElementAttribute(weightSelector, 'value');
 
 	const transformSelector = await instance.$(
 		'.maxi-typography-control__transform .maxi-base-control__field select'
 	);
-	await transformSelector.select(transform);
+	if (transform) await transformSelector.select(transform);
+	response.transform = await getElementAttribute(transformSelector, 'value');
 
 	const fontStyleSelector = await instance.$(
 		'.maxi-typography-control__font-style .maxi-base-control__field select'
 	);
-	await fontStyleSelector.select(style);
+	if (style) await fontStyleSelector.select(style);
+	response.style = await getElementAttribute(fontStyleSelector, 'value');
 
 	const decorationSelector = await instance.$(
 		'.maxi-typography-control__decoration .maxi-base-control__field select'
 	);
-	await decorationSelector.select(decoration);
+	if (decoration) await decorationSelector.select(decoration);
+	response.decoration = await getElementAttribute(
+		decorationSelector,
+		'value'
+	);
 
 	const orientationSelector = await instance.$(
 		'.maxi-typography-control__orientation .maxi-base-control__field select'
 	);
-	await orientationSelector.select(orientation);
+	if (orientation) await orientationSelector.select(orientation);
+	response.orientation = await getElementAttribute(
+		orientationSelector,
+		'value'
+	);
+
+	const directionSelector = await instance.$(
+		'.maxi-typography-control__direction .maxi-base-control__field select'
+	);
+	if (direction) await directionSelector.select(direction);
+	response.direction = await getElementAttribute(directionSelector, 'value');
+
+	const textIndextInput = await instance.$(
+		'.maxi-typography-control__text-indent input'
+	);
+
+	if (indent) {
+		textIndextInput.focus();
+		await pressKeyWithModifier('primary', 'a');
+		await textIndextInput.type(indent);
+	}
+	response.indent = await getElementAttribute(textIndextInput, 'value');
+
+	return response;
 };
 
 export default addTypographyStyle;

7e9758ac8715eae7db9b4a94d90d3466326ca781
Andrew Valenya <46112160+Fybex@users.noreply.github.com>
2022-08-10 16:22:51 +0300

    Simplify `getMapContainer`

--- e2e-tests/blocks/map-maxi/utils/getMapContainer.js
+++ e2e-tests/blocks/map-maxi/utils/getMapContainer.js
@@ -1,9 +1,4 @@
-const getMapContainer = async page => {
-	const map = await page.$(
-		'.maxi-map-block .maxi-map-block__container .leaflet-container'
-	);
-
-	return map;
-};
+const getMapContainer = async page =>
+	page.$('.maxi-map-block .maxi-map-block__container .leaflet-container');
 
 export default getMapContainer;

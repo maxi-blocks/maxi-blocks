0a0338752ed9ef82b4d7df14898aa0449a4df662
Andrew Valenya <46112160+Fybex@users.noreply.github.com>
2022-08-10 17:26:23 +0300

    Remove unnecessary return

--- src/blocks/map-maxi/components/map-interaction-control/index.js
+++ src/blocks/map-maxi/components/map-interaction-control/index.js
@@ -8,49 +8,47 @@ import { __ } from '@wordpress/i18n';
  */
 import { ToggleSwitch } from '../../../../components';
 
-const MapInteractionControl = ({ onChange, ...attributes }) => {
-	return (
-		<>
-			<ToggleSwitch
-				label={__('Map dragging', 'maxi-blocks')}
-				selected={attributes['map-dragging']}
-				onChange={() => {
-					onChange({
-						'map-dragging': !attributes['map-dragging'],
-					});
-				}}
-			/>
-			<ToggleSwitch
-				label={__('Touch zoom', 'maxi-blocks')}
-				selected={attributes['map-touch-zoom']}
-				onChange={() => {
-					onChange({
-						'map-touch-zoom': !attributes['map-touch-zoom'],
-					});
-				}}
-			/>
-			<ToggleSwitch
-				label={__('Double click zoom', 'maxi-blocks')}
-				selected={attributes['map-double-click-zoom']}
-				onChange={() => {
-					onChange({
-						'map-double-click-zoom':
-							!attributes['map-double-click-zoom'],
-					});
-				}}
-			/>
-			<ToggleSwitch
-				label={__('Scroll wheel zoom', 'maxi-blocks')}
-				selected={attributes['map-scroll-wheel-zoom']}
-				onChange={() => {
-					onChange({
-						'map-scroll-wheel-zoom':
-							!attributes['map-scroll-wheel-zoom'],
-					});
-				}}
-			/>
-		</>
-	);
-};
+const MapInteractionControl = ({ onChange, ...attributes }) => (
+	<>
+		<ToggleSwitch
+			label={__('Map dragging', 'maxi-blocks')}
+			selected={attributes['map-dragging']}
+			onChange={() => {
+				onChange({
+					'map-dragging': !attributes['map-dragging'],
+				});
+			}}
+		/>
+		<ToggleSwitch
+			label={__('Touch zoom', 'maxi-blocks')}
+			selected={attributes['map-touch-zoom']}
+			onChange={() => {
+				onChange({
+					'map-touch-zoom': !attributes['map-touch-zoom'],
+				});
+			}}
+		/>
+		<ToggleSwitch
+			label={__('Double click zoom', 'maxi-blocks')}
+			selected={attributes['map-double-click-zoom']}
+			onChange={() => {
+				onChange({
+					'map-double-click-zoom':
+						!attributes['map-double-click-zoom'],
+				});
+			}}
+		/>
+		<ToggleSwitch
+			label={__('Scroll wheel zoom', 'maxi-blocks')}
+			selected={attributes['map-scroll-wheel-zoom']}
+			onChange={() => {
+				onChange({
+					'map-scroll-wheel-zoom':
+						!attributes['map-scroll-wheel-zoom'],
+				});
+			}}
+		/>
+	</>
+);
 
 export default MapInteractionControl;

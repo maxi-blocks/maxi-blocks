44221ce2133fe4382c26b6199234c28cc3d6e379
Marko64 <marko.niksicafe@gmail.com>
2022-08-12 11:28:46 +0200

    Removed BaseControl and props we don't need

--- src/components/text-input/index.js
+++ src/components/text-input/index.js
@@ -3,22 +3,11 @@
  */
 import { useInstanceId } from '@wordpress/compose';
 import { useState } from '@wordpress/element';
-/**
- * Internal dependencies
- */
-import BaseControl from '../base-control';
-
-/**
- * External dependencies
- */
-import classnames from 'classnames';
 
 /**
  * Component
  */
 export default function TextInput({
-	label,
-	hideLabelFromVision,
 	value,
 	help,
 	className,
@@ -31,12 +20,6 @@ export default function TextInput({
 	const instanceId = useInstanceId(TextInput);
 	const id = `inspector-text-input-${instanceId}`;
 
-	const classes = classnames(
-		'maxi-text-input',
-		isFullwidth && ' maxi-text-input--fullwidth',
-		className
-	);
-
 	const [inputValue, setInputValue] = useState('');
 	const valueChange = e => {
 		setInputValue(e.target.value);
@@ -46,22 +29,14 @@ export default function TextInput({
 	};
 
 	return (
-		<BaseControl
-			label={label}
-			hideLabelFromVision={hideLabelFromVision}
+		<input
+			className='maxi-text-input__input'
+			type={type}
 			id={id}
-			help={help}
-			className={classes}
-		>
-			<input
-				className='maxi-text-input__input'
-				type={type}
-				id={id}
-				value={inputValue || ''}
-				onChange={valueChange}
-				aria-describedby={help ? `${id}__help` : undefined}
-				{...props}
-			/>
-		</BaseControl>
+			value={inputValue || ''}
+			onChange={valueChange}
+			aria-describedby={help ? `${id}__help` : undefined}
+			{...props}
+		/>
 	);
 }

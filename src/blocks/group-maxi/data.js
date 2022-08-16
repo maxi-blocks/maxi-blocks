f2cd0a4d68c1f136dca94692110d234be4ba568c
Andrew Valenya <46112160+Fybex@users.noreply.github.com>
2022-08-16 15:40:22 +0300

    `group-maxi` new structure

---
+++ src/blocks/group-maxi/data.js
@@ -0,0 +1,64 @@
+import { createSelectors } from '../../extensions/styles/custom-css';
+import getCanvasSettings from '../../components/relation-control/getCanvasSettings';
+
+const data = {
+	copyPasteMapping: {
+		settings: {
+			'Callout arrow': {
+				group: {
+					'Show arrow': 'arrow-status',
+					'Arrow side': 'arrow-side',
+					'Arrow position': 'arrow-position',
+					'Arrow size': 'arrow-width',
+				},
+				hasBreakpoints: true,
+			},
+			Background: {
+				template: 'blockBackground',
+			},
+			Border: {
+				template: 'border',
+			},
+			'Box shadow': {
+				template: 'boxShadow',
+			},
+			Size: {
+				template: 'size',
+			},
+			'Margin/Padding': {
+				template: 'marginPadding',
+			},
+		},
+		advanced: {
+			template: 'advanced',
+			Opacity: {
+				template: 'opacity',
+			},
+		},
+	},
+	customCss: {
+		selectors: createSelectors({
+			group: {
+				label: 'group',
+				target: '',
+			},
+		}),
+		categories: [
+			'group',
+			'before group',
+			'after group',
+			'background',
+			'background hover',
+		],
+	},
+	get interactionBuilderSettings() {
+		delete this.interactionBuilderSettings;
+		this.interactionBuilderSettings = getCanvasSettings(this);
+		return this.interactionBuilderSettings;
+	},
+};
+
+const { copyPasteMapping, customCss, interactionBuilderSettings } = data;
+
+export { copyPasteMapping, customCss, interactionBuilderSettings };
+export default data;

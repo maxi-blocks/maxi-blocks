be5e288f518faab79aed16910ae185777033b344
Andrew Valenya <46112160+Fybex@users.noreply.github.com>
2022-08-16 14:55:14 +0300

    `container-maxi` new structure

---
+++ src/blocks/container-maxi/data.js
@@ -0,0 +1,86 @@
+import { createSelectors } from '../../extensions/styles/custom-css';
+import getCanvasSettings from '../../components/relation-control/getCanvasSettings';
+
+import { capitalize } from 'lodash';
+
+const shapeDividerCopyPasteGenerator = position => {
+	return {
+		[`${capitalize(position)} shape divider`]: {
+			group: {
+				'Divider status': `shape-divider-${position}-status`,
+				'Divider style': `shape-divider-${position}-shape-style`,
+				'Divider opacity': `shape-divider-${position}-opacity`,
+				'Divider color': [
+					`shape-divider-${position}-palette-color`,
+					`shape-divider-${position}-color`,
+					`shape-divider-${position}-palette-status`,
+				],
+				'Divider height': `shape-divider-${position}-height`,
+				'Divider height unit': `shape-divider-${position}-height-unit`,
+				'Divider scroll effect': `shape-divider-${position}-effects-status`,
+			},
+		},
+	};
+};
+
+const data = {
+	name: 'container-maxi',
+	copyPasteMapping: {
+		settings: {
+			'Callout arrow': {
+				group: {
+					'Show arrow': 'arrow-status',
+					'Arrow side': 'arrow-side',
+					'Arrow position': 'arrow-position',
+					'Arrow size': 'arrow-width',
+				},
+				hasBreakpoints: true,
+			},
+			...shapeDividerCopyPasteGenerator('top'),
+			...shapeDividerCopyPasteGenerator('bottom'),
+			Background: {
+				template: 'blockBackground',
+			},
+			Border: {
+				template: 'border',
+			},
+			'Box shadow': {
+				template: 'boxShadow',
+			},
+			Size: {
+				template: 'size',
+			},
+			'Margin/Padding': {
+				template: 'marginPadding',
+			},
+		},
+		advanced: {
+			template: 'advanced',
+		},
+	},
+	customCss: {
+		selectors: createSelectors({
+			container: {
+				label: 'container',
+				target: '',
+			},
+		}),
+		categories: [
+			'container',
+			'before container',
+			'after container',
+			'background',
+			'background hover',
+		],
+	},
+	get interactionBuilderSettings() {
+		delete this.interactionBuilderSettings;
+		this.interactionBuilderSettings = [...getCanvasSettings(this)];
+		return this.interactionBuilderSettings;
+	},
+};
+
+const { copyPasteMapping, customCss, interactionBuilderSettings } = data;
+
+export { copyPasteMapping, customCss, interactionBuilderSettings };
+export default data;

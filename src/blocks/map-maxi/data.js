c5f6647781669a9e19306b6d022691e675392efd
Andrew Valenya <46112160+Fybex@users.noreply.github.com>
2022-08-18 18:50:37 +0300

    Fix `map-maxi` data after #2719

--- src/blocks/map-maxi/data.js
+++ src/blocks/map-maxi/data.js
@@ -7,34 +7,126 @@ import getCanvasSettings from '../../components/relation-control/getCanvasSettin
 /**
  * Classnames
  */
-const infoWindowClass = ' .map-marker-info-window';
+const blockClass = ' .maxi-map-block';
+const popupClass = `${blockClass}__popup`;
+const popupContentClass = `${popupClass}__content`;
+const popupTitleClass = `${popupContentClass}__title`;
+const popupDescriptionClass = `${popupContentClass}__description`;
 
 /**
  * Data object
  */
 const data = {
 	name: 'map-maxi',
+	copyPasteMapping: {
+		settings: {
+			'Configure map': {
+				group: {
+					'Map provider': 'map-provider',
+					'Minium zoom': 'map-min-zoom',
+					'Maximum zoom': 'map-max-zoom',
+				},
+			},
+			'Map marker': {
+				group: {
+					Markers: 'map-markers',
+					'Marker icon': ['map-marker-icon', 'map-marker'],
+					'Marker fill colour': {
+						props: 'svg-fill',
+						isPalette: true,
+					},
+					'Marker line colour': {
+						props: 'svg-line',
+						isPalette: true,
+					},
+					'Marker size': {
+						props: 'svg-width',
+						hasBreakpoints: true,
+					},
+				},
+			},
+			'Map popup text': {
+				group: {
+					'Title text level': 'map-marker-heading-level',
+					'Title typography': {
+						groupAttributes: 'typography',
+					},
+					'Description typography': {
+						groupAttributes: 'typography',
+						prefix: 'description-',
+					},
+				},
+			},
+			'Map popup': {
+				group: {
+					Background: {
+						groupAttributes: ['background', 'backgroundColor'],
+						prefix: 'popup-',
+					},
+					'Box shadow': {
+						groupAttributes: 'boxShadow',
+						prefix: 'popup-',
+					},
+				},
+			},
+			Border: {
+				template: 'border',
+			},
+			'Box shadow': {
+				template: 'boxShadow',
+			},
+			Size: {
+				template: 'size',
+			},
+			'Margin/Padding': {
+				template: 'marginPadding',
+			},
+			'Map interaction': {
+				group: {
+					'Map dragging': 'map-dragging',
+					'Map touch zoom': 'map-touch-zoom',
+					'Map double click zoom': 'map-double-click-zoom',
+					'Map scroll wheel zoom': 'map-scroll-wheel-zoom',
+				},
+			},
+		},
+		advanced: {
+			template: 'advanced',
+			Opacity: {
+				template: 'opacity',
+			},
+		},
+	},
 	customCss: {
 		selectors: {
 			...createSelectors({
 				map: '',
+				popup: popupClass,
+				title: popupTitleClass,
+				description: popupDescriptionClass,
 			}),
-			...createSelectors(
-				{
-					title: `${infoWindowClass}__title`,
-					address: `${infoWindowClass}__address`,
-				},
-				false
-			),
 		},
-		categories: ['map', 'before map', 'after map', 'title', 'address'],
+		categories: [
+			'map',
+			'before map',
+			'after map',
+			'popup',
+			'after popup',
+			'popup arrow',
+			'title',
+			'after title',
+			'before title',
+			'description',
+			'after description',
+			'before description',
+		],
 	},
 	get interactionBuilderSettings() {
 		return getCanvasSettings(this);
 	},
 };
 
-const { customCss, interactionBuilderSettings } = data;
+const { copyPasteMapping, customCss, interactionBuilderSettings } = data;
 
-export { customCss, interactionBuilderSettings };
+export { copyPasteMapping, customCss, interactionBuilderSettings };
 export default data;

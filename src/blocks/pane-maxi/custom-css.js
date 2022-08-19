c91eef46304d11722d0357d7c923bdfb77b0e0fd
TeksPeks <verstyukm@gmail.com>
2022-08-19 16:19:59 +0300

    update custom css for pane

--- src/blocks/pane-maxi/custom-css.js
+++ src/blocks/pane-maxi/custom-css.js
@@ -32,21 +32,75 @@ export const selectorsPane = {
 	header: {
 		normal: {
 			label: 'pane header',
-			target: ' .maxi-pane-block__header',
+			target: '[aria-expanded] .maxi-pane-block__header',
 		},
 		hover: {
 			label: 'pane header on hover',
-			target: ' .maxi-pane-block__header:hover',
+			target: '[aria-expanded] .maxi-pane-block__header:hover',
+		},
+		active: {
+			label: 'pane header on active state',
+			target: '[aria-expanded=true] .maxi-pane-block__header',
+		},
+	},
+	icon: {
+		normal: {
+			label: 'icon',
+			target: '[aria-expanded=false] .maxi-pane-block__icon',
+		},
+		svg: {
+			label: "icon's svg",
+			target: '[aria-expanded=false] .maxi-pane-block__icon svg',
+		},
+		insideSvg: {
+			label: 'everything inside svg (svg > *)',
+			target: '[aria-expanded=false] .maxi-pane-block__icon svg > *',
+		},
+		path: {
+			label: "svg's path",
+			target: '[aria-expanded=false] .maxi-pane-block__icon svg path',
+		},
+		hover: {
+			label: 'icon on hover',
+			target: '[aria-expanded] .maxi-pane-block__icon:hover',
+		},
+		hoverSvg: {
+			label: "icon's svg on hover",
+			target: '[aria-expanded] .maxi-pane-block__icon:hover svg',
+		},
+		hoverInsideSvg: {
+			label: 'everything inside svg on hover (:hover svg > *)',
+			target: '[aria-expanded] .maxi-pane-block__icon:hover svg > *',
+		},
+		hoverPath: {
+			label: "svg's path on hover",
+			target: '[aria-expanded] .maxi-pane-block__icon:hover svg path',
+		},
+		active: {
+			label: 'active icon',
+			target: '[aria-expanded=true] .maxi-pane-block__icon',
+		},
+		activeSvg: {
+			label: "active icon's svg",
+			target: '[aria-expanded=true] .maxi-pane-block__icon svg',
+		},
+		activeInsideSvg: {
+			label: 'everything inside active svg (svg > *)',
+			target: '[aria-expanded=true] .maxi-pane-block__icon svg > *',
+		},
+		activePath: {
+			label: "active svg's path",
+			target: '[aria-expanded=true] .maxi-pane-block__icon svg path',
 		},
 	},
 	content: {
 		normal: {
 			label: 'pane content',
-			target: ' .maxi-pane-block__content',
+			target: '[aria-expanded] .maxi-pane-block__content',
 		},
 		hover: {
 			label: 'pane content on hover',
-			target: ' .maxi-pane-block__content:hover',
+			target: '[aria-expanded] .maxi-pane-block__content:hover',
 		},
 	},
 };
@@ -57,6 +111,7 @@ export const categoriesPane = [
 	'after pane',
 	'header',
 	'content',
+	'icon',
 	'background',
 	'background hover',
 ];

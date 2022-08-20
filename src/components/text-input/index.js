751312ee94da8bbe4807f4d06f9cde35091b8ce2
Marko64 <marko.niksicafe@gmail.com>
2022-08-20 08:16:07 +0200

    Update index.js

--- src/components/text-input/index.js
+++ src/components/text-input/index.js
@@ -1,25 +1,17 @@
 /**
  * WordPress dependencies
  */
-import { useInstanceId } from '@wordpress/compose';
 import { useState } from '@wordpress/element';
 
 /**
  * Component
  */
 export default function TextInput({
-	value,
-	help,
 	className,
 	onChange,
 	type = 'text',
-	validationText,
-	isFullwidth,
 	...props
 }) {
-	const instanceId = useInstanceId(TextInput);
-	const id = `inspector-text-input-${instanceId}`;
-
 	const [inputValue, setInputValue] = useState('');
 	const valueChange = e => {
 		setInputValue(e.target.value);
@@ -32,10 +24,8 @@ export default function TextInput({
 		<input
 			className='maxi-text-input__input'
 			type={type}
-			id={id}
-			value={inputValue || ''}
+			value={inputValue}
 			onChange={valueChange}
-			aria-describedby={help ? `${id}__help` : undefined}
 			{...props}
 		/>
 	);

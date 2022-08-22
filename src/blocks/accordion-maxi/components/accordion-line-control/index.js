76b0c7562497e6f01d8566465ef73bd831217590
TeksPeks <verstyukm@gmail.com>
2022-08-22 16:01:23 +0300

    line alignment

--- src/blocks/accordion-maxi/components/accordion-line-control/index.js
+++ src/blocks/accordion-maxi/components/accordion-line-control/index.js
@@ -10,55 +10,89 @@ import {
 	SettingTabsControl,
 	DividerControl,
 	ToggleSwitch,
+	SelectControl,
 } from '../../../../components';
+import { getLastBreakpointAttribute } from '../../../../extensions/styles';
 
 const AccordionLineControl = props => {
-	const { onChange } = props;
+	const { onChange, breakpoint } = props;
 
 	return (
-		<SettingTabsControl
-			depth={2}
-			items={[
-				{
-					label: __('Normal state', 'maxi-blocks'),
-					content: <DividerControl {...props} />,
-				},
-				{
-					label: __('Hover state', 'maxi-blocks'),
-					content: (
-						<>
-							<ToggleSwitch
-								label={__('Enable hover', 'maxi-blocks')}
-								selected={props['line-status-hover']}
-								onChange={val =>
-									onChange({ 'line-status-hover': val })
-								}
-							/>
-							{props['line-status-hover'] && (
-								<DividerControl {...props} isHover />
-							)}
-						</>
-					),
-				},
-				{
-					label: __('Active state', 'maxi-blocks'),
-					content: (
-						<>
-							<ToggleSwitch
-								label={__('Enable active', 'maxi-blocks')}
-								selected={props['line-status-active']}
-								onChange={val =>
-									onChange({ 'line-status-active': val })
-								}
-							/>
-							{props['line-status-active'] && (
-								<DividerControl {...props} prefix='active-' />
-							)}
-						</>
-					),
-				},
-			]}
-		/>
+		<>
+			<SelectControl
+				label={__('Line horizontal position', 'maxi-blocks')}
+				value={getLastBreakpointAttribute({
+					target: 'line-horizontal',
+					breakpoint,
+					props,
+				})}
+				options={[
+					{
+						label: __('Left', 'maxi-blocks'),
+						value: 'flex-start',
+					},
+					{
+						label: __('Center', 'maxi-blocks'),
+						value: 'center',
+					},
+					{
+						label: __('Right', 'maxi-blocks'),
+						value: 'flex-end',
+					},
+				]}
+				onChange={val =>
+					onChange({
+						[`line-horizontal-${breakpoint}`]: val,
+					})
+				}
+			/>
+			<SettingTabsControl
+				depth={2}
+				items={[
+					{
+						label: __('Normal state', 'maxi-blocks'),
+						content: <DividerControl {...props} />,
+					},
+					{
+						label: __('Hover state', 'maxi-blocks'),
+						content: (
+							<>
+								<ToggleSwitch
+									label={__('Enable hover', 'maxi-blocks')}
+									selected={props['line-status-hover']}
+									onChange={val =>
+										onChange({ 'line-status-hover': val })
+									}
+								/>
+								{props['line-status-hover'] && (
+									<DividerControl {...props} isHover />
+								)}
+							</>
+						),
+					},
+					{
+						label: __('Active state', 'maxi-blocks'),
+						content: (
+							<>
+								<ToggleSwitch
+									label={__('Enable active', 'maxi-blocks')}
+									selected={props['line-status-active']}
+									onChange={val =>
+										onChange({ 'line-status-active': val })
+									}
+								/>
+								{props['line-status-active'] && (
+									<DividerControl
+										{...props}
+										prefix='active-'
+									/>
+								)}
+							</>
+						),
+					},
+				]}
+			/>
+		</>
 	);
 };
 

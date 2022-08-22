4c5534e40f2896402cb51ba8920b66b6e37ed9f6
Andrew Valenya <46112160+Fybex@users.noreply.github.com>
2022-08-22 17:27:23 +0300

    Add jsdoc to `getTemplatePartSlug`

--- src/extensions/fse/getTemplatePartSlug.js
+++ src/extensions/fse/getTemplatePartSlug.js
@@ -8,11 +8,20 @@ import { select } from '@wordpress/data';
  */
 import getIsSiteEditor from './getIsSiteEditor';
 
+/**
+ * Returns the slug of the template part if we are on the FSE template part editor.
+ *
+ * @param {string} clientId The clientId of the block in template part from which we want to get the slug.
+ * @returns {string|false} The slug of the template part or false if we are not on the FSE editor.
+ */
+
 const getTemplatePartSlug = clientId => {
+	if (!getIsSiteEditor()) return false;
+
 	const { getEditedPostType, getEditedPostId } = select('core/edit-site');
 
 	// Checking if we on the FSE template part editor.
-	if (getIsSiteEditor() && getEditedPostType() === 'wp_template_part')
+	if (getEditedPostType() === 'wp_template_part')
 		return getEditedPostId().split('//', 2)[1];
 
 	if (!clientId) return false;

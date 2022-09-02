7e03acb788c6890390bb29aa721f5f65fefa3f8a
Andrew Valenya <46112160+Fybex@users.noreply.github.com>
2022-09-02 16:43:51 +0300

    Fix map e2e

--- e2e-tests/blocks/map-maxi/index.spec.js
+++ e2e-tests/blocks/map-maxi/index.spec.js
@@ -68,7 +68,7 @@ describe('Map Maxi', () => {
 
 		// Check frontend
 		const previewPage = await openPreviewPage(page);
-		await previewPage.waitForSelector('.maxi-map-block__container');
+		await previewPage.waitForSelector('.leaflet-container');
 		const container = await previewPage.$eval(
 			'.maxi-map-block__container',
 			container => container.innerHTML.trim()

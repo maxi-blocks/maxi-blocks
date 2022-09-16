91a07603adbb3aa8faea1766041783767baf1548
senior-dev-1019 <seniordeveloper1019@gmail.com>
2022-09-16 15:16:07 +0200

    changed maxRange values

--- src/components/background-control/sizeAndPositionLayerControl.js
+++ src/components/background-control/sizeAndPositionLayerControl.js
@@ -31,18 +31,38 @@ const Size = ({
 		px: {
 			min: 0,
 			max: 1999,
+			minRange: -1999,
+			maxRange: 1999,
 		},
 		em: {
 			min: 0,
-			max: 300,
+			max: 1999,
+			minRange: -1999,
+			maxRange: 1999,
 		},
 		vw: {
 			min: 0,
-			max: 300,
+			max: 1999,
+			minRange: -1999,
+			maxRange: 1999,
+		},
+		vh: {
+			min: 0,
+			max: 1999,
+			minRange: -1999,
+			maxRange: 1999,
 		},
 		'%': {
 			min: 0,
-			max: 100,
+			max: 1999,
+			minRange: -1999,
+			maxRange: 1999,
+		},
+		'-': {
+			min: 0,
+			max: 16,
+			minRange: 0,
+			maxRange: 16,
 		},
 	};
 

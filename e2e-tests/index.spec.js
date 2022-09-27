71c451bf88e2be46443d12fdbbe927090ce766e7
Josh <jjuancap@gmail.com>
2022-09-27 12:12:30 +0200

    testing

--- e2e-tests/index.spec.js
+++ e2e-tests/index.spec.js
@@ -1,9 +1,16 @@
 import { createNewPost, insertBlock } from '@wordpress/e2e-test-utils';
+import { getBlockStyle, getEditedPostContent } from './utils';
 
 describe('Github actions errors', () => {
 	it('Should pass', async () => {
 		await createNewPost();
 		await insertBlock('Button Maxi');
+
+		await page.keyboard.type('Hello', { delay: 100 });
+		await page.waitForTimeout(150);
+
+		expect(await getEditedPostContent(page)).toMatchSnapshot();
+		expect(await getBlockStyle(page)).toMatchSnapshot();
 
 		expect(true).toBeTruthy();
 	});

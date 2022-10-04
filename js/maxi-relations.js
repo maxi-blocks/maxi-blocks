ece3a792e505883c22f5248c4943c91ce116b480
Josh <jjuancap@gmail.com>
2022-10-04 19:51:16 +0200

    Fixes transition issue 👍

--- js/maxi-relations.js
+++ js/maxi-relations.js
@@ -82,10 +82,12 @@ class Relation {
 	generateStylesEls() {
 		this.stylesEl = document.createElement('style');
 		this.stylesEl.id = `relations--${this.uniqueID}-styles`;
+		this.stylesEl.setAttribute('data-type', this.action);
 		this.stylesEl.innerText = this.stylesString;
 
 		this.transitionEl = document.createElement('style');
 		this.transitionEl.id = `relations--${this.uniqueID}-transitions`;
+		this.transitionEl.setAttribute('data-type', this.action);
 		this.transitionEl.innerText = this.transitionString;
 	}
 
@@ -96,10 +98,35 @@ class Relation {
 				'#maxi-blocks-inline-css'
 			);
 
-		this.inlineStylesEl.parentNode.insertBefore(
-			styleEl,
-			this.inlineStylesEl.nextSibling
-		);
+		const currentEl = document.querySelector(`#${styleEl.id}`);
+
+		if (currentEl) {
+			if (currentEl.getAttribute('data-type') === this.action)
+				currentEl.replaceWith(styleEl);
+			else currentEl.insertAdjacentElement('afterend', styleEl);
+		} else
+			this.inlineStylesEl.parentNode.insertBefore(
+				styleEl,
+				this.inlineStylesEl.nextSibling
+			);
+	}
+
+	getCurrentBreakpoint() {
+		const winWidth = window.innerWidth;
+
+		let currentBreakpoint = 'general';
+
+		Object.entries(this.breakpointsObj).forEach(([breakpoint, value]) => {
+			if (!['general', 'xxl'].includes(breakpoint)) {
+				if (breakpoint === 'general') return;
+
+				if (winWidth <= this.breakpointsObj.xl)
+					currentBreakpoint = breakpoint;
+			}
+			if (winWidth <= value) currentBreakpoint = breakpoint;
+		});
+
+		return currentBreakpoint;
 	}
 
 	getLastUsableBreakpoint(currentBreakpoint, callback) {
@@ -107,6 +134,27 @@ class Relation {
 			.splice(0, this.breakpoints.indexOf(currentBreakpoint) + 1)
 			.reverse()
 			.find(breakpoint => callback(breakpoint));
+	}
+
+	getTransitionTimeout() {
+		const currentBreakpoint = this.getCurrentBreakpoint();
+
+		const getTransitionValue = prop =>
+			this.effects[
+				`transition-${prop}-${this.getLastUsableBreakpoint(
+					currentBreakpoint,
+					breakpoint =>
+						Object.prototype.hasOwnProperty.call(
+							this.effects,
+							`transition-${prop}-${breakpoint}`
+						)
+				)}`
+			];
+
+		const transitionDuration = getTransitionValue('duration');
+		const transitionDelay = getTransitionValue('delay');
+
+		return (transitionDuration + transitionDelay) * 1000;
 	}
 
 	/**
@@ -561,7 +609,6 @@ class Relation {
 				// Remove transitions to let the original ones be applied
 				this.removeTransition();
 
-				this.isNestedHoverTransition = true;
 				clearTimeout(this.contentTimeout);
 			});
 
@@ -619,10 +666,9 @@ class Relation {
 		this.removeStyles();
 
 		this.transitionTimeout = setTimeout(() => {
-			// Removing transition after transition-duration + 1s to make sure it's done
 			this.removeTransition();
 			this.removeAddAttrToBlock();
-		}, this.effects['transition-duration-general'] * 1000 + 1000);
+		}, this.getTransitionTimeout());
 	}
 
 	addClickEvents() {
@@ -633,6 +679,10 @@ class Relation {
 		this.addDataAttrToBlock();
 		this.addTransition();
 		this.addStyles();
+
+		this.transitionTimeout = setTimeout(() => {
+			this.removeTransition();
+		}, this.getTransitionTimeout());
 	}
 }
 

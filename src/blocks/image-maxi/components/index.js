e91432ef940a7c401a40763908819b879b76596d
Andrew Valenya <46112160+Fybex@users.noreply.github.com>
2022-10-13 22:56:31 +0300

    `DimensionTab` to component

--- src/blocks/image-maxi/components/index.js
+++ src/blocks/image-maxi/components/index.js
@@ -1,2 +1,2 @@
-// eslint-disable-next-line import/prefer-default-export
 export { default as HoverEffectControl } from './hover-effect-control';
+export { default as DimensionTab } from './dimension-tab';

f3ae591b2c96fb7156608e14f5fa90f60e34691c
Josh <50477222+Naaaaiix@users.noreply.github.com>
2022-10-21 09:14:54 +0200

    Fixes 3758 - Canvas size breakpoints and attributes saving mechanism (#3796)
    * Fixes styleGenerator
    Fixes displaying styles depending on canvas width
    Adds new attributes saving filters for responsive
    Fixes bg layers resetting
    Fixes bg layers created from non-base responsive
    Adds `onReset` to ColorControl
    
    * Added state management of value on ANC
    Fixed responsive tests
    Added handleSetAttributes tests
    Fix tests
    
    * Fixes minor bugs
    Updates most of tests
    
    * Fix minor bugs
    Fix major tests
    
    * Fix tests
    
    * Mock unit tests
    
    * Fix tests importing issues
    
    * Fix small issue
    
    * Enhance handleSetAttributes and cleanAttributes
    Fix BGColorLayer resetting
    Update some tests
    
    * Fixes small glitch
    
    * Fixes tests
    Fixes small issues
    
    * Fix tests

---
+++ src/extensions/dom/test/getWinBreakpoint.js
@@ -0,0 +1,10 @@
+import getWinBreakpoint from '../getWinBreakpoint';
+
+describe('getWinBreakpoint', () => {
+	it('Returns the correct breakpoint', () => {
+		expect(getWinBreakpoint(1025)).toBe('l');
+		expect(getWinBreakpoint(1024)).toBe('m');
+		expect(getWinBreakpoint(767)).toBe('s');
+		expect(getWinBreakpoint(480)).toBe('xs');
+	});
+});

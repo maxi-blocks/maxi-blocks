5e76244211cb979e442f5ff30ef5e95dcb8e0b05
Andrew Valenya <46112160+Fybex@users.noreply.github.com>
2022-11-01 16:30:29 +0200

    `setSVGContent` remove negative lookbehind
    Safari support

--- src/extensions/svg/setSVGContent.js
+++ src/extensions/svg/setSVGContent.js
@@ -1,9 +1,11 @@
 const setSVGContent = (content, color, type) => {
+	const typeRegexString = ` ${type}=[^-]([^none])([^\\"]+)`;
+
 	const svgRegExp = new RegExp(
-		`(?<!data-${type})(?<!data-${type} data-hover-${type})( ${type}=[^-]([^none])([^\\"]+))`,
+		`data-${type}( data-hover-${type}|)${typeRegexString}`,
 		'g'
 	);
-	const svgStr = ` data-${type}$1`;
+	const svgStr = ` data-${type}$&`;
 
 	const fillRegExp = new RegExp(`${type}:([^none])([^\\}]+)`, 'g');
 	const fillStr = `${type}:${color}`;
@@ -11,8 +13,11 @@ const setSVGContent = (content, color, type) => {
 	const fillRegExp2 = new RegExp(`${type}=[^-]([^none])([^\\"]+)`, 'g');
 	const fillStr2 = `${type}="${color}`;
 
-	const newContent = content
-		.replace(svgRegExp, svgStr)
+	const newContent = (
+		!content.match(svgRegExp) && (type === 'fill' || type === 'stroke')
+			? content.replace(new RegExp(typeRegexString, 'g'), svgStr)
+			: content
+	)
 		.replace(fillRegExp, fillStr)
 		.replace(fillRegExp2, fillStr2);
 

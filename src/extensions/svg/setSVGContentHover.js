24f38db9c2837d030c700ba326812d32a52ec17c
Andrew Valenya <46112160+Fybex@users.noreply.github.com>
2022-11-02 16:21:43 +0200

    Revert regexp backslash change

--- src/extensions/svg/setSVGContentHover.js
+++ src/extensions/svg/setSVGContentHover.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-useless-escape */
 const setSVGContentHover = (content, color, type) => {
 	let newContent = content;
 
@@ -6,12 +7,12 @@ const setSVGContentHover = (content, color, type) => {
 	const svgStr = ` data-hover-${type}$&`;
 
 	const cssRegExpOld = new RegExp(
-		`((.maxi-svg-icon-block__icon:hover .-?[_a-zA-Z]+[_a-zA-Z0-9-]* .-?[_a-zA-Z]+[_a-zA-Z0-9-]*s*){${type}:([^none])([^\\}]+))(})`,
+		`((\.maxi-svg-icon-block__icon:hover \.-?[_a-zA-Z]+[_a-zA-Z0-9-]* \.-?[_a-zA-Z]+[_a-zA-Z0-9-]*\s*)\{${type}:([^none])([^\\}]+))(})`,
 		'g'
 	);
 	const cssStrOld = '';
 
-	const cssRegexString = `((\\.-?[_a-zA-Z]+[_a-zA-Z0-9-]* .-?[_a-zA-Z]+[_a-zA-Z0-9-]*s*){${type}:([^none])([^\\}]+))`;
+	const cssRegexString = `((\\.-?[_a-zA-Z]+[_a-zA-Z0-9-]* \.-?[_a-zA-Z]+[_a-zA-Z0-9-]*\s*)\{${type}:([^none])([^\\}]+))`;
 	const cssRegExp = new RegExp(`hover${cssRegexString}`, 'g');
 	const cssStr = `$1}.maxi-svg-icon-block__icon:hover $2{${type}:${color}`;
 

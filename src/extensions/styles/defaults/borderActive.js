90b4556e61fed994cd0b8e4d6f34a08c9b737b8f
TeksPeks <verstyukm@gmail.com>
2022-12-05 19:54:19 +0200

    change default border colour on active state

--- src/extensions/styles/defaults/borderActive.js
+++ src/extensions/styles/defaults/borderActive.js
@@ -5,7 +5,7 @@ const prefix = 'active-';
 
 export const borderActive = prefixAttributesCreator({
 	obj: border,
-	diffValAttr: { 'border-palette-color-general': 6 },
+	diffValAttr: { [`${prefix}border-palette-color-general`]: 6 },
 	newAttr: {
 		'border-status-active': {
 			type: 'boolean',

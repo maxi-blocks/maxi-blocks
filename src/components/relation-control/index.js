0079ad056d1a3bca09bd0707c37b7bbc472d92b1
Andrew Valenya <46112160+Fybex@users.noreply.github.com>
2022-12-07 20:14:36 +0200

    Remove IB pseudo-migrator

--- src/components/relation-control/index.js
+++ src/components/relation-control/index.js
@@ -185,46 +185,6 @@ const RelationControl = props => {
 			}, {}),
 		};
 
-		// As an alternative to a migrator... Remove after used!
-		if (
-			!(
-				'transitionTrigger' in item.effects &&
-				'transitionTarget' in item.effects &&
-				'hoverStatus' in item.effects
-			) ||
-			item.effects.hoverStatus !==
-				getHoverStatus(selectedSettingsObj.hoverProp, blockAttributes)
-		) {
-			const {
-				transitionTarget: rawTransitionTarget,
-				transitionTrigger,
-				hoverProp,
-			} = selectedSettingsObj;
-			const transitionTarget =
-				item.settings === 'Transform'
-					? Object.keys(item.css)
-					: rawTransitionTarget;
-
-			let hoverStatus = null;
-
-			if (!('hoverStatus' in item.effects))
-				hoverStatus = getHoverStatus(
-					hoverProp,
-					blockAttributes,
-					item.attributes
-				);
-
-			if (transitionTarget || transitionTrigger)
-				onChangeRelation(relations, item.id, {
-					effects: {
-						...item.effects,
-						...(transitionTarget && { transitionTarget }),
-						...(transitionTrigger && { transitionTrigger }),
-						...(!isNil(hoverStatus) && { hoverStatus }),
-					},
-				});
-		}
-
 		// Merging into empty object because lodash `merge` mutates first argument
 		const mergedAttributes = merge({}, blockAttributes, item.attributes);
 

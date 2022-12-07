212f409304c33e0131d2742e52eb2ad075202f73
Josh <jjuancap@gmail.com>
2022-12-07 20:00:06 +0100

    Fix minor issue with max-width-unit on Container

--- src/blocks/container-maxi/data.js
+++ src/blocks/container-maxi/data.js
@@ -109,7 +109,6 @@ const maxiAttributes = {
 	'max-width-xxl': '1690',
 	'max-width-xl': '1170',
 	'max-width-l': '90',
-	'max-width-unit-general': undefined,
 	'max-width-unit-xxl': 'px',
 	'max-width-unit-xl': 'px',
 	'max-width-unit-l': '%',

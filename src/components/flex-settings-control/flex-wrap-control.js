f0296b4fc83be839f54987d353c2adbf7878524c
Rustam Samsonyan <83059229+Rustam198804@users.noreply.github.com>
2022-12-14 18:43:17 +0400

    Change some styles

--- src/components/flex-settings-control/flex-wrap-control.js
+++ src/components/flex-settings-control/flex-wrap-control.js
@@ -8,7 +8,25 @@ import { __ } from '@wordpress/i18n';
  */
 import SelectControl from '../select-control';
 import { getLastBreakpointAttribute } from '../../extensions/styles';
-import getOptions from './utils';
+// import getOptions from './utils';
+import SettingTabsControl from '../setting-tabs-control';
+import Icon from '../icon';
+
+
+/**
+ * Icons
+ */
+import {
+	alignLeft,
+	alignCenter,
+	alignRight,
+	alignJustify,
+	toolbarAlignCenter,
+	toolbarAlignLeft,
+	toolbarAlignRight,
+	toolbarAlignJustify,
+} from '../../icons';
+
 
 /**
  * Component
@@ -17,10 +35,57 @@ import getOptions from './utils';
 const FlexWrapControl = props => {
 	const { breakpoint, onChange } = props;
 
+	const getOptions = () => {
+		const options = [];
+
+			options.push({
+				icon: <Icon icon={alignLeft} />,
+				value: 'nowrap',
+			});
+
+			options.push({
+				icon: (
+					<Icon icon={alignCenter} />
+				),
+				value: 'wrap',
+			});
+
+			options.push({
+				icon: (
+					<Icon icon={alignRight} />
+				),
+				value: 'wrap-reverse',
+			});
+
+		return options;
+	};
+
 	return (
-		<SelectControl
+		<>
+			{/* <SelectControl
+				label={__('Flex wrap', 'maxi-blocks')}
+				className='maxi-flex-wrap-control'
+				value={
+					getLastBreakpointAttribute({
+						target: 'flex-wrap',
+						breakpoint,
+						attributes: props,
+					}) ?? ''
+				}
+				options={getOptions(['nowrap', 'wrap', 'wrap-reverse'])}
+				onChange={val =>
+					onChange({
+						[`flex-wrap-${breakpoint}`]: val,
+					})
+				}
+			/> */}
+			<SettingTabsControl
 			label={__('Flex wrap', 'maxi-blocks')}
+			type='buttons'
+			fullWidthMode
 			className='maxi-flex-wrap-control'
+			hasBorder
+			items={getOptions()}
 			value={
 				getLastBreakpointAttribute({
 					target: 'flex-wrap',
@@ -28,13 +93,20 @@ const FlexWrapControl = props => {
 					attributes: props,
 				}) ?? ''
 			}
-			options={getOptions(['nowrap', 'wrap', 'wrap-reverse'])}
+			selected={
+				getLastBreakpointAttribute({
+					target: 'flex-wrap',
+					breakpoint,
+					attributes: props,
+				}) || getOptions()[0].value
+			}
 			onChange={val =>
 				onChange({
 					[`flex-wrap-${breakpoint}`]: val,
 				})
 			}
 		/>
+		</>
 	);
 };
 

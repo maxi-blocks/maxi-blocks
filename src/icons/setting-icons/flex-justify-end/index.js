67a5829a9f1508c32e4a4bf8b6100dfc1c8f6306
Rustam Samsonyan <83059229+Rustam198804@users.noreply.github.com>
2022-12-15 16:54:47 +0400

    Change styles for justify horizontal

---
+++ src/icons/setting-icons/flex-justify-end/index.js
@@ -0,0 +1,34 @@
+/**
+ * WordPress dependencies
+ */
+import { SVG, Path } from '@wordpress/primitives';
+
+const flexJustifyEnd = (
+    <SVG
+    width={20}
+    height={20}
+    viewBox="0 0 20 20"
+  >
+    <g>
+      <g
+        fill="#090f13"
+        transform="rotate(180 10 10)"
+      >
+        <Path
+          d="M19 20H1a1 1 0 01-1-1V1a1 1 0 011-1h18a1 1 0 011 1v18a1 1 0 01-1 1M2 18h16V2H2z"
+        />
+        <Path
+          d="M3.755 17.475a1 1 0 01-1-1V3.525a1 1 0 112 0v12.95a1 1 0 01-1 1"
+        />
+        <Path
+          d="M7.208 17.475a1 1 0 01-1-1V3.525a1 1 0 112 0v12.95a1 1 0 01-1 1"
+        />
+        <Path
+          d="M10.661 17.475a1 1 0 01-1-1V3.525a1 1 0 112 0v12.95a1 1 0 01-1 1"
+        />
+      </g>
+    </g>
+  </SVG>
+);
+
+export default flexJustifyEnd;
--- src/icons/setting-icons/flex-wrap-reverse/index.js
+++ src/icons/setting-icons/flex-wrap-reverse/index.js
@@ -1,52 +1,40 @@
 /**
  * WordPress dependencies
  */
-import { SVG, Path } from '@wordpress/primitives';
+import { SVG, Path, G } from '@wordpress/primitives';
 
 const flexWrapReverse = (
     <SVG
-    data-name="Group 731"
     width={17.863}
     height={18.381}
     viewBox="0 0 17.863 18.381"
   >
-    <defs>
-      <clipPath id="a">
-        <Path data-name="Rectangle 2" fill="none" d="M0 0H17.863V18.381H0z" />
-      </clipPath>
-    </defs>
-    <g data-name="Group 729" clipPath="url(#a)" fill="#090f13">
+    <G fill="#090f13">
       <Path
-        data-name="Path 89"
         d="M1 8.863a1 1 0 001-1V1a1 1 0 10-2 0v6.863a1 1 0 001 1"
         transform="translate(15.863)"
       />
       <Path
-        data-name="Path 90"
         d="M6.5 8.863a1 1 0 001-1V1a1 1 0 10-2 0v6.863a1 1 0 001 1"
         transform="translate(4.871)"
       />
       <Path
-        data-name="Path 91"
         d="M1 18.381a1 1 0 001-1v-6.863a1 1 0 10-2 0v6.863a1 1 0 001 1"
         transform="translate(15.863)"
       />
       <Path
-        data-name="Path 92"
         d="M6.5 18.381a1 1 0 001-1v-6.863a1 1 0 10-2 0v6.863a1 1 0 001 1"
         transform="translate(4.871)"
       />
       <Path
-        data-name="Path 93"
         d="M11.367 8.863a1 1 0 001-1V1a1 1 0 10-2 0v6.863a1 1 0 001 1"
         transform="translate(-4.871)"
       />
       <Path
-        data-name="Path 94"
         d="M16.863 8.863a1 1 0 001-1V1a1 1 0 10-2 0v6.863a1 1 0 001 1"
         transform="translate(-15.863)"
       />
-    </g>
+    </G>
   </SVG>
 );
 

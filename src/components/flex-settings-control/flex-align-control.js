0c0f502f724e880bc7c908d1a210f06e4c33919a
Rustam Samsonyan <83059229+Rustam198804@users.noreply.github.com>
2022-12-16 14:39:10 +0400

    Change styles for align content

--- src/components/flex-settings-control/flex-align-control.js
+++ src/components/flex-settings-control/flex-align-control.js
@@ -6,10 +6,8 @@ import { __ } from '@wordpress/i18n';
 /**
  * Internal dependencies
  */
-import SelectControl from '../select-control';
 import SettingTabsControl from '../setting-tabs-control';
 import { getLastBreakpointAttribute } from '../../extensions/styles';
-import getOptions from './utils';
 import Icon from '../icon';
 
 /**
@@ -173,53 +171,6 @@ const FlexAlignControl = props => {
 				})
 			}
 		/>
-			{/* <SelectControl
-				label={__('Justify content horizontally', 'maxi-blocks')}
-				className='maxi-flex-align-control__justify-content'
-				value={
-					getLastBreakpointAttribute({
-						target: 'justify-content',
-						breakpoint,
-						attributes: props,
-					}) ?? ''
-				}
-				options={getOptions([
-					'flex-start',
-					'flex-end',
-					'center',
-					'space-between',
-					'space-around',
-					'space-evenly',
-				])}
-				onChange={val =>
-					onChange({
-						[`justify-content-${breakpoint}`]: val,
-					})
-				}
-			/> */}
-			{/* <SelectControl
-				label={__('Align items vertically', 'maxi-blocks')}
-				className='maxi-flex-align-control__align-items'
-				value={
-					getLastBreakpointAttribute({
-						target: 'align-items',
-						breakpoint,
-						attributes: props,
-					}) ?? ''
-				}
-				options={getOptions([
-					'flex-start',
-					'flex-end',
-					'center',
-					'stretch',
-					'baseline',
-				])}
-				onChange={val =>
-					onChange({
-						[`align-items-${breakpoint}`]: val,
-					})
-				}
-			/> */}
 		</>
 	);
 };

b735f36b65ed259021b6dff0852a8e91694484c6
TeksPeks <verstyukm@gmail.com>
2022-12-20 20:51:03 +0200

    create `AspectRatioControl`

---
+++ src/components/aspect-ratio-control/index.js
@@ -0,0 +1,49 @@
+/**
+ * Wordpress dependencies
+ */
+import { __ } from '@wordpress/i18n';
+
+/**
+ * Internal dependencies
+ */
+import SelectControl from '../select-control';
+
+const AspectRatioControl = props => {
+	const { onChange, value, onReset, label, className, additionalOptions } =
+		props;
+
+	return (
+		<SelectControl
+			className={className}
+			label={label}
+			value={value}
+			options={[
+				...additionalOptions,
+				{
+					label: __('1:1 Aspect ratio', 'maxi-blocks'),
+					value: 'ar11',
+				},
+				{
+					label: __('2:3 Aspect ratio', 'maxi-blocks'),
+					value: 'ar23',
+				},
+				{
+					label: __('3:2 Aspect ratio', 'maxi-blocks'),
+					value: 'ar32',
+				},
+				{
+					label: __('4:3 Aspect ratio', 'maxi-blocks'),
+					value: 'ar43',
+				},
+				{
+					label: __('16:9 Aspect ratio', 'maxi-blocks'),
+					value: 'ar169',
+				},
+			]}
+			onChange={onChange}
+			onReset={onReset}
+		/>
+	);
+};
+
+export default AspectRatioControl;

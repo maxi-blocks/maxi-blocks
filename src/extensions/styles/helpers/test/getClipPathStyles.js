977e08c5dbd9b78fad595cad383fd5feb642e2de
Andrew Valenya <46112160+Fybex@users.noreply.github.com>
2022-12-20 20:28:07 +0200

    Add, update unit tests

--- src/extensions/styles/helpers/test/getClipPathStyles.js
+++ src/extensions/styles/helpers/test/getClipPathStyles.js
@@ -11,15 +11,14 @@ describe('getClipPathStyles', () => {
 		'clip-path-status-xxl': true,
 		'clip-path-xxl': 'polygon(50% 0%, 0% 100%, 100% 100%)',
 		'clip-path-status-m': true,
-		'clip-path-m': 'polygon(50% 0%, 0% 100%, 100% 100%)',
-		'clip-path-status-s': true,
+		'clip-path-m': 'none',
+		'clip-path-status-s': false,
 		'clip-path-s': 'polygon(50% 0%, 0% 100%, 100% 100%)',
 		'clip-path-status-xs': true,
 		'clip-path-xs': 'polygon(50% 0%, 0% 100%, 100% 100%)',
 		// hover attributes
 		'clip-path-status-hover': true,
-		'clip-path-general-hover':
-			'polygon(50% 0%, 100% 50%, 50% 100%, 0% 50%)',
+		'clip-path-general-hover': 'none',
 		'clip-path-l-hover': 'circle(50% at 50% 50%)',
 		'clip-path-xl-hover': 'polygon(0% 75%, 100% 25%, 62.5% 75%)',
 		'clip-path-xxl-hover':
@@ -41,6 +40,27 @@ describe('getClipPathStyles', () => {
 	it('Get an empty hover clipPath styles, when hover-status is false', () => {
 		const result = getClipPathStyles({
 			obj: { ...object, 'clip-path-status-hover': false },
+			isHover: true,
+		});
+		expect(result).toMatchSnapshot();
+	});
+
+	it('Get an empty clipPath styles, when clipPath - none', () => {
+		const result = getClipPathStyles({
+			obj: {
+				'clip-path-general': 'none',
+				'clip-path-status-general': true,
+			},
+		});
+		expect(result).toMatchSnapshot();
+	});
+
+	it('Get a none hover clipPath styles, when clipPath - none', () => {
+		const result = getClipPathStyles({
+			obj: {
+				'clip-path-general-hover': 'none',
+				'clip-path-status-hover': true,
+			},
 			isHover: true,
 		});
 		expect(result).toMatchSnapshot();

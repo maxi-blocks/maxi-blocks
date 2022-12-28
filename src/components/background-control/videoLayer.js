a74092e2d893b56afbd1d4393b1f0c73f6646a0d
Myroslav <53875005+TeksPeks@users.noreply.github.com>
2022-12-28 09:57:03 +0200

    Issue 4094 - BG video (#4146)
    * fix background fallback preview
    
    * add default video URL
    
    * update test

--- src/components/background-control/videoLayer.js
+++ src/components/background-control/videoLayer.js
@@ -76,7 +76,7 @@ const VideoLayerContent = props => {
 								isHover,
 								prefix,
 								breakpoint
-							)]: val.order,
+							)]: val.id,
 							[getAttributeKey(
 								'background-video-fallbackURL',
 								isHover,

cfc79abd6938a5f8db1b60f45f26c1dc0473de10
TeksPeks <verstyukm@gmail.com>
2022-12-29 13:20:55 +0200

    refactor `AspectRatioControl`

--- src/components/aspect-ratio-control/index.js
+++ src/components/aspect-ratio-control/index.js
@@ -8,42 +8,33 @@ import { __ } from '@wordpress/i18n';
  */
 import SelectControl from '../select-control';
 
-const AspectRatioControl = props => {
-	const { onChange, value, onReset, label, className, additionalOptions } =
-		props;
-
-	return (
-		<SelectControl
-			className={className}
-			label={label}
-			value={value}
-			options={[
-				...additionalOptions,
-				{
-					label: __('1:1 Aspect ratio', 'maxi-blocks'),
-					value: 'ar11',
-				},
-				{
-					label: __('2:3 Aspect ratio', 'maxi-blocks'),
-					value: 'ar23',
-				},
-				{
-					label: __('3:2 Aspect ratio', 'maxi-blocks'),
-					value: 'ar32',
-				},
-				{
-					label: __('4:3 Aspect ratio', 'maxi-blocks'),
-					value: 'ar43',
-				},
-				{
-					label: __('16:9 Aspect ratio', 'maxi-blocks'),
-					value: 'ar169',
-				},
-			]}
-			onChange={onChange}
-			onReset={onReset}
-		/>
-	);
-};
+const AspectRatioControl = ({ additionalOptions, ...props }) => (
+	<SelectControl
+		{...props}
+		options={[
+			...additionalOptions,
+			{
+				label: __('1:1 Aspect ratio', 'maxi-blocks'),
+				value: 'ar11',
+			},
+			{
+				label: __('2:3 Aspect ratio', 'maxi-blocks'),
+				value: 'ar23',
+			},
+			{
+				label: __('3:2 Aspect ratio', 'maxi-blocks'),
+				value: 'ar32',
+			},
+			{
+				label: __('4:3 Aspect ratio', 'maxi-blocks'),
+				value: 'ar43',
+			},
+			{
+				label: __('16:9 Aspect ratio', 'maxi-blocks'),
+				value: 'ar169',
+			},
+		]}
+	/>
+);
 
 export default AspectRatioControl;

e5c3ba2641b0132e54d58e96f848ce6245a37158
Andrew Valenya <46112160+Fybex@users.noreply.github.com>
2023-01-06 16:11:18 +0200

    `uniqueIDStructureChecker` update
    - add more details to JSDoc description
    - check in default editor template block

--- src/extensions/maxi-block/uniqueIDStructureChecker.js
+++ src/extensions/maxi-block/uniqueIDStructureChecker.js
@@ -8,16 +8,20 @@ import {
 } from '../fse';
 
 /**
+ * Checks if the `uniqueID` has valid structure(suffix).
+ * On pasting code editor content, `uniqueID` needs to have the same structure as
+ * other blocks in the current editor.
+ *
  * @param {string} uniqueID
  * @param {string} clientId
  * @returns {boolean} Returns `true` if the `uniqueID` has valid structure(suffix), `false` otherwise.
  */
-
 const uniqueIDStructureChecker = (uniqueID, clientId) => {
 	const templatePartSlug = getTemplatePartSlug(clientId);
 	const templatePartUniqueIDSuffix = 'template-part';
 
 	if (
+		(!getIsSiteEditor() && uniqueID.includes('template-')) ||
 		(getIsSiteEditor() && !uniqueID.includes('template-')) ||
 		(!getTemplatePartChooseList() &&
 			((templatePartSlug &&

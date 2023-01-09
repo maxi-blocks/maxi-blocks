8bc014fdeaca73a92a29221963603115b40646d6
Svitlana <2401618+elzadj@users.noreply.github.com>
2023-01-09 17:35:31 +0200

    Adjust code to work with custom meta

--- js/maxi-dynamic-content.js
+++ js/maxi-dynamic-content.js
@@ -20,7 +20,7 @@ class DynamicContent {
 		console.log(maxiDynamicContent);
 
 		// eslint-disable-next-line no-undef
-		const elements = Array.from(maxiDynamicContent[0]);
+		const elements = maxiDynamicContent[0];
 		// eslint-disable-next-line no-undef
 		const restUrl = maxiDynamicContent[1];
 
@@ -28,41 +28,46 @@ class DynamicContent {
 
 		const response = {};
 
-		const getTitle = async (type, id) => {
+		const getRESTContent = async (type, id, field) => {
 			let response = '';
+			console.log(`${restUrl}wp/v2/${type}/${id}?&_fields=${field}`);
 			await apiFetch({
-				path: `${restUrl}wp/v2/${type}/${id}?&_fields=title`,
+				path: `${restUrl}wp/v2/${type}/${id}?&_fields=${field}`,
 			})
 				.catch(err => console.error(err))
 				.then(result => {
 					if (!result) return null;
-					response = result?.title?.rendered;
+					switch (field) {
+						case 'date':
+							response = result?.[field];
+							break;
+
+						default:
+							response = result?.[field]?.rendered;
+					}
 				});
 
 			return response;
 		};
 
-		async function getContent(element) {
-			const dcType = element?.getAttribute('data-dynamic-content');
-			const dcTypeArray = dcType?.trim()?.split(' ');
+		async function getContent(elementId, elementSettings) {
+			const type = elementSettings['dc-type'];
+			const author = elementSettings['dc-author'];
+			const relation = elementSettings['dc-relation'];
+			const field = elementSettings['dc-field'];
+			const id = elementSettings['dc-id'];
 
-			const type = dcTypeArray[0];
-			const author = dcTypeArray[1];
-			const relation = dcTypeArray[2];
-			const id = dcTypeArray[3];
-
-			await getTitle(type, id)
+			await getRESTContent(type, id, field)
 				.catch(err => console.error(err))
 				.then(result => {
-					element.innerHTML = result;
+					document.querySelector(
+						`#${elementId} .maxi-text-block__content`
+					).innerHTML = result;
 				});
 		}
 
-		elements.forEach(element => {
-			// getContent(element);
-			// dcTypeArray.forEach(element => {
-			// 	// response[id][type] = this.getScrollData(element, type);
-			// });
+		Object.keys(elements).forEach(key => {
+			getContent(key, elements[key]);
 		});
 
 		return response;

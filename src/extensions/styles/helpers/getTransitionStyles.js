f277d449751b38300f926502d2ca4a4dd33e2305
Andrew Valenya <46112160+Fybex@users.noreply.github.com>
2023-01-24 16:33:04 +0200

    Transition in/out support

--- src/extensions/styles/helpers/getTransitionStyles.js
+++ src/extensions/styles/helpers/getTransitionStyles.js
@@ -12,7 +12,7 @@ import transitionDefault from '../transitions/transitionDefault';
 /**
  * External dependencies
  */
-import { isNil, isEqual, isEmpty, isArray } from 'lodash';
+import { isArray, isEmpty, isEqual, isNil } from 'lodash';
 
 /**
  * Generates size styles object
@@ -41,80 +41,100 @@ const getTransitionStyles = (props, transitionObj = transitionDefault) => {
 				? rawProperty
 				: [rawProperty];
 
-			targets.forEach(target => {
-				const transitionContent = transition[type][key];
+			const transitionContent = transition[type][key];
+			const split = !!transitionContent?.split;
 
+			targets.forEach(target => {
 				if (isNil(response[target]))
 					response[target] = { transition: {} };
 
-				breakpoints.forEach(breakpoint => {
-					let transitionString = '';
+				const hoverTarget = `${target}:hover`;
+				if (split && isNil(response[hoverTarget]))
+					response[hoverTarget] = { transition: {} };
 
-					const getLastTransitionAttribute = target =>
-						getLastBreakpointAttribute({
-							target,
-							breakpoint,
-							attributes: transitionContent,
+				breakpoints.forEach(breakpoint => {
+					const generateTransitionString = (
+						target,
+						isHover = false
+					) => {
+						const transitionContent =
+							isHover || !split
+								? transition[type][key]
+								: transition[type][key]?.out;
+
+						let transitionString = '';
+
+						const getLastTransitionAttribute = target =>
+							getLastBreakpointAttribute({
+								target,
+								breakpoint,
+								attributes: transitionContent,
+							});
+
+						const getTransitionAttribute = target =>
+							transitionContent?.[`${target}-${breakpoint}`];
+
+						const lastTransitionDuration =
+							getLastTransitionAttribute('transition-duration');
+						const transitionDuration = getTransitionAttribute(
+							'transition-duration'
+						);
+
+						const lastTransitionDelay =
+							getLastTransitionAttribute('transition-delay');
+						const transitionDelay =
+							getTransitionAttribute('transition-delay');
+
+						const lastTransitionTimingFunction =
+							getLastTransitionAttribute('easing');
+						const transitionTimingFunction =
+							getTransitionAttribute('easing');
+
+						const lastTransitionStatus =
+							getLastTransitionAttribute('transition-status');
+						const transitionStatus =
+							getTransitionAttribute('transition-status');
+
+						properties.forEach(property => {
+							const transitionProperty = property || 'all';
+							const isSomeValue =
+								isEqual(
+									transitionDuration,
+									lastTransitionDuration
+								) ||
+								isEqual(transitionDelay, lastTransitionDelay) ||
+								isEqual(
+									transitionTimingFunction,
+									lastTransitionTimingFunction
+								) ||
+								isEqual(transitionStatus, lastTransitionStatus);
+
+							if (isSomeValue)
+								if (!lastTransitionStatus) {
+									transitionString += `${transitionProperty} 0s 0s, `;
+								} else if (lastTransitionStatus) {
+									transitionString += `${transitionProperty} ${lastTransitionDuration}s ${lastTransitionDelay}s ${lastTransitionTimingFunction}, `;
+								}
 						});
 
-					const getTransitionAttribute = target =>
-						transitionContent?.[`${target}-${breakpoint}`];
+						transitionString = transitionString.replace(
+							/,\s*$/,
+							''
+						);
 
-					const lastTransitionDuration = getLastTransitionAttribute(
-						'transition-duration'
-					);
-					const transitionDuration = getTransitionAttribute(
-						'transition-duration'
-					);
+						if (transitionString)
+							if (isNil(response[target].transition[breakpoint]))
+								response[target].transition[breakpoint] = {
+									transition: transitionString,
+								};
+							else
+								response[target].transition[
+									breakpoint
+								].transition += `, ${transitionString}`;
+					};
 
-					const lastTransitionDelay =
-						getLastTransitionAttribute('transition-delay');
-					const transitionDelay =
-						getTransitionAttribute('transition-delay');
-
-					const lastTransitionTimingFunction =
-						getLastTransitionAttribute('easing');
-					const transitionTimingFunction =
-						getTransitionAttribute('easing');
-
-					const lastTransitionStatus =
-						getLastTransitionAttribute('transition-status');
-					const transitionStatus =
-						getTransitionAttribute('transition-status');
-
-					properties.forEach(property => {
-						const transitionProperty = property || 'all';
-						const isSomeValue =
-							isEqual(
-								transitionDuration,
-								lastTransitionDuration
-							) ||
-							isEqual(transitionDelay, lastTransitionDelay) ||
-							isEqual(
-								transitionTimingFunction,
-								lastTransitionTimingFunction
-							) ||
-							isEqual(transitionStatus, lastTransitionStatus);
-
-						if (isSomeValue)
-							if (!lastTransitionStatus) {
-								transitionString += `${transitionProperty} 0s 0s, `;
-							} else if (lastTransitionStatus) {
-								transitionString += `${transitionProperty} ${lastTransitionDuration}s ${lastTransitionDelay}s ${lastTransitionTimingFunction}, `;
-							}
-					});
-
-					transitionString = transitionString.replace(/,\s*$/, '');
-
-					if (transitionString)
-						if (isNil(response[target].transition[breakpoint]))
-							response[target].transition[breakpoint] = {
-								transition: transitionString,
-							};
-						else
-							response[target].transition[
-								breakpoint
-							].transition += `, ${transitionString}`;
+					generateTransitionString(target);
+					if (split) generateTransitionString(hoverTarget, true);
 				});
 			});
 		});

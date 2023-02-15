4222d856ffd2d6d8f8064a1fe3b3c9f695992e0c
Svitlana <2401618+elzadj@users.noreply.github.com>
2023-02-15 17:56:45 +0200

    Fix hover prefix for global colours

--- src/editor/style-cards/maxiStyleCardsTab.js
+++ src/editor/style-cards/maxiStyleCardsTab.js
@@ -49,6 +49,7 @@ const GlobalColor = props => {
 		onChangeValue,
 		SCStyle,
 		disableOpacity = false,
+		isHover = false,
 	} = props;
 
 	return (
@@ -134,6 +135,7 @@ const GlobalColor = props => {
 						}}
 						blockStyle={SCStyle}
 						disableOpacity={disableOpacity}
+						isHover={isHover}
 						disableGradient
 					/>
 				</>
@@ -200,6 +202,7 @@ const SCAccordion = props => {
 							onChangeValue={onChangeValue}
 							SCStyle={SCStyle}
 							disableOpacity={disableOpacity}
+							isHover={label.includes('hover')}
 						/>
 					)
 				)}

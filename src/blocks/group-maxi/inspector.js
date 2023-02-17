7d2dbdada95147bf292ade1da28d90f85b546e9e
Andrew Valenya <46112160+Fybex@users.noreply.github.com>
2023-02-17 18:51:05 +0200

    Remove unused variables/imports

--- src/blocks/group-maxi/inspector.js
+++ src/blocks/group-maxi/inspector.js
@@ -11,13 +11,12 @@ import { AccordionControl, SettingTabsControl } from '../../components';
 import * as inspectorTabs from '../../components/inspector-tabs';
 import { customCss } from './data';
 import { withMaxiInspector } from '../../extensions/inspector';
-import { getTransformSelectors } from '../../components/transform-control/utils';
 
 /**
  * Inspector
  */
 const Inspector = props => {
-	const { attributes, deviceType } = props;
+	const { deviceType } = props;
 	const { selectors, categories } = customCss;
 
 	return (

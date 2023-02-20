051689ff35c1fca003896cd59cd7c194fda37e1d
Josh <50477222+Naaaaiix@users.noreply.github.com>
2023-02-20 08:55:22 +0100

    Fix typography styles (#4387)
    * Fixes typography styles
    
    * Clean code

--- src/extensions/styles/helpers/getTypographyStyles.js
+++ src/extensions/styles/helpers/getTypographyStyles.js
@@ -118,7 +118,6 @@ const getTypographyStyles = ({
 			target: `${prefix}${prop}`,
 			breakpoint,
 			attributes: isCustomFormat ? customFormatTypography : obj,
-			avoidXXL: breakpoint === 'general',
 		});
 
 		if (!normalTypography || unit) return unit === '-' ? '' : unit;
@@ -127,7 +126,6 @@ const getTypographyStyles = ({
 			target: `${prefix}${prop}`,
 			breakpoint,
 			attributes: normalTypography,
-			avoidXXL: breakpoint === 'general',
 		});
 	};
 

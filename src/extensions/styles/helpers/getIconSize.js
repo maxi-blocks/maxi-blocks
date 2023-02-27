e7fad0bdc1ba1c27b6ca9bf9edc8b464a0496d92
Andrii Valenia <46112160+Fybex@users.noreply.github.com>
2023-02-27 11:34:07 +0200

    Issue 4388 - `svg-icon-maxi` fit-height support (#4422)
    * Merge `getIconSize` and `getSVGStyles`
    
    * `svg-icon-maxi`: fit-height support
    
    * `togglePreserveAspectRatio`
    
    * Use `togglePreserveAspectRatio`
    
    * Update e2e
    
    height property was added

--- src/extensions/styles/helpers/getIconSize.js
+++ src/extensions/styles/helpers/getIconSize.js
@@ -1,113 +1,19 @@
 /**
  * Internal dependencies
  */
-import getAttributeValue from '../getAttributeValue';
-import getLastBreakpointAttribute from '../getLastBreakpointAttribute';
-
-/**
- * External dependencies
- */
-import { isEmpty, isNil, round } from 'lodash';
-
-const breakpoints = ['general', 'xxl', 'xl', 'l', 'm', 's', 'xs'];
+import { getSVGWidthStyles } from './getSVGStyles';
 
 const getIconSize = (
 	obj,
 	isHover = false,
 	prefix = '',
 	iconWidthHeightRatio = 1
-) => {
-	const response = {
-		label: 'Icon size',
-		general: {},
-	};
-
-	const svgType = getAttributeValue({
-		target: 'svgType',
-		props: obj,
+) =>
+	getSVGWidthStyles({
+		obj,
 		isHover,
-		prefix,
+		prefix: `${prefix}icon-`,
+		iconWidthHeightRatio,
 	});
-
-	breakpoints.forEach(breakpoint => {
-		response[breakpoint] = {};
-
-		const iconSize =
-			getLastBreakpointAttribute({
-				target: `${prefix}icon-width`,
-				isHover,
-				breakpoint,
-				attributes: obj,
-			}) ??
-			getLastBreakpointAttribute({
-				target: `${prefix}icon-height`,
-				isHover,
-				breakpoint,
-				attributes: obj,
-			});
-
-		const iconUnit =
-			getLastBreakpointAttribute({
-				target: `${prefix}icon-width-unit`,
-				isHover,
-				breakpoint,
-				attributes: obj,
-			}) ??
-			getLastBreakpointAttribute({
-				target: `${prefix}icon-height-unit`,
-				isHover,
-				breakpoint,
-				attributes: obj,
-			}) ??
-			'px';
-
-		const iconWidthFitContent = getLastBreakpointAttribute({
-			target: `${prefix}icon-width-fit-content`,
-			isHover,
-			breakpoint,
-			attributes: obj,
-		});
-
-		const iconStrokeWidth =
-			svgType !== 'Shape'
-				? getLastBreakpointAttribute({
-						target: `${prefix}icon-stroke`,
-						isHover,
-						breakpoint,
-						attributes: obj,
-				  })
-				: 1;
-
-		const perStrokeWidthCoefficient = 4;
-
-		const heightToStrokeWidthCoefficient =
-			1 +
-			((iconStrokeWidth - 1) *
-				perStrokeWidthCoefficient *
-				iconWidthHeightRatio) /
-				100;
-
-		if (!isNil(iconSize) && !isEmpty(iconSize)) {
-			response[breakpoint].height = `${
-				iconWidthFitContent && iconWidthHeightRatio !== 1
-					? round(
-							iconWidthHeightRatio > 1
-								? (iconSize * heightToStrokeWidthCoefficient) /
-										iconWidthHeightRatio
-								: iconSize /
-										(iconWidthHeightRatio *
-											heightToStrokeWidthCoefficient)
-					  )
-					: iconSize
-			}${iconUnit}`;
-			response[breakpoint].width = `${iconSize}${iconUnit}`;
-		}
-
-		if (isEmpty(response[breakpoint]) && breakpoint !== 'general')
-			delete response[breakpoint];
-	});
-
-	return { iconSize: response };
-};
 
 export default getIconSize;

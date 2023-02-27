40ca004184f588f9747fa94630036efecf3bba92
Andrii Valenia <46112160+Fybex@users.noreply.github.com>
2023-02-27 11:34:07 +0200

    Issue 4388 - `svg-icon-maxi` fit-height support (#4422)
    * Merge `getIconSize` and `getSVGStyles`
    
    * `svg-icon-maxi`: fit-height support
    
    * `togglePreserveAspectRatio`
    
    * Use `togglePreserveAspectRatio`
    
    * Update e2e
    
    height property was added

--- src/blocks/svg-icon-maxi/styles.js
+++ src/blocks/svg-icon-maxi/styles.js
@@ -4,20 +4,20 @@
 import { getGroupAttributes, styleProcessor } from '../../extensions/styles';
 import {
 	getAlignmentFlexStyles,
+	getBackgroundStyles,
 	getBlockBackgroundStyles,
 	getBorderStyles,
 	getBoxShadowStyles,
 	getDisplayStyles,
+	getFlexStyles,
 	getMarginPaddingStyles,
 	getOpacityStyles,
+	getOverflowStyles,
 	getPositionStyles,
 	getSizeStyles,
 	getSVGStyles,
-	getZIndexStyles,
-	getOverflowStyles,
 	getSVGWidthStyles,
-	getFlexStyles,
-	getBackgroundStyles,
+	getZIndexStyles,
 } from '../../extensions/styles/helpers';
 import data from './data';
 
@@ -113,7 +113,7 @@ const getWrapperObjectHover = props => {
 	return response;
 };
 
-const getNormalObject = props => {
+const getNormalObject = (props, iconWidthHeightRatio) => {
 	const response = {
 		boxShadow: getBoxShadowStyles({
 			obj: {
@@ -146,7 +146,11 @@ const getNormalObject = props => {
 			blockStyle: props.blockStyle,
 			prefix: 'svg-',
 		}),
-		...getSVGWidthStyles({ obj: getGroupAttributes(props, 'svg') }),
+		...getSVGWidthStyles({
+			obj: getGroupAttributes(props, 'svg'),
+			prefix: 'svg-',
+			iconWidthHeightRatio,
+		}),
 		...getBackgroundStyles({
 			...getGroupAttributes(
 				props,
@@ -205,7 +209,7 @@ const getHoverObject = props => {
 	return response;
 };
 
-const getStyles = props => {
+const getStyles = (props, iconWidthHeightRatio) => {
 	const { uniqueID, blockStyle } = props;
 
 	const response = {
@@ -213,7 +217,10 @@ const getStyles = props => {
 			{
 				'': getWrapperObject(props),
 				':hover': getWrapperObjectHover(props),
-				' .maxi-svg-icon-block__icon': getNormalObject(props),
+				' .maxi-svg-icon-block__icon': getNormalObject(
+					props,
+					iconWidthHeightRatio
+				),
 				' .maxi-svg-icon-block__icon:hover': getHoverObject(props),
 				...getSVGStyles({
 					obj: {

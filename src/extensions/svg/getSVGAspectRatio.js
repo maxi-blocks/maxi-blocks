839a812925a8ba7cdddcbd6f534e22321bb2035d
Svitlana <2401618+elzadj@users.noreply.github.com>
2023-03-13 13:16:40 +0200

    Try to fix createRoot issue

--- src/extensions/svg/getSVGAspectRatio.js
+++ src/extensions/svg/getSVGAspectRatio.js
@@ -2,3 +2,5 @@ const getSVGAspectRatio = svg => {
 	if (!svg || !svg.includes('preserveaspectratio')) return null;
 	return svg?.split('preserveaspectratio="')?.pop()?.split('"')[0];
 };
+
+export default getSVGAspectRatio;

daa166b303eae0aeb6fafe6c4c79fd7b567e1d3d
Svitlana <2401618+elzadj@users.noreply.github.com>
2023-03-15 15:17:31 +0200

    Add code for blocks help button

--- src/components/toolbar/components/help/index.js
+++ src/components/toolbar/components/help/index.js
@@ -25,16 +25,35 @@ import './editor.scss';
  * Duplicate
  */
 const Help = props => {
-	const { blockName, tooltipsHide } = props;
+	const { blockName, tooltipsHide, supportChat } = props;
 
 	if (blockName === 'maxi-blocks/column-maxi') return null;
 
+	console.log('supportChat');
+	console.log(supportChat);
+
 	const helpContent = () => {
+		if (supportChat)
+			return (
+				<div className='toolbar-item toolbar-item__help'>
+					<CrispChat>
+						<Icon
+							className='toolbar-item__icon'
+							icon={toolbarHelp}
+						/>
+					</CrispChat>
+				</div>
+			);
+
 		return (
 			<div className='toolbar-item toolbar-item__help'>
-				<CrispChat>
+				<a
+					href='https://maxiblocks.com/go/help-center'
+					target='_blank'
+					rel='noopener noreferrer'
+				>
 					<Icon className='toolbar-item__icon' icon={toolbarHelp} />
-				</CrispChat>
+				</a>
 			</div>
 		);
 	};

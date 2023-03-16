e92e710239a8e772071888089e0a95bc9e44fce1
Andrew Valenya <46112160+Fybex@users.noreply.github.com>
2023-03-16 19:07:12 +0200

    Calculate correct `keepStylesOnEditor`

--- src/extensions/maxi-block/maxiBlockComponent.js
+++ src/extensions/maxi-block/maxiBlockComponent.js
@@ -264,33 +264,9 @@ class MaxiBlockComponent extends Component {
 	componentWillUnmount() {
 		// If it's site editor, when swapping from pages we need to keep the styles
 		// On post editor, when entering to `code editor` page, we need to keep the styles
-		let keepStylesOnEditor = false;
-		entityRecordsWrapper(({ key: id, name }) => {
-			const { getEditedEntityRecord } = select('core');
-
-			const { blocks } = getEditedEntityRecord('postType', name, id);
-
-			if (!blocks) return;
-
-			const getName = block => {
-				const {
-					attributes: { uniqueID },
-					innerBlocks,
-				} = block;
-
-				if (uniqueID === this.props.attributes.uniqueID) return true;
-
-				if (innerBlocks.length)
-					return innerBlocks.some(block => getName(block));
-
-				return false;
-			};
-
-			keepStylesOnEditor ||= blocks?.some(block => getName(block));
-		}, true);
-		// const keepStylesOnEditor = !!select('core/block-editor').getBlock(
-		// 	this.props.clientId
-		// );
+		const keepStylesOnEditor = !!select('core/block-editor').getBlock(
+			this.props.clientId
+		);
 
 		// When duplicating Gutenberg creates a copy of the current copied block twice, making the first keep the same uniqueID and second
 		// has a different one. The original block is removed so componentWillUnmount method is triggered, and as its uniqueID coincide with

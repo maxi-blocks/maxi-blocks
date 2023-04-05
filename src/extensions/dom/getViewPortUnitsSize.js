96e1e3e8aa0c425d8437dac7482b905c67ac7a0c
TeksPeks <verstyukm@gmail.com>
2023-04-05 20:13:46 +0300

    get viewport width from maxi breakpoint value instead of actual editor width fixes inconsistency on test runs on different environments

--- src/extensions/dom/getViewPortUnitsSize.js
+++ src/extensions/dom/getViewPortUnitsSize.js
@@ -1,6 +1,15 @@
-import getEditorWrapper from './getEditorWrapper';
+import { select } from '@wordpress/data';
 
-const getVwSize = () => getEditorWrapper().offsetWidth * 0.01;
+const getVwSize = () => {
+	const currentBreakpoint = select('maxiBlocks').receiveMaxiDeviceType();
+	const breakpoint =
+		currentBreakpoint === 'general'
+			? select('maxiBlocks').receiveBaseBreakpoint()
+			: currentBreakpoint;
+
+	return select('maxiBlocks').receiveMaxiBreakpoints()[breakpoint] * 0.01;
+};
+
 const getVhSize = () => window.innerHeight * 0.01;
 
 export { getVwSize, getVhSize };

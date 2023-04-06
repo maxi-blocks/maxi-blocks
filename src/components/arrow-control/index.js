44653e64cd7770a662618d77e7410f6c866ab015
Marko64 <marko.niksicafe@gmail.com>
2023-04-06 12:17:37 +0200

    Issue-4673-missing-u-in-color

--- src/components/arrow-control/index.js
+++ src/components/arrow-control/index.js
@@ -102,7 +102,7 @@ const ArrowControl = props => {
 				props['show-warning-box'] && (
 					<InfoBox
 						message={__(
-							'Please ensure that the background color is not the same as the page background color.'
+							'Please ensure that the background colour is not the same as the page background colouur.'
 						)}
 						onClose={() => onChange({ 'show-warning-box': false })}
 					/>

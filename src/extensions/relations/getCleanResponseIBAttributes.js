b231896dfd8d857be28e778e40dedbe10843ade7
Josh <50477222+Naaaaiix@users.noreply.github.com>
2023-04-17 15:26:52 +0200

    Fix IB breakpoints issue

--- src/extensions/relations/getCleanResponseIBAttributes.js
+++ src/extensions/relations/getCleanResponseIBAttributes.js
@@ -3,6 +3,7 @@
  */
 import { getClientIdFromUniqueId } from '../attributes';
 import { handleSetAttributes } from '../maxi-block';
+import { replaceAttrKeyBreakpoint } from '../styles';
 import getRelatedAttributes from './getRelatedAttributes';
 import getTempAttributes from './getTempAttributes';
 
@@ -43,36 +44,48 @@ const getCleanResponseIBAttributes = (
 	});
 
 	/**
-	 * XXL exception: there's a concrete situation that needs to be saved as exception.
-	 * In cases where the block attributes for XXL and General are different, but we save
-	 * the same value for XXL and General in IB, the XXL value become undefined, so in IB
-	 * component the XXL value from the block attribute is the one shown. Here's an example:
+	 * Exception: there's a concrete situation that needs to be saved as exception.
+	 * In cases where the block attributes for a breakpoint and General are different,
+	 * but we save the same value for this concrete breakpoint and General in IB,
+	 * the breakpoint attribute value become undefined, so in IB component the current
+	 * breakpoint value from the block attribute is the one shown. Here's an example:
 	 * Block attributes: { test-general: 10, test-xxl: 20 }
 	 * IB attributes: { test-general: 15: test-xxl: undefined} (because we save 15 for both)
 	 *
-	 * In this case, we need to save the XXL value from the block attributes, so the IB
+	 * In this case, we need to save the current breakpoint value from the block attributes, so the IB
 	 * will display the correct value. If not, as it is on the example, IB component will
 	 * display the value from the block attributes on XXL, which is 20.
 	 */
-	if (breakpoint === 'xxl') {
-		const newXXLUndefinedAttrs = compact(
+	if (breakpoint !== 'general') {
+		const newUndefinedAttrs = compact(
 			Object.entries(cleanAttributesObject).map(([key, value]) => {
-				if (key.includes('xxl') && value === undefined) return key;
+				if (key.endsWith(`-${breakpoint}`) && value === undefined)
+					return key;
 
 				return null;
 			})
 		);
 
-		if (newXXLUndefinedAttrs.length) {
-			newXXLUndefinedAttrs.forEach(attr => {
-				if (filteredAttributesObj[attr] !== cleanAttributesObject[attr])
+		if (newUndefinedAttrs.length) {
+			newUndefinedAttrs.forEach(attr => {
+				if (
+					filteredAttributesObj[attr] !== cleanAttributesObject[attr]
+				) {
 					cleanAttributesObject[attr] = filteredAttributesObj[attr];
-				else if (
+					return;
+				}
+
+				const breakpointAttrKey = replaceAttrKeyBreakpoint(
+					attr,
+					breakpoint
+				);
+
+				if (
 					isNil(cleanAttributesObject[attr]) &&
-					cleanAttributesObject[attr.replace('xxl', 'general')]
+					cleanAttributesObject[breakpointAttrKey]
 				)
 					cleanAttributesObject[attr] =
-						cleanAttributesObject[attr.replace('xxl', 'general')];
+						cleanAttributesObject[breakpointAttrKey];
 			});
 		}
 	}

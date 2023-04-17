8cc77d36f65fd33bd5fef247235e81559d3f5c70
Josh <50477222+Naaaaiix@users.noreply.github.com>
2023-04-17 13:12:54 +0200

    Fixes #4695

--- src/extensions/maxi-block/cleanAttributes.js
+++ src/extensions/maxi-block/cleanAttributes.js
@@ -388,6 +388,11 @@ const flatNewAttributes = (
 
 			if (shouldPreserveAttribute) result[key] = value;
 			else {
+				if (breakpoint === 'xxl' && value === generalAttr) {
+					result[key] = undefined;
+					return;
+				}
+
 				const defaultAttribute =
 					defaultAttributes?.[key] ??
 					getDefaultAttribute(key, clientId, true);
@@ -694,6 +699,7 @@ const cleanAttributes = ({
 			});
 
 			return (
+				!isNil(value) &&
 				value !== attributes[key] &&
 				(isNil(higherAttr) || attributes[key] !== higherAttr)
 			);

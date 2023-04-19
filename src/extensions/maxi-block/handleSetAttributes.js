adb3fd77920bca2168cc9db6234cca6c74ae72b9
Josh <50477222+Naaaaiix@users.noreply.github.com>
2023-04-19 09:33:24 +0200

    Minor fix in `handleSetAttributes`

--- src/extensions/maxi-block/handleSetAttributes.js
+++ src/extensions/maxi-block/handleSetAttributes.js
@@ -65,7 +65,7 @@ const handleSetAttributes = ({
 			attrExistOnBaseBreakpoint &&
 			defaultGeneralAttribute !== value
 		) {
-			response[attrLabelOnBaseBreakpoint] = value;
+			response[attrLabelOnBaseBreakpoint] = undefined;
 
 			return;
 		}

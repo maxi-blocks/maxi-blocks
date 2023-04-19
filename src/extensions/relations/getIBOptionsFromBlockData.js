6638385a1efda8c8757551a1f2fb23fde9837474
Josh <50477222+Naaaaiix@users.noreply.github.com>
2023-04-19 11:14:57 +0200

    Issue 4434 - Implement `handleSetAttributes` to IB (#4715)
    * Implement `handleSetAttributes` to IB
    
    * Fixes IB color issues
    
    * Implements `styleAttrs` on relation objects
    
    * Clean console.log
    
    * fixed bg gradient issue
    
    * fixed implementation
    
    * Replaced `addRelatedAttributes` function
    Add palette support to IB in case is necessary for styling
    Updates IB tests
    
    * Fixed Border IB exception
    Abstracted main IB onChange functions
    Added relations store
    Implement UpdateRelationsRemotely
    
    * Implements "migrator" for IB
    Fix minor issues
    
    * Fix responsive issue on IB
    
    * Fixed minor bugs
    
    * Fixes minor bugs
    
    * Update tests
    
    * Fixes XXL exception on IB
    
    * Fixes #4723
    
    * Fix frontend issue with XXL for IB
    
    * Fix XXL displayed values on IB
    
    * Fixed minor bug
    
    * Fix minor bug
    
    * Fix minor bug
    
    * Fix IB breakpoints issue
    
    * Fixes minor issues
    
    * Fix minor issues
    
    * Fixes issues on IB styles object generation
    Created tests for `getIBStyles`
    
    * Fixes transitions on frontend
    
    * Fixes transitions on frontend
    
    * Small updates
    
    * Fixes BGLayers exception
    
    * Fix minor issue with `getTempAttributes`
    
    ---------
    
    Co-authored-by: Fitsum Abebe <engfitsum@gmail.com>
    Co-authored-by: Svitlana <2401618+elzadj@users.noreply.github.com>

---
+++ src/extensions/relations/getIBOptionsFromBlockData.js
@@ -0,0 +1,24 @@
+/**
+ * WordPress dependencies
+ */
+import { select } from '@wordpress/data';
+
+/**
+ * Internal dependencies
+ */
+import { getBlockData } from '../attributes';
+
+const getIBOptionsFromBlockData = clientId => {
+	const blockName = select('core/block-editor')
+		.getBlock(clientId)
+		?.name.replace('maxi-blocks/', '');
+
+	// TODO: without this line, the block may break after copy/pasting
+	if (!blockName) return {};
+
+	const blockOptions = getBlockData(blockName).interactionBuilderSettings;
+
+	return blockOptions || {};
+};
+
+export default getIBOptionsFromBlockData;

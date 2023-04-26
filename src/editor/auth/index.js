d032e5d5a66e770369dfb7b8c323e824babf1c85
Svitlana <2401618+elzadj@users.noreply.github.com>
2023-04-26 13:02:45 +0300

    Remove appwrite package

--- src/editor/auth/index.js
+++ src/editor/auth/index.js
@@ -259,6 +259,7 @@ export const logOut = () => {
 };
 
 export const isValidEmail = email => {
+	console.log('isValidEmail', email);
 	const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
 	return emailPattern.test(email);
 };

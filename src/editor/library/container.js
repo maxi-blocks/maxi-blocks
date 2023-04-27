87f35c827ff14ad2f49dd87d7b395f0474785ae9
Svitlana <2401618+elzadj@users.noreply.github.com>
2023-04-27 18:02:27 +0300

    simplier code

--- src/editor/library/container.js
+++ src/editor/library/container.js
@@ -1121,11 +1121,11 @@ const LibraryContainer = props => {
 											item => item.label === name
 										);
 										if (item) {
-											item.label = `${name}`;
+											item.label = name;
 											return item;
 										}
 										return {
-											label: `${name}`,
+											label: name,
 											value: name,
 											count: 0,
 											isRefined: false,

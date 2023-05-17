b1291c5fdd4d949a049d88b8ce087dc02d0fc2b5
Andrew Valenya <46112160+Fybex@users.noreply.github.com>
2023-05-17 21:43:41 +0300

    Add search to disallowed blocks

--- src/extensions/repeater/disallowedBlocks.js
+++ src/extensions/repeater/disallowedBlocks.js
@@ -2,6 +2,7 @@ const DISALLOWED_BLOCKS = [
 	'maxi-blocks/slider-maxi',
 	'maxi-blocks/accordion-maxi',
 	'maxi-blocks/map-maxi',
+	'maxi-blocks/search-maxi',
 ];
 
 export default DISALLOWED_BLOCKS;

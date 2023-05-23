0dfb6590000cdd97a6dec119f4b83e6e5c9a29ef
Fitsum Abebe <engfitsum@gmail.com>
2023-05-23 11:57:04 +0300

    Updated test

--- src/extensions/attributes/dictionary/blockAttributesShorter.js
+++ src/extensions/attributes/dictionary/blockAttributesShorter.js
@@ -1,12 +1,12 @@
-import parseLongAttrKey from './parseLongAttrKey';
 import parseShortAttrKey from './parseShortAttrKey';
+import parseLongAttrKey from './parseLongAttrKey';
 
 const blockAttributesShorter = attributes => {
 	const newAttributes = {};
 
 	Object.entries(attributes).forEach(([objKey, objVal]) => {
-		const newKey = parseLongAttrKey(objKey);
-		const longKey = parseShortAttrKey(newKey);
+		const longKey = parseShortAttrKey(objKey);
+		const newKey = parseLongAttrKey(longKey);
 
 		newAttributes[newKey] = { ...objVal, longLabel: longKey };
 	});

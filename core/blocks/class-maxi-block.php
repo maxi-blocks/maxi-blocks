04535cbfce8c09bd77eda5b735e19b4e0e57dad7
Svitlana <2401618+elzadj@users.noreply.github.com>
2023-07-04 13:00:47 +0300

    More optimizations

--- core/blocks/class-maxi-block.php
+++ core/blocks/class-maxi-block.php
@@ -11,56 +11,29 @@ if (!defined('ABSPATH')) {
     exit();
 }
 
-// Utils
-require_once MAXI_PLUGIN_DIR_PATH . 'core/blocks/utils/get_attribute_key.php';
-require_once MAXI_PLUGIN_DIR_PATH . 'core/blocks/utils/get_attributes_value.php';
-require_once MAXI_PLUGIN_DIR_PATH . 'core/blocks/utils/get_color_rgba_string.php';
-require_once MAXI_PLUGIN_DIR_PATH . 'core/blocks/utils/get_default_attribute.php';
-require_once MAXI_PLUGIN_DIR_PATH . 'core/blocks/utils/get_group_attributes.php';
-require_once MAXI_PLUGIN_DIR_PATH . 'core/blocks/utils/get_palette_attributes.php';
-require_once MAXI_PLUGIN_DIR_PATH . 'core/blocks/utils/get_row_gap_attributes.php';
-require_once MAXI_PLUGIN_DIR_PATH . 'core/blocks/utils/style_processor.php';
-require_once MAXI_PLUGIN_DIR_PATH . 'core/blocks/utils/get_last_breakpoint_attribute.php';
-require_once MAXI_PLUGIN_DIR_PATH . 'core/blocks/utils/get_prev_breakpoint.php';
+// Define the directory paths.
+$utilsDir = MAXI_PLUGIN_DIR_PATH . 'core/blocks/utils/';
+$styleHelpersDir = MAXI_PLUGIN_DIR_PATH . 'core/blocks/style-helpers/';
 
-// Style Helpers
-require_once MAXI_PLUGIN_DIR_PATH . 'core/blocks/style-helpers/get_alignment_styles.php';
-require_once MAXI_PLUGIN_DIR_PATH . 'core/blocks/style-helpers/get_alignment_text_styles.php';
-require_once MAXI_PLUGIN_DIR_PATH . 'core/blocks/style-helpers/get_arrow_styles.php';
-require_once MAXI_PLUGIN_DIR_PATH . 'core/blocks/style-helpers/get_aspect_ratio.php';
-require_once MAXI_PLUGIN_DIR_PATH . 'core/blocks/style-helpers/get_background_styles.php';
-require_once MAXI_PLUGIN_DIR_PATH . 'core/blocks/style-helpers/get_border_styles.php';
-require_once MAXI_PLUGIN_DIR_PATH . 'core/blocks/style-helpers/get_box_shadow_styles.php';
-require_once MAXI_PLUGIN_DIR_PATH . 'core/blocks/style-helpers/get_button_icon_styles.php';
-require_once MAXI_PLUGIN_DIR_PATH . 'core/blocks/style-helpers/get_clip_path_styles.php';
-require_once MAXI_PLUGIN_DIR_PATH . 'core/blocks/style-helpers/get_column_size_styles.php';
-// require_once MAXI_PLUGIN_DIR_PATH . 'core/blocks/style-helpers/get_custom_css.php';
-require_once MAXI_PLUGIN_DIR_PATH . 'core/blocks/style-helpers/get_custom_formats_styles.php';
-require_once MAXI_PLUGIN_DIR_PATH . 'core/blocks/style-helpers/get_display_styles.php';
-require_once MAXI_PLUGIN_DIR_PATH . 'core/blocks/style-helpers/get_divider_styles.php';
-require_once MAXI_PLUGIN_DIR_PATH . 'core/blocks/style-helpers/get_flex_styles.php';
-require_once MAXI_PLUGIN_DIR_PATH . 'core/blocks/style-helpers/get_hover_effects_background_styles.php';
-require_once MAXI_PLUGIN_DIR_PATH . 'core/blocks/style-helpers/get_icon_path_styles.php';
-require_once MAXI_PLUGIN_DIR_PATH . 'core/blocks/style-helpers/get_icon_size_styles.php';
-require_once MAXI_PLUGIN_DIR_PATH . 'core/blocks/style-helpers/get_icon_styles.php';
-require_once MAXI_PLUGIN_DIR_PATH . 'core/blocks/style-helpers/get_image_shape_styles.php';
-require_once MAXI_PLUGIN_DIR_PATH . 'core/blocks/style-helpers/get_link_styles.php';
-require_once MAXI_PLUGIN_DIR_PATH . 'core/blocks/style-helpers/get_margin_padding_styles.php';
-require_once MAXI_PLUGIN_DIR_PATH . 'core/blocks/style-helpers/get_number_counter_styles.php';
-require_once MAXI_PLUGIN_DIR_PATH . 'core/blocks/style-helpers/get_opacity_styles.php';
-require_once MAXI_PLUGIN_DIR_PATH . 'core/blocks/style-helpers/get_overflow_styles.php';
-require_once MAXI_PLUGIN_DIR_PATH . 'core/blocks/style-helpers/get_position_styles.php';
-require_once MAXI_PLUGIN_DIR_PATH . 'core/blocks/style-helpers/get_shape_divider_styles.php';
-require_once MAXI_PLUGIN_DIR_PATH . 'core/blocks/style-helpers/get_shape_styles.php';
-require_once MAXI_PLUGIN_DIR_PATH . 'core/blocks/style-helpers/get_size_styles.php';
-require_once MAXI_PLUGIN_DIR_PATH . 'core/blocks/style-helpers/get_svg_styles.php';
-require_once MAXI_PLUGIN_DIR_PATH . 'core/blocks/style-helpers/get_transform_styles.php';
-require_once MAXI_PLUGIN_DIR_PATH . 'core/blocks/style-helpers/get_transition_styles.php';
-require_once MAXI_PLUGIN_DIR_PATH . 'core/blocks/style-helpers/get_typography_styles.php';
-require_once MAXI_PLUGIN_DIR_PATH . 'core/blocks/style-helpers/get_zindex_styles.php';
+// Load the utility files.
+foreach (glob($utilsDir . '*.php') as $file) {
+    require_once $file;
+}
 
+// Load the style helper files.
+foreach (glob($styleHelpersDir . '*.php') as $file) {
+    require_once $file;
+}
 
+// Check if the class is already defined.
 if (!class_exists('MaxiBlocks_Block')):
+
+    /**
+     * MaxiBlocks_Block Class
+     *
+     * Handles the main operations for the Maxi Block, such as registration,
+     * rendering, and getting block data.
+     */
     class MaxiBlocks_Block
     {
         /**
@@ -129,13 +102,20 @@ if (!class_exists('MaxiBlocks_Block')):
             return $this->block_name;
         }
 
+        /**
+        * Get block metadata.
+        *
+        * Reads the block.json file to retrieve the block metadata.
+        *
+        * @return array The block metadata.
+        */
         public function get_block_metadata()
         {
-            if (empty($this->block_metadata)) {
-                $this->block_metadata = json_decode(file_get_contents(MAXI_PLUGIN_DIR_PATH . 'src/blocks/' . $this->block_name . '/block.json'), true);
-            }
-
-            return $this->block_metadata;
+            // If the block metadata is not yet retrieved, read it from the block.json file.
+            return $this->block_metadata ??= json_decode(
+                file_get_contents(MAXI_PLUGIN_DIR_PATH . 'src/blocks/' . $this->block_name . '/block.json'),
+                true
+            );
         }
 
         public function get_block_attributes($props)
@@ -143,50 +123,56 @@ if (!class_exists('MaxiBlocks_Block')):
             return $this->block->prepare_attributes_for_render($props);
         }
 
+        /**
+         * Get block custom CSS.
+         *
+         * Retrieves the custom CSS from the block metadata.
+         *
+         * @return array The custom CSS for the block.
+         */
         public function get_block_custom_css()
         {
-            if (empty($this->block_custom_css)) {
-                $block_metadata = $this->get_block_metadata();
-
-                if($block_metadata && array_key_exists('customCss', $block_metadata)) {
-                    $this->block_custom_css = $block_metadata['customCss'];
-                }
-            }
-
-            return $this->block_custom_css;
+            // If the custom CSS is not yet retrieved, get it from the block metadata.
+            return $this->block_custom_css ??= $this->get_block_metadata()['customCss'] ?? [];
         }
 
+
+        /**
+         * Get block style card properties.
+         *
+         * Retrieves the style card properties from the block metadata.
+         *
+         * @return array The style card properties for the block.
+         */
         public function get_block_sc_props()
         {
-            if (empty($this->block_sc_props)) {
-                $block_metadata = $this->get_block_metadata();
-
-                if($block_metadata && array_key_exists('scProps', $block_metadata)) {
-                    $this->block_sc_props = $block_metadata['scProps'];
-                }
-            }
-
-            return $this->block_sc_props;
+            // If the style card properties are not yet retrieved, get them from the block metadata.
+            return $this->block_sc_props ??= $this->get_block_metadata()['scProps'] ?? [];
         }
 
+        /**
+         * Get block style card variables.
+         *
+         * Retrieves the style card variables using the style card properties and a given block style.
+         *
+         * @param array $block_style The block style used to calculate the style card variables.
+         * @return array The style card variables for the block.
+         */
         public function get_block_sc_vars($block_style)
         {
-            if (empty($this->block_sc_vars)) {
-                $block_sc_props = $this->get_block_sc_props();
-
-                if(!empty($block_sc_props)) {
-                    if(class_exists('MaxiBlocks_StyleCards')) {
-                        $sc_props = $block_sc_props['scElements'];
-                        $sc_entry = $block_sc_props['scType'];
-
-
-                        $this->block_sc_vars = MaxiBlocks_StyleCards::get_style_cards_values($sc_props, $block_style, $sc_entry);
-                    }
-                }
+            if (isset($this->block_sc_vars)) {
+                return $this->block_sc_vars;
+            }
+            $block_sc_props = $this->get_block_sc_props();
+            if (empty($block_sc_props)) {
+                return $this->block_sc_vars = [];
             }
 
-            return $this->block_sc_vars;
+            $sc_props = $block_sc_props['scElements'];
+            $sc_entry = $block_sc_props['scType'];
+            return $this->block_sc_vars = MaxiBlocks_StyleCards::get_style_cards_values($sc_props, $block_style, $sc_entry);
         }
+
     }
 
 endif;

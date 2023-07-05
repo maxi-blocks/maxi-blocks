5a0c9989a7ec28d7e5af2180c58fe4f72ce213f8
TeksPeks <verstyukm@gmail.com>
2023-07-05 13:26:39 +0300

    add DC link attributes to container blocks show DC link switch if context loop enabled

--- src/blocks/row-maxi/attributes.js
+++ src/blocks/row-maxi/attributes.js
@@ -38,6 +38,7 @@ const attributes = {
 	 * Advanced
 	 */
 	...attributesData.contextLoop,
+	...attributesData.dynamicContentLink,
 	...attributesData.transform,
 	...{
 		...attributesData.transition,

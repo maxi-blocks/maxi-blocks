8e2d4fa5808734044b464ad3950312638d1580c1
Svitlana <2401618+elzadj@users.noreply.github.com>
2023-07-14 16:05:56 +0300

    remove logs

--- src/extensions/attributes/uniqueIDGenerator.js
+++ src/extensions/attributes/uniqueIDGenerator.js
@@ -4,18 +4,13 @@
 import apiFetch from '@wordpress/api-fetch';
 
 const generateUniqueID = async blockName => {
-	console.log('generateUniqueID');
-	console.log('blockName', blockName);
 	const newID = await apiFetch({
 		path: `/maxi-blocks/v1.0/unique-id/${blockName}`,
 	});
-	console.log('newID', newID);
 	return newID;
 };
 
 const uniqueIDGenerator = async blockName => {
-	console.log('uniqueIDGenerator');
-	console.log('blockName', blockName);
 	if (!blockName) return null;
 	const modifiedBlockName = blockName.replace('maxi-blocks/', '');
 	const newID = await generateUniqueID(modifiedBlockName);

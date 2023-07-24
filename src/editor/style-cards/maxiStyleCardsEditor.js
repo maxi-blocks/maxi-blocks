2aa1b51c886fae0909a26fcd0c42864e25ce1e2b
Andrew Valenya <46112160+Fybex@users.noreply.github.com>
2023-07-24 13:18:57 +0300

    Create `ReactSelectControl` component
    `react-select` wrapper with styles

--- src/editor/style-cards/maxiStyleCardsEditor.js
+++ src/editor/style-cards/maxiStyleCardsEditor.js
@@ -17,6 +17,7 @@ import {
 	Button,
 	DialogBox,
 	Icon,
+	ReactSelectControl,
 	SettingTabsControl,
 	ToggleSwitch,
 } from '../../components';
@@ -30,7 +31,6 @@ import standardSC from '../../../core/defaults/defaultSC.json';
  * External dependencies
  */
 import { isEmpty, isNil, isEqual, cloneDeep, merge } from 'lodash';
-import Select from 'react-select';
 
 /**
  * Icons
@@ -365,33 +365,6 @@ const MaxiStyleCardsEditor = forwardRef(({ styleCards, setIsVisible }, ref) => {
 		return response;
 	};
 
-	const customStyles = {
-		option: (base, state, selected) => ({
-			...base,
-			padding: '8px',
-			whiteSpace: 'wrap',
-			borderBottom: '1px solid #E3E3E3',
-			backgroundColor: state.isSelected
-				? 'rgb(var(--maxi-light-color-4)) !important'
-				: '#fff !important' && state.isFocused
-				? 'rgba(229, 242, 248, 0.7) !important'
-				: '#fff !important',
-			color: state.isSelected
-				? '#fff !important'
-				: 'color: var(--maxi-grey-5-color) !important',
-			width: 'auto',
-			cursor: 'pointer',
-		}),
-		control: () => ({
-			display: 'flex',
-			padding: '0',
-			marginBottom: '8px',
-			borderRadius: '0px',
-			border: '1px solid rgb(var(--maxi-light-color-4))',
-			cursor: 'pointer',
-		}),
-	};
-
 	const listForDropdown = getOptionsSCList();
 	const selectedForDropdown =
 		listForDropdown[getSelectedInList(listForDropdown)];
@@ -532,7 +505,7 @@ const MaxiStyleCardsEditor = forwardRef(({ styleCards, setIsVisible }, ref) => {
 						)}
 						<div className='maxi-style-cards__active-edit-options'>
 							<div className='maxi-style-cards__sc__more-sc--select'>
-								<Select
+								<ReactSelectControl
 									options={listForDropdown}
 									value={
 										selectedForDropdown || activeForDropdown
@@ -541,7 +514,6 @@ const MaxiStyleCardsEditor = forwardRef(({ styleCards, setIsVisible }, ref) => {
 										'Type to search…',
 										'maxi-blocks'
 									)}
-									styles={customStyles}
 									onChange={val => {
 										const newSCKey = val?.value;
 										setSelectedStyleCard(newSCKey);

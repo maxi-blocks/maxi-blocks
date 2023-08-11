0ad78c5a8ebf242864619feed12830e4c286186e
Svitlana <2401618+elzadj@users.noreply.github.com>
2023-08-11 17:43:23 +0300

    Fix removing blocks from DB

--- src/extensions/attributes/uniqueIDRemover.js
+++ src/extensions/attributes/uniqueIDRemover.js
@@ -8,22 +8,52 @@ import apiFetch from '@wordpress/api-fetch';
  */
 import { isNil } from 'lodash';
 
-const uniqueIDRemover = async uniqueID => {
+const uniqueIDRemover = async (uniqueID, innerBlocks = null) => {
 	if (!uniqueID || isNil(uniqueID)) return null;
-	try {
-		const response = await apiFetch({
-			path: `/maxi-blocks/v1.0/unique-id/remove/${uniqueID}`,
-		});
+	const uniqueIDArray = [uniqueID];
 
-		if (response === false) {
-			throw new Error(`Could not remove block ${uniqueID} from the DB`);
-		} else if (response !== true) {
-			throw new Error(
-				`Unexpected response data: ${JSON.stringify(response)}`
+	const processBlock = async block => {
+		if (block.attributes.uniqueID) {
+			uniqueIDArray.push(block.attributes.uniqueID);
+		}
+		if (block.innerBlocks && block.innerBlocks.length > 0) {
+			for (const innerBlock of block.innerBlocks) {
+				// eslint-disable-next-line no-await-in-loop
+				await processBlock(innerBlock); // Recursive call for inner blocks
+			}
+		}
+	};
+
+	if (!isNil(innerBlocks)) {
+		for (const block of innerBlocks) {
+			// eslint-disable-next-line no-await-in-loop
+			await processBlock(block);
+		}
+	}
+
+	for (const uniqueID of uniqueIDArray) {
+		try {
+			// eslint-disable-next-line no-await-in-loop
+			const response = await apiFetch({
+				path: `/maxi-blocks/v1.0/unique-id/remove/${uniqueID}`,
+				method: 'DELETE',
+			});
+
+			if (response === false) {
+				throw new Error(
+					`Could not remove block ${uniqueID} from the DB`
+				);
+			} else if (response !== true) {
+				throw new Error(
+					`Unexpected response data: ${JSON.stringify(response)}`
+				);
+			}
+		} catch (error) {
+			console.error(
+				'There was an error with the fetch call',
+				error.message
 			);
 		}
-	} catch (error) {
-		console.error('There was an error with the fetch call', error.message);
 	}
 
 	return { response: 'true' };

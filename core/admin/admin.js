c9911e87cff9c12dad03f664be8258bd087b2bc6
Andrew Valenya <46112160+Fybex@users.noreply.github.com>
2023-08-14 15:13:36 +0300

    Add openai validation, improve validation process

--- core/admin/admin.js
+++ core/admin/admin.js
@@ -60,7 +60,7 @@ document.addEventListener('DOMContentLoaded', function maxiAdmin() {
 		const uluru = { lat: -25.344, lng: 131.031 };
 		// The map, centered at Uluru
 		const map = new google.maps.Map(
-			document.getElementById('maxi-google-test-map'),
+			document.getElementById('maxi-api-test'),
 			{
 				zoom: 4,
 				center: uluru,
@@ -73,22 +73,70 @@ document.addEventListener('DOMContentLoaded', function maxiAdmin() {
 		});
 	};
 
-	const googleApiKeyInput = document.getElementById('google_api_key_option');
-	const validationDiv = document.getElementById(
-		'maxi-google-test-map_validation-message'
+	// Google API Key validation
+	const googleApiKeyVisibleInput = document.querySelector(
+		'.google-api-key-option-visible-input'
 	);
+	const googleApiKeyHiddenInput = document.getElementById(
+		'google_api_key_option'
+	);
+	const googleValidationDiv = document.getElementById(
+		'maxi-api-test__validation-message'
+	);
+
 	const head = document.getElementsByTagName('head')[0];
 
-	const getGoogleApiKey = () =>
-		document.getElementById('google_api_key_option').value;
+	const getGoogleApiKey = () => googleApiKeyVisibleInput.value;
+
+	const checkForInvalidCharactersError = googleApiKey =>
+		!googleApiKey.match(/^[a-zA-Z0-9_$.[\]]+$/);
+
+	const googleMapsCustomValidation = type => {
+		const googleApiKey = getGoogleApiKey();
+		let validationMessage = '';
+
+		if (googleApiKey === '') {
+			googleApiKeyHiddenInput.value = googleApiKey;
+			validationMessage = 'Please add your Google Maps API key';
+		} else {
+			switch (type) {
+				case 'InvalidKeyMapError':
+					validationMessage =
+						'Invalid API Key, please check your key and try again';
+					break;
+				case 'RefererNotAllowedMapError':
+					validationMessage =
+						'Referer not allowed, please allow your domain for that key';
+					break;
+				case 'EmptyKeyMapError':
+					validationMessage = 'Please add your Google Maps API key';
+					break;
+				case 'InvalidCharactersError':
+					validationMessage =
+						'Only alphabet, number, "_", "$", ".", "[", and "]" are allowed in the API key.';
+					break;
+				default: // Update the hidden input with the validated API key
+					googleApiKeyHiddenInput.value = googleApiKey;
+					break;
+			}
+		}
+
+		googleValidationDiv.classList.add('api-error');
+		googleValidationDiv.innerHTML = validationMessage;
+	};
 
 	const testGoogleApiKey = () => {
+		const googleApiKey = getGoogleApiKey();
+
+		if (checkForInvalidCharactersError(googleApiKey)) {
+			googleMapsCustomValidation('InvalidCharactersError');
+			return;
+		}
+
 		const oldScript = document.getElementById(
 			'maxi-test-google-map-script'
 		);
 		if (oldScript) head.removeChild(oldScript);
-
-		const googleApiKey = getGoogleApiKey();
 
 		const script = document.createElement('script');
 		script.src = `https://maps.googleapis.com/maps/api/js?key=${googleApiKey}&callback=initMap`;
@@ -98,34 +146,8 @@ document.addEventListener('DOMContentLoaded', function maxiAdmin() {
 
 		head.appendChild(script);
 		window.initMap = initTestMap;
-		validationDiv.innerHTML = '';
-		validationDiv.classList.remove('map-error');
-	};
-
-	const googleMapsCustomValidation = type => {
-		const googleApiKey = getGoogleApiKey();
-		let validationMessage = '';
-
-		if (googleApiKey === '') {
-			validationMessage = 'Please add your Google Maps API key';
-		} else
-			switch (type) {
-				case 'InvalidKeyMapError':
-					validationMessage =
-						'Invalid API Key, please check your key and try again';
-					break;
-				case 'RefererNotAllowedMapError':
-					validationMessage =
-						'Referer not allowed, please allow your domain for that key';
-				case 'EmptyKeyMapError':
-					validationMessage = 'Please add your Google Maps API key';
-
-				default:
-					break;
-			}
-
-		validationDiv.classList.add('map-error');
-		validationDiv.innerHTML = validationMessage;
+		googleValidationDiv.innerHTML = '';
+		googleValidationDiv.classList.remove('api-error');
 	};
 
 	const catchGoogleMapsApiErrors = () => {
@@ -137,14 +159,19 @@ document.addEventListener('DOMContentLoaded', function maxiAdmin() {
 			const original = console[method];
 			console[method] = function () {
 				if (arguments[0]) {
-					if (arguments[0].includes('InvalidKeyMapError')) {
+					if (
+						arguments[0].includes('InvalidKeyMapError') ||
+						arguments[0].includes('API multiple times')
+					) {
 						googleMapsCustomValidation('InvalidKeyMapError');
-					}
-					if (arguments[0].includes('RefererNotAllowedMapError')) {
+					} else if (
+						arguments[0].includes('RefererNotAllowedMapError')
+					) {
 						googleMapsCustomValidation('RefererNotAllowedMapError');
-					}
-					if (arguments[0].includes('API without a key')) {
+					} else if (arguments[0].includes('API without a key')) {
 						googleMapsCustomValidation('EmptyKeyMapError');
+					} else {
+						googleMapsCustomValidation(true);
 					}
 				}
 
@@ -156,8 +183,104 @@ document.addEventListener('DOMContentLoaded', function maxiAdmin() {
 		intercept(['error']);
 	};
 
-	googleApiKeyInput?.addEventListener('input', function () {
+	googleApiKeyVisibleInput?.addEventListener('input', function () {
 		testGoogleApiKey();
 		catchGoogleMapsApiErrors();
+	});
+
+	const openAIApiKeyVisibleInput = document.querySelector(
+		'.openai-api-key-option-visible-input'
+	);
+	const openAIApiKeyHiddenInput = document.getElementById(
+		'openai_api_key_option'
+	);
+	const openAIValidationDiv = document.getElementById(
+		'maxi-api-test__validation-message'
+	);
+
+	const getOpenAIApiKey = () => openAIApiKeyVisibleInput.value;
+
+	const openAIApiKeyCustomValidation = type => {
+		const isLoading = type === 'loading';
+
+		if (!isLoading) {
+			openAIValidationDiv.classList.remove('api-loading'); // Remove loading status
+		}
+		let validationMessage = '';
+
+		const openAIKey = getOpenAIApiKey();
+
+		if (type === true) {
+			openAIApiKeyHiddenInput.value = openAIKey;
+			openAIValidationDiv.classList.remove('api-error');
+		} else if (isLoading) {
+			validationMessage = 'Validating...'; // Loading status message
+			openAIValidationDiv.classList.add('api-loading');
+			openAIValidationDiv.classList.remove('api-error');
+		} else if (openAIKey === '') {
+			openAIApiKeyHiddenInput.value = '';
+			openAIValidationDiv.classList.add('api-error');
+			validationMessage = 'Please add your OpenAI API key';
+		} else {
+			openAIValidationDiv.classList.add('api-error');
+			switch (type) {
+				case 'InvalidKeyError':
+					validationMessage =
+						'Invalid API Key, please check your key and try again';
+					break;
+				case 'ServerError':
+					validationMessage =
+						'Error validating API Key, please try again later';
+					break;
+				default:
+					break;
+			}
+		}
+
+		openAIValidationDiv.innerHTML = validationMessage;
+	};
+
+	const testOpenAIApiKey = () => {
+		const openAIApiKey = getOpenAIApiKey();
+
+		if (openAIApiKey === '') {
+			openAIApiKeyCustomValidation(''); // Handle empty input case here
+			return;
+		}
+
+		openAIApiKeyCustomValidation('loading');
+
+		fetch('https://api.openai.com/v1/chat/completions', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: `Bearer ${openAIApiKey}`,
+			},
+			body: JSON.stringify({
+				messages: [
+					{
+						role: 'user',
+						content: 'Hello',
+					},
+				],
+				model: 'gpt-3.5-turbo',
+				max_tokens: 1,
+			}),
+		})
+			.then(response => {
+				if (response.ok) {
+					openAIApiKeyCustomValidation(true);
+				} else {
+					openAIApiKeyCustomValidation('InvalidKeyError');
+				}
+			})
+			.catch(error => {
+				console.error(error);
+				openAIApiKeyCustomValidation('ServerError');
+			});
+	};
+
+	openAIApiKeyVisibleInput?.addEventListener('input', function () {
+		testOpenAIApiKey();
 	});
 });

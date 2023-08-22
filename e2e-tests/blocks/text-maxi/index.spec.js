bd8fbf3da3cd106de4bdaa393aeaf4e444e48036
Svitlana <2401618+elzadj@users.noreply.github.com>
2023-08-22 15:59:14 +0300

    Change copy-paste test

--- e2e-tests/blocks/text-maxi/index.spec.js
+++ e2e-tests/blocks/text-maxi/index.spec.js
@@ -706,7 +706,7 @@ describe('TextMaxi', () => {
 		await page.$$eval('.maxi-color-control__palette-box', paletteButtons =>
 			paletteButtons[3].click()
 		);
-		await page.waitForTimeout(150);
+		await page.waitForTimeout(250);
 
 		const {
 			'palette-color-general': expectedColor,

a5ad24d55dbe25663b2b201be70058c07b1d2654
Svitlana <2401618+elzadj@users.noreply.github.com>
2023-08-22 17:48:38 +0300

    fix bg and other tests

--- src/components/relation-control/index.js
+++ src/components/relation-control/index.js
@@ -63,10 +63,8 @@ const RelationControl = props => {
 		!isEmpty(relations) ? cloneDeep(relations) : [];
 
 	// Ensure that each relation of `relations` array has a valid block
-	const relations = cloneRelations(rawRelations).filter(
-		relation =>
-			isEmpty(relation.uniqueID) ||
-			!!getClientIdFromUniqueId(relation.uniqueID)
+	const relations = cloneRelations(rawRelations).filter(relation =>
+		isEmpty(relation.uniqueID)
 	);
 
 	const getRelationId = relations => {

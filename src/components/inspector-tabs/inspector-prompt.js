572ad2c54b2173be4d70203b332df950e7199198
Andrew Valenya <46112160+Fybex@users.noreply.github.com>
2023-08-22 19:04:24 +0300

    Disable prompt maxi for lists

--- src/components/inspector-tabs/inspector-prompt.js
+++ src/components/inspector-tabs/inspector-prompt.js
@@ -11,22 +11,24 @@ import PromptControl from '../prompt-control';
 /**
  * Component
  */
-const position = ({ props }) => {
+const prompt = ({ props }) => {
 	const { clientId, attributes, maxiSetAttributes } = props;
-	const { content } = attributes;
+	const { content, isList } = attributes;
 
-	return {
-		label: __('Prompt', 'maxi-blocks'),
-		content: (
-			<PromptControl
-				clientId={clientId}
-				content={content}
-				onContentChange={newContent =>
-					maxiSetAttributes({ content: newContent })
-				}
-			/>
-		),
-	};
+	return (
+		!isList && {
+			label: __('Prompt', 'maxi-blocks'),
+			content: (
+				<PromptControl
+					clientId={clientId}
+					content={content}
+					onContentChange={newContent =>
+						maxiSetAttributes({ content: newContent })
+					}
+				/>
+			),
+		}
+	);
 };
 
-export default position;
+export default prompt;

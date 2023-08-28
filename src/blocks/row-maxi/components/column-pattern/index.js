4fcb4b3feeae635585e3cc90c39da7ed475168cf
Andrew Valenya <46112160+Fybex@users.noreply.github.com>
2023-08-28 10:45:00 +0300

    Repeater: nested row scheme change fix

--- src/blocks/row-maxi/components/column-pattern/index.js
+++ src/blocks/row-maxi/components/column-pattern/index.js
@@ -23,7 +23,6 @@ import {
 } from '../../../../extensions/styles';
 import { validateRowColumnsStructure } from '../../../../extensions/repeater';
 import { getBlockPosition } from '../../../../extensions/repeater/utils';
-import { cleanInnerBlocks } from '../../../../extensions/copy-paste';
 
 /**
  * External dependencies
@@ -246,54 +245,28 @@ const ColumnPattern = props => {
 											innerBlockPositions
 										);
 
-										const { getBlock } =
-											select('core/block-editor');
-										const {
-											__unstableMarkNextChangeAsNotPersistent:
-												markNextChangeAsNotPersistent,
-											replaceInnerBlocks,
-										} = dispatch('core/block-editor');
-
-										const rowToValidateByInnerBlocks =
-											getBlock(clientId).innerBlocks;
-
 										innerBlockPositions[
 											blockPosition
 										].forEach(rowClientId => {
 											if (rowClientId === clientId)
 												return;
 
-											const oldInnerBlocks =
-												getBlock(
-													rowClientId
-												).innerBlocks;
-
-											const clonedInnerBlocks =
-												cleanInnerBlocks(
-													rowToValidateByInnerBlocks
-												);
-
-											clonedInnerBlocks.forEach(
-												(column, i) => {
-													if (
-														i >=
-														oldInnerBlocks.length
-													)
-														return;
-
-													column.clientId =
-														oldInnerBlocks[
-															i
-														].clientId;
-												}
-											);
-
-											markNextChangeAsNotPersistent();
-											replaceInnerBlocks(
+											loadColumnsTemplate(
+												newRowPattern,
 												rowClientId,
-												clonedInnerBlocks
+												breakpoint,
+												numCol,
+												repeaterStatus,
+												repeaterStatus
 											);
 										});
+
+										// Undo/redo fix
+										const {
+											__unstableMarkNextChangeAsNotPersistent:
+												markNextChangeAsNotPersistent,
+										} = dispatch('core/block-editor');
+										markNextChangeAsNotPersistent();
 									}
 								}
 

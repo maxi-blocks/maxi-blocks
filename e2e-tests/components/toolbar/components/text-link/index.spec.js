43eda9bd66eb7639b8f7a67df3bce0495eb5ff31
Andrew Valenya <46112160+Fybex@users.noreply.github.com>
2023-08-30 11:43:42 +0300

    Make link e2e tests fail less attempt

--- e2e-tests/components/toolbar/components/text-link/index.spec.js
+++ e2e-tests/components/toolbar/components/text-link/index.spec.js
@@ -21,6 +21,10 @@ describe('Text link', () => {
 
 		await page.waitForSelector('.block-editor-url-input__input:focus');
 
+		await page.waitForSelector(
+			'.maxi-link-control .block-editor-url-input__input'
+		);
+
 		await page.keyboard.type('test.com', { delay: 100 });
 		await page.keyboard.press('Enter');
 

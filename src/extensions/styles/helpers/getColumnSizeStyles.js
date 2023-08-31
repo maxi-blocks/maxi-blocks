1b3e228cc97ecbe3035845bf21f197ec5bdeb2ca
Svitlana <2401618+elzadj@users.noreply.github.com>
2023-08-31 18:37:24 +0300

    remove logs

--- src/extensions/styles/helpers/getColumnSizeStyles.js
+++ src/extensions/styles/helpers/getColumnSizeStyles.js
@@ -49,9 +49,6 @@ export const getColumnNum = (columnsSize, clientId, breakpoint) => {
 const getColumnSizeStyles = (obj, rowGapProps, clientId) => {
 	const response = {};
 
-	console.log('rowGapProps');
-	console.log(rowGapProps);
-
 	breakpoints.forEach(breakpoint => {
 		const fitContent = getLastBreakpointAttribute({
 			target: 'column-fit-content',
@@ -94,11 +91,7 @@ const getColumnSizeStyles = (obj, rowGapProps, clientId) => {
 				attributes: rowGapProps,
 			});
 
-			console.log('gap', gap);
-
 			const gapValue = gap ? `${round(gap, 4)}${gapUnit}` : '0px';
-
-			console.log('gapValue', gapValue);
 
 			const value =
 				columnSize !== 100

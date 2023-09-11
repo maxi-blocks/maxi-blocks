0977d7471da8a27463c4c9ed875db2be47891c98
Rustam Samsonyan <83059229+Rustam198804@users.noreply.github.com>
2023-09-11 15:04:04 +0400

    Fix some styles

--- src/icons/setting-icons/dashed/index.js
+++ src/icons/setting-icons/dashed/index.js
@@ -8,7 +8,7 @@ const alignLeft = (
 		<Path d='M1 2h2' />
 		<Path
 			fill='none'
-			stroke='var(--maxi-grey-1-color)'
+			stroke='var(--maxi-grey-4-color)'
 			strokeWidth='2'
 			strokeMiterlimit='10'
 			strokeDasharray='3.7333,3.7333'

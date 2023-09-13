5443a0ef1008a8a3e1001e1c84e82a040173c133
Svitlana <2401618+elzadj@users.noreply.github.com>
2023-09-13 14:20:48 +0300

    attempt to feed guthub with secret keys number 2

--- src/editor/library/toolbar.js
+++ src/editor/library/toolbar.js
@@ -72,8 +72,14 @@ const LibraryToolbar = props => {
 		showNotValidEmail,
 	} = props;
 
-	const apiKey = process.env.REACT_APP_TYPESENSE_API_KEY;
-	const apiHost = process.env.REACT_APP_TYPESENSE_API_URL;
+	const apiKey =
+		process.env.REACT_APP_TYPESENSE_API_KEY ||
+		// eslint-disable-next-line no-undef
+		secrets.REACT_APP_TYPESENSE_API_KEY;
+	const apiHost =
+		process.env.REACT_APP_TYPESENSE_API_URL ||
+		// eslint-disable-next-line no-undef
+		secrets.REACT_APP_TYPESENSE_API_URL;
 	const [userEmail, setUserEmail] = useState(false);
 
 	const client = new TypesenseSearchClient({

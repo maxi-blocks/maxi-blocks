d66f7f72e989a44f3fd55f24149c091d5a37dd2b
Andrew Valenya <46112160+Fybex@users.noreply.github.com>
2023-09-13 21:42:33 +0300

    `getAdvancedCssObject` unit tests

---
+++ src/extensions/styles/helpers/test/getAdvancedCssObject.js
@@ -0,0 +1,75 @@
+import getAdvancedCssObject from '../getAdvancedCssObject';
+
+describe('getAdvancedCssObject', () => {
+	it('should handle basic CSS correctly', () => {
+		const input = {
+			'advanced-css': `
+				background: red;
+				.maxi-block-button {
+					color: yellow;
+				}
+			`,
+		};
+		const result = getAdvancedCssObject(input);
+		expect(result).toMatchSnapshot();
+	});
+
+	it('should handle CSS with no selectors', () => {
+		const input = {
+			'advanced-css': 'background: blue;',
+		};
+		const result = getAdvancedCssObject(input);
+		expect(result).toMatchSnapshot();
+	});
+
+	// Edge/invalid cases
+	it('should handle CSS with missing closing brace', () => {
+		const input = {
+			'advanced-css': `
+				background: yellow;
+				p {
+					text-align: center;
+			`,
+		};
+		const result = getAdvancedCssObject(input);
+		expect(result).toMatchSnapshot();
+	});
+
+	it('should handle CSS with missing closing brace in nested braces', () => {
+		const input = {
+			'advanced-css': `
+				background: yellow;
+				p {
+					text-align: center;
+
+					div {
+						color: red;
+				}
+			`,
+		};
+		const result = getAdvancedCssObject(input);
+		expect(result).toMatchSnapshot();
+	});
+
+	it('should handle CSS with multiple unmatched braces', () => {
+		const input = {
+			'advanced-css': `
+				background: teal;
+				div {
+					padding: 10px;
+				p {
+					margin: 5px;
+			`,
+		};
+		const result = getAdvancedCssObject(input);
+		expect(result).toMatchSnapshot();
+	});
+
+	it('should handle empty CSS correctly', () => {
+		const input = {
+			'advanced-css': '',
+		};
+		const result = getAdvancedCssObject(input);
+		expect(result).toMatchSnapshot();
+	});
+});

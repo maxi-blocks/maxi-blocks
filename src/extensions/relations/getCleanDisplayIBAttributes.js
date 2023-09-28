7011d7a0e908456a07d3ef322c2448d1d5dd9acb
Andrew Valenya <46112160+Fybex@users.noreply.github.com>
2023-09-28 21:00:45 +0300

    `getCleanDisplayIBAttributes`: fix displaying
    for all breakpoints + bg-layers

--- src/extensions/relations/getCleanDisplayIBAttributes.js
+++ src/extensions/relations/getCleanDisplayIBAttributes.js
@@ -9,16 +9,24 @@ import { replaceAttrKeyBreakpoint } from '../styles/utils';
  */
 import { merge } from 'lodash';
 
-const getCleanDisplayIBAttributes = (blockAttributes, IBAttributes) => {
-	// Merging into empty object because lodash `merge` mutates first argument
-	const mergedAttributes = merge({}, blockAttributes, IBAttributes);
-
-	// In case IBAttributes contains a general attribute and the block attribute
-	// has a XXL attribute different from the default, on IB the general attribute
-	// will overwrite the XXL block attribute, so when the attributes we need to pass
-	// to the component to be displayed need to be cleaned from this XXL attribute
-	// or it will be wrongly displayed and will cause confusion.
-	Object.keys(IBAttributes).forEach(key => {
+/**
+ * Cleans up the merged attributes based on criteria from inputAttributes.
+ *
+ * Criteria:
+ * - If inputAttributes has a 'general' attribute and originalObj has an attribute for a specific
+ *   breakpoint (XXL to XS) that differs from the default, the breakpoint-specific attribute from
+ *   originalObj might overwrite the 'general' one in mergedObj. This can lead to misleading display
+ *   in components.
+ * - This function identifies such cases and removes these potentially confusing attributes from
+ *   mergedObj for all breakpoints.
+ *
+ * @param {Object} mergedObj       - Merged attributes.
+ * @param {Object} originalObj     - Original block attributes.
+ * @param {Object} inputAttributes - Attributes guiding the cleanup.
+ * @returns {Object} - Cleaned mergedObj.
+ */
+const cleanAttributes = (mergedObj, originalObj, inputAttributes) => {
+	Object.keys(inputAttributes).forEach(key => {
 		const breakpoint = getBreakpointFromAttribute(key);
 
 		if (breakpoint === 'general') {
@@ -28,19 +36,49 @@ const getCleanDisplayIBAttributes = (blockAttributes, IBAttributes) => {
 					breakpoint
 				);
 
-				if (!(breakpointAttrKey in blockAttributes)) return;
+				if (!(breakpointAttrKey in originalObj)) return;
 
-				if (breakpoint === 'xxl') {
-					if (
-						blockAttributes[key] !==
-							mergedAttributes[breakpointAttrKey] &&
-						!(breakpointAttrKey in IBAttributes)
-					)
-						delete mergedAttributes[breakpointAttrKey];
-				}
+				const shouldDelete =
+					originalObj[key] !== mergedObj[breakpointAttrKey] &&
+					!(breakpointAttrKey in inputAttributes);
+
+				if (shouldDelete) delete mergedObj[breakpointAttrKey];
 			});
 		}
 	});
+
+	return mergedObj;
+};
+
+/**
+ * Merges blockAttributes and IBAttributes and then cleans up the merged result
+ * based on specific criteria (as defined in the cleanAttributes function).
+ *
+ * - Initially, it merges the two sets of attributes.
+ * - Then it cleans the merged attributes using the `cleanAttributes` function.
+ * - If the merged attributes contain a 'background-layers' property, it cleans that as well.
+ *
+ * @param {Object} blockAttributes - The original block attributes.
+ * @param {Object} IBAttributes    - Attributes from IB (or other source) that dictate the cleanup process.
+ * @returns {Object} - The cleaned merged attributes.
+ */
+const getCleanDisplayIBAttributes = (blockAttributes, IBAttributes) => {
+	// Merging into empty object because lodash `merge` mutates first argument
+	const mergedAttributes = merge({}, blockAttributes, IBAttributes);
+
+	// Clean main mergedAttributes
+	cleanAttributes(mergedAttributes, blockAttributes, IBAttributes);
+
+	// If 'background-layers' exists, clean it as well
+	if (mergedAttributes['background-layers']) {
+		mergedAttributes['background-layers'].forEach((layer, index) => {
+			mergedAttributes['background-layers'][index] = cleanAttributes(
+				layer,
+				blockAttributes['background-layers'][index],
+				IBAttributes['background-layers'][index]
+			);
+		});
+	}
 
 	return mergedAttributes;
 };

0f6b90fa4747b242950e2221f5e1b88f373c4566
Svitlana <2401618+elzadj@users.noreply.github.com>
2023-10-05 17:28:47 +0300

    Fix FSE width for 4k

--- src/extensions/dom/dom.js
+++ src/extensions/dom/dom.js
@@ -2,18 +2,20 @@
  * WordPress dependencies
  */
 import { select, dispatch, subscribe, resolveSelect } from '@wordpress/data';
+import apiFetch from '@wordpress/api-fetch';
 
 /**
  * Internal dependencies
  */
+import { updateSCOnEditor } from '../style-cards';
 import {
-	getIsSiteEditor as originalGetIsSiteEditor,
+	getIsSiteEditor,
 	getIsTemplatePart,
-	getIsTemplatesListOpened as originalGetIsTemplatesListOpened,
+	getIsTemplatesListOpened,
 	getSiteEditorIframeBody,
 } from '../fse';
 import getWinBreakpoint from './getWinBreakpoint';
-import { default as originalGetEditorWrapper } from './getEditorWrapper';
+import getEditorWrapper from './getEditorWrapper';
 import { setScreenSize } from '../styles';
 import { authConnect, getMaxiCookieKey } from '../../editor/auth';
 
@@ -27,73 +29,10 @@ import { isNil } from 'lodash';
  *
  */
 // Adds window.process to fix browserslist error when using
-// postcss and auto fixer on the controls of style store
+// postcss and autofixer on the controls of style store
 window.process = window.process || {};
 window.process.env = window.process.env || {};
 window.process.env.BROWSERSLIST_DISABLE_CACHE = false;
-
-/**
- * Cache
- */
-const cache = {
-	siteEditor: null,
-	editorWrapper: null,
-	resizeObserverTarget: null,
-	blockContainer: null,
-	isSiteEditor: null,
-	isTemplatesListOpened: null,
-	breakpoints: ['xxl', 'xl', 'l', 'm', 's', 'xs'],
-};
-
-/**
- * Caching functions
- */
-const getSiteEditor = () => {
-	if (!cache.siteEditor) {
-		cache.siteEditor = document.querySelector('.edit-site-visual-editor');
-	}
-	return cache.siteEditor;
-};
-
-const getEditorWrapper = () => {
-	if (!cache.editorWrapper) {
-		cache.editorWrapper = originalGetEditorWrapper();
-	}
-	return cache.editorWrapper;
-};
-
-const getIsSiteEditor = () => {
-	if (cache.isSiteEditor === null) {
-		cache.isSiteEditor = originalGetIsSiteEditor();
-	}
-	return cache.isSiteEditor;
-};
-
-const getIsTemplatesListOpened = () => {
-	if (cache.isTemplatesListOpened === null) {
-		cache.isTemplatesListOpened = originalGetIsTemplatesListOpened();
-	}
-	return cache.isTemplatesListOpened;
-};
-
-const getResizeObserverTarget = () => {
-	if (!cache.resizeObserverTarget) {
-		cache.resizeObserverTarget = document.querySelector(
-			'.interface-interface-skeleton__content'
-		);
-	}
-	return cache.resizeObserverTarget;
-};
-
-const getBlockContainer = () => {
-	if (!cache.blockContainer) {
-		const editorWrapper =
-			getEditorWrapper()?.contentDocument ?? getEditorWrapper();
-		cache.blockContainer =
-			editorWrapper?.querySelector('.is-root-container');
-	}
-	return cache.blockContainer;
-};
 
 wp.domReady(() => {
 	const changeHandlesDisplay = (display, wrapper) =>
@@ -104,7 +43,7 @@ wp.domReady(() => {
 		});
 
 	const changeSiteEditorWidth = (width = '') => {
-		getSiteEditor().style.width = width;
+		document.querySelector('.edit-site-visual-editor').style.width = width;
 	};
 
 	const templatePartResizeObserver = new ResizeObserver(entries => {
@@ -128,8 +67,10 @@ wp.domReady(() => {
 			const deviceType = receiveMaxiDeviceType();
 			const baseBreakpoint = receiveBaseBreakpoint();
 
-			const { breakpoints } = cache;
+			const breakpoints = ['xxl', 'xl', 'l', 'm', 's', 'xs'];
 
+			// Hiding handles if current breakpoint smaller than baseBreakpoint,
+			// because resizing is broken in this case
 			if (
 				baseBreakpoint &&
 				deviceType &&
@@ -146,18 +87,28 @@ wp.domReady(() => {
 		}, 150);
 	});
 
+	const resizeObserverSelector = '.interface-interface-skeleton__content';
+
 	const setBaseBreakpoint = () => {
-		const resizeObserverTarget = getResizeObserverTarget();
+		const resizeObserverTarget = document.querySelector(
+			resizeObserverSelector
+		);
 		if (resizeObserverTarget) {
-			const { width, height } =
-				resizeObserverTarget.getBoundingClientRect();
+			const { width, height } = document
+				.querySelector(resizeObserverSelector)
+				.getBoundingClientRect();
 			dispatch('maxiBlocks').setEditorContentSize({ width, height });
 		}
 	};
 
 	const resizeObserver = new ResizeObserver(() => {
 		setBaseBreakpoint();
-		const editorWrapper = getEditorWrapper();
+
+		// On changing the canvas editor size, we must update the winBreakpoint
+		// to add the necessary attributes to display styles. The observer can't
+		// rely on the next element cause it disappears when selecting 's' or 'xs'
+		// due to the appearance of the iframe.
+		const editorWrapper = document.querySelector('.editor-styles-wrapper');
 
 		if (editorWrapper) {
 			const { width: winWidth } = editorWrapper.getBoundingClientRect();
@@ -173,6 +124,12 @@ wp.domReady(() => {
 		}
 	});
 
+	/**
+	 * Block margin css
+	 * Some themes contain a margin on each block that centers it in the editor.
+	 * As Maxi removes that margin, we need to add it back to the block in order
+	 * to keep a good UX.
+	 */
 	const blockMarginObserver = new ResizeObserver(() => {
 		const rawWrapper = getEditorWrapper();
 		const editorWrapper = rawWrapper?.contentDocument ?? rawWrapper;
@@ -190,57 +147,125 @@ wp.domReady(() => {
 		blockContainer.removeChild(fullWidthElement);
 
 		const blockMargin = (editorWidth - fullWidthElementWidth) / 2;
+
 		dispatch('maxiBlocks/styles').saveBlockMarginValue(blockMargin);
 	});
 
+	const isSiteEditor = getIsSiteEditor();
+
+	let isNewEditorContentObserver = true;
+	let isNewObserver = true;
+	let isSCLoaded = false;
+	let id = null;
+
 	const editorContentUnsubscribe = subscribe(() => {
-		if (getIsSiteEditor()) {
+		const resizeObserverTarget = document.querySelector(
+			resizeObserverSelector
+		);
+		if (!resizeObserverTarget) {
+			isNewEditorContentObserver = true;
+		}
+
+		if (isSiteEditor) {
+			const currentId = select('core/edit-site').getEditedPostId();
+			const isTemplatesListOpened = getIsTemplatesListOpened();
 			const siteEditorIframeBody = getSiteEditorIframeBody();
 
-			if (!getIsTemplatesListOpened() && siteEditorIframeBody) {
+			if (
+				!isTemplatesListOpened &&
+				isNewEditorContentObserver &&
+				siteEditorIframeBody
+			) {
 				setTimeout(() => {
 					dispatch('maxiBlocks').setMaxiDeviceType({
 						deviceType: 'general',
 					});
 				}, 150);
 
-				resizeObserver.observe(getResizeObserverTarget());
+				isNewEditorContentObserver = false;
+				resizeObserver.observe(resizeObserverTarget);
 				setBaseBreakpoint();
+			} else if (
+				(isTemplatesListOpened || id !== currentId) &&
+				!isNewEditorContentObserver
+			) {
+				isNewEditorContentObserver = true;
+				resizeObserver.disconnect();
 			}
 
+			// Need to add 'maxi-blocks--active' class to the FSE iframe body
+			// because gutenberg is filtering the iframe classList
+			// https://github.com/WordPress/gutenberg/blob/trunk/packages/block-editor/src/components/iframe/index.js#L213-L220
 			if (
 				siteEditorIframeBody &&
 				!siteEditorIframeBody.classList.contains('maxi-blocks--active')
 			)
 				siteEditorIframeBody.classList.add('maxi-blocks--active');
 
+			if ((getIsTemplatesListOpened() || id !== currentId) && isSCLoaded)
+				isSCLoaded = false;
+
+			// Adding the SC styles after switching between the templates
+			if (siteEditorIframeBody && !isSCLoaded) {
+				isSCLoaded = true;
+				setTimeout(() => {
+					const SC = select(
+						'maxiBlocks/style-cards'
+					).receiveMaxiActiveStyleCard();
+					if (SC) {
+						updateSCOnEditor(SC.value);
+					} else {
+						isSCLoaded = false;
+					}
+				}, 150);
+			}
+
 			if (getIsTemplatePart()) {
 				const resizableBox = document.querySelector(
 					'.edit-site-visual-editor .components-resizable-box__container'
 				);
+				const isTemplatesListOpened = getIsTemplatesListOpened();
 
 				if (
-					!getIsTemplatesListOpened() &&
+					!isTemplatesListOpened &&
+					isNewObserver &&
 					getSiteEditorIframeBody() &&
 					resizableBox
 				) {
+					isNewObserver = false;
 					templatePartResizeObserver.observe(resizableBox);
-				} else {
+				} else if (
+					(isTemplatesListOpened || id !== currentId) &&
+					!isNewObserver
+				) {
+					isNewObserver = true;
 					templatePartResizeObserver.disconnect();
 				}
 			}
-		} else {
-			if (getResizeObserverTarget()) {
-				resizeObserver.observe(getResizeObserverTarget());
-			}
-			resizeObserver.observe(document.body);
 
-			const blockContainer = getBlockContainer();
+			id = currentId;
+		} else {
+			if (!resizeObserverTarget) return;
+
+			[resizeObserverTarget, document.body].forEach(
+				element => element && resizeObserver.observe(element)
+			);
+
+			// Block margin value
+			const rawWrapper = getEditorWrapper();
+			const editorWrapper = rawWrapper?.contentDocument ?? rawWrapper;
+			const blockContainer =
+				editorWrapper?.querySelector('.is-root-container');
+
 			if (blockContainer) blockMarginObserver.observe(blockContainer);
+
+			editorContentUnsubscribe();
 		}
 	});
 
+	// authentication for maxi pro
 	const maxiCookie = getMaxiCookieKey();
+
 	if (maxiCookie) {
 		const { receiveMaxiProStatus } = resolveSelect('maxiBlocks/pro');
 		const { email } = maxiCookie;
@@ -261,7 +286,7 @@ wp.domReady(() => {
 						'.block-editor-inserter__preview-container'
 					);
 					if (preview) {
-						preview.style.display = 'none';
+						preview.style.display = 'none'; // Hide the preview
 					}
 				}
 			}

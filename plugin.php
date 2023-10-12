a5933343814d510a2873b8e37074ade00afb3305
Svitlana <2401618+elzadj@users.noreply.github.com>
2023-10-12 17:05:57 +0300

    Add notices for PHP and MySql version to the dashboard

--- plugin.php
+++ plugin.php
@@ -19,7 +19,7 @@ if (!defined('ABSPATH')) {
 }
 
 // Define the required MySQL version.
-define('REQUIRED_MYSQL_VERSION', '13.7');
+define('REQUIRED_MYSQL_VERSION', '8.0');
 
 // Hook for checking the MySQL version in the admin area.
 add_action('admin_init', 'check_mysql_version');
@@ -40,10 +40,10 @@ function check_mysql_version()
 /**
  * Show an admin notice under the plugin's name if the required MySQL version is not met.
  */
-function show_mysql_version_notice($plugin_file, $plugin_data, $status)
+function show_mysql_version_notice()
 {
     $message = 'This plugin is optimized for MySQL version ' . REQUIRED_MYSQL_VERSION . ' or higher. Please update your MySQL version or contact your hosting provider for the best experience.';
-    printf('<tr class="active"><td colspan="3" class="plugin-update colspanchange"><div class="update-message notice inline notice-error notice-alt"><p>%s</p></div></td></tr>', esc_html($message));
+    printf('<tr class="active"><td colspan="4" class="plugin-update colspanchange"><div class="update-message notice inline notice-error notice-alt"><p>%s</p></div></td></tr>', esc_html($message));
 }
 
 define('MAXI_PLUGIN_DIR_PATH', plugin_dir_path(__FILE__));

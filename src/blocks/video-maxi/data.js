86a3a598a0a1fdc690a37e82164e4c947338f98d
Andrii Valenia <46112160+Fybex@users.noreply.github.com>
2023-11-14 17:23:48 +0200

    Use `createIconSelectors`

--- src/blocks/video-maxi/data.js
+++ src/blocks/video-maxi/data.js
@@ -6,7 +6,10 @@ import {
 	getAdvancedSettings,
 } from '../../extensions/relations';
 import { createIconTransitions } from '../../extensions/styles';
-import { createSelectors } from '../../extensions/styles/custom-css';
+import {
+	createIconSelectors,
+	createSelectors,
+} from '../../extensions/styles/custom-css';
 import transitionDefault from '../../extensions/styles/transitions/transitionDefault';
 
 /**
@@ -102,79 +105,13 @@ const customCss = {
 	selectors: {
 		...createSelectors({
 			canvas: '',
-		}),
-		...createSelectors({
 			video: videoClass,
 			overlay: overlayClass,
 		}),
-		'close icon': {
-			normal: {
-				label: 'close icon',
-				target: ' .maxi-video-block__close-button',
-			},
-			svg: {
-				label: "close icon's svg",
-				target: ' .maxi-video-block__close-button svg',
-			},
-			insideSvg: {
-				label: 'everything inside svg (svg > *)',
-				target: ' .maxi-video-block__close-button svg > *',
-			},
-			path: {
-				label: "svg's path",
-				target: ' .maxi-video-block__close-button svg path',
-			},
-			hover: {
-				label: 'close icon on hover',
-				target: ' .maxi-video-block__close-button:hover',
-			},
-			hoverSvg: {
-				label: "close icon's svg on hover",
-				target: ' .maxi-video-block__close-button:hover svg',
-			},
-			hoverInsideSvg: {
-				label: 'everything inside svg on hover (:hover svg > *)',
-				target: ' .maxi-video-block__close-button:hover svg > *',
-			},
-			hoverPath: {
-				label: "svg's path on hover",
-				target: ' .maxi-video-block__close-button:hover svg path',
-			},
-		},
-		'play icon': {
-			normal: {
-				label: 'play icon',
-				target: ' .maxi-video-block__play-button',
-			},
-			svg: {
-				label: "play icon's svg",
-				target: ' .maxi-video-block__play-button svg',
-			},
-			insideSvg: {
-				label: 'everything inside svg (svg > *)',
-				target: ' .maxi-video-block__play-button svg > *',
-			},
-			path: {
-				label: "svg's path",
-				target: ' .maxi-video-block__play-button svg path',
-			},
-			hover: {
-				label: 'play icon on hover',
-				target: ' .maxi-video-block__play-button:hover',
-			},
-			hoverSvg: {
-				label: "play icon's svg on hover",
-				target: ' .maxi-video-block__play-button:hover svg',
-			},
-			hoverInsideSvg: {
-				label: 'everything inside svg on hover (:hover svg > *)',
-				target: ' .maxi-video-block__play-button:hover svg > *',
-			},
-			hoverPath: {
-				label: "svg's path on hover",
-				target: ' .maxi-video-block__play-button:hover svg path',
-			},
-		},
+		...createIconSelectors({
+			'close icon': ' .maxi-video-block__close-button',
+			'play icon': ' .maxi-video-block__play-button',
+		}),
 	},
 	categories: [
 		'canvas',

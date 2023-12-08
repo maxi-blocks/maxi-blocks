12555a62761dfaea5f4dad09aa61be171e8e5bce
Svitlana <2401618+elzadj@users.noreply.github.com>
2023-12-08 16:36:15 +0200

    Fix translations domain and add headers

--- plugin.php
+++ plugin.php
@@ -11,6 +11,8 @@
  * Requires PHP: 8.0
  * License: GPL v2 or later
  * License URI: https://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain: maxi-blocks
+ * Domain Path: /languages
  */
 
 // Exit if accessed directly.

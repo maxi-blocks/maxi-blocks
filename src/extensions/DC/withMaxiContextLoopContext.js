e18837993d9bb7acb60740df919fd98c3528c6a0
Andrii Valenia <46112160+Fybex@users.noreply.github.com>
2023-12-14 15:52:35 +0200

    Call `useMaxiDCLink` for `DC_LINK_BLOCKS`

--- src/extensions/DC/withMaxiContextLoopContext.js
+++ src/extensions/DC/withMaxiContextLoopContext.js
@@ -1,15 +1,32 @@
+/**
+ * WordPress dependencies
+ */
 import { createHigherOrderComponent, pure } from '@wordpress/compose';
+import { useContext } from '@wordpress/element';
+
+/**
+ * Internal dependencies
+ */
+import useMaxiDCLink from './useMaxiDCLink';
 import LoopContext from './loopContext';
+import DC_LINK_BLOCKS from '../../components/toolbar/components/link/dcLinkBlocks';
 
 const withMaxiContextLoopContext = createHigherOrderComponent(
 	WrappedComponent =>
-		pure(props => (
-			<LoopContext.Consumer>
-				{context => (
-					<WrappedComponent contextLoopContext={context} {...props} />
-				)}
-			</LoopContext.Consumer>
-		)),
+		pure(props => {
+			const context = useContext(LoopContext);
+
+			if (DC_LINK_BLOCKS.includes(props.name)) {
+				useMaxiDCLink(
+					props.attributes,
+					props.clientId,
+					context,
+					props.setAttributes
+				);
+			}
+
+			return <WrappedComponent contextLoopContext={context} {...props} />;
+		}),
 	'withMaxiContextLoopContext'
 );
 

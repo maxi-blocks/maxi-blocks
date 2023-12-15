45b855a91513e84a0e87f007967512ad4ce814be
Andrii Valenia <46112160+Fybex@users.noreply.github.com>
2023-12-15 23:07:21 +0200

    Add wait load option to `loadFonts`

--- src/extensions/text/fonts/loadFonts.js
+++ src/extensions/text/fonts/loadFonts.js
@@ -19,142 +19,160 @@ import { isEmpty, uniq } from 'lodash';
  * @param {HTMLElement} target      Element, where the font will be loaded
  */
 const loadFonts = (font, backendOnly = true, target = document) => {
-	if (typeof font === 'object' && font !== null) {
-		Object.entries(font).forEach(([fontName, fontData]) => {
-			if (isEmpty(fontName)) return;
+	return new Promise(resolve => {
+		const fontLoadPromises = [];
 
-			const fontWeight = fontData?.weight || '400';
-			const fontStyle = fontData?.style;
+		if (typeof font === 'object' && font !== null) {
+			Object.entries(font).forEach(([fontName, fontData]) => {
+				if (isEmpty(fontName)) return;
 
-			let fontWeightArr = [];
-			let fontDataNew;
+				const fontWeight = fontData?.weight || '400';
+				const fontStyle = fontData?.style;
 
-			if (Array.isArray(fontWeight)) {
-				font[fontName].weight = uniq(fontWeight).join();
-				fontWeightArr = fontWeight;
-			} else if (typeof fontWeight === 'string') {
-				fontWeightArr = uniq(fontWeight.split(',')).filter(
-					weight => !isEmpty(weight)
-				);
-				fontDataNew = {
-					...fontData,
-					...{ weight: fontWeightArr.join() },
-				};
-				font[fontName].weight = fontWeightArr.join();
-			} else {
-				fontWeightArr = [fontWeight];
-				fontDataNew = { ...fontData, ...{ weight: fontWeight } };
-			}
+				let fontWeightArr = [];
+				let fontDataNew;
 
-			let fontStyleArr = [];
-
-			if (Array.isArray(fontStyle) && !isEmpty(fontStyle)) {
-				font[fontName].style = uniq(fontStyle).join();
-			} else if (typeof fontStyle === 'string') {
-				fontStyleArr = uniq(fontStyle.split(',')).filter(
-					style => !isEmpty(style)
-				);
-
-				font[fontName].style = fontStyleArr.join();
-			} else {
-				fontStyleArr = ['normal'];
-			}
-
-			if (isEmpty(fontDataNew.style)) delete fontDataNew.style;
-
-			const fontFiles =
-				select('maxiBlocks/text').getFont(fontName)?.files;
-
-			if (isEmpty(fontFiles)) return;
-
-			const loadBackendFont = url => {
-				// Need to ensure the fontName contains quotes when it has space in the string.
-				// Also seems FF is more strict than Chrome, so need to ensure the fontName is
-				// ready depending the browser.
-				const isFirefox =
-					navigator.userAgent.toLowerCase().indexOf('firefox') > -1;
-
-				const cleanFontName = (
-					fontName.includes(' ') && isFirefox
-						? `'${fontName}'`
-						: fontName
-				).replaceAll("''", "'");
-
-				const fontLoad = new FontFace(
-					cleanFontName,
-					`url(${url})`,
-					fontDataNew
-				);
-				fontLoad.load().then(() => {
-					target.fonts.add(fontLoad);
-				});
-				fontLoad.loaded.catch(err => {
-					console.error(
-						__(`Font hasn't been able to download: ${err}`)
+				if (Array.isArray(fontWeight)) {
+					font[fontName].weight = uniq(fontWeight).join();
+					fontWeightArr = fontWeight;
+				} else if (typeof fontWeight === 'string') {
+					fontWeightArr = uniq(fontWeight.split(',')).filter(
+						weight => !isEmpty(weight)
 					);
-				});
-			};
+					fontDataNew = {
+						...fontData,
+						...{ weight: fontWeightArr.join() },
+					};
+					font[fontName].weight = fontWeightArr.join();
+				} else {
+					fontWeightArr = [fontWeight];
+					fontDataNew = { ...fontData, ...{ weight: fontWeight } };
+				}
 
-			const getWeightFile = (weight, style) =>
-				style === 'italic'
-					? `${weight === '400' ? '' : weight}italic`
-					: weight;
+				let fontStyleArr = [];
 
-			/**
-			 * Returns font weight from weightFile
-			 *
-			 * @example getWeight('100italic') // returns 100;
-			 */
-			const getWeight = weightFile => {
-				const weightStr = weightFile.replace(/\D+/, '');
+				if (Array.isArray(fontStyle) && !isEmpty(fontStyle)) {
+					font[fontName].style = uniq(fontStyle).join();
+				} else if (typeof fontStyle === 'string') {
+					fontStyleArr = uniq(fontStyle.split(',')).filter(
+						style => !isEmpty(style)
+					);
 
-				return isEmpty(weightStr) ? '400' : weightStr;
-			};
+					font[fontName].style = fontStyleArr.join();
+				} else {
+					fontStyleArr = ['normal'];
+				}
 
-			fontWeightArr.forEach(weight => {
-				fontStyleArr.forEach(currentFontStyle => {
-					let weightFile = getWeightFile(weight, currentFontStyle);
-					if (!(weightFile in fontFiles)) {
-						weightFile = '400';
-						const newFontWeightArr = uniq(fontWeightArr).filter(
-							value => {
-								return value !== weight;
-							}
+				if (isEmpty(fontDataNew.style)) delete fontDataNew.style;
+
+				const fontFiles =
+					select('maxiBlocks/text').getFont(fontName)?.files;
+
+				if (isEmpty(fontFiles)) return;
+
+				const loadBackendFont = url => {
+					// Need to ensure the fontName contains quotes when it has space in the string.
+					// Also seems FF is more strict than Chrome, so need to ensure the fontName is
+					// ready depending the browser.
+					const isFirefox =
+						navigator.userAgent.toLowerCase().indexOf('firefox') >
+						-1;
+
+					const cleanFontName = (
+						fontName.includes(' ') && isFirefox
+							? `'${fontName}'`
+							: fontName
+					).replaceAll("''", "'");
+
+					const fontLoad = new FontFace(
+						cleanFontName,
+						`url(${url})`,
+						fontDataNew
+					);
+					fontLoadPromises.push(
+						fontLoad
+							.load()
+							.then(() => {
+								target.fonts.add(fontLoad);
+							})
+							.catch(err => {
+								console.error(
+									__(
+										`Font hasn't been able to download: ${err}`
+									)
+								);
+							})
+					);
+				};
+
+				const getWeightFile = (weight, style) =>
+					style === 'italic'
+						? `${weight === '400' ? '' : weight}italic`
+						: weight;
+
+				/**
+				 * Returns font weight from weightFile
+				 *
+				 * @example getWeight('100italic') // returns 100;
+				 */
+				const getWeight = weightFile => {
+					const weightStr = weightFile.replace(/\D+/, '');
+
+					return isEmpty(weightStr) ? '400' : weightStr;
+				};
+
+				fontWeightArr.forEach(weight => {
+					fontStyleArr.forEach(currentFontStyle => {
+						let weightFile = getWeightFile(
+							weight,
+							currentFontStyle
 						);
+						if (!(weightFile in fontFiles)) {
+							weightFile = '400';
+							const newFontWeightArr = uniq(fontWeightArr).filter(
+								value => {
+									return value !== weight;
+								}
+							);
 
-						newFontWeightArr.push(weightFile);
+							newFontWeightArr.push(weightFile);
 
-						const newFontWeight = uniq(newFontWeightArr).join();
-						font[fontName].weight = newFontWeight;
-					}
-
-					Object.entries(fontFiles).forEach(variant => {
-						if (variant[0].toString() === weightFile) {
-							fontDataNew = {
-								...fontData,
-								...{ style: currentFontStyle },
-								...{ weight: getWeight(weightFile) },
-							};
-
-							loadBackendFont(variant[1]);
+							const newFontWeight = uniq(newFontWeightArr).join();
+							font[fontName].weight = newFontWeight;
 						}
+
+						Object.entries(fontFiles).forEach(variant => {
+							if (variant[0].toString() === weightFile) {
+								fontDataNew = {
+									...fontData,
+									...{ style: currentFontStyle },
+									...{ weight: getWeight(weightFile) },
+								};
+
+								loadBackendFont(variant[1]);
+							}
+						});
 					});
 				});
 			});
-		});
 
-		if (!backendOnly)
-			dispatch('maxiBlocks/text').updateFonts(JSON.stringify(font));
-	}
+			if (!backendOnly)
+				dispatch('maxiBlocks/text').updateFonts(JSON.stringify(font));
 
-	return null;
+			Promise.all(fontLoadPromises).then(() => {
+				resolve();
+			});
+		} else {
+			resolve();
+		}
+	});
 };
 
-const loadFontsInEditor = objFont => {
+const loadFontsInEditor = async objFont => {
 	const iframeEditor = document.querySelector('iframe[name="editor-canvas"]');
 	if (iframeEditor) {
-		loadFonts(objFont, true, iframeEditor.contentDocument);
-	} else loadFonts(objFont);
+		await loadFonts(objFont, true, iframeEditor.contentDocument);
+	} else await loadFonts(objFont, true, document);
 };
 
 export { loadFontsInEditor, loadFonts };

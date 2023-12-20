104a9a2c9c04bf28fa46f3a4cc1ddf76b819b522
Svitlana <2401618+elzadj@users.noreply.github.com>
2023-12-20 14:01:58 +0200

    Optimize border styles

--- src/extensions/styles/helpers/getBorderStyles.js
+++ src/extensions/styles/helpers/getBorderStyles.js
@@ -35,11 +35,22 @@ const getBorderStyles = ({
 
 	// Clean `palette-sc-status` traces on obj. This is an MVP, considering future implementation of
 	// #4679 will implement a new border style helper.
-	Object.keys(obj).forEach(key => {
-		if (key.includes('palette-sc-status')) delete obj[key];
-	});
+	const newObj = {};
+	let borderObj = obj;
+	let hasValidKeys = false;
 
-	const hoverStatus = obj[`${prefix}border-status-hover`];
+	for (const key in obj) {
+		if (!key.includes('palette-sc-status')) {
+			newObj[key] = obj[key];
+			hasValidKeys = true;
+		}
+	}
+
+	if (hasValidKeys) {
+		borderObj = newObj;
+	}
+
+	const hoverStatus = borderObj[`${prefix}border-status-hover`];
 	const {
 		'hover-border-color-global': isActive,
 		'hover-border-color-all': affectAll,
@@ -61,73 +72,73 @@ const getBorderStyles = ({
 	];
 
 	let omitBorderStyle = !isIB && !hoverStatus && !globalHoverStatus;
+
+	const getColorString = breakpoint => {
+		const {
+			paletteStatus,
+			paletteSCStatus,
+			paletteColor,
+			paletteOpacity,
+			color,
+		} = getPaletteAttributes({
+			borderObj,
+			prefix: `${prefix}border-`,
+			isHover,
+			breakpoint,
+		});
+
+		if (!paletteStatus) {
+			return color;
+		}
+
+		const isButtonCondition =
+			isButton && (!isHover || hoverStatus || globalHoverStatus);
+
+		if (!paletteSCStatus && isButtonCondition) {
+			return getColorRGBAString({
+				firstVar: `button-border-color${isHover ? '-hover' : ''}`,
+				secondVar: `color-${paletteColor}`,
+				opacity: paletteOpacity,
+				blockStyle,
+			});
+		}
+
+		return getColorRGBAString({
+			firstVar: `color-${paletteColor}`,
+			opacity: paletteOpacity,
+			blockStyle,
+		});
+	};
+
 	breakpoints.forEach(breakpoint => {
 		response[breakpoint] = {};
 
 		const borderStyle = getLastBreakpointAttribute({
 			target: `${prefix}border-style`,
 			breakpoint,
-			attributes: obj,
+			attributes: borderObj,
 			isHover,
 		});
 		const isBorderNone = isUndefined(borderStyle) || borderStyle === 'none';
 		omitBorderStyle = omitBorderStyle ? isBorderNone : false;
 
-		const getColorString = () => {
-			const {
-				paletteStatus,
-				paletteSCStatus,
-				paletteColor,
-				paletteOpacity,
-				color,
-			} = getPaletteAttributes({
-				obj,
-				prefix: `${prefix}border-`,
-				isHover,
-				breakpoint,
-			});
+		const regexPattern = `\\b-${breakpoint}${
+			isHover ? '-hover' : ''
+		}\\b(?!.*\\b-${breakpoint}${isHover ? '-hover' : ''}\\b)`;
+		const replacer = new RegExp(regexPattern, 'gm');
 
-			if (paletteStatus)
-				if (
-					!paletteSCStatus &&
-					isButton &&
-					(!isHover || hoverStatus || globalHoverStatus)
-				)
-					return getColorRGBAString({
-						firstVar: `${isButton ? 'button-' : ''}border-color${
-							isHover ? '-hover' : ''
-						}`,
-						secondVar: `color-${paletteColor}`,
-						opacity: paletteOpacity,
-						blockStyle,
-					});
-				else
-					return getColorRGBAString({
-						firstVar: `color-${paletteColor}`,
-						opacity: paletteOpacity,
-						blockStyle,
-					});
-			return color;
-		};
-
-		Object.entries(obj).forEach(([key, rawValue]) => {
+		Object.entries(borderObj).forEach(([key, rawValue]) => {
 			const newKey = prefix ? key.replace(prefix, '') : key;
 			const includesBreakpoint =
 				newKey.lastIndexOf(`-${breakpoint}${isHover ? '-hover' : ''}`) +
 					`-${breakpoint}${isHover ? '-hover' : ''}`.length ===
 				newKey.length;
-			const replacer = new RegExp(
-				`\\b-${breakpoint}${
-					isHover ? '-hover' : ''
-				}\\b(?!.*\\b-${breakpoint}${isHover ? '-hover' : ''}\\b)`,
-				'gm'
-			);
 			const newLabel = newKey.replace(replacer, '');
 			const value = getLastBreakpointAttribute({
 				target: `${prefix}${newLabel}`,
 				isHover,
 				breakpoint,
-				attributes: obj,
+				attributes: borderObj,
 			});
 
 			if (
@@ -146,32 +157,40 @@ const getBorderStyles = ({
 					getLastBreakpointAttribute({
 						target: `${prefix}${newLabel.replace(unitKey, 'unit')}`,
 						breakpoint,
-						attributes: obj,
+						attributes: borderObj,
 						isHover,
 					}) || 'px';
 
-				if (key.includes('style')) {
-					if (!omitBorderStyle)
-						if ((isHover || isIB) && isBorderNone) {
-							response[breakpoint].border = 'none';
-						} else
-							response[breakpoint]['border-style'] = borderStyle;
-				} else if (!keyWords.some(key => newKey.includes(key))) {
+				// Initial check for 'style' condition
+				if (key.includes('style') && !omitBorderStyle) {
+					if ((isHover || isIB) && isBorderNone) {
+						response[breakpoint].border = 'none';
+					} else {
+						response[breakpoint]['border-style'] = borderStyle;
+					}
+				}
+				// Check for keyWords and other conditions
+				else if (!keyWords.some(keyword => newKey.includes(keyword))) {
 					if (
 						(key.includes('color') || key.includes('opacity')) &&
 						(!isBorderNone || (isHover && globalHoverStatus))
 					) {
 						response[breakpoint][borderColorProperty] =
-							getColorString();
-					} else if (
+							getColorString(breakpoint);
+					}
+					// Check for palette conditions
+					else if (
 						![
 							'border-palette-status',
 							'border-palette-color',
 							'border-palette-opacity',
 						].includes(newLabel)
-					)
+					) {
 						response[breakpoint][newLabel] = `${value}`;
-				} else if (
+					}
+				}
+				// Check for specific border width properties
+				else if (
 					[
 						'border-top-width',
 						'border-right-width',
@@ -179,16 +198,17 @@ const getBorderStyles = ({
 						'border-bottom-width',
 					].includes(newLabel)
 				) {
-					if (isBorderNone) return;
-					if (Number.isFinite(value)) {
-						response[breakpoint][newLabel] = `${value}${unit}`;
-					} else {
-						response[breakpoint][newLabel] = `0${unit}`;
+					if (!isBorderNone) {
+						response[breakpoint][newLabel] = Number.isFinite(value)
+							? `${value}${unit}`
+							: `0${unit}`;
 					}
-				} else if (Number.isFinite(value)) {
-					response[breakpoint][newLabel] = `${value}${unit}`;
-				} else {
-					response[breakpoint][newLabel] = `0${unit}`;
+				}
+				// Final else condition for remaining cases
+				else {
+					response[breakpoint][newLabel] = Number.isFinite(value)
+						? `${value}${unit}`
+						: `0${unit}`;
 				}
 			}
 		});

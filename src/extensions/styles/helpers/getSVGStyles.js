1b4bcf01230b7c80e6fb5dd4a778b5a75558a72a
Svitlana <2401618+elzadj@users.noreply.github.com>
2023-12-20 16:12:31 +0200

    Optimize svg styles

--- src/extensions/styles/helpers/getSVGStyles.js
+++ src/extensions/styles/helpers/getSVGStyles.js
@@ -34,56 +34,29 @@ export const getSVGWidthStyles = ({
 		prefix,
 	});
 
+	const perStrokeWidthCoefficient = 4;
+	const attributeCache = {};
+
 	breakpoints.forEach(breakpoint => {
-		response[breakpoint] = {};
+		const getAttribute = attr => {
+			const key = `${attr}-${breakpoint}-${isHover}`;
+			if (attributeCache[key] === undefined) {
+				attributeCache[key] = getLastBreakpointAttribute({
+					target: `${prefix}${attr}`,
+					isHover,
+					breakpoint,
+					attributes: obj,
+				});
+			}
+			return attributeCache[key];
+		};
 
-		const iconSize =
-			getLastBreakpointAttribute({
-				target: `${prefix}width`,
-				isHover,
-				breakpoint,
-				attributes: obj,
-			}) ??
-			getLastBreakpointAttribute({
-				target: `${prefix}height`,
-				isHover,
-				breakpoint,
-				attributes: obj,
-			});
-
+		const iconSize = getAttribute('width') ?? getAttribute('height');
 		const iconUnit =
-			getLastBreakpointAttribute({
-				target: `${prefix}width-unit`,
-				isHover,
-				breakpoint,
-				attributes: obj,
-			}) ??
-			getLastBreakpointAttribute({
-				target: `${prefix}height-unit`,
-				isHover,
-				breakpoint,
-				attributes: obj,
-			}) ??
-			'px';
-
-		const iconWidthFitContent = getLastBreakpointAttribute({
-			target: `${prefix}width-fit-content`,
-			isHover,
-			breakpoint,
-			attributes: obj,
-		});
-
+			getAttribute('width-unit') ?? getAttribute('height-unit') ?? 'px';
+		const iconWidthFitContent = getAttribute('width-fit-content');
 		const iconStrokeWidth =
-			svgType !== 'Shape'
-				? getLastBreakpointAttribute({
-						target: `${prefix}stroke`,
-						isHover,
-						breakpoint,
-						attributes: obj,
-				  })
-				: 1;
-
-		const perStrokeWidthCoefficient = 4;
+			svgType !== 'Shape' ? getAttribute('stroke') : 1;
 
 		const heightToStrokeWidthCoefficient =
 			1 +
@@ -93,12 +66,9 @@ export const getSVGWidthStyles = ({
 				100;
 
 		if (!isNil(iconSize) && !isEmpty(iconSize)) {
-			/**
-			 * Ignore `disableHeight` if `iconWidthFitContent` is true,
-			 * because it's necessary to have height in this case.
-			 */
-			if (iconWidthFitContent || !disableHeight)
-				response[breakpoint].height = `${
+			response[breakpoint] = response[breakpoint] || {}; // Ensure the object exists
+			if (iconWidthFitContent || !disableHeight) {
+				const calculatedHeight =
 					iconWidthFitContent && iconWidthHeightRatio !== 1
 						? round(
 								iconWidthHeightRatio > 1
@@ -109,13 +79,15 @@ export const getSVGWidthStyles = ({
 											(iconWidthHeightRatio *
 												heightToStrokeWidthCoefficient)
 						  )
-						: iconSize
-				}${iconUnit}`;
+						: iconSize;
+				response[breakpoint].height = `${calculatedHeight}${iconUnit}`;
+			}
 			response[breakpoint].width = `${iconSize}${iconUnit}`;
 		}
 
-		if (isEmpty(response[breakpoint]) && breakpoint !== 'general')
+		if (isEmpty(response[breakpoint]) && breakpoint !== 'general') {
 			delete response[breakpoint];
+		}
 	});
 
 	return { iconSize: response };
@@ -163,22 +135,21 @@ const getSVGPathFillStyles = (
 		color,
 	} = getPaletteAttributes({ obj, prefix: `${prefix}fill-`, isHover });
 
-	if (paletteStatus && paletteColor)
-		response.general.fill = getColorRGBAString(
-			paletteSCStatus
-				? {
-						firstVar: `color-${paletteColor}`,
-						opacity: paletteOpacity,
-						blockStyle,
-				  }
-				: {
-						firstVar: `icon-fill${isHover ? '-hover' : ''}`,
-						secondVar: `color-${paletteColor}`,
-						opacity: paletteOpacity,
-						blockStyle,
-				  }
-		);
-	else if (!paletteStatus && !isNil(color)) response.general.fill = color;
+	if (paletteStatus && paletteColor) {
+		const colorParams = {
+			firstVar: paletteSCStatus
+				? `color-${paletteColor}`
+				: `icon-fill${isHover ? '-hover' : ''}`,
+			opacity: paletteOpacity,
+			blockStyle,
+		};
+		if (!paletteSCStatus) {
+			colorParams.secondVar = `color-${paletteColor}`;
+		}
+		response.general.fill = getColorRGBAString(colorParams);
+	} else if (!paletteStatus && !isNil(color)) {
+		response.general.fill = color;
+	}
 
 	return { SVGPathFill: response };
 };
@@ -195,30 +166,24 @@ const getSVGPathStrokeStyles = (
 	};
 
 	if (isHover && !useIconColor && !obj['typography-status-hover']) {
-		response.general = {};
-		response.general.stroke = '';
-
-		return response;
+		return {
+			SVGPathStroke: { label: response.label, general: { stroke: '' } },
+		};
 	}
 
-	(!useIconColor ? breakpoints : ['general']).forEach(breakpoint => {
-		response[breakpoint] = {};
+	const breakpointsToUse = useIconColor ? ['general'] : breakpoints;
+	const linePrefix = [
+		'icon-',
+		'close-icon-',
+		'active-icon-',
+		'navigation-arrow-both-icon-',
+		'navigation-dot-icon-',
+		'active-navigation-dot-icon-',
+	].includes(prefix)
+		? `${prefix}stroke-`
+		: `${prefix}line-`;
 
-		let linePrefix = '';
-		switch (prefix) {
-			case 'icon-':
-			case 'close-icon-':
-			case 'active-icon-':
-			case 'navigation-arrow-both-icon-':
-			case 'navigation-dot-icon-':
-			case 'active-navigation-dot-icon-':
-				linePrefix = `${prefix}stroke-`;
-				break;
-			default:
-				linePrefix = `${prefix}line-`;
-				break;
-		}
-
+	breakpointsToUse.forEach(breakpoint => {
 		const {
 			paletteStatus,
 			paletteSCStatus,
@@ -233,31 +198,27 @@ const getSVGPathStrokeStyles = (
 		});
 
 		if (paletteStatus && paletteColor) {
-			if (useIconColor)
-				response.general.stroke = getColorRGBAString(
-					paletteSCStatus
-						? {
-								firstVar: `color-${paletteColor}`,
-								opacity: paletteOpacity,
-								blockStyle,
-						  }
-						: {
-								firstVar: `icon-stroke${
-									isHover ? '-hover' : ''
-								}`,
-								secondVar: `color-${paletteColor}`,
-								opacity: paletteOpacity,
-								blockStyle,
-						  }
-				);
-			else
-				response[breakpoint].stroke = getColorRGBAString({
-					firstVar: `color-${paletteColor}`,
-					opacity: paletteOpacity,
-					blockStyle,
-				});
-		} else if (!paletteStatus && !isNil(color))
-			response[breakpoint].stroke = color;
+			const strokeColor = getColorRGBAString({
+				firstVar:
+					paletteSCStatus || !useIconColor
+						? `color-${paletteColor}`
+						: `icon-stroke${isHover ? '-hover' : ''}`,
+				secondVar:
+					!paletteSCStatus && useIconColor
+						? `color-${paletteColor}`
+						: undefined,
+				opacity: paletteOpacity,
+				blockStyle,
+			});
+
+			if (useIconColor) {
+				response.general = { stroke: strokeColor };
+			} else {
+				response[breakpoint] = { stroke: strokeColor };
+			}
+		} else if (!paletteStatus && !isNil(color)) {
+			response[breakpoint] = { stroke: color };
+		}
 	});
 
 	return { SVGPathStroke: response };
@@ -286,64 +247,64 @@ export const getSVGStyles = ({
 		useIconColor
 	);
 	const pathStyles = getSVGPathStyles(obj, prefix, isHover);
+
+	const fillSelectors = [
+		`${target} svg[data-fill]:not([fill^="none"])`,
+		`${target} svg[data-fill]:not([fill^="none"]) *`,
+		`${target} svg g[data-fill]:not([fill^="none"])`,
+		`${target} svg use[data-fill]:not([fill^="none"])`,
+		`${target} svg circle[data-fill]:not([fill^="none"])`,
+		`${target} svg path[data-fill]:not([fill^="none"])`,
+	];
+
+	const strokeSelectors = [
+		`${target} svg[data-stroke]:not([stroke^="none"]) *`,
+		`${target} svg path[data-stroke]:not([stroke^="none"])`,
+		`${target} svg[data-stroke]:not([stroke^="none"])`,
+		`${target} svg g[data-stroke]:not([stroke^="none"])`,
+		`${target} svg use[data-stroke]:not([stroke^="none"])`,
+		`${target} svg circle[data-stroke]:not([stroke^="none"])`,
+	];
+
 	const response = {
-		...(iconType !== 'line' && {
-			[`${target} svg[data-fill]:not([fill^="none"])`]: pathFillStyles,
-			[`${target} svg[data-fill]:not([fill^="none"]) *`]: pathFillStyles,
-			[`${target} svg g[data-fill]:not([fill^="none"])`]: pathFillStyles,
-			[`${target} svg use[data-fill]:not([fill^="none"])`]:
-				pathFillStyles,
-			[`${target} svg circle[data-fill]:not([fill^="none"])`]:
-				pathFillStyles,
-			[`${target} svg path[data-fill]:not([fill^="none"])`]:
-				pathFillStyles,
-		}),
 		[`${target} svg path`]: pathStyles,
-		...(iconType !== 'shape' && {
-			[`${target} svg[data-stroke]:not([stroke^="none"]) *`]:
-				pathStrokeStyles,
-			[`${target} svg path[data-stroke]:not([stroke^="none"])`]:
-				pathStrokeStyles,
-			[`${target} svg[data-stroke]:not([stroke^="none"])`]:
-				pathStrokeStyles,
-			[`${target} svg g[data-stroke]:not([stroke^="none"])`]:
-				pathStrokeStyles,
-			[`${target} svg use[data-stroke]:not([stroke^="none"])`]:
-				pathStrokeStyles,
-			[`${target} svg circle[data-stroke]:not([stroke^="none"])`]:
-				pathStrokeStyles,
-		}),
+		...(!isHover
+			? {}
+			: {
+					[`${target} svg[data-hover-stroke] path`]: pathStyles,
+					[`${target} svg path[data-hover-stroke]`]: pathStyles,
+			  }),
 	};
 
-	if (isHover) {
-		return {
-			...response,
-			...{
-				[`${target} svg[data-hover-stroke] path`]: pathStyles,
-				[`${target} svg path[data-hover-stroke]`]: pathStyles,
-				...(iconType !== 'line' && {
-					[`${target} svg[data-hover-fill] path:not([fill^="none"])`]:
-						pathFillStyles,
-					[`${target} svg path[data-hover-fill]:not([fill^="none"])`]:
-						pathFillStyles,
-					[`${target} svg g[data-hover-fill]:not([fill^="none"])`]:
-						pathFillStyles,
-					[`${target} svg use[data-hover-fill]:not([fill^="none"])`]:
-						pathFillStyles,
-				}),
-				...(iconType !== 'shape' && {
-					[`${target} svg[data-hover-stroke] path:not([stroke^="none"])`]:
-						pathStrokeStyles,
-					[`${target} svg path[data-hover-stroke]:not([stroke^="none"])`]:
-						pathStrokeStyles,
-					[`${target} svg g[data-hover-stroke]:not([stroke^="none"])`]:
-						pathStrokeStyles,
-					[`${target} svg use[data-hover-stroke]:not([stroke^="none"])`]:
-						pathStrokeStyles,
-				}),
-			},
-		};
+	if (iconType !== 'line') {
+		fillSelectors.forEach(selector => {
+			response[selector] = pathFillStyles;
+		});
+		if (isHover) {
+			fillSelectors
+				.map(selector =>
+					selector.replace('[data-fill]', '[data-hover-fill]')
+				)
+				.forEach(hoverSelector => {
+					response[hoverSelector] = pathFillStyles;
+				});
+		}
 	}
 
-	return response;
+	if (iconType !== 'shape') {
+		strokeSelectors.forEach(selector => {
+			response[selector] = pathStrokeStyles;
+		});
+		if (isHover) {
+			strokeSelectors
+				.map(selector =>
+					selector.replace('[data-stroke]', '[data-hover-stroke]')
+				)
+				.forEach(hoverSelector => {
+					response[hoverSelector] = pathStrokeStyles;
+				});
+		}
+	}
+
+	return { SVGPathStroke: response };
 };

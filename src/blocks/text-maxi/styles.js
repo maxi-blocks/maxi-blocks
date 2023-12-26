74ab9637e7e4493a6330a2d9c09dd6f16304e82e
Marko64 <marko.niksicafe@gmail.com>
2023-12-26 11:26:27 +0100

    More fixes

--- src/blocks/text-maxi/styles.js
+++ src/blocks/text-maxi/styles.js
@@ -566,14 +566,17 @@ const getMarkerObject = props => {
 						transform: `translateY(${verticalOffsetMarkerNum}${verticalOffsetMarkerUnit})`,
 					}),
 					[isRTL ? 'right' : 'left']: markerPosition,
+
 					...(listStylePosition === 'outside' &&
 						(listStyle !== 'custom'
 							? {
 									width: '1em',
-									'margin-left': '-1em',
+									[isRTL ? 'margin-right' : 'margin-left']:
+										'-1em',
 							  }
 							: {
-									'margin-left': -sizeNum + sizeUnit,
+									[isRTL ? 'margin-right' : 'margin-left']:
+										-sizeNum + sizeUnit,
 							  })),
 					...(listStylePosition === 'inside' && {
 						'margin-right': indentMarkerSum,
@@ -700,9 +703,7 @@ const getStyles = props => {
 								'typographyHover',
 							]),
 						},
-						[
-							`.maxi-block--has-link ${element}`,
-						],
+						[`.maxi-block--has-link ${element}`],
 						props.blockStyle
 					),
 				}),

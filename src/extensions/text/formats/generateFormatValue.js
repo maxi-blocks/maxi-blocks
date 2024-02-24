d0a26af6c69400854afc6bbb84110c6344fa0fe4
Andrii Valenia <46112160+Fybex@users.noreply.github.com>
2024-02-24 18:35:25 +0200

    `window` -> `node.ownerDocument.defaultView`
    eslint error fix

--- src/extensions/text/formats/generateFormatValue.js
+++ src/extensions/text/formats/generateFormatValue.js
@@ -12,7 +12,7 @@ import { create } from '@wordpress/rich-text';
  * @returns {Object} RichText format value
  */
 const generateFormatValue = node => {
-	const selection = window.getSelection();
+	const selection = node.ownerDocument.defaultView.getSelection();
 	const formatElement = {};
 
 	if (node) {

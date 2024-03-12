47f6f3e1358b8cc9e16f2e46389a2228e014818a
Ian Jennings <ian@meetjennings.com>
2024-03-12 13:18:10 -0500

    first draft of script

---
+++ .testdriver/prerun.sh
@@ -0,0 +1,4 @@
+rm ~/Desktop/WITH-LOVE-FROM-AMERICA.txt
+npm install dashcam-chrome --save
+/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --start-maximized --load-extension=./node_modules/dashcam-chrome/build/ 1>/dev/null 2>&1 &
+exit

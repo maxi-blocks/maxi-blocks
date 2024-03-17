14100edbf9348a9247f7a6bc42e8855f92d3c0ed
Andrii Valenia <46112160+Fybex@users.noreply.github.com>
2024-03-17 19:27:47 +0200

    `get_palette_color`: use isset to avoid errors

--- core/blocks/utils/get_palette_color.php
+++ core/blocks/utils/get_palette_color.php
@@ -2,19 +2,21 @@
 
 require_once MAXI_PLUGIN_DIR_PATH . 'core/class-maxi-style-cards.php';
 
-function get_palette_color($color, $block_style = 'light') {
+function get_palette_color($color, $block_style = 'light')
+{
     if (class_exists('MaxiBlocks_StyleCards')) {
         $sc_value = MaxiBlocks_StyleCards::get_instance()->get_maxi_blocks_active_style_card();
 
         if (!isset($sc_value)) {
             return false;
         }
-        
-        return $sc_value[$block_style]['styleCard']['color'][$color] 
-            ?? $sc_value[$block_style]['defaultStyleCard']['color'][$color];
+
+        if (isset($sc_value[$block_style]['styleCard']['color'][$color])) {
+            return $sc_value[$block_style]['styleCard']['color'][$color];
+        } elseif (isset($sc_value[$block_style]['defaultStyleCard']['color'][$color])) {
+            return $sc_value[$block_style]['defaultStyleCard']['color'][$color];
+        }
     }
 
     return false;
 }
-
-?>

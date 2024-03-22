c18d5eda8eb11d7b8a5a3a710ad1529f0fbb6445
TeksPeks <verstyukm@gmail.com>
2024-03-22 22:45:06 +0200

    add `SelectControl` to `AriaLabelControl`

--- src/components/aria-label-control/index.js
+++ src/components/aria-label-control/index.js
@@ -2,14 +2,32 @@
  * WordPress dependencies
  */
 import { __ } from '@wordpress/i18n';
+import { useMemo } from '@wordpress/element';
 
 /**
  * Internal dependencies
  */
 import TextControl from '../text-control';
+import SelectControl from '../select-control';
 
 const AriaLabelControl = ({ props, targets }) => {
-	return <TextControl label={__('Aria Label', 'maxi-blocks')} newStyle />;
+	const targetsOptions = useMemo(() => {
+		return targets.map(value => ({
+			label: value.charAt(0).toUpperCase() + value.slice(1),
+			value,
+		}));
+	}, [targets]);
+
+	return (
+		<>
+			<SelectControl
+				label={__('Target', 'maxi-blocks')}
+				newStyle
+				options={targetsOptions}
+			/>
+			<TextControl label={__('Aria Label', 'maxi-blocks')} newStyle />
+		</>
+	);
 };
 
 export default AriaLabelControl;

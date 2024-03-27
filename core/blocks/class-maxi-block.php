81864f45ee78e83a699ef32e31e36932628d3f16
Andrii Valenia <46112160+Fybex@users.noreply.github.com>
2024-03-27 17:22:23 +0200

    `get_block_attributes` merge attrs & default attrs

--- core/blocks/class-maxi-block.php
+++ core/blocks/class-maxi-block.php
@@ -300,6 +300,14 @@ if (!class_exists('MaxiBlocks_Block')):
 
         public function get_block_attributes($props)
         {
+            $default_attributes = $this->get_block_metadata()['attributes'] ?? [];
+            
+            foreach (array_keys($default_attributes) as $key) {
+                if (isset($default_attributes[$key]['default'])) {
+                    $props[$key] = $props[$key] ?? $default_attributes[$key]['default'];
+                }
+            }
+
             return $this->block->prepare_attributes_for_render($props);
         }
 

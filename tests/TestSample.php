2cc2026f073e2d566086edb0d63a935fe95ea6f5
Andrii Valenia <46112160+Fybex@users.noreply.github.com>
2024-04-06 17:31:35 +0300

    PHPUnit setup

---
+++ tests/TestSample.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Class SampleTest
+ *
+ * @package Maxi_Blocks
+ */
+
+/**
+ * Sample test case.
+ */
+class TestSample extends WP_UnitTestCase
+{
+    /**
+     * A single example test.
+     */
+    public function test_sample()
+    {
+        // Replace this with some actual testing code.
+        $this->assertTrue(true);
+    }
+}

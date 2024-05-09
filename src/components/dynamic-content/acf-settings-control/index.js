6bccfedce1afe0ea04b069e4cc2f765dc9cbc3ef
TeksPeks <verstyukm@gmail.com>
2024-05-09 13:08:32 +0300

    Abstract validation of ACF attributes and options from component

--- src/components/dynamic-content/acf-settings-control/index.js
+++ src/components/dynamic-content/acf-settings-control/index.js
@@ -2,18 +2,16 @@
  * WordPress dependencies
  */
 import { useEffect, useState } from '@wordpress/element';
-import { resolveSelect } from '@wordpress/data';
 
 /**
  * Internal dependencies
  */
 import SelectControl from '../../select-control';
-import { acfFieldTypes } from '../../../extensions/DC/constants';
 
 /**
  * External dependencies
  */
-import { isEmpty } from 'lodash';
+import { getACFOptions } from './utils';
 
 const ACFSettingsControl = props => {
 	const { onChange, contentType, isCL, group, field } = props;
@@ -23,80 +21,15 @@ const ACFSettingsControl = props => {
 	const [fieldsOptions, setFieldsOptions] = useState(null);
 
 	useEffect(() => {
-		resolveSelect('maxiBlocks/dynamic-content')
-			.getACFGroups()
-			.then(groups => {
-				const options = groups.map(group => {
-					return {
-						label: group.title,
-						value: group.id,
-					};
-				});
+		getACFOptions(group, field, contentType, prefix).then(
+			({ validatedAttributes, groupOptions, fieldOptions }) => {
+				setGroupOptions(groupOptions);
+				setFieldsOptions(fieldOptions);
 
-				if (!isEmpty(options)) {
-					if (!options.find(option => option.value === group))
-						onChange({
-							[`${prefix}acf-group`]: options[0].value,
-						});
-
-					setGroupOptions(options);
-				} else {
-					setGroupOptions([
-						{
-							label: 'No groups found',
-							value: '',
-						},
-					]);
-				}
-			});
-	}, []);
-
-	useEffect(() => {
-		if (isCL) return;
-
-		resolveSelect('maxiBlocks/dynamic-content')
-			.getACFFields(group)
-			.then(fields => {
-				const options = fields
-					.filter(field =>
-						acfFieldTypes[contentType].includes(field.type)
-					)
-					.map(field => {
-						return {
-							label: field.title,
-							value: field.id,
-							type: field.type,
-						};
-					});
-
-				if (!isEmpty(options)) {
-					if (!options.find(option => option.value === field))
-						onChange({
-							[`${prefix}field`]: options[0].value,
-							[`${prefix}acf-field-type`]: options[0].type,
-						});
-
-					setFieldsOptions(options);
-				} else {
-					// In case we receive fields but none of them are suitable for the current content type
-					if (!isEmpty(fields)) {
-						setFieldsOptions([
-							{
-								label: 'No suitable fields found',
-								value: '',
-							},
-						]);
-					}
-
-					setFieldsOptions([
-						{
-							label: 'No fields found',
-							value: '',
-						},
-					]);
-				}
-			});
-	}, [group]);
+				onChange(validatedAttributes);
+			}
+		);
+	}, [group, field, contentType, prefix, onChange]);
 
 	return (
 		<>

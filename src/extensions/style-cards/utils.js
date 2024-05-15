2846d330cb4f5e9c8a8bb7cbd7ead824c40b9f17
TeksPeks <verstyukm@gmail.com>
2024-05-16 00:44:23 +0300

    Refactor `replaceUndefinedWithNull` function

--- src/extensions/style-cards/utils.js
+++ src/extensions/style-cards/utils.js
@@ -1,13 +1,11 @@
 const replaceUndefinedWithNull = obj => {
-	for (const key in obj) {
-		if (Object.prototype.hasOwnProperty.call(obj, key)) {
-			if (typeof obj[key] === 'undefined') {
-				obj[key] = null;
-			} else if (typeof obj[key] === 'object' && obj[key] !== null) {
-				replaceUndefinedWithNull(obj[key]);
-			}
+	Object.entries(obj).forEach(([key, value]) => {
+		if (value === undefined) {
+			obj[key] = null;
+		} else if (typeof value === 'object' && value !== null) {
+			replaceUndefinedWithNull(value);
 		}
-	}
+	});
 	return obj;
 };
 

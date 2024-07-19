ee1fef7bcafe096c2f122c16739b3a2aa4793c64
Svitlana <2401618+elzadj@users.noreply.github.com>
2024-07-19 14:10:24 +0300

    Fix cropping

--- core/class-maxi-image-crop.php
+++ core/class-maxi-image-crop.php
@@ -48,9 +48,7 @@ class MaxiBlocks_ImageCrop
         if (isset($_POST['old_media_src'])) {//phpcs:ignore
             error_log('maxi_add_custom_image_size');
             error_log('$old_media_src: '. $_POST['old_media_src']);
-            $old_media = esc_url_raw($_POST['old_media_src']);//phpcs:ignore
-
-            $this->delete_old_file($old_media);
+            $this->validate_and_delete_old_file($_POST['old_media_src']);//phpcs:ignore
         }
 
         if (isset($_POST['src'], $_POST['src_x'], $_POST['src_y'], $_POST['src_w'], $_POST['src_h'], $_POST['dst_w'], $_POST['dst_h'])) {//phpcs:ignore
@@ -81,13 +79,18 @@ class MaxiBlocks_ImageCrop
         $uploads_dir = wp_upload_dir()['basedir'];
         $old_media_path = str_replace(get_site_url() . '/', '', $old_media);
 
-        error_log('$old_media_path: '. $old_media_path);
         error_log('$uploads_dir: '. $uploads_dir);
 
+        // Convert $old_media_path to an absolute path
+        $old_media_path = ABSPATH . $old_media_path;
+        error_log('$old_media_path: '. $old_media_path);
+
+
         if (strpos($old_media_path, $uploads_dir) !== 0) {
-            wp_die('Invalid file path.');
+            error_log('Delete old file: invalid file path.');
+            wp_die(__('Delete old file: invalid file path', 'maxi-blocks'));
         }
-        wp_delete_file(ABSPATH . $old_media_path);
+        wp_delete_file($old_media_path);
 
     }
 
@@ -126,8 +129,9 @@ class MaxiBlocks_ImageCrop
             wp_die(__('You do not have sufficient permissions to access this page.', 'maxi-blocks'));
         }
         if (isset($_POST['old_media_src'])) {//phpcs:ignore
-            $old_media = esc_url_raw($_POST['old_media_src']);//phpcs:ignore
-            $this->delete_old_file($old_media);
+            error_log('maxi_remove_custom_image_size');
+            error_log('$old_media_src: '. $_POST['old_media_src']);
+            $this->validate_and_delete_old_file($_POST['old_media_src']);//phpcs:ignore
 
             global $wp_filesystem;
             WP_Filesystem();
@@ -135,4 +139,30 @@ class MaxiBlocks_ImageCrop
 
         die();
     }
+
+    private function validate_and_delete_old_file($old_media_src)
+    {
+        // Sanitize the input
+        $old_media = sanitize_text_field($old_media_src);
+
+        // Check if it's a number
+        if (is_numeric($old_media)) {
+            // If it's a number, it's likely an image ID, so we ignore it
+            error_log('Ignoring numeric old_media_src: ' . $old_media);
+            return;
+        }
+
+        // If it's not a number, we treat it as a URL
+        $old_media = esc_url_raw($old_media);
+
+        // Check if it's a valid URL
+        if (filter_var($old_media, FILTER_VALIDATE_URL) !== false) {
+            // If it's a valid URL, we pass it to delete_old_file
+            $this->delete_old_file($old_media);
+        } else {
+            // If it's not a valid URL, we log an error
+            error_log('Invalid URL in old_media_src: ' . $old_media);
+        }
+    }
+
 }

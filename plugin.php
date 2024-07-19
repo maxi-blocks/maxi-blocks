87210ca147222ef49ef0c1bb5d0be4def701a1ef
Svitlana <2401618+elzadj@users.noreply.github.com>
2024-07-19 11:55:57 +0300

    Remove maxi_insert_block function

--- plugin.php
+++ plugin.php
@@ -218,35 +218,6 @@ function maxi_blocks_dismiss_plugin_update_notice()
 
 require_once(MAXI_PLUGIN_DIR_PATH . 'core/admin/maxi-allowed-html-tags.php');
 
-function maxi_insert_block()
-{
-    if (isset($_POST['maxi_title']) && isset($_POST['maxi_content'])) {//phpcs:ignore
-        $this_title =  sanitize_title($_POST['maxi_title']);//phpcs:ignore
-        $this_content = sanitize_text_field($_POST['maxi_content']);//phpcs:ignore
-
-        if ($this_content && $this_title) {
-
-            wp_insert_post([
-            'post_content' =>  $this_content,
-            'post_title' => $this_title,
-            'post_type' => 'wp_block',
-            'post_status' => 'publish',
-            'comment_status' => 'closed',
-            'ping_status' => 'closed',
-            'guid' => sprintf(
-                '%s/wp_block/%s',
-                site_url(),
-                sanitize_title($this_title)
-            ),
-        ]);
-            echo 'success';
-        } else {
-            echo 'JSON Error';
-        }
-    }
-    wp_die();
-} //function maxi_insert_block()
-
 // remove noopener noreferrer from gutenberg links
 function maxi_links_control($rel, $link)
 {
@@ -254,7 +225,6 @@ function maxi_links_control($rel, $link)
 }
 add_filter('wp_targeted_link_rel', 'maxi_links_control', 10, 2);
 add_action('wp_ajax_maxi_get_option', 'maxi_get_option', 9, 1);
-add_action('wp_ajax_maxi_insert_block', 'maxi_insert_block', 10, 2);
 
 
 //======================================================================

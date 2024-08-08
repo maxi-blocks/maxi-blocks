c0542c57ed75c3b3e2b7c2a5adbddef6710dcf62
Svitlana <2401618+elzadj@users.noreply.github.com>
2024-08-08 17:40:42 +0300

    move fonts to json

--- src/extensions/text/store/actions.js
+++ src/extensions/text/store/actions.js
@@ -1,9 +1,36 @@
-const actions = {
-	updateFonts(fonts) {
-		return {
-			type: 'UPDATE_FONTS',
-			fonts,
-		};
-	},
+import { apiFetch } from '@wordpress/data-controls';
+import { uniq } from 'lodash';
+
+export const updateFonts = (fonts) => {
+	return {
+		type: 'UPDATE_FONTS',
+		fonts,
+	};
 };
-export default actions;
+
+// Action to set fonts after fetching from JSON
+export const setFonts = (fonts) => ({
+	type: 'SET_FONTS',
+	fonts,
+});
+
+// Generator function to fetch fonts using data-controls
+export function* fetchFonts() {
+	try {
+		const defaultFontsUrl = '/wp-content/plugins/maxi-blocks/fonts/fonts.json';
+		let fontsUrl = defaultFontsUrl;
+
+		const linkElement = document.querySelector('#maxi-blocks-block-css');
+		const href = linkElement?.getAttribute('href');
+
+		if (href) {
+			const pluginsPath = href.substring(0, href.lastIndexOf('/build'));
+			fontsUrl = `${pluginsPath}/fonts/fonts.json`;
+		}
+
+		const fonts = yield apiFetch({ url: fontsUrl });
+		yield setFonts(fonts);
+	} catch (error) {
+		console.error('Failed to load fonts:', error);
+	}
+}

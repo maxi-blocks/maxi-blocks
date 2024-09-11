5eb3c0f23cb1ef3b71ca84f3518f03aa5c4922b5
Svitlana <2401618+elzadj@users.noreply.github.com>
2024-09-11 12:47:24 +0300

    performance tests

--- src/extensions/maxi-block/maxiBlockComponent.js
+++ src/extensions/maxi-block/maxiBlockComponent.js
@@ -1081,11 +1081,6 @@ class MaxiBlockComponent extends Component {
 			const end = performance.now();
 			const time = end - start;
 			totalGetStylesObjectTime += time;
-			console.log(
-				`Time to get styles object for ${uniqueID}: ${time.toFixed(
-					2
-				)}ms`
-			);
 			breakpoints = this.getBreakpoints;
 
 			// When duplicating, need to change the obj target for the new uniqueID
@@ -1233,14 +1228,10 @@ class MaxiBlockComponent extends Component {
 		}
 
 		console.log(
-			`Total time to get styles object: ${totalGetStylesObjectTime.toFixed(
-				2
-			)}ms`
+			`Total time to get styles object: ${totalGetStylesObjectTime}ms`
 		);
 		console.log(
-			`Total time to render style component: ${totalRenderStyleComponentTime.toFixed(
-				2
-			)}ms`
+			`Total time to render style component: ${totalRenderStyleComponentTime}ms`
 		);
 	}
 

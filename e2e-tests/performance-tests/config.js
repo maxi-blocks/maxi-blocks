995825454390fe849ade3fe0510d6b2c06f3595a
Svitlana <2401618+elzadj@users.noreply.github.com>
2024-09-13 12:41:34 +0300

    Adjust the performance tests

--- e2e-tests/performance-tests/config.js
+++ e2e-tests/performance-tests/config.js
@@ -12,7 +12,7 @@ import path from 'path';
 /**
  * Number of warmup iterations to perform.
  */
-export const WARMUP_ITERATIONS = 5;
+export const WARMUP_ITERATIONS = 7;
 
 /**
  * Timeout for warmup.
@@ -27,12 +27,12 @@ export const COOL_DOWN_TIME = 1000;
 /**
  * Number of iterations to perform for each pattern.
  */
-export const PATTERNS_ITERATIONS = 5;
+export const PATTERNS_ITERATIONS = 7;
 
 /**
  * Number of iterations to perform for each block.
  */
-export const BLOCKS_ITERATIONS = 5;
+export const BLOCKS_ITERATIONS = 7;
 
 /**
  * Timeout for performance tests.
@@ -67,7 +67,7 @@ export const PATTERNS = [
 		patterns: [
 			'HOL-PRO-95',
 			'ANL-PRO-09',
-			'NML-PRO-18',
+			// 'NML-PRO-18',
 			// 'PIL-PRO-164',
 			// 'SML-PRO-139',
 			// 'SML-38',
@@ -78,7 +78,7 @@ export const PATTERNS = [
 		patterns: [
 			'PBGL-PRO-60',
 			'PBGL-PRO-97',
-			'WCL-PRO-01',
+			// 'WCL-PRO-01',
 			// 'PBGL-PRO-12',
 			// 'WCL-PRO-06',
 		],
@@ -91,7 +91,7 @@ export const PATTERNS = [
 			// 'PIL-PRO-83',
 			// 'PIL-PRO-118',
 			'HOL-PRO-133',
-			'SML-PRO-137',
+			// 'SML-PRO-137',
 			// 'PTML-PRO-77',
 		],
 	},
@@ -100,7 +100,7 @@ export const PATTERNS = [
 		patterns: [
 			'HOL-PRO-62',
 			'SML-PRO-134',
-			'PFL-PRO-62',
+			// 'PFL-PRO-62',
 			// 'HOL-PRO-53',
 			// 'HOL-PRO-47',
 			// 'SML-PRO-162',
@@ -112,7 +112,7 @@ export const PATTERNS = [
 			'DSP-PRO-01',
 			'GMP-PRO-01',
 			// 'NGOP-PRO-02',
-			'HEP-PRO-05',
+			// 'HEP-PRO-05',
 			// 'GDSP-PRO-01',
 			// 'CSPY-PRO-01',
 		],

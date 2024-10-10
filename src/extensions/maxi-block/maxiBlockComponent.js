f59fe792fd989c2f073407052aa4f0eb887609d1
Svitlana <2401618+elzadj@users.noreply.github.com>
2024-10-10 13:32:07 +0300

    Separate injectStyles into smaller functions

--- src/extensions/maxi-block/maxiBlockComponent.js
+++ src/extensions/maxi-block/maxiBlockComponent.js
@@ -1,3 +1,4 @@
+/* eslint-disable class-methods-use-this */
 /**
  * MaxiBlocks Block component extension
  *
@@ -1223,147 +1224,178 @@ class MaxiBlockComponent extends Component {
 		iframe
 	) {
 		if (iframe?.contentDocument?.body) {
-			const iframeDocument = iframe.contentDocument;
-			const editorWrapper = iframeDocument.body;
-			// In case Gutenberg native preview responsive is enabled
-			const tabletPreview =
-				editorWrapper.querySelector('.is-tablet-preview');
-			const mobilePreview =
-				editorWrapper.querySelector('.is-mobile-preview');
+			this.handleIframeStyles(iframe, currentBreakpoint);
+		}
 
-			if (tabletPreview || mobilePreview) {
-				const previewTarget = tabletPreview ?? mobilePreview;
+		const target = this.getStyleTarget(isSiteEditor, iframe);
+		const styleElement = this.getOrCreateStyleElement(target, uniqueID);
 
-				const postEditor = document?.body?.querySelector(
-					'.edit-post-visual-editor'
+		const styleContent = this.generateStyleContent(
+			uniqueID,
+			stylesObj,
+			currentBreakpoint,
+			breakpoints,
+			isBreakpointChange,
+			isBlockStyleChange,
+			iframe,
+			isSiteEditor
+		);
+
+		this.updateStyleElement(styleElement, styleContent);
+	}
+
+	handleIframeStyles(iframe, currentBreakpoint) {
+		const iframeDocument = iframe.contentDocument;
+		const editorWrapper = iframeDocument.body;
+		const tabletPreview = editorWrapper.querySelector('.is-tablet-preview');
+		const mobilePreview = editorWrapper.querySelector('.is-mobile-preview');
+
+		if (tabletPreview || mobilePreview) {
+			this.handleResponsivePreview(
+				editorWrapper,
+				tabletPreview,
+				mobilePreview
+			);
+		}
+
+		if (editorWrapper) {
+			this.setupIframeForMaxi(
+				iframe,
+				iframeDocument,
+				editorWrapper,
+				currentBreakpoint
+			);
+		}
+	}
+
+	handleResponsivePreview(editorWrapper, tabletPreview, mobilePreview) {
+		const previewTarget = tabletPreview ?? mobilePreview;
+		const postEditor = document?.body?.querySelector(
+			'.edit-post-visual-editor'
+		);
+		const responsiveWidth = postEditor.getAttribute(
+			'maxi-blocks-responsive-width'
+		);
+		const isMaxiPreview = postEditor.getAttribute('is-maxi-preview');
+
+		if (isMaxiPreview) {
+			previewTarget.style.width = `${responsiveWidth}px`;
+			previewTarget.style.boxSizing = 'content-box';
+		}
+	}
+
+	setupIframeForMaxi(
+		iframe,
+		iframeDocument,
+		editorWrapper,
+		currentBreakpoint
+	) {
+		if (
+			iframe &&
+			!iframeDocument.body.classList.contains('maxi-blocks--active')
+		) {
+			this.addMaxiClassesToIframe(
+				iframeDocument,
+				editorWrapper,
+				currentBreakpoint
+			);
+			this.copyFontsToIframe(iframeDocument, iframe);
+			this.copyMaxiStylesToIframe(iframeDocument, iframe);
+			this.copyMaxiVariablesToIframe(iframeDocument, iframe);
+			this.ensureMaxiStylesLoaded(iframeDocument, iframe);
+		}
+	}
+
+	addMaxiClassesToIframe(iframeDocument, editorWrapper, currentBreakpoint) {
+		iframeDocument.body.classList.add('maxi-blocks--active');
+		editorWrapper.setAttribute(
+			'maxi-blocks-responsive',
+			currentBreakpoint === 's' ? 's' : 'xs'
+		);
+		iframeDocument.documentElement.style.scrollbarWidth = 'none';
+	}
+
+	copyFontsToIframe(iframeDocument, iframe) {
+		loadFonts(getPageFonts(), true, iframeDocument);
+		const maxiFonts = Array.from(
+			document.querySelectorAll(
+				'link[rel="stylesheet"][id*="maxi-blocks-styles-font"]'
+			)
+		);
+		if (!isEmpty(maxiFonts)) {
+			maxiFonts.forEach(rawMaxiFont => {
+				const maxiFont = rawMaxiFont.cloneNode(true);
+				iframe.contentDocument.head.appendChild(maxiFont);
+			});
+		}
+	}
+
+	copyMaxiStylesToIframe(iframeDocument, iframe) {
+		const maxiStyles = Array.from(
+			document.querySelectorAll('div.maxi-blocks__styles')
+		);
+		if (!isEmpty(maxiStyles)) {
+			maxiStyles.forEach(rawMaxiStyle => {
+				const maxiStyle = rawMaxiStyle.cloneNode(true);
+				const { id } = maxiStyle;
+				iframeDocument.querySelector(`#${id}`)?.remove();
+				maxiStyle.children[0].innerText =
+					maxiStyle.children[0].innerText.replaceAll(
+						' .edit-post-visual-editor',
+						'.editor-styles-wrapper'
+					);
+				iframe.contentDocument.head.appendChild(maxiStyle);
+			});
+		}
+	}
+
+	copyMaxiVariablesToIframe(iframeDocument, iframe) {
+		const maxiVariables = document
+			.querySelector('#maxi-blocks-sc-vars-inline-css')
+			?.cloneNode(true);
+		if (maxiVariables) {
+			iframeDocument
+				.querySelector('#maxi-blocks-sc-vars-inline-css')
+				?.remove();
+			iframe.contentDocument.head.appendChild(maxiVariables);
+		}
+	}
+
+	ensureMaxiStylesLoaded(iframeDocument, iframe) {
+		const editStyles = iframeDocument.querySelector(
+			'#maxi-blocks-block-editor-css'
+		);
+		const frontStyles = iframeDocument.querySelector(
+			'#maxi-blocks-block-css'
+		);
+
+		if (!editStyles) {
+			const rawEditStyles = document.querySelector(
+				'#maxi-blocks-block-editor-css'
+			);
+			iframe.contentDocument.head.appendChild(
+				rawEditStyles.cloneNode(true)
+			);
+		}
+
+		if (!frontStyles) {
+			const rawFrontStyles = document.querySelector(
+				'#maxi-blocks-block-css'
+			);
+			if (rawFrontStyles) {
+				iframe.contentDocument.head.appendChild(
+					rawFrontStyles.cloneNode(true)
 				);
-				const responsiveWidth = postEditor.getAttribute(
-					'maxi-blocks-responsive-width'
-				);
-				const isMaxiPreview =
-					postEditor.getAttribute('is-maxi-preview');
-
-				if (isMaxiPreview) {
-					previewTarget.style.width = `${responsiveWidth}px`;
-					previewTarget.style.boxSizing = 'content-box';
-				}
-
-				if (editorWrapper) {
-					if (
-						iframe &&
-						!iframeDocument.body.classList.contains(
-							'maxi-blocks--active'
-						)
-					) {
-						// Iframe needs Maxi classes and attributes
-						iframeDocument.body.classList.add(
-							'maxi-blocks--active'
-						);
-
-						editorWrapper.setAttribute(
-							'maxi-blocks-responsive',
-							tabletPreview ? 's' : 'xs'
-						);
-
-						// Hides scrollbar in firefox
-						iframeDocument.documentElement.style.scrollbarWidth =
-							'none';
-
-						// Copy all fonts to iframe
-						loadFonts(getPageFonts(), true, iframeDocument);
-
-						const maxiFonts = Array.from(
-							document.querySelectorAll(
-								'link[rel="stylesheet"][id*="maxi-blocks-styles-font"]'
-							)
-						);
-
-						if (!isEmpty(maxiFonts))
-							maxiFonts.forEach(rawMaxiFont => {
-								const maxiFont = rawMaxiFont.cloneNode(true);
-
-								iframe.contentDocument.head.appendChild(
-									maxiFont
-								);
-							});
-
-						// Get all Maxi blocks <style> from <head>
-						// and move to new iframe
-						const maxiStyles = Array.from(
-							document.querySelectorAll('div.maxi-blocks__styles')
-						);
-
-						if (!isEmpty(maxiStyles))
-							maxiStyles.forEach(rawMaxiStyle => {
-								const maxiStyle = rawMaxiStyle.cloneNode(true);
-								const { id } = maxiStyle;
-								iframeDocument
-									.querySelector(`#${id}`)
-									?.remove();
-
-								maxiStyle.children[0].innerText =
-									maxiStyle.children[0].innerText.replaceAll(
-										' .edit-post-visual-editor',
-										'.editor-styles-wrapper'
-									);
-
-								iframe.contentDocument.head.appendChild(
-									maxiStyle
-								);
-							});
-
-						// Move Maxi variables to iframe
-						const maxiVariables = document
-							.querySelector('#maxi-blocks-sc-vars-inline-css')
-							?.cloneNode(true);
-
-						if (maxiVariables) {
-							iframeDocument
-								.querySelector(
-									'#maxi-blocks-sc-vars-inline-css'
-								)
-								?.remove();
-
-							iframe.contentDocument.head.appendChild(
-								maxiVariables
-							);
-						}
-
-						// Ensures all Maxi styles are loaded on iframe
-						const editStyles = iframeDocument.querySelector(
-							'#maxi-blocks-block-editor-css'
-						);
-						const frontStyles = iframeDocument.querySelector(
-							'#maxi-blocks-block-css'
-						);
-
-						if (!editStyles) {
-							const rawEditStyles = document.querySelector(
-								'#maxi-blocks-block-editor-css'
-							);
-
-							iframe.contentDocument.head.appendChild(
-								rawEditStyles.cloneNode(true)
-							);
-						}
-
-						if (!frontStyles) {
-							const rawFrontStyles = document.querySelector(
-								'#maxi-blocks-block-css'
-							);
-
-							if (rawFrontStyles) {
-								iframe.contentDocument.head.appendChild(
-									rawFrontStyles.cloneNode(true)
-								);
-							}
-						}
-					}
-				}
 			}
 		}
+	}
+
+	getStyleTarget(isSiteEditor, iframe) {
 		const siteEditorIframe = isSiteEditor ? getSiteEditorIframe() : null;
-		const target = siteEditorIframe || iframe?.contentDocument || document;
+		return siteEditorIframe || iframe?.contentDocument || document;
+	}
+
+	getOrCreateStyleElement(target, uniqueID) {
 		const styleId = `maxi-blocks__styles--${uniqueID}`;
 		let styleElement = target.getElementById(styleId);
 
@@ -1373,6 +1405,19 @@ class MaxiBlockComponent extends Component {
 			target.head.appendChild(styleElement);
 		}
 
+		return styleElement;
+	}
+
+	generateStyleContent(
+		uniqueID,
+		stylesObj,
+		currentBreakpoint,
+		breakpoints,
+		isBreakpointChange,
+		isBlockStyleChange,
+		iframe,
+		isSiteEditor
+	) {
 		let styleContent;
 		let styles;
 
@@ -1404,6 +1449,10 @@ class MaxiBlockComponent extends Component {
 			);
 		}
 
+		return styleContent;
+	}
+
+	updateStyleElement(styleElement, styleContent) {
 		if (styleElement.textContent !== styleContent) {
 			styleElement.textContent = styleContent;
 		}

a0d7012d034fc8470c27bda89142677c3a77db14
TeksPeks <verstyukm@gmail.com>
2024-12-25 20:18:32 +0200

    Update test configuration to support path aliases

--- jest/jest-e2e.config.js
+++ jest/jest-e2e.config.js
@@ -1,7 +1,11 @@
 module.exports = {
 	...require('@wordpress/scripts/config/jest-e2e.config'),
+
+	rootDir: '../',
+	roots: ['<rootDir>/e2e-tests'],
+
 	setupFilesAfterEnv: [
-		'<rootDir>/setup-test-framework.js',
+		'<rootDir>/e2e-tests/setup-test-framework.js',
 		'@wordpress/jest-console',
 	],
 	maxConcurrency: 20,

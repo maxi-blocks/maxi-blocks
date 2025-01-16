acb3bb80139aa7ef1ae809c085cd286887d7c757
Svitlana <2401618+elzadj@users.noreply.github.com>
2025-01-16 15:31:10 +0200

    Longer timeout for font test

--- e2e-tests/components/font-family-selector/index.spec.js
+++ e2e-tests/components/font-family-selector/index.spec.js
@@ -71,7 +71,7 @@ describe('FontFamilySelector', () => {
 			'Montserrat'
 		);
 
-		await page.waitForTimeout(500);
+		await page.waitForTimeout(1500);
 
 		const hasBeenLoaded = await page.evaluate(() =>
 			document.fonts.check('12px Montserrat')

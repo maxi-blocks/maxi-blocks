6c9f9d269410b3225a407038cfcc178e3a7c1500
Svitlana <2401618+elzadj@users.noreply.github.com>
2025-02-06 11:41:49 +0200

    fix responsive mode state in the sidebar

--- src/components/inspector-tabs/inspector-responsive-info-box.js
+++ src/components/inspector-tabs/inspector-responsive-info-box.js
@@ -7,15 +7,18 @@ import { __ } from '@wordpress/i18n';
  * Internal dependencies
  */
 import InfoBox from '@components/info-box';
+import { select } from '@wordpress/data';
 
 /**
  * Component
  */
 const responsiveInfoBox = ({ props }) => {
 	const { deviceType } = props;
+	const baseBreakpoint = select('maxiBlocks').receiveBaseBreakpoint();
 
 	return (
-		deviceType !== 'general' && (
+		deviceType !== 'general' &&
+		deviceType !== baseBreakpoint && (
 			<InfoBox
 				message={__(
 					'Responsive mode activated. Edits apply exclusively to selected break points. Return to "Your size" for global editing (recommended).',

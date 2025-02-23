6bcc038f4a44dbda0e23af48d6a20f8befa982f4
kyrapieterse <kyra@yeahcan.com>
2025-02-23 10:39:57 +0800

    SVG from G fill='var(--maxi-grey-4-color)'to Maxi var black

--- src/icons/setting-icons/flex-wrap-nowrap/index.js
+++ src/icons/setting-icons/flex-wrap-nowrap/index.js
@@ -5,7 +5,7 @@ import { SVG, Path, G } from '@wordpress/primitives';
 
 const flexWrapNowrap = (
 	<SVG width={17.863} height={18.381} viewBox='0 0 17.863 18.381'>
-		<G fill='var(--maxi-grey-4-color)'>
+		<G fill='var(--maxi-black)'>
 			<Path d='M1 18.381a1 1 0 01-1-1V1a1 1 0 012 0v16.381a1 1 0 01-1 1' />
 			<Path d='M6.5 18.381a1 1 0 01-1-1V1a1 1 0 012 0v16.381a1 1 0 01-1 1' />
 			<Path d='M11.367 18.381a1 1 0 01-1-1V1a1 1 0 012 0v16.381a1 1 0 01-1 1' />

1dc5717088f2a8ee49bc2c9af9d126c0935751c9
kyrapieterse <kyra@yeahcan.com>
2025-03-06 15:34:03 +0800

    pushy push push

--- src/icons/toolbar-icons/toolbar-align-justify/index.js
+++ src/icons/toolbar-icons/toolbar-align-justify/index.js
@@ -4,14 +4,14 @@
 import { SVG, Path } from '@wordpress/primitives';
 
 const toolbarAlignJustify = (
-	<SVG width={16} height={17.5} viewBox='0 0 16 17.5'>
-		<g strokeLinejoin='round' strokeWidth={1.5}>
-			<Path d='M16 0H0' transform='translate(0 .75)' />
-			<Path d='M16 3.9H0' transform='translate(0 .75)' />
-			<Path d='M16 7.7H0' transform='translate(0 .75)' />
-			<Path d='M16 11.6H0' transform='translate(0 .75)' />
-			<Path d='M16 16H0' transform='translate(0 .75)' />
-		</g>
+	<SVG
+		viewBox='0 0 16 17.5'
+		stroke='#141516'
+		strokeLinecap='round'
+		strokeLinejoin='round'
+		strokeWidth={1.5}
+	>
+		<Path d='M15.2.8H.8m14.4 3.9H.8m14.4 3.7H.8m14.4 4H.8m14.4 4.4H.8' />
 	</SVG>
 );
 

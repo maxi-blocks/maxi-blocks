072f500b107308a8a05b4806b6e9316b4508bc93
Svitlana <2401618+elzadj@users.noreply.github.com>
2025-03-21 15:17:58 +0200

    starters site dev mode

--- core/admin/starter-sites/craco.config.js
+++ core/admin/starter-sites/craco.config.js
@@ -1,45 +1,46 @@
 const webpack = require('webpack');
 
 module.exports = {
-    webpack: {
-        configure: {
-            output: {
-                filename: "js/[name].js",
-            },
-            optimization: {
-                runtimeChunk: false,
-                splitChunks: {
-                    chunks(chunk) {
-                        return false;
-                    },
-                },
-            },
-        },
-        plugins: [
-            // Add Lodash noConflict configuration
-            new webpack.ProvidePlugin({
-                _: ['lodash', 'noConflict']
-            })
-        ]
-    },
-    plugins: [
-        {
-            plugin: {
-                overrideWebpackConfig: ({ webpackConfig }) => {
-                    // Find MiniCssExtractPlugin in the plugins array
-                    const cssPlugin = webpackConfig.plugins.find(
-                        plugin => plugin.constructor.name === 'MiniCssExtractPlugin'
-                    );
+	webpack: {
+		configure: {
+			output: {
+				filename: 'js/[name].js',
+			},
+			optimization: {
+				runtimeChunk: false,
+				splitChunks: {
+					chunks(chunk) {
+						return false;
+					},
+				},
+			},
+		},
+		plugins: [
+			// Add Lodash noConflict configuration
+			new webpack.ProvidePlugin({
+				_: ['lodash', 'noConflict'],
+			}),
+		],
+	},
+	plugins: [
+		{
+			plugin: {
+				overrideWebpackConfig: ({ webpackConfig }) => {
+					// Find MiniCssExtractPlugin in the plugins array
+					const cssPlugin = webpackConfig.plugins.find(
+						plugin =>
+							plugin.constructor.name === 'MiniCssExtractPlugin'
+					);
 
-                    // Only modify if plugin exists
-                    if (cssPlugin) {
-                        cssPlugin.options.filename = "css/[name].css";
-                    }
+					// Only modify if plugin exists
+					if (cssPlugin) {
+						cssPlugin.options.filename = 'css/[name].css';
+					}
 
-                    return webpackConfig;
-                },
-            },
-            options: {},
-        },
-    ],
+					return webpackConfig;
+				},
+			},
+			options: {},
+		},
+	],
 };

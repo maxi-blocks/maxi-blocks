e9098a60e2ea7570ffbc75e30446b9c922ed1496
Myroslav <verstyukm@gmail.com>
2025-04-05 01:22:28 +0300

    Split utils into separate files for testing purposes

---
+++ src/editor/style-cards/downloadTextFile.js
@@ -0,0 +1,17 @@
+const downloadTextFile = (data, fileName) => {
+	const a = document.createElement('a');
+	document.body.appendChild(a);
+	a.style = 'display: none';
+
+	// Need stringify twice to get 'text/plain' mime type
+	const json = JSON.stringify(JSON.stringify(data));
+	const blob = new Blob([json], { type: 'text/plain' });
+	const url = window.URL.createObjectURL(blob);
+
+	a.href = url;
+	a.download = fileName;
+	a.click();
+	document.body.removeChild(a);
+};
+
+export default downloadTextFile;

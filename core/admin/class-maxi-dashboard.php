3d150827bb8feaf09fd9e29450ff4a0108095c08
Svitlana <2401618+elzadj@users.noreply.github.com>
2025-08-27 19:28:06 +0300

    Switch middleware to live urls

--- core/admin/class-maxi-dashboard.php
+++ core/admin/class-maxi-dashboard.php
@@ -3,7 +3,7 @@ if (!defined('ABSPATH')) {
     exit();
 }
 
-define('MAXI_BLOCKS_AUTH_MIDDLEWARE_URL', 'https://maxi2025.staging.maxiblocks.com/middleware/verify');
+define('MAXI_BLOCKS_AUTH_MIDDLEWARE_URL', 'https://my.maxiblocks.com/middleware/verify');
 define('MAXI_BLOCKS_AUTH_MIDDLEWARE_KEY', '4d8af9b4d6f221cf7a41271cb7b82c92');
 
 if (!class_exists('MaxiBlocks_Dashboard')):

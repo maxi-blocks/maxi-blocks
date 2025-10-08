f7a57ddb64d486ad4a75044621e76eed7bbd5529
Svitlana <2401618+elzadj@users.noreply.github.com>
2025-10-08 18:17:28 +0300

    remove comemnted out svg code

--- src/icons/map-icons/markers/map-marker-2/index.js
+++ src/icons/map-icons/markers/map-marker-2/index.js
@@ -6,8 +6,6 @@ import { SVG, Path } from '@wordpress/primitives';
 const mapMarker2 = (
 	<SVG xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'>
 		<Path
-			// fill='var(--maxi-icon-block-orange)'
-			// stroke='#081219'
 			strokeLinecap='round'
 			strokeLinejoin='round'
 			strokeMiterlimit='10'

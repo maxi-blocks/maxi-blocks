8542aa576a1e815d5de60e6181b86f2b61419068
Svitlana <2401618+elzadj@users.noreply.github.com>
2025-11-24 15:34:12 +0200

    hide tag setting for search and button

--- src/components/inspector-tabs/inspector-typography.js
+++ src/components/inspector-tabs/inspector-typography.js
@@ -61,13 +61,20 @@ const typography = ({
 		content: (
 			<>
 				{/* Font Level Control (Heading/Paragraph tags) */}
-				<div className='maxi-typography-control__font-level maxi-typography-panel__font-level'>
-					<FontLevelControl
-						{...getGroupAttributes(attributes, 'typography', true)}
-						value={textLevel || attributes.textLevel}
-						onChange={obj => maxiSetAttributes(obj)}
-					/>
-				</div>
+				{props.name !== 'maxi-blocks/search-maxi' &&
+					props.name !== 'maxi-blocks/button-maxi' && (
+						<div className='maxi-typography-control__font-level maxi-typography-panel__font-level'>
+							<FontLevelControl
+								{...getGroupAttributes(
+									attributes,
+									'typography',
+									true
+								)}
+								value={textLevel || attributes.textLevel}
+								onChange={obj => maxiSetAttributes(obj)}
+							/>
+						</div>
+					)}
 				{!hideAlignment && (
 					<div className='maxi-typography-control__alignment-buttons'>
 						<AlignmentControl

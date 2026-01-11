961416ad593c8a0671af643364ffcc7578b0cfc2
Kyra Pieterse <kyra@yeahcan.com>
2026-01-11 13:56:21 +0000

    Remove pink highlight from current tab

--- src/components/relation-control/index.js
+++ src/components/relation-control/index.js
@@ -375,12 +375,6 @@ const RelationControl = props => {
 		);
 	};
 
-	const hasInteractionAttributes = item =>
-		!isEmpty(item.attributes) &&
-		Object.values(item.attributes).some(
-			value => value !== undefined && value !== null && value !== ''
-		);
-
 	return (
 		<div className='maxi-relation-control'>
 			{!isEmpty(relations) && (
@@ -506,9 +500,6 @@ const RelationControl = props => {
 														'Current',
 														'maxi-blocks'
 													),
-													className: hasInteractionAttributes(item)
-														? 'maxi-relation-control__interaction-indicator'
-														: undefined,
 													content: displayBeforeSetting(item),
 												},
 												{

eceafc67ef08e58035ed0ac53cc662bfe283fa25
Kyra Pieterse <kyra@yeahcan.com>
2026-01-11 07:28:14 +0000

    Highlight interaction tabs in pink

--- src/components/relation-control/index.js
+++ src/components/relation-control/index.js
@@ -438,6 +438,7 @@ const RelationControl = props => {
 									{item.uniqueID && item.sid && (
 										<SettingTabsControl
 											deviceType={deviceType}
+											className='maxi-relation-control__interaction-tabs'
 											items={[
 												{
 													label: __(

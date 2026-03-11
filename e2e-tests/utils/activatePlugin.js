b3c6c970f070420cafd935bedbdea307fe5e5010
Svitlana <2401618+elzadj@users.noreply.github.com>
2026-03-11 17:16:43 +0200

    Fi NC test

--- e2e-tests/utils/activatePlugin.js
+++ e2e-tests/utils/activatePlugin.js
@@ -29,14 +29,18 @@ export default async function activatePlugin(slug) {
 		return;
 	}
 
-	await page.click(`tr[data-plugin^="${slug}/"] .activate a`);
+	await Promise.all([
+		page.waitForNavigation({ waitUntil: 'networkidle0', timeout: 60000 }),
+		page.click(`tr[data-plugin^="${slug}/"] .activate a`),
+	]);
 
 	if (!isCurrentURL('plugins.php')) {
 		await visitAdminPage('plugins.php');
 	}
 
 	await page.waitForSelector(
-		`tr[data-plugin^="${slug}/"] .deactivate a`
+		`tr[data-plugin^="${slug}/"] .deactivate a`,
+		{ timeout: 60000 }
 	);
 
 	await switchUserToTest();

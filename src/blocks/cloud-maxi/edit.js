395fbc95aab74e513e1e59dcdad0405a27fe235b
Svitlana <2401618+elzadj@users.noreply.github.com>
2026-03-19 17:07:13 +0200

    Add toolbars for cloud and icon placeholders back

--- src/blocks/cloud-maxi/edit.js
+++ src/blocks/cloud-maxi/edit.js
@@ -1,7 +1,8 @@
 /**
  * WordPress dependencies.
  */
-import { Component, RawHTML } from '@wordpress/element';
+import { useBlockProps } from '@wordpress/block-editor';
+import { RawHTML } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 
 /**
@@ -15,40 +16,38 @@ import { isEmpty } from 'lodash';
 import MaxiModal from '@editor/library/modal';
 import { withMaxiProps } from '@extensions/maxi-block';
 
-class edit extends Component {
-	render() {
-		const { attributes, clientId, maxiSetAttributes } = this.props;
-		const { content, openFirstTime, preview } = attributes;
+const edit = props => {
+	const { attributes, clientId, maxiSetAttributes } = props;
+	const { content, openFirstTime, preview } = attributes;
+	const blockProps = useBlockProps();
 
-		if (preview)
-			return (
-				<img
-					// eslint-disable-next-line no-undef
-					src={previews.library_preview}
-					alt={__('Cloud library block preview', 'maxi-blocks')}
-				/>
-			);
+	if (preview)
+		return (
+			<img
+				// eslint-disable-next-line no-undef
+				src={previews.library_preview}
+				alt={__('Cloud library block preview', 'maxi-blocks')}
+			/>
+		);
 
-		/* Placeholder with layout modal */
-		return [
-			<div key={this.props.clientId}>
-				{isEmpty(content) && (
-					<div className='maxi-block-library__placeholder'>
-						<MaxiModal
-							clientId={clientId}
-							type='patterns'
-							openFirstTime={openFirstTime}
-							onOpen={obj => maxiSetAttributes(obj)}
-							onSelect={obj => maxiSetAttributes(obj)}
-							onRemove={obj => maxiSetAttributes(obj)}
-							{...this.props}
-						/>
-					</div>
-				)}
-				{!isEmpty(content) && <RawHTML>{content}</RawHTML>}
-			</div>,
-		];
-	}
-}
+	return (
+		<div {...blockProps}>
+			{isEmpty(content) && (
+				<div className='maxi-block-library__placeholder'>
+					<MaxiModal
+						clientId={clientId}
+						type='patterns'
+						openFirstTime={openFirstTime}
+						onOpen={obj => maxiSetAttributes(obj)}
+						onSelect={obj => maxiSetAttributes(obj)}
+						onRemove={obj => maxiSetAttributes(obj)}
+						{...props}
+					/>
+				</div>
+			)}
+			{!isEmpty(content) && <RawHTML>{content}</RawHTML>}
+		</div>
+	);
+};
 
 export default withMaxiProps(edit);

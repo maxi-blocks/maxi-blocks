9affe873c4b3793b641b1b58d02628b3fb1f7941
Svitlana <2401618+elzadj@users.noreply.github.com>
2026-03-26 17:29:00 +0200

    an attempts to fix a failing test

--- e2e-tests/components/dynamic-content/media.spec.js
+++ e2e-tests/components/dynamic-content/media.spec.js
@@ -202,6 +202,10 @@ describe('Dynamic content', () => {
 
 		const getFrontImageResults = async (block, type) => {
 			try {
+				await previewPage.waitForSelector(
+					`.${block}.maxi-image-block .maxi-image-block__image`,
+					{ visible: true, timeout: 30000 }
+				);
 				return await previewPage.$eval(
 					`.${block}.maxi-image-block .maxi-image-block__image`,
 					(el, expect) => {
